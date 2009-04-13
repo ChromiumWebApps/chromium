ChromiumWebApps/chromium

commit 07f95333a47323bfbd65c8443e0fcc470956cb27

A tricky fix for Issue 1845 (Take 2).
This is almost the same change as <http://codereview.chromium.org/39252/show>, which caused a build break on a Linux buildbot while compiling my new template function in "chrome/common/render_messages.h".
Even though I was not able to reproduce the build errors on my Linux box, I removed this function and use the int type in my IPC message 'ViewMsg_SetTextDirection'.

BUG=1845
Review URL: http://codereview.chromium.org/42495

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12434 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/renderer_host/render_widget_host.cc
@@ -17,6 +17,7 @@
 #include "chrome/views/view.h"
 #include "webkit/glue/webcursor.h"
 #include "webkit/glue/webinputevent.h"
+#include "webkit/glue/webtextdirection.h"
 
 #if defined(OS_WIN)
 #include "base/gfx/gdi_util.h"
@@ -53,7 +54,9 @@ RenderWidgetHost::RenderWidgetHost(RenderProcessHost* process,
       mouse_move_pending_(false),
       needs_repainting_on_restore_(false),
       is_unresponsive_(false),
-      view_being_painted_(false) {
+      view_being_painted_(false),
+      text_direction_updated_(false),
+      text_direction_(WEB_TEXT_DIRECTION_LTR) {
   if (routing_id_ == MSG_ROUTING_NONE)
     routing_id_ = process_->GetNextRoutingID();
 
@@ -350,6 +353,19 @@ void RenderWidgetHost::RendererExited() {
   }
 
   BackingStoreManager::RemoveBackingStore(this);
+}
+
+void RenderWidgetHost::UpdateTextDirection(WebTextDirection direction) {
+  text_direction_updated_ = true;
+  text_direction_ = direction;
+}
+
+void RenderWidgetHost::NotifyTextDirection() {
+  if (text_direction_updated_) {
+    text_direction_updated_ = false;
+    Send(new ViewMsg_SetTextDirection(routing_id(),
+                                      static_cast<int>(text_direction_)));
+  }
 }
 
 gfx::Rect RenderWidgetHost::GetRootWindowResizerRect() const {

chrome/browser/renderer_host/render_widget_host.h
@@ -15,6 +15,7 @@
 #include "chrome/common/native_web_keyboard_event.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"
 #include "webkit/glue/webinputevent.h"
+#include "webkit/glue/webtextdirection.h"
 
 namespace gfx {
 class Rect;
@@ -211,6 +212,38 @@ class RenderWidgetHost : public IPC::Channel::Listener {
   void ForwardWheelEvent(const WebMouseWheelEvent& wheel_event);
   void ForwardKeyboardEvent(const NativeWebKeyboardEvent& key_event);
 
+  // Update the text direction of the focused input element and notify it to a
+  // renderer process.
+  // These functions have two usage scenarios: changing the text direction
+  // from a menu (as Safari does), and; changing the text direction when a user
+  // presses a set of keys (as IE and Firefox do).
+  // 1. Change the text direction from a menu.
+  // In this scenario, we receive a menu event only once and we should update
+  // the text direction immediately when a user chooses a menu item. So, we
+  // should call both functions at once as listed in the following snippet.
+  //   void RenderViewHost::SetTextDirection(WebTextDirection direction) {
+  //     UpdateTextDirection(direction);
+  //     NotifyTextDirection();
+  //   }
+  // 2. Change the text direction when pressing a set of keys.
+  // Becauses of auto-repeat, we may receive the same key-press event many
+  // times while we presses the keys and it is nonsense to send the same IPC
+  // messsage every time when we receive a key-press event.
+  // To suppress the number of IPC messages, we just update the text direction
+  // when receiving a key-press event and send an IPC message when we release
+  // the keys as listed in the following snippet.
+  //   if (key_event.type == WebKeyboardEvent::KEY_DOWN) {
+  //     if (key_event.windows_key_code == 'A' &&
+  //         key_event.modifiers == WebKeyboardEvent::CTRL_KEY) {
+  //       UpdateTextDirection(dir);
+  //     }
+  //   } else if (key_event.type == WebKeyboardEvent::KEY_UP) {
+  //     NotifyTextDirection();
+  //   }
+  // Note: we cannot undo this change for compatibility with Firefox and IE.
+  void UpdateTextDirection(WebTextDirection direction);
+  void NotifyTextDirection();
+
   // This is for derived classes to give us access to the resizer rect.
   // And to also expose it to the RenderWidgetHostView.
   virtual gfx::Rect GetRootWindowResizerRect() const;
@@ -362,6 +395,10 @@ class RenderWidgetHost : public IPC::Channel::Listener {
   // stuff key events into a queue and pop them out on ACK, feeding our copy
   // back to whatever unhandled handler instead of the returned version.
   KeyQueue key_queue_;
+
+  // Set when we update the text direction of the selected input element.
+  bool text_direction_updated_;
+  WebTextDirection text_direction_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHost);
 };

chrome/browser/renderer_host/render_widget_host_view_win.cc
@@ -823,6 +823,26 @@ LRESULT RenderWidgetHostViewWin::OnKeyEvent(UINT message, WPARAM wparam,
     return ::SendMessage(parent_hwnd_, message, wparam, lparam);
   }
 
+  if (wparam == VK_SHIFT || wparam == VK_CONTROL) {
+    // Bug 1845: we need to update the text direction when a user releases
+    // either a right-shift key or a right-control key after pressing both of
+    // them. So, we just update the text direction while a user is pressing the
+    // keys, and we notify the text direction when a user releases either of
+    // them.
+    if (message == WM_KEYDOWN) {
+      const int kKeyDownMask = 0x8000;
+      if ((GetKeyState(VK_RSHIFT) & kKeyDownMask) &&
+          (GetKeyState(VK_RCONTROL) & kKeyDownMask)) {
+        render_widget_host_->UpdateTextDirection(WEB_TEXT_DIRECTION_RTL);
+      } else if ((GetKeyState(VK_LSHIFT) & kKeyDownMask) &&
+                 (GetKeyState(VK_LCONTROL) & kKeyDownMask)) {
+        render_widget_host_->UpdateTextDirection(WEB_TEXT_DIRECTION_LTR);
+      }
+    } else if (message == WM_KEYUP) {
+      render_widget_host_->NotifyTextDirection();
+    }
+  }
+
   render_widget_host_->ForwardKeyboardEvent(
       NativeWebKeyboardEvent(m_hWnd, message, wparam, lparam));
   return 0;

chrome/common/render_messages_internal.h
@@ -529,6 +529,17 @@ IPC_BEGIN_MESSAGES(View)
   IPC_MESSAGE_ROUTED2(ViewMsg_HandleExtensionMessage,
                       std::string /* message */,
                       int /* channel_id */)
+
+  // Changes the text direction of a selected input field.
+  // * direction (int)
+  //   Represents the new text direction.
+  //   Its possible values are listed below:
+  //     Value                      New Text Direction
+  //     WEB_TEXT_DIRECTION_DEFAULT NaturalWritingDirection ("inherit")
+  //     WEB_TEXT_DIRECTION_LTR     LeftToRightWritingDirection ("rtl")
+  //     WEB_TEXT_DIRECTION_RTL     RightToLeftWritingDirection ("ltr")
+  IPC_MESSAGE_ROUTED1(ViewMsg_SetTextDirection,
+                      int /* direction */)
 IPC_END_MESSAGES(View)
 
 

chrome/renderer/render_view.h
@@ -386,6 +386,7 @@ class RenderView : public RenderWidget,
   FRIEND_TEST(RenderViewTest, OnNavStateChanged);
   FRIEND_TEST(RenderViewTest, OnImeStateChanged);
   FRIEND_TEST(RenderViewTest, ImeComposition);
+  FRIEND_TEST(RenderViewTest, OnSetTextDirection);
 
   explicit RenderView(RenderThreadBase* render_thread);
 

chrome/renderer/render_view_unittest.cc
@@ -316,3 +316,51 @@ TEST_F(RenderViewTest, ImeComposition) {
     }
   }
 }
+
+// Test that the RenderView::OnSetTextDirection() function can change the text
+// direction of the selected input element.
+TEST_F(RenderViewTest, OnSetTextDirection) {
+  // Load an HTML page consisting of a <textarea> element and a <div> element.
+  // This test changes the text direction of the <textarea> element, and
+  // writes the values of its 'dir' attribute and its 'direction' property to
+  // verify that the text direction is changed.
+  view_->set_delay_seconds_for_form_state_sync(0);
+  LoadHTML("<html>"
+           "<head>"
+           "</head>"
+           "<body>"
+           "<textarea id=\"test\"></textarea>"
+           "<div id=\"result\" contenteditable=\"true\"></div>"
+           "</body>"
+           "</html>");
+  render_thread_.sink().ClearMessages();
+
+  static const struct {
+    WebTextDirection direction;
+    const wchar_t* expected_result;
+  } kTextDirection[] = {
+    {WEB_TEXT_DIRECTION_RTL, L"\x000A" L"rtl,rtl"},
+    {WEB_TEXT_DIRECTION_LTR, L"\x000A" L"ltr,ltr"},
+  };
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(kTextDirection); ++i) {
+    // Set the text direction of the <textarea> element.
+    ExecuteJavaScript("document.getElementById('test').focus();");
+    view_->OnSetTextDirection(kTextDirection[i].direction);
+
+    // Write the values of its DOM 'dir' attribute and its CSS 'direction'
+    // property to the <div> element.
+    ExecuteJavaScript("var result = document.getElementById('result');"
+                      "var node = document.getElementById('test');"
+                      "var style = getComputedStyle(node, null);"
+                      "result.innerText ="
+                      "    node.getAttribute('dir') + ',' +"
+                      "    style.getPropertyValue('direction');");
+
+    // Copy the document content to std::wstring and compare with the
+    // expected result.
+    const int kMaxOutputCharacters = 16;
+    std::wstring output;
+    GetMainFrame()->GetContentAsPlainText(kMaxOutputCharacters, &output);
+    EXPECT_EQ(output, kTextDirection[i].expected_result);
+  }
+}

chrome/renderer/render_widget.cc
@@ -21,6 +21,7 @@
 #endif  // defined(OS_POSIX)
 
 #include "webkit/glue/webinputevent.h"
+#include "webkit/glue/webtextdirection.h"
 #include "webkit/glue/webwidget.h"
 
 RenderWidget::RenderWidget(RenderThreadBase* render_thread, bool activatable)
@@ -119,6 +120,7 @@ IPC_DEFINE_MESSAGE_MAP(RenderWidget)
   IPC_MESSAGE_HANDLER(ViewMsg_ImeSetInputMode, OnImeSetInputMode)
   IPC_MESSAGE_HANDLER(ViewMsg_ImeSetComposition, OnImeSetComposition)
   IPC_MESSAGE_HANDLER(ViewMsg_Repaint, OnMsgRepaint)
+  IPC_MESSAGE_HANDLER(ViewMsg_SetTextDirection, OnSetTextDirection)
   IPC_MESSAGE_UNHANDLED_ERROR()
 IPC_END_MESSAGE_MAP()
 
@@ -642,6 +644,20 @@ void RenderWidget::OnMsgRepaint(const gfx::Size& size_to_paint) {
   set_next_paint_is_repaint_ack();
   gfx::Rect repaint_rect(size_to_paint.width(), size_to_paint.height());
   DidInvalidateRect(webwidget_, repaint_rect);
+}
+
+void RenderWidget::OnSetTextDirection(int direction) {
+  if (!webwidget_)
+    return;
+
+  WebTextDirection new_direction = static_cast<WebTextDirection>(direction);
+  if (new_direction == WEB_TEXT_DIRECTION_DEFAULT ||
+      new_direction == WEB_TEXT_DIRECTION_LTR ||
+      new_direction == WEB_TEXT_DIRECTION_RTL) {
+    webwidget_->SetTextDirection(new_direction);
+  } else {
+    NOTREACHED();
+  }
 }
 
 bool RenderWidget::next_paint_is_resize_ack() const {

chrome/renderer/render_widget.h
@@ -133,6 +133,7 @@ class RenderWidget : public IPC::Channel::Listener,
                            int target_start, int target_end,
                            const std::wstring& ime_string);
   void OnMsgRepaint(const gfx::Size& size_to_paint);
+  void OnSetTextDirection(int direction);
 
   // True if a PaintRect_ACK message is pending.
   bool paint_reply_pending() const {

commit 34ff04759ed15031330cd7006a2003ce4504a49f

A quick fix for Issue 9762 and 9763. This is caused by my change r12434 that does not check if there are any other keys pressed when a user presses left-control and left-shift keys (or right-control and right-shift keys). To fix this issue, this change does not only add the above check, but also it adds code that cancels updating the text direction if a user presses another key while he/she is pressing control and shift keys.
BUG=9762 "Regression: Shift+Ctrl+Arrow selection combination also changes text direction in RTL UIs"
BUG=9763 "Ctrl+Shift text alignment depends of the sides of both the Ctrl and the Shift keys"

TEST=In a LTR <textarea> element, pressing a control key, pressing a right-shift key, releasing the right-shift key, and verify its text direction is changed to RTL.
TEST=In an RTL <textarea> element, pressing a control key, pressing a left-shift key, releasing the left-shift key, and verify its text direction is changed to LTR.
TEST=In a LTR <textarea> element, pressing a right-shift key, pressing a control key, releasing the right-shift key, and verify its text direction is still LTR.
TEST=In a LTR <textarea> element, pressing a control key, pressing a right-shift key, releasing the control key, and verify its text direction is changed to RTL.
TEST=In an RTL <textarea> element, pressing a left-shift key, pressing a control key, releasing the left-shift key, and verify its text direction is still RTL.
TEST=In an RTL <textarea> element, pressing a control key, pressing a left-shift key, releasing the control key, and verify its text direction is changed to LTR.
TEST=In a LTR <textarea> element, pressing a control key, pressing a right-shift key, pressing an arrow key, releasing the right-shift key, and verify its text direction is still LTR.
Review URL: http://codereview.chromium.org/63117

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13588 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/renderer_host/render_widget_host.cc
@@ -60,7 +60,8 @@ RenderWidgetHost::RenderWidgetHost(RenderProcessHost* process,
       is_unresponsive_(false),
       view_being_painted_(false),
       text_direction_updated_(false),
-      text_direction_(WEB_TEXT_DIRECTION_LTR) {
+      text_direction_(WEB_TEXT_DIRECTION_LTR),
+      text_direction_canceled_(false) {
   if (routing_id_ == MSG_ROUTING_NONE)
     routing_id_ = process_->GetNextRoutingID();
 
@@ -364,11 +365,18 @@ void RenderWidgetHost::UpdateTextDirection(WebTextDirection direction) {
   text_direction_ = direction;
 }
 
+void RenderWidgetHost::CancelUpdateTextDirection() {
+  if (text_direction_updated_)
+    text_direction_canceled_ = true;
+}
+
 void RenderWidgetHost::NotifyTextDirection() {
   if (text_direction_updated_) {
+    if (!text_direction_canceled_)
+      Send(new ViewMsg_SetTextDirection(routing_id(),
+                                        static_cast<int>(text_direction_)));
     text_direction_updated_ = false;
-    Send(new ViewMsg_SetTextDirection(routing_id(),
-                                      static_cast<int>(text_direction_)));
+    text_direction_canceled_ = false;
   }
 }
 

chrome/browser/renderer_host/render_widget_host.h
@@ -238,12 +238,19 @@ class RenderWidgetHost : public IPC::Channel::Listener {
   //     if (key_event.windows_key_code == 'A' &&
   //         key_event.modifiers == WebKeyboardEvent::CTRL_KEY) {
   //       UpdateTextDirection(dir);
+  //     } else {
+  //       CancelUpdateTextDirection();
   //     }
   //   } else if (key_event.type == WebKeyboardEvent::KEY_UP) {
   //     NotifyTextDirection();
   //   }
+  // Once we cancel updating the text direction, we have to ignore all
+  // succeeding UpdateTextDirection() requests until calling
+  // NotifyTextDirection(). (We may receive keydown events even after we
+  // canceled updating the text direction because of auto-repeat.)
   // Note: we cannot undo this change for compatibility with Firefox and IE.
   void UpdateTextDirection(WebTextDirection direction);
+  void CancelUpdateTextDirection();
   void NotifyTextDirection();
 
   // This is for derived classes to give us access to the resizer rect.
@@ -402,6 +409,11 @@ class RenderWidgetHost : public IPC::Channel::Listener {
   // Set when we update the text direction of the selected input element.
   bool text_direction_updated_;
   WebTextDirection text_direction_;
+
+  // Set when we cancel updating the text direction.
+  // This flag also ignores succeeding update requests until we call
+  // NotifyTextDirection().
+  bool text_direction_canceled_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHost);
 };

chrome/browser/renderer_host/render_widget_host_view_win.cc
@@ -65,6 +65,57 @@ BOOL CALLBACK DismissOwnedPopups(HWND window, LPARAM arg) {
   return TRUE;
 }
 
+// Returns the text direction according to the keyboard status.
+// This function retrieves the status of all keys and returns the following
+// values:
+// * WEB_TEXT_DIRECTION_RTL
+//   If only a control key and a right-shift key are down.
+// * WEB_TEXT_DIRECTION_LTR
+//   If only a control key and a left-shift key are down.
+static bool GetNewTextDirection(WebTextDirection* direction) {
+  uint8_t keystate[256];
+  if (!GetKeyboardState(&keystate[0]))
+    return false;
+
+  // To check if a user is pressing only a control key and a right-shift key
+  // (or a left-shift key), we use the steps below:
+  // 1. Check if a user is pressing a control key and a right-shift key (or
+  //    a left-shift key).
+  // 2. If the condition 1 is true, we should check if there are any other
+  //    keys pressed at the same time.
+  //    To ignore the keys checked in 1, we set their status to 0 before
+  //    checking the key status.
+  const int kKeyDownMask = 0x80;
+  if ((keystate[VK_CONTROL] & kKeyDownMask) == 0)
+    return false;
+
+  if (keystate[VK_RSHIFT] & kKeyDownMask) {
+    keystate[VK_RSHIFT] = 0;
+    *direction = WEB_TEXT_DIRECTION_RTL;
+  } else if (keystate[VK_LSHIFT] & kKeyDownMask) {
+    keystate[VK_LSHIFT] = 0;
+    *direction = WEB_TEXT_DIRECTION_LTR;
+  } else {
+    return false;
+  }
+
+  // Scan the key status to find pressed keys. We should adandon changing the
+  // text direction when there are other pressed keys.
+  // This code is executed only when a user is pressing a control key and a
+  // right-shift key (or a left-shift key), i.e. we should ignore the status of
+  // the keys: VK_SHIFT, VK_CONTROL, VK_RCONTROL, and VK_LCONTROL.
+  // So, we reset their status to 0 and ignore them.
+  keystate[VK_SHIFT] = 0;
+  keystate[VK_CONTROL] = 0;
+  keystate[VK_RCONTROL] = 0;
+  keystate[VK_LCONTROL] = 0;
+  for (int i = 0; i <= VK_PACKET; ++i) {
+    if (keystate[i] & kKeyDownMask)
+      return false;
+  }
+  return true;
+}
+
 }  // namespace
 
 // RenderWidgetHostView --------------------------------------------------------
@@ -848,24 +899,28 @@ LRESULT RenderWidgetHostViewWin::OnKeyEvent(UINT message, WPARAM wparam,
     return ::SendMessage(parent_hwnd_, message, wparam, lparam);
   }
 
-  if (wparam == VK_SHIFT || wparam == VK_CONTROL) {
-    // Bug 1845: we need to update the text direction when a user releases
-    // either a right-shift key or a right-control key after pressing both of
-    // them. So, we just update the text direction while a user is pressing the
-    // keys, and we notify the text direction when a user releases either of
-    // them.
-    if (message == WM_KEYDOWN) {
-      const int kKeyDownMask = 0x8000;
-      if ((GetKeyState(VK_RSHIFT) & kKeyDownMask) &&
-          (GetKeyState(VK_RCONTROL) & kKeyDownMask)) {
-        render_widget_host_->UpdateTextDirection(WEB_TEXT_DIRECTION_RTL);
-      } else if ((GetKeyState(VK_LSHIFT) & kKeyDownMask) &&
-                 (GetKeyState(VK_LCONTROL) & kKeyDownMask)) {
-        render_widget_host_->UpdateTextDirection(WEB_TEXT_DIRECTION_LTR);
-      }
-    } else if (message == WM_KEYUP) {
-      render_widget_host_->NotifyTextDirection();
+  // Bug 1845: we need to update the text direction when a user releases
+  // either a right-shift key or a right-control key after pressing both of
+  // them. So, we just update the text direction while a user is pressing the
+  // keys, and we notify the text direction when a user releases either of them.
+  if (message == WM_KEYDOWN) {
+    if (wparam == VK_SHIFT) {
+      WebTextDirection direction;
+      if (GetNewTextDirection(&direction))
+        render_widget_host_->UpdateTextDirection(direction);
+    } else if (wparam != VK_CONTROL) {
+      // A user pressed a key except shift and control keys.
+      // When a user presses a key while he/she holds control and shift keys,
+      // we adandon sending an IPC message in a succeeding NotifyTextDirection()
+      // call. To adandon it, this call set a flag that prevents sending an IPC
+      // message in NotifyTextDirection() only if we are going to send it.
+      // So, it is harmless to call this function if we aren't going to send it.
+      render_widget_host_->CancelUpdateTextDirection();
     }
+  } else if (message == WM_KEYUP &&
+             (wparam == VK_SHIFT || wparam == VK_CONTROL)) {
+    // We send an IPC message only if we need to update the text direction.
+    render_widget_host_->NotifyTextDirection();
   }
 
   render_widget_host_->ForwardKeyboardEvent(