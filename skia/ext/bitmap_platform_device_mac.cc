ChromiumWebApps/chromium

commit bd63dc9cdf640a6d1e833ce0b5ed7ed9090973b0

Refactor the PlatformContext layer to have only one class.
Previously we had three classes of PlatformCanvas*, one for each platform. Then
we had a typedef of PlatformContext to PlatformCanvas[Mac|Win|Linux] for the
specific platform.

This means that it was almost impossible to forward-declare PlatformCanvas and
there were a bunch of unnecessary includes of platform_canvas.h in header
files.

This change makes there be only one platform_canvas.h header with ifdefs, which
removes a decent amount of duplicated code. There is a platform-independent
file, and one platform-dependent file of platform_canvas for each platform.

I also renamed PlatformDevice[Mac|Win|Linux] to PlatformDevice, althouth in
this case I kept the separate headers since there was much less overlap.

I also broke out CanvasPaint into separate headers so this template doesn't
need to be included all over the project (only a couple of files actually need
it).

Review URL: http://codereview.chromium.org/125109

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18363 0039d316-1c4b-4281-b951-d872f2087c98

skia/ext/bitmap_platform_device_mac.cc
@@ -46,9 +46,9 @@ bool Constrain(int available_size, int* position, int *size) {
 
 }  // namespace
 
-class BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData : public SkRefCnt {
+class BitmapPlatformDevice::BitmapPlatformDeviceData : public SkRefCnt {
  public:
-  explicit BitmapPlatformDeviceMacData(CGContextRef bitmap);
+  explicit BitmapPlatformDeviceData(CGContextRef bitmap);
 
   // Create/destroy CoreGraphics context for our bitmap data.
   CGContextRef GetBitmapContext() {
@@ -88,19 +88,18 @@ class BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData : public SkRefCnt {
   SkRegion clip_region_;
 
  private:
-  friend class base::RefCounted<BitmapPlatformDeviceMacData>;
-  ~BitmapPlatformDeviceMacData() {
+  friend class base::RefCounted<BitmapPlatformDeviceData>;
+  ~BitmapPlatformDeviceData() {
     if (bitmap_context_)
       CGContextRelease(bitmap_context_);
   }
 
   // Disallow copy & assign.
-  BitmapPlatformDeviceMacData(const BitmapPlatformDeviceMacData&);
-  BitmapPlatformDeviceMacData& operator=(const BitmapPlatformDeviceMacData&);
+  BitmapPlatformDeviceData(const BitmapPlatformDeviceData&);
+  BitmapPlatformDeviceData& operator=(const BitmapPlatformDeviceData&);
 };
 
-BitmapPlatformDeviceMac::\
-    BitmapPlatformDeviceMacData::BitmapPlatformDeviceMacData(
+BitmapPlatformDevice::BitmapPlatformDeviceData::BitmapPlatformDeviceData(
     CGContextRef bitmap)
     : bitmap_context_(bitmap),
       config_dirty_(true) {  // Want to load the config next time.
@@ -119,7 +118,7 @@ BitmapPlatformDeviceMac::\
   CGContextSaveGState(bitmap_context_);
 }
 
-void BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData::SetMatrixClip(
+void BitmapPlatformDevice::BitmapPlatformDeviceData::SetMatrixClip(
     const SkMatrix& transform,
     const SkRegion& region) {
   transform_ = transform;
@@ -127,7 +126,7 @@ void BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData::SetMatrixClip(
   config_dirty_ = true;
 }
 
-void BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData::LoadConfig() {
+void BitmapPlatformDevice::BitmapPlatformDeviceData::LoadConfig() {
   if (!config_dirty_ || !bitmap_context_)
     return;  // Nothing to do.
   config_dirty_ = false;
@@ -154,10 +153,10 @@ void BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData::LoadConfig() {
 // that we can create the pixel data before calling the constructor. This is
 // required so that we can call the base class' constructor with the pixel
 // data.
-BitmapPlatformDeviceMac* BitmapPlatformDeviceMac::Create(CGContextRef context,
-                                                         int width,
-                                                         int height,
-                                                         bool is_opaque) {
+BitmapPlatformDevice* BitmapPlatformDevice::Create(CGContextRef context,
+                                                   int width,
+                                                   int height,
+                                                   bool is_opaque) {
   SkBitmap bitmap;
   bitmap.setConfig(SkBitmap::kARGB_8888_Config, width, height);
   if (bitmap.allocPixels() != true)
@@ -197,51 +196,51 @@ BitmapPlatformDeviceMac* BitmapPlatformDeviceMac::Create(CGContextRef context,
   }
 
   // The device object will take ownership of the graphics context.
-  return new BitmapPlatformDeviceMac(
-      new BitmapPlatformDeviceMacData(context), bitmap);
+  return new BitmapPlatformDevice(
+      new BitmapPlatformDeviceData(context), bitmap);
 }
 
 // The device will own the bitmap, which corresponds to also owning the pixel
 // data. Therefore, we do not transfer ownership to the SkDevice's bitmap.
-BitmapPlatformDeviceMac::BitmapPlatformDeviceMac(
-    BitmapPlatformDeviceMacData* data, const SkBitmap& bitmap)
-    : PlatformDeviceMac(bitmap),
+BitmapPlatformDevice::BitmapPlatformDevice(
+    BitmapPlatformDeviceData* data, const SkBitmap& bitmap)
+    : PlatformDevice(bitmap),
       data_(data) {
 }
 
 // The copy constructor just adds another reference to the underlying data.
 // We use a const cast since the default Skia definitions don't define the
 // proper constedness that we expect (accessBitmap should really be const).
-BitmapPlatformDeviceMac::BitmapPlatformDeviceMac(
-    const BitmapPlatformDeviceMac& other)
-    : PlatformDeviceMac(
-          const_cast<BitmapPlatformDeviceMac&>(other).accessBitmap(true)),
+BitmapPlatformDevice::BitmapPlatformDevice(
+    const BitmapPlatformDevice& other)
+    : PlatformDevice(
+          const_cast<BitmapPlatformDevice&>(other).accessBitmap(true)),
       data_(other.data_) {
   data_->ref();
 }
 
-BitmapPlatformDeviceMac::~BitmapPlatformDeviceMac() {
+BitmapPlatformDevice::~BitmapPlatformDevice() {
   data_->unref();
 }
 
-BitmapPlatformDeviceMac& BitmapPlatformDeviceMac::operator=(
-    const BitmapPlatformDeviceMac& other) {
+BitmapPlatformDevice& BitmapPlatformDevice::operator=(
+    const BitmapPlatformDevice& other) {
   data_ = other.data_;
   data_->ref();
   return *this;
 }
 
-CGContextRef BitmapPlatformDeviceMac::GetBitmapContext() {
+CGContextRef BitmapPlatformDevice::GetBitmapContext() {
   return data_->GetBitmapContext();
 }
 
-void BitmapPlatformDeviceMac::setMatrixClip(const SkMatrix& transform,
-                                            const SkRegion& region) {
+void BitmapPlatformDevice::setMatrixClip(const SkMatrix& transform,
+                                         const SkRegion& region) {
   data_->SetMatrixClip(transform, region);
 }
 
-void BitmapPlatformDeviceMac::DrawToContext(CGContextRef context, int x, int y,
-                                            const CGRect* src_rect) {
+void BitmapPlatformDevice::DrawToContext(CGContextRef context, int x, int y,
+                                         const CGRect* src_rect) {
   bool created_dc = false;
   if (!data_->bitmap_context_) {
     created_dc = true;
@@ -273,20 +272,20 @@ void BitmapPlatformDeviceMac::DrawToContext(CGContextRef context, int x, int y,
 }
 
 // Returns the color value at the specified location.
-SkColor BitmapPlatformDeviceMac::getColorAt(int x, int y) {
+SkColor BitmapPlatformDevice::getColorAt(int x, int y) {
   const SkBitmap& bitmap = accessBitmap(true);
   SkAutoLockPixels lock(bitmap);
   uint32_t* data = bitmap.getAddr32(0, 0);
   return static_cast<SkColor>(data[x + y * width()]);
 }
 
-void BitmapPlatformDeviceMac::onAccessBitmap(SkBitmap*) {
+void BitmapPlatformDevice::onAccessBitmap(SkBitmap*) {
   // Not needed in CoreGraphics
 }
 
-void BitmapPlatformDeviceMac::processPixels(int x, int y,
-                                            int width, int height,
-                                            adjustAlpha adjustor) {
+void BitmapPlatformDevice::processPixels(int x, int y,
+                                         int width, int height,
+                                         adjustAlpha adjustor) {
   const SkBitmap& bitmap = accessBitmap(true);
   SkMatrix& matrix = data_->transform_;
   int bitmap_start_x = SkScalarRound(matrix.getTranslateX()) + x;

skia/ext/bitmap_platform_device_mac.h
@@ -11,7 +11,7 @@ namespace skia {
 
 // A device is basically a wrapper around SkBitmap that provides a surface for
 // SkCanvas to draw into. Our device provides a surface CoreGraphics can also
-// write to. BitmapPlatformDeviceMac creates a bitmap using
+// write to. BitmapPlatformDevice creates a bitmap using
 // CGCreateBitmapContext() in a format that Skia supports and can then use this
 // to draw text into, etc. This pixel data is provided to the bitmap that the
 // device contains so that it can be shared.
@@ -23,7 +23,7 @@ namespace skia {
 // For us, that other bitmap will become invalid as soon as the device becomes
 // invalid, which may lead to subtle bugs. Therefore, DO NOT ASSIGN THE
 // DEVICE'S PIXEL DATA TO ANOTHER BITMAP, make sure you copy instead.
-class BitmapPlatformDeviceMac : public PlatformDeviceMac {
+class BitmapPlatformDevice : public PlatformDevice {
  public:
   // Factory function. The screen DC is used to create the bitmap, and will not
   // be stored beyond this function. is_opaque should be set if the caller
@@ -32,10 +32,10 @@ class BitmapPlatformDeviceMac : public PlatformDeviceMac {
   // The shared_section parameter is optional (pass NULL for default behavior).
   // If shared_section is non-null, then it must be a handle to a file-mapping
   // object returned by CreateFileMapping.  See CreateDIBSection for details.
-  static BitmapPlatformDeviceMac* Create(CGContextRef context,
-                                         int width,
-                                         int height,
-                                         bool is_opaque);
+  static BitmapPlatformDevice* Create(CGContextRef context,
+                                      int width,
+                                      int height,
+                                      bool is_opaque);
 
   // Copy constructor. When copied, devices duplicate their internal data, so
   // stay linked. This is because their implementation is very heavyweight
@@ -48,11 +48,11 @@ class BitmapPlatformDeviceMac : public PlatformDeviceMac {
   //
   // Copy constucting and "=" is designed for saving the device or passing it
   // around to another routine willing to deal with the bitmap data directly.
-  BitmapPlatformDeviceMac(const BitmapPlatformDeviceMac& other);
-  virtual ~BitmapPlatformDeviceMac();
+  BitmapPlatformDevice(const BitmapPlatformDevice& other);
+  virtual ~BitmapPlatformDevice();
 
   // See warning for copy constructor above.
-  BitmapPlatformDeviceMac& operator=(const BitmapPlatformDeviceMac& other);
+  BitmapPlatformDevice& operator=(const BitmapPlatformDevice& other);
 
   virtual CGContextRef GetBitmapContext();
   virtual void setMatrixClip(const SkMatrix& transform, const SkRegion& region);
@@ -69,10 +69,10 @@ class BitmapPlatformDeviceMac : public PlatformDeviceMac {
   // Reference counted data that can be shared between multiple devices. This
   // allows copy constructors and operator= for devices to work properly. The
   // bitmaps used by the base device class are already refcounted and copyable.
-  class BitmapPlatformDeviceMacData;
+  class BitmapPlatformDeviceData;
 
-  BitmapPlatformDeviceMac(BitmapPlatformDeviceMacData* data,
-                          const SkBitmap& bitmap);
+  BitmapPlatformDevice(BitmapPlatformDeviceData* data,
+                       const SkBitmap& bitmap);
 
   // Flushes the CoreGraphics context so that the pixel data can be accessed
   // directly by Skia. Overridden from SkDevice, this is called when Skia
@@ -85,7 +85,7 @@ class BitmapPlatformDeviceMac : public PlatformDeviceMac {
 
   // Data associated with this device, guaranteed non-null. We hold a reference
   // to this object.
-  BitmapPlatformDeviceMacData* data_;
+  BitmapPlatformDeviceData* data_;
 };
 
 }  // namespace skia

skia/ext/platform_canvas_mac.cc
@@ -2,118 +2,68 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "skia/ext/platform_canvas_mac.h"
+#include "skia/ext/platform_canvas.h"
 
 #include "skia/ext/bitmap_platform_device_mac.h"
 #include "third_party/skia/include/core/SkTypes.h"
 
 namespace skia {
 
-PlatformCanvasMac::PlatformCanvasMac() : SkCanvas() {
+PlatformCanvas::PlatformCanvas() : SkCanvas() {
 }
 
-PlatformCanvasMac::PlatformCanvasMac(int width, int height, bool is_opaque)
+PlatformCanvas::PlatformCanvas(int width, int height, bool is_opaque)
     : SkCanvas() {
   initialize(width, height, is_opaque);
 }
 
-PlatformCanvasMac::PlatformCanvasMac(int width,
-                                     int height,
-                                     bool is_opaque,
-                                     CGContextRef context)
+PlatformCanvas::PlatformCanvas(int width,
+                               int height,
+                               bool is_opaque,
+                               CGContextRef context)
     : SkCanvas() {
   initialize(width, height, is_opaque);
 }
 
-PlatformCanvasMac::PlatformCanvasMac(int width,
-                                     int height,
-                                     bool is_opaque,
-                                     uint8_t* data)
+PlatformCanvas::PlatformCanvas(int width,
+                               int height,
+                               bool is_opaque,
+                               uint8_t* data)
     : SkCanvas() {
   initialize(width, height, is_opaque, data);
 }
 
-PlatformCanvasMac::~PlatformCanvasMac() {
+PlatformCanvas::~PlatformCanvas() {
 }
 
-bool PlatformCanvasMac::initialize(int width,
-                                   int height,
-                                   bool is_opaque) {
-  SkDevice* device = createPlatformDevice(width, height, is_opaque, NULL);
+bool PlatformCanvas::initialize(int width,
+                                int height,
+                                bool is_opaque,
+                                uint8_t* data) {
+  SkDevice* device = BitmapPlatformDevice::Create(NULL, width, height,
+                                                  is_opaque);
   if (!device)
     return false;
 
   setDevice(device);
-  device->unref();  // was created with refcount 1, and setDevice also refs
+  device->unref();  // Was created with refcount 1, and setDevice also refs.
   return true;
 }
 
-bool PlatformCanvasMac::initialize(int width,
-                                   int height,
-                                   bool is_opaque,
-                                   uint8_t* data) {
-  CGContextRef context = NULL;
-  CGColorSpaceRef colorSpace;
-
-  colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
-  context = CGBitmapContextCreate(
-      data, width, height, 8 /* bits per plane */, 4 * width /* stride */,
-      colorSpace, kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host);
-  CGColorSpaceRelease(colorSpace);
-  if (!context)
-    return false;
-  // Change the coordinate system to match WebCore's
-  CGContextTranslateCTM(context, 0, height);
-  CGContextScaleCTM(context, 1.0, -1.0);
-
-  SkDevice* device = createPlatformDevice(width, height, is_opaque, context);
-  if (!device)
-    return false;
-
-  setDevice(device);
-  device->unref();  // was created with refcount 1, and setDevice also refs
-  return true;
-}
-
-CGContextRef PlatformCanvasMac::beginPlatformPaint() {
+CGContextRef PlatformCanvas::beginPlatformPaint() {
   return getTopPlatformDevice().GetBitmapContext();
 }
 
-void PlatformCanvasMac::endPlatformPaint() {
-  // flushing will be done in onAccessBitmap
+void PlatformCanvas::endPlatformPaint() {
+  // Flushing will be done in onAccessBitmap.
 }
 
-PlatformDeviceMac& PlatformCanvasMac::getTopPlatformDevice() const {
-  // All of our devices should be our special PlatformDeviceMac.
-  SkCanvas::LayerIter iter(const_cast<PlatformCanvasMac*>(this), false);
-  return *static_cast<PlatformDeviceMac*>(iter.device());
-}
-
-SkDevice* PlatformCanvasMac::createDevice(SkBitmap::Config config,
-                                          int width,
-                                          int height,
-                                          bool is_opaque, bool isForLayer) {
+SkDevice* PlatformCanvas::createDevice(SkBitmap::Config config,
+                                       int width,
+                                       int height,
+                                       bool is_opaque, bool isForLayer) {
   SkASSERT(config == SkBitmap::kARGB_8888_Config);
-  return createPlatformDevice(width, height, is_opaque, NULL);
-}
-
-SkDevice* PlatformCanvasMac::createPlatformDevice(int width,
-                                                  int height,
-                                                  bool is_opaque,
-                                                  CGContextRef context) {
-  SkDevice* device = BitmapPlatformDeviceMac::Create(context, width, height,
-                                                     is_opaque);
-  return device;
-}
-
-SkDevice* PlatformCanvasMac::setBitmapDevice(const SkBitmap&) {
-  SkASSERT(false);
-  return NULL;
-}
-
-// static
-size_t PlatformCanvasMac::StrideForWidth(unsigned width) {
-  return 4 * width;
+  return BitmapPlatformDevice::Create(NULL, width, height, is_opaque);
 }
 
 }  // namespace skia

app/gfx/canvas.h
@@ -208,10 +208,6 @@ class Canvas : public skia::PlatformCanvas {
   DISALLOW_COPY_AND_ASSIGN(Canvas);
 };
 
-#if defined(OS_WIN) || defined(OS_LINUX)
-typedef skia::CanvasPaintT<Canvas> CanvasPaint;
-#endif
-
 }  // namespace gfx;
 
 #endif  // #ifndef APP_GFX_CANVAS_H_

app/gfx/canvas_linux.cc
@@ -4,7 +4,9 @@
 
 #include "app/gfx/canvas.h"
 
+#include <cairo/cairo.h>
 #include <pango/pango.h>
+#include <pango/pangocairo.h>
 
 #include "app/gfx/font.h"
 #include "base/gfx/rect.h"
@@ -44,10 +46,10 @@ PangoFontDescription* PangoFontFromGfxFont(const gfx::Font& gfx_font) {
 namespace gfx {
 
 Canvas::Canvas(int width, int height, bool is_opaque)
-    : skia::PlatformCanvasLinux(width, height, is_opaque) {
+    : skia::PlatformCanvas(width, height, is_opaque) {
 }
 
-Canvas::Canvas() : skia::PlatformCanvasLinux() {
+Canvas::Canvas() : skia::PlatformCanvas() {
 }
 
 Canvas::~Canvas() {
@@ -58,7 +60,7 @@ void Canvas::SizeStringInt(const std::wstring& text,
                            const gfx::Font& font,
                            int* width, int* height, int flags) {
   cairo_surface_t* surface =
-    cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 0, 0);
+      cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 0, 0);
   cairo_t* cr = cairo_create(surface);
   PangoLayout* layout = pango_cairo_create_layout(cr);
 

app/gfx/canvas_paint.h
@@ -1,0 +1,22 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef APP_GFX_CANVAS_PAINT_H_
+#define APP_GFX_CANVAS_PAINT_H_
+
+#include "app/gfx/canvas.h"
+#include "skia/ext/canvas_paint.h"
+
+// Define a skia::CanvasPaint type that wraps our gfx::Canvas like the
+// skia::PlatformCanvasPaint wraps PlatformCanvas.
+
+namespace gfx {
+
+#if defined(OS_WIN) || defined(OS_LINUX)
+typedef skia::CanvasPaintT<Canvas> CanvasPaint;
+#endif
+
+}  // namespace gfx
+
+#endif  // APP_GFX_CANVAS_PAINT_H_

app/gfx/canvas_win.cc
@@ -120,10 +120,10 @@ int ComputeFormatFlags(int flags, const std::wstring& text) {
 namespace gfx {
 
 Canvas::Canvas(int width, int height, bool is_opaque)
-    : skia::PlatformCanvasWin(width, height, is_opaque) {
+    : skia::PlatformCanvas(width, height, is_opaque) {
 }
 
-Canvas::Canvas() : skia::PlatformCanvasWin() {
+Canvas::Canvas() : skia::PlatformCanvas() {
 }
 
 Canvas::~Canvas() {

base/gfx/native_theme.cc
@@ -330,7 +330,7 @@ HRESULT NativeTheme::PaintScrollbarTrack(
     int classic_state,
     RECT* target_rect,
     RECT* align_rect,
-    skia::PlatformCanvasWin* canvas) const {
+    skia::PlatformCanvas* canvas) const {
   HANDLE handle = GetThemeHandle(SCROLLBAR);
   if (handle && draw_theme_)
     return draw_theme_(handle, hdc, part_id, state_id, target_rect, NULL);
@@ -401,7 +401,7 @@ HRESULT NativeTheme::PaintTrackbar(HDC hdc,
                                    int state_id,
                                    int classic_state,
                                    RECT* rect,
-                                   skia::PlatformCanvasWin* canvas) const {
+                                   skia::PlatformCanvas* canvas) const {
   // Make the channel be 4 px thick in the center of the supplied rect.  (4 px
   // matches what XP does in various menus; GetThemePartSize() doesn't seem to
   // return good values here.)

commit 18ca68b79a60b2de95ae89da3075df2ee3241471

Properly use data when creating a bitmap platform canvas.  Gets things painting on the screen again.  Regressed in r18363. Review URL: http://codereview.chromium.org/126140
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18415 0039d316-1c4b-4281-b951-d872f2087c98

skia/ext/bitmap_platform_device_mac.cc
@@ -44,6 +44,28 @@ bool Constrain(int available_size, int* position, int *size) {
   return true;
 }
 
+static CGContextRef CGContextForData(void* data, int width, int height) {
+  CGColorSpaceRef color_space =
+      CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
+  // Allocate a bitmap context with 4 components per pixel (BGRA).  Apple
+  // recommends these flags for improved CG performance.
+  CGContextRef context =
+      CGBitmapContextCreate(data, width, height, 8, width * 4,
+                            color_space,
+                            kCGImageAlphaPremultipliedFirst |
+                                kCGBitmapByteOrder32Host);
+  CGColorSpaceRelease(color_space);
+
+  if (!context)
+    return NULL;
+
+  // Change the coordinate system to match WebCore's
+  CGContextTranslateCTM(context, 0, height);
+  CGContextScaleCTM(context, 1.0, -1.0);
+
+  return context;
+}
+
 }  // namespace
 
 class BitmapPlatformDevice::BitmapPlatformDeviceData : public SkRefCnt {
@@ -153,15 +175,20 @@ void BitmapPlatformDevice::BitmapPlatformDeviceData::LoadConfig() {
 // that we can create the pixel data before calling the constructor. This is
 // required so that we can call the base class' constructor with the pixel
 // data.
-BitmapPlatformDevice* BitmapPlatformDevice::Create(CGContextRef context,
-                                                   int width,
-                                                   int height,
-                                                   bool is_opaque) {
+BitmapPlatformDevice* BitmapPlatformDevice::CreateWithContext(
+    CGContextRef context, int width, int height, bool is_opaque) {
   SkBitmap bitmap;
   bitmap.setConfig(SkBitmap::kARGB_8888_Config, width, height);
   if (bitmap.allocPixels() != true)
     return NULL;
-  void* data = bitmap.getPixels();
+
+  void* data = NULL;
+  if (context) {
+    data = CGBitmapContextGetData(context);
+    bitmap.setPixels(data);
+  } else {
+    data = bitmap.getPixels();
+  }
 
   // Note: The Windows implementation clears the Bitmap later on.
   // This bears mentioning since removal of this line makes the
@@ -170,34 +197,31 @@ BitmapPlatformDevice* BitmapPlatformDevice::Create(CGContextRef context,
 
   bitmap.setIsOpaque(is_opaque);
 
-  if (is_opaque) {
 #ifndef NDEBUG
+  if (is_opaque) {
     // To aid in finding bugs, we set the background color to something
     // obviously wrong so it will be noticable when it is not cleared
     bitmap.eraseARGB(255, 0, 255, 128);  // bright bluish green
+  }
 #endif
-  }
 
-  if (!context) {
-    CGColorSpaceRef color_space =
-      CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
-    // allocate a bitmap context with 4 components per pixel (BGRA). Apple
-    // recommends these flags for improved CG performance.
-    context =
-      CGBitmapContextCreate(data, width, height, 8, width*4,
-                            color_space,
-                            kCGImageAlphaPremultipliedFirst |
-                                kCGBitmapByteOrder32Host);
-
-    // Change the coordinate system to match WebCore's
-    CGContextTranslateCTM(context, 0, height);
-    CGContextScaleCTM(context, 1.0, -1.0);
-    CGColorSpaceRelease(color_space);
-  }
+  if (!context)
+    context = CGContextForData(data, width, height);
 
   // The device object will take ownership of the graphics context.
   return new BitmapPlatformDevice(
       new BitmapPlatformDeviceData(context), bitmap);
+}
+
+BitmapPlatformDevice* BitmapPlatformDevice::CreateWithData(uint8_t* data,
+                                                           int width,
+                                                           int height,
+                                                           bool is_opaque) {
+  CGContextRef context = NULL;
+  if (data)
+    context = CGContextForData(data, width, height);
+
+  return CreateWithContext(context, width, height, is_opaque);
 }
 
 // The device will own the bitmap, which corresponds to also owning the pixel

skia/ext/bitmap_platform_device_mac.h
@@ -25,17 +25,15 @@ namespace skia {
 // DEVICE'S PIXEL DATA TO ANOTHER BITMAP, make sure you copy instead.
 class BitmapPlatformDevice : public PlatformDevice {
  public:
-  // Factory function. The screen DC is used to create the bitmap, and will not
-  // be stored beyond this function. is_opaque should be set if the caller
-  // knows the bitmap will be completely opaque and allows some optimizations.
-  //
-  // The shared_section parameter is optional (pass NULL for default behavior).
-  // If shared_section is non-null, then it must be a handle to a file-mapping
-  // object returned by CreateFileMapping.  See CreateDIBSection for details.
-  static BitmapPlatformDevice* Create(CGContextRef context,
-                                      int width,
-                                      int height,
-                                      bool is_opaque);
+  // |context| may be NULL.
+  static BitmapPlatformDevice* CreateWithContext(CGContextRef context,
+                                                 int width, int height,
+                                                 bool is_opaque);
+
+  // Creates a context for |data| and calls CreateWithContext.
+  static BitmapPlatformDevice* CreateWithData(uint8_t* data,
+                                              int width, int height,
+                                              bool is_opaque);
 
   // Copy constructor. When copied, devices duplicate their internal data, so
   // stay linked. This is because their implementation is very heavyweight

skia/ext/platform_canvas_mac.cc
@@ -40,8 +40,8 @@ bool PlatformCanvas::initialize(int width,
                                 int height,
                                 bool is_opaque,
                                 uint8_t* data) {
-  SkDevice* device = BitmapPlatformDevice::Create(NULL, width, height,
-                                                  is_opaque);
+  SkDevice* device = BitmapPlatformDevice::CreateWithData(data, width, height,
+                                                          is_opaque);
   if (!device)
     return false;
 
@@ -63,7 +63,8 @@ SkDevice* PlatformCanvas::createDevice(SkBitmap::Config config,
                                        int height,
                                        bool is_opaque, bool isForLayer) {
   SkASSERT(config == SkBitmap::kARGB_8888_Config);
-  return BitmapPlatformDevice::Create(NULL, width, height, is_opaque);
+  return BitmapPlatformDevice::CreateWithContext(NULL, width, height,
+                                                 is_opaque);
 }
 
 }  // namespace skia