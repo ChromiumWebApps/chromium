ChromiumWebApps/chromium

commit 75f5f9a1b9764033bec1fe9dcf546c5c5dcb0940

Fix a few issues with the drop arrow in the Linux tab strip: * Reset the drop info when completing a drag (successful or otherwise). * Reset the drop info when the drag leaves the tab strip zone. * Report success or failure of the drop back to the source widget. * Move and resize the drop arrow container before showing it to reduce visual jank.
BUG=none
TEST=Drag a image into the tab strip so that the drop arrow appears.  Release the drop over the tab strip and then move the window and repeat the process.  The drop arrow should be in the correct position.  Notice that there is no visual jank when the drop arrow is shown.
Review URL: http://codereview.chromium.org/149426

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20404 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/gtk/tabs/tab_strip_gtk.cc
@@ -494,8 +494,6 @@ void TabStripGtk::Init() {
                    G_CALLBACK(OnDragDrop), this);
   g_signal_connect(G_OBJECT(tabstrip_.get()), "drag-leave",
                    G_CALLBACK(OnDragLeave), this);
-  g_signal_connect(G_OBJECT(tabstrip_.get()), "drag-failed",
-                   G_CALLBACK(OnDragFailed), this);
   g_signal_connect(G_OBJECT(tabstrip_.get()), "drag-data-received",
                    G_CALLBACK(OnDragDataReceived), this);
 
@@ -1224,19 +1222,19 @@ void TabStripGtk::SetDropIndex(int index, bool drop_before) {
                     drop_bounds.width(), drop_bounds.height());
 }
 
-void TabStripGtk::CompleteDrop(guchar* data) {
+bool TabStripGtk::CompleteDrop(guchar* data) {
   if (!drop_info_.get())
-    return;
+    return false;
 
   const int drop_index = drop_info_->drop_index;
   const bool drop_before = drop_info_->drop_before;
 
-  // Hide the drop indicator.
-  SetDropIndex(-1, false);
+  // Destroy the drop indicator.
+  drop_info_.reset();
 
   GURL url(reinterpret_cast<char*>(data));
   if (!url.is_valid())
-    return;
+    return false;
 
   if (drop_before) {
     // Insert a new tab.
@@ -1251,6 +1249,8 @@ void TabStripGtk::CompleteDrop(guchar* data) {
         url, GURL(), PageTransition::GENERATED);
     model_->SelectTabContentsAt(drop_index, true);
   }
+
+  return true;
 }
 
 // static
@@ -1272,13 +1272,12 @@ TabStripGtk::DropInfo::DropInfo(int drop_index, bool drop_before,
   g_signal_connect(G_OBJECT(container), "expose-event",
                    G_CALLBACK(OnExposeEvent), this);
   gtk_widget_add_events(container, GDK_STRUCTURE_MASK);
-  gtk_widget_show_all(container);
-
-  drop_arrow = GetDropArrowImage(point_down);
-
   gtk_window_move(GTK_WINDOW(container), 0, 0);
   gtk_window_resize(GTK_WINDOW(container),
                     drop_indicator_width, drop_indicator_height);
+  gtk_widget_show_all(container);
+
+  drop_arrow = GetDropArrowImage(point_down);
 }
 
 TabStripGtk::DropInfo::~DropInfo() {
@@ -1550,17 +1549,8 @@ gboolean TabStripGtk::OnDragDrop(GtkWidget* widget, GdkDragContext* context,
 // static
 gboolean TabStripGtk::OnDragLeave(GtkWidget* widget, GdkDragContext* context,
                                   guint time, TabStripGtk* tabstrip) {
-  // Hide the drop indicator.
-  tabstrip->SetDropIndex(-1, false);
-  return FALSE;
-}
-
-// static
-gboolean TabStripGtk::OnDragFailed(GtkWidget* widget, GdkDragContext* context,
-                                   GtkDragResult result,
-                                   TabStripGtk* tabstrip) {
-  // Hide the drop indicator.
-  tabstrip->SetDropIndex(-1, false);
+  // Destroy the drop indicator.
+  tabstrip->drop_info_.reset();
   return FALSE;
 }
 
@@ -1571,12 +1561,14 @@ gboolean TabStripGtk::OnDragDataReceived(GtkWidget* widget,
                                          GtkSelectionData* data,
                                          guint info, guint time,
                                          TabStripGtk* tabstrip) {
+  bool success = false;
+
   // TODO(jhawkins): Parse URI lists.
   if (info == GtkDndUtil::X_CHROME_TEXT_PLAIN) {
-    tabstrip->CompleteDrop(data->data);
-    gtk_drag_finish(context, TRUE, TRUE, time);
+    success = tabstrip->CompleteDrop(data->data);
   }
 
+  gtk_drag_finish(context, success, success, time);
   return TRUE;
 }
 

chrome/browser/gtk/tabs/tab_strip_gtk.h
@@ -307,8 +307,9 @@ class TabStripGtk : public TabStripModelObserver,
   void SetDropIndex(int index, bool drop_before);
 
   // Determines whether the data is acceptable by the tabstrip and opens a new
-  // tab with the data as URL if it is.
-  void CompleteDrop(guchar* data);
+  // tab with the data as URL if it is.  Returns true if the drop was
+  // successful.
+  bool CompleteDrop(guchar* data);
 
   // Returns the image to use for indicating a drop on a tab. If is_down is
   // true, this returns an arrow pointing down.

commit 2d209edb29c44fda75e3816e8170c565ef104f23

gtk: Fix a regression from r20404 which keeps a drag into the tabstrip from working.  Only destroy the container widget and not the drop info itself when the drag leaves the tab strip; otherwise, we'll lose the drop information used to open the link.
BUG=none
TEST=Drag a link into the tab strip.  The drop arrow should appear and disappear as the mouse enters and leaves the tab strip.  Drop the link into the tab strip.  The link should be opened in the new tab (or the current tab).
Review URL: http://codereview.chromium.org/159178

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21285 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/gtk/tabs/tab_strip_gtk.cc
@@ -1471,26 +1471,19 @@ void TabStripGtk::UpdateDropIndex(GdkDragContext* context, gint x, gint y) {
 }
 
 void TabStripGtk::SetDropIndex(int index, bool drop_before) {
-  if (index == -1) {
-    if (drop_info_.get())
-      gtk_widget_hide(drop_info_->container);
-    return;
-  }
-
-  if (drop_info_.get() && !GTK_WIDGET_VISIBLE(drop_info_->container))
-    gtk_widget_show(drop_info_->container);
-
-  if (drop_info_.get() && drop_info_->drop_index == index &&
-      drop_info_->drop_before == drop_before) {
-    return;
-  }
-
   bool is_beneath;
   gfx::Rect drop_bounds = GetDropBounds(index, drop_before, &is_beneath);
 
   if (!drop_info_.get()) {
     drop_info_.reset(new DropInfo(index, drop_before, !is_beneath));
   } else {
+    if (!GTK_IS_WIDGET(drop_info_->container)) {
+      drop_info_->CreateContainer();
+    } else if (drop_info_->drop_index == index &&
+               drop_info_->drop_before == drop_before) {
+      return;
+    }
+
     drop_info_->drop_index = index;
     drop_info_->drop_before = drop_before;
     if (is_beneath == drop_info_->point_down) {
@@ -1549,22 +1542,12 @@ TabStripGtk::DropInfo::DropInfo(int drop_index, bool drop_before,
     : drop_index(drop_index),
       drop_before(drop_before),
       point_down(point_down) {
-  container = gtk_window_new(GTK_WINDOW_POPUP);
-  SetContainerColorMap();
-  gtk_widget_set_app_paintable(container, TRUE);
-  g_signal_connect(G_OBJECT(container), "expose-event",
-                   G_CALLBACK(OnExposeEvent), this);
-  gtk_widget_add_events(container, GDK_STRUCTURE_MASK);
-  gtk_window_move(GTK_WINDOW(container), 0, 0);
-  gtk_window_resize(GTK_WINDOW(container),
-                    drop_indicator_width, drop_indicator_height);
-  gtk_widget_show_all(container);
-
+  CreateContainer();
   drop_arrow = GetDropArrowImage(point_down);
 }
 
 TabStripGtk::DropInfo::~DropInfo() {
-  gtk_widget_destroy(container);
+  DestroyContainer();
 }
 
 // static
@@ -1642,6 +1625,24 @@ void TabStripGtk::DropInfo::SetContainerShapeMask() {
   // Set the shape mask.
   gdk_window_shape_combine_mask(container->window, pixmap, 0, 0);
   g_object_unref(pixmap);
+}
+
+void TabStripGtk::DropInfo::CreateContainer() {
+  container = gtk_window_new(GTK_WINDOW_POPUP);
+  SetContainerColorMap();
+  gtk_widget_set_app_paintable(container, TRUE);
+  g_signal_connect(G_OBJECT(container), "expose-event",
+                   G_CALLBACK(OnExposeEvent), this);
+  gtk_widget_add_events(container, GDK_STRUCTURE_MASK);
+  gtk_window_move(GTK_WINDOW(container), 0, 0);
+  gtk_window_resize(GTK_WINDOW(container),
+                    drop_indicator_width, drop_indicator_height);
+  gtk_widget_show_all(container);
+}
+
+void TabStripGtk::DropInfo::DestroyContainer() {
+  if (GTK_IS_WIDGET(container))
+    gtk_widget_destroy(container);
 }
 
 // Called from:
@@ -1852,7 +1853,7 @@ gboolean TabStripGtk::OnDragDrop(GtkWidget* widget, GdkDragContext* context,
 gboolean TabStripGtk::OnDragLeave(GtkWidget* widget, GdkDragContext* context,
                                   guint time, TabStripGtk* tabstrip) {
   // Destroy the drop indicator.
-  tabstrip->drop_info_.reset();
+  tabstrip->drop_info_->DestroyContainer();
   return FALSE;
 }
 

chrome/browser/gtk/tabs/tab_strip_gtk.h
@@ -176,6 +176,12 @@ class TabStripGtk : public TabStripModelObserver,
     // screen.
     void SetContainerShapeMask();
 
+    // Creates the container widget.
+    void CreateContainer();
+
+    // Destroys the container widget.
+    void DestroyContainer();
+
     // Index of the tab to drop on. If drop_before is true, the drop should
     // occur between the tab at drop_index - 1 and drop_index.
     // WARNING: if drop_before is true it is possible this will == tab_count,