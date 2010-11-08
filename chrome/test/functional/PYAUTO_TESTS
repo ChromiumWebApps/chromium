ChromiumWebApps/chromium

commit 44b94b8f2d7a1b9a4990f0ad5c0805397215813a

Use .crdownload file path for 'Show In Folder' while download is in progress
BUG=57037
TEST=download a huge file and click 'Show In Folder' during the download

Review URL: http://codereview.chromium.org/4460002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65418 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/automation/automation_provider.cc
@@ -282,8 +282,10 @@ DictionaryValue* AutomationProvider::GetDictionaryFromDownloadItem(
   dl_item_value->SetInteger("id", static_cast<int>(download->id()));
   dl_item_value->SetString("url", download->url().spec());
   dl_item_value->SetString("referrer_url", download->referrer_url().spec());
-  dl_item_value->SetString("file_name", download->GetFileName().value());
-  dl_item_value->SetString("full_path", download->full_path().value());
+  dl_item_value->SetString("file_name",
+                           download->GetFileNameToReportUser().value());
+  dl_item_value->SetString("full_path",
+                           download->GetTargetFilePath().value());
   dl_item_value->SetBoolean("is_paused", download->is_paused());
   dl_item_value->SetBoolean("open_when_complete",
                             download->open_when_complete());

chrome/browser/cocoa/download/download_item_cell.mm
@@ -235,7 +235,7 @@ NSGradient* BackgroundTheme::GetNSGradient(int id) const {
 
 - (void)setStateFromDownload:(BaseDownloadItemModel*)downloadModel {
   // Set the name of the download.
-  downloadPath_ = downloadModel->download()->GetFileName();
+  downloadPath_ = downloadModel->download()->GetFileNameToReportUser();
 
   std::wstring statusText = downloadModel->GetStatusText();
   if (statusText.empty()) {

chrome/browser/cocoa/download/download_item_controller.mm
@@ -181,8 +181,8 @@ class DownloadShelfContextMenuMac : public DownloadShelfContextMenu {
       // This basic fixup copies Windows DownloadItemView::DownloadItemView().
 
       // Extract the file extension (if any).
-      FilePath filepath(downloadModel->download()->original_name());
-      FilePath::StringType extension = filepath.Extension();
+      FilePath filename(downloadModel->download()->target_name());
+      FilePath::StringType extension = filename.Extension();
 
       // Remove leading '.' from the extension
       if (extension.length() > 0)
@@ -197,15 +197,14 @@ class DownloadShelfContextMenuMac : public DownloadShelfContextMenu {
       }
 
       // Rebuild the filename.extension.
-      std::wstring rootname =
-          UTF8ToWide(filepath.BaseName().RemoveExtension().value());
+      std::wstring rootname = UTF8ToWide(filename.RemoveExtension().value());
       ElideString(rootname, kFileNameMaxLength - extension.length(), &rootname);
-      std::string filename = WideToUTF8(rootname);
+      std::string new_filename = WideToUTF8(rootname);
       if (extension.length())
-        filename += std::string(".") + extension;
+        new_filename += std::string(".") + extension;
 
       dangerousWarning = l10n_util::GetNSStringFWithFixup(
-          IDS_PROMPT_DANGEROUS_DOWNLOAD, UTF8ToUTF16(filename));
+          IDS_PROMPT_DANGEROUS_DOWNLOAD, UTF8ToUTF16(new_filename));
       confirmButtonTitle = l10n_util::GetNSStringWithFixup(IDS_SAVE_DOWNLOAD);
     }
     [dangerousDownloadLabel_ setStringValue:dangerousWarning];
@@ -268,7 +267,7 @@ class DownloadShelfContextMenuMac : public DownloadShelfContextMenu {
 
 - (void)updateToolTip {
   string16 elidedFilename = gfx::ElideFilename(
-      [self download]->GetFileName(),
+      [self download]->GetFileNameToReportUser(),
       gfx::Font(), kToolTipMaxWidth);
   [progressView_ setToolTip:base::SysUTF16ToNSString(elidedFilename)];
 }

chrome/browser/cocoa/download/download_item_mac.h
@@ -55,7 +55,7 @@ class DownloadItemMac : DownloadItem::Observer {
   CancelableRequestConsumerT<int, 0> icon_consumer_;
 
   // Stores the last known name where the file will be saved.
-  FilePath lastFilePath_;
+  FilePath lastFileName_;
 
   DISALLOW_COPY_AND_ASSIGN(DownloadItemMac);
 };

chrome/browser/cocoa/download/download_item_mac.mm
@@ -34,12 +34,12 @@ void DownloadItemMac::OnDownloadUpdated(DownloadItem* download) {
     [item_controller_ clearDangerousMode];
   }
 
-  if (download->full_path() != lastFilePath_) {
-    // Turns out the file path is "unconfirmed %d.download" for dangerous
+  if (download->GetUserVerifiedFileName() != lastFileName_) {
+    // Turns out the file path is "unconfirmed %d.crdownload" for dangerous
     // downloads. When the download is confirmed, the file is renamed on
     // another thread, so reload the icon if the download filename changes.
     LoadIcon();
-    lastFilePath_ = download->full_path();
+    lastFileName_ = download->GetUserVerifiedFileName();
 
     [item_controller_ updateToolTip];
   }
@@ -72,7 +72,7 @@ void DownloadItemMac::LoadIcon() {
   }
 
   // We may already have this particular image cached.
-  FilePath file = download_model_->download()->full_path();
+  FilePath file = download_model_->download()->GetUserVerifiedFileName();
   SkBitmap* icon_bitmap = icon_manager->LookupIcon(file, IconLoader::SMALL);
   if (icon_bitmap) {
     NSImage* icon = gfx::SkBitmapToNSImage(*icon_bitmap);

chrome/browser/dom_ui/downloads_dom_handler.cc
@@ -159,7 +159,8 @@ void DownloadsDOMHandler::HandleDrag(const ListValue* args) {
   DownloadItem* file = GetDownloadByValue(args);
   if (file) {
     IconManager* im = g_browser_process->icon_manager();
-    SkBitmap* icon = im->LookupIcon(file->full_path(), IconLoader::NORMAL);
+    SkBitmap* icon = im->LookupIcon(file->GetUserVerifiedFileName(),
+                                    IconLoader::NORMAL);
     gfx::NativeView view = dom_ui_->tab_contents()->GetNativeView();
     download_util::DragDownload(file, icon, view);
   }

chrome/browser/download/download_file_manager.cc
@@ -286,7 +286,7 @@ void DownloadFileManager::OnIntermediateDownloadName(
     return;
 
   DownloadFile* download = it->second;
-  if (!download->Rename(full_path, false)) {
+  if (!download->Rename(full_path, false /* is_final_rename */)) {
     // Error. Between the time the UI thread generated 'full_path' to the time
     // this code runs, something happened that prevents us from renaming.
     CancelDownloadOnRename(id);
@@ -303,17 +303,16 @@ void DownloadFileManager::OnIntermediateDownloadName(
 // 1. tmp -> foo            (need_delete_crdownload=T)
 // 2. foo.crdownload -> foo (need_delete_crdownload=F)
 // 3. tmp-> unconfirmed.xxx.crdownload (need_delete_crdownload=F)
-void DownloadFileManager::OnFinalDownloadName(int id,
-                                              const FilePath& full_path,
-                                              bool need_delete_crdownload,
-                                              DownloadManager* manager) {
+void DownloadFileManager::OnFinalDownloadName(
+    int id, const FilePath& full_path, bool need_delete_crdownload,
+    DownloadManager* download_manager) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
 
   DownloadFile* download = GetDownloadFile(id);
   if (!download)
     return;
 
-  if (download->Rename(full_path, true)) {
+  if (download->Rename(full_path, true /* is_final_rename */)) {
 #if defined(OS_MACOSX)
     // Done here because we only want to do this once; see
     // http://crbug.com/13120 for details.
@@ -322,7 +321,7 @@ void DownloadFileManager::OnFinalDownloadName(int id,
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
         NewRunnableMethod(
-            manager, &DownloadManager::DownloadRenamedToFinalName, id,
+            download_manager, &DownloadManager::DownloadRenamedToFinalName, id,
             full_path));
   } else {
     // Error. Between the time the UI thread generated 'full_path' to the time

chrome/browser/download/download_item.cc
@@ -57,7 +57,7 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       open_when_complete_(false),
       safety_state_(SAFE),
       auto_opened_(false),
-      original_name_(info.original_name),
+      target_name_(info.original_name),
       render_process_id_(-1),
       request_id_(-1),
       save_as_(false),
@@ -65,7 +65,6 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_extension_install_(info.is_extension_install),
       name_finalized_(false),
       is_temporary_(false),
-      need_final_rename_(false),
       opened_(false) {
   if (state_ == IN_PROGRESS)
     state_ = CANCELLED;
@@ -94,7 +93,7 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       open_when_complete_(false),
       safety_state_(info.is_dangerous ? DANGEROUS : SAFE),
       auto_opened_(false),
-      original_name_(info.original_name),
+      target_name_(info.original_name),
       render_process_id_(info.child_id),
       request_id_(info.request_id),
       save_as_(info.prompt_user_for_save_location),
@@ -102,7 +101,6 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_extension_install_(info.is_extension_install),
       name_finalized_(false),
       is_temporary_(!info.save_info.file_path.empty()),
-      need_final_rename_(false),
       opened_(false) {
   Init(true /* start progress timer */);
 }
@@ -130,7 +128,6 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       open_when_complete_(false),
       safety_state_(SAFE),
       auto_opened_(false),
-      original_name_(FilePath()),
       render_process_id_(-1),
       request_id_(-1),
       save_as_(false),
@@ -138,7 +135,6 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_extension_install_(false),
       name_finalized_(false),
       is_temporary_(false),
-      need_final_rename_(false),
       opened_(false) {
   Init(true /* start progress timer */);
 }
@@ -165,24 +161,21 @@ void DownloadItem::NotifyObserversDownloadFileCompleted() {
 }
 
 bool DownloadItem::CanOpenDownload() {
-  FilePath file_to_use = full_path();
-  if (!original_name().value().empty())
-    file_to_use = original_name();
-
-  return !Extension::IsExtension(file_to_use) &&
-      !download_util::IsExecutableFile(file_to_use);
+  return !Extension::IsExtension(target_name_) &&
+      !download_util::IsExecutableFile(target_name_);
 }
 
 bool DownloadItem::ShouldOpenFileBasedOnExtension() {
-  return download_manager_->ShouldOpenFileBasedOnExtension(full_path());
+  return download_manager_->ShouldOpenFileBasedOnExtension(
+      GetUserVerifiedFileName());
 }
 
 void DownloadItem::OpenFilesBasedOnExtension(bool open) {
   DownloadPrefs* prefs = download_manager_->download_prefs();
   if (open)
-    prefs->EnableAutoOpenBasedOnExtension(full_path());
+    prefs->EnableAutoOpenBasedOnExtension(GetUserVerifiedFileName());
   else
-    prefs->DisableAutoOpenBasedOnExtension(full_path());
+    prefs->DisableAutoOpenBasedOnExtension(GetUserVerifiedFileName());
 }
 
 void DownloadItem::OpenDownload() {
@@ -281,7 +274,8 @@ void DownloadItem::Finished() {
                                        *this);
     auto_opened_ = true;
   } else if (open_when_complete() ||
-             download_manager_->ShouldOpenFileBasedOnExtension(full_path()) ||
+             download_manager_->ShouldOpenFileBasedOnExtension(
+                 GetUserVerifiedFileName()) ||
              is_temporary()) {
     // If the download is temporary, like in drag-and-drop, do not open it but
     // we still need to set it auto-opened so that it can be removed from the
@@ -344,7 +338,6 @@ int DownloadItem::PercentComplete() const {
 void DownloadItem::Rename(const FilePath& full_path) {
   DCHECK(!full_path.empty());
   full_path_ = full_path;
-  file_name_ = full_path_.BaseName();
 }
 
 void DownloadItem::TogglePause() {
@@ -386,26 +379,35 @@ bool DownloadItem::MatchesQuery(const string16& query) const {
   if (url_formatted.find(query) != string16::npos)
     return true;
 
-  string16 path(l10n_util::ToLower(WideToUTF16(full_path_.ToWStringHack())));
+  string16 path(l10n_util::ToLower(WideToUTF16(full_path().ToWStringHack())));
   if (path.find(query) != std::wstring::npos)
     return true;
 
   return false;
 }
 
-FilePath DownloadItem::GetFileName() const {
-  if (safety_state_ == DownloadItem::SAFE)
-    return file_name_;
+FilePath DownloadItem::GetTargetFilePath() const {
+  return full_path_.DirName().Append(target_name_);
+}
+
+FilePath DownloadItem::GetFileNameToReportUser() const {
   if (path_uniquifier_ > 0) {
-    FilePath name(original_name_);
+    FilePath name(target_name_);
     download_util::AppendNumberToPath(&name, path_uniquifier_);
     return name;
   }
-  return original_name_;
+  return target_name_;
+}
+
+FilePath DownloadItem::GetUserVerifiedFileName() const {
+  if (safety_state_ == DownloadItem::SAFE)
+    return target_name_;
+  return full_path_.BaseName();
 }
 
 void DownloadItem::Init(bool start_timer) {
-  file_name_ = full_path_.BaseName();
+  if (target_name_.value().empty())
+    target_name_ = full_path_.BaseName();
   if (start_timer)
     StartProgressTimer();
 }

commit 07ef48cef1e5486999c682e083486a41bbc8df02

Disable failing testDownloadsPersistence, testNoUnsafeDownloadsOnRestart tests
These downloads tests were broken in r65418. Disabling until fix.

BUG=62398
TEST=

Review URL: http://codereview.chromium.org/4720001

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65436 0039d316-1c4b-4281-b951-d872f2087c98

chrome/test/functional/PYAUTO_TESTS
@@ -31,6 +31,9 @@
     'cookies',
     'crash_reporter',
     'downloads',
+    # crbug.com/62398. Also look in the chromeos section while removing this.
+    '-downloads.DownloadsTest.testDownloadsPersistence',
+    '-downloads.DownloadsTest.testNoUnsafeDownloadsOnRestart',
     'find_in_page',
     # Turkish I problem. crbug.com/60638
     '-find_in_page.FindMatchTests.testLocalizationAndCaseOrder',
@@ -106,7 +109,9 @@
     '-prefs.PrefsTest.testSessionRestoreURLs',
     '-prefs.PrefsTest.testSessionRestore',
     '-translate.TranslateTest.testSessionRestore',
-    '-downloads.DownloadsTest.testNoUnsafeDownloadsOnRestart',
+    # Uncomment the next line when the suppression for bug 62398 (above)
+    # is removed.
+    # '-downloads.DownloadsTest.testNoUnsafeDownloadsOnRestart',
     '-downloads.DownloadsTest.testZip',
     '-downloads.DownloadsTest.testZipInIncognito',
     '-downloads.DownloadsTest.testCrazyFilenames',