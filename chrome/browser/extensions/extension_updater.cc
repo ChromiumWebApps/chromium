ChromiumWebApps/chromium

commit 312066067a8f06eef6a642473d9c953568f11fbd

[Extensions] Add CheckSoon() method to ExtensionUpdater
Change ExtensionService::CheckForUpdates() to use CheckSoon().

Change sync code to not worry about calling CheckForUpdates() at most
once.

BUG=
TEST=

Review URL: http://codereview.chromium.org/6832001

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81501 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/extensions/extension_updater.cc
@@ -476,8 +476,9 @@ ExtensionUpdater::ExtensionUpdater(ExtensionServiceInterface* service,
     : alive_(false),
       weak_ptr_factory_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
       service_(service), frequency_seconds_(frequency_seconds),
-      extension_prefs_(extension_prefs), prefs_(prefs), profile_(profile),
-      blacklist_checks_enabled_(true) {
+      method_factory_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
+      will_check_soon_(false), extension_prefs_(extension_prefs),
+      prefs_(prefs), profile_(profile), blacklist_checks_enabled_(true) {
   Init();
 }
 
@@ -579,6 +580,8 @@ void ExtensionUpdater::Stop() {
   prefs_ = NULL;
   profile_ = NULL;
   timer_.Stop();
+  will_check_soon_ = false;
+  method_factory_.RevokeAll();
   manifest_fetcher_.reset();
   extension_fetcher_.reset();
   STLDeleteElements(&manifests_pending_);
@@ -906,6 +909,30 @@ void ExtensionUpdater::TimerFired() {
   int64 now = Time::Now().ToInternalValue();
   prefs_->SetInt64(kLastExtensionsUpdateCheck, now);
   ScheduleNextCheck(TimeDelta::FromSeconds(frequency_seconds_));
+}
+
+void ExtensionUpdater::CheckSoon() {
+  DCHECK(alive_);
+  if (will_check_soon_) {
+    return;
+  }
+  if (BrowserThread::PostTask(
+          BrowserThread::UI, FROM_HERE,
+          method_factory_.NewRunnableMethod(&ExtensionUpdater::CheckNow))) {
+    will_check_soon_ = true;
+  } else {
+    NOTREACHED();
+  }
+}
+
+bool ExtensionUpdater::WillCheckSoon() const {
+  return will_check_soon_;
+}
+
+void ExtensionUpdater::DoCheckSoon() {
+  DCHECK(will_check_soon_);
+  CheckNow();
+  will_check_soon_ = false;
 }
 
 void ExtensionUpdater::CheckNow() {

chrome/browser/extensions/extension_updater_unittest.cc
@@ -127,7 +127,7 @@ class MockService : public ExtensionServiceInterface {
     FAIL();
   }
 
-  virtual void CheckForUpdates() {
+  virtual void CheckForUpdatesSoon() {
     FAIL();
   }
 
@@ -349,6 +349,11 @@ class ExtensionUpdaterTest : public testing::Test {
     EXPECT_TRUE(updater->timer_.IsRunning());
     updater->timer_.Stop();
     updater->TimerFired();
+  }
+
+  static void SimulateCheckSoon(ExtensionUpdater* updater) {
+    EXPECT_TRUE(updater->will_check_soon_);
+    updater->DoCheckSoon();
   }
 
   // Adds a Result with the given data to results.
@@ -1194,6 +1199,32 @@ TEST(ExtensionUpdaterTest, TestStartUpdateCheckMemory) {
         GURL("http://www.google.com")));
     // This should clear out |manifests_pending_|.
     updater.Stop();
+}
+
+TEST(ExtensionUpdaterTest, TestCheckSoon) {
+    MessageLoop message_loop;
+    BrowserThread ui_thread(BrowserThread::UI, &message_loop);
+    BrowserThread file_thread(BrowserThread::FILE, &message_loop);
+
+    ServiceForManifestTests service;
+    TestURLFetcherFactory factory;
+    URLFetcher::set_factory(&factory);
+    ExtensionUpdater updater(
+        &service, service.extension_prefs(), service.pref_service(),
+        service.profile(), kUpdateFrequencySecs);
+    EXPECT_FALSE(updater.WillCheckSoon());
+    updater.Start();
+    EXPECT_FALSE(updater.WillCheckSoon());
+    updater.CheckSoon();
+    EXPECT_TRUE(updater.WillCheckSoon());
+    updater.CheckSoon();
+    EXPECT_TRUE(updater.WillCheckSoon());
+    ExtensionUpdaterTest::SimulateCheckSoon(&updater);
+    EXPECT_FALSE(updater.WillCheckSoon());
+    updater.CheckSoon();
+    EXPECT_TRUE(updater.WillCheckSoon());
+    updater.Stop();
+    EXPECT_FALSE(updater.WillCheckSoon());
 }
 
 // TODO(asargent) - (http://crbug.com/12780) add tests for:

chrome/browser/extensions/extension_service.cc
@@ -1156,11 +1156,11 @@ void ExtensionService::CheckAdminBlacklist() {
     UnloadExtension(to_be_removed[i], UnloadedExtensionInfo::DISABLE);
 }
 
-void ExtensionService::CheckForUpdates() {
+void ExtensionService::CheckForUpdatesSoon() {
   if (updater()) {
-    updater()->CheckNow();
+    updater()->CheckSoon();
   } else {
-    LOG(WARNING) << "CheckForUpdates() called with auto-update turned off";
+    LOG(WARNING) << "CheckForUpdatesSoon() called with auto-update turned off";
   }
 }
 

chrome/browser/extensions/extension_service.h
@@ -79,7 +79,11 @@ class ExtensionServiceInterface {
   virtual void SetIsIncognitoEnabled(const std::string& extension_id,
                                      bool enabled) = 0;
 
-  virtual void CheckForUpdates() = 0;
+  // Safe to call multiple times in a row.
+  //
+  // TODO(akalin): Remove this method (and others) once we add
+  // ProcessSyncData().
+  virtual void CheckForUpdatesSoon() = 0;
 };
 
 // Manages installed and running Chromium extensions.
@@ -339,7 +343,7 @@ class ExtensionService
   // set of extensions.
   virtual void CheckAdminBlacklist();
 
-  virtual void CheckForUpdates();
+  virtual void CheckForUpdatesSoon();
 
   void set_extensions_enabled(bool enabled) { extensions_enabled_ = enabled; }
   bool extensions_enabled() { return extensions_enabled_; }

chrome/browser/extensions/extension_updater.h
@@ -185,14 +185,23 @@ class ExtensionUpdater : public URLFetcher::Delegate {
   // crx downloads. Does not cancel any in-progress installs.
   void Stop();
 
-  // Starts an update check right now, instead of waiting for the next regularly
-  // scheduled check.
+  // Posts a task to do an update check.  Does nothing if there is
+  // already a pending task that has not yet run.
+  void CheckSoon();
+
+  // Starts an update check right now, instead of waiting for the next
+  // regularly scheduled check or a pending check from CheckSoon().
   void CheckNow();
 
   // Set blacklist checks on or off.
   void set_blacklist_checks_enabled(bool enabled) {
     blacklist_checks_enabled_ = enabled;
   }
+
+  // Returns true iff CheckSoon() has been called but the update check
+  // hasn't been performed yet.  This is used mostly by tests; calling
+  // code should just call CheckSoon().
+  bool WillCheckSoon() const;
 
  private:
   friend class ExtensionUpdaterTest;
@@ -266,6 +275,9 @@ class ExtensionUpdater : public URLFetcher::Delegate {
   // BaseTimer::ReceiverMethod callback.
   void TimerFired();
 
+  // Posted by CheckSoon().
+  void DoCheckSoon();
+
   // Begins an update check. Takes ownership of |fetch_data|.
   void StartUpdateCheck(ManifestFetchData* fetch_data);
 
@@ -328,6 +340,10 @@ class ExtensionUpdater : public URLFetcher::Delegate {
 
   base::OneShotTimer<ExtensionUpdater> timer_;
   int frequency_seconds_;
+
+  ScopedRunnableMethodFactory<ExtensionUpdater> method_factory_;
+
+  bool will_check_soon_;
 
   ExtensionPrefs* extension_prefs_;
   PrefService* prefs_;

chrome/browser/extensions/mock_extension_service.h
@@ -38,7 +38,7 @@ class MockExtensionService : public ExtensionServiceInterface {
   MOCK_METHOD0(HasInstalledExtensions, bool());
   MOCK_CONST_METHOD1(IsIncognitoEnabled, bool(const std::string&));
   MOCK_METHOD2(SetIsIncognitoEnabled, void(const std::string&, bool));
-  MOCK_METHOD0(CheckForUpdates, void());
+  MOCK_METHOD0(CheckForUpdatesSoon, void());
 };
 
 #endif  // CHROME_BROWSER_EXTENSIONS_MOCK_EXTENSION_SERVICE_H_

chrome/browser/sync/glue/extension_sync.cc
@@ -317,7 +317,6 @@ bool FlushExtensionData(const ExtensionSyncTraits& traits,
   }
 
   // Update server and client as necessary.
-  bool should_nudge_extension_updater = false;
   for (ExtensionDataMap::const_iterator it = extension_data_map.begin();
        it != extension_data_map.end(); ++it) {
     ExtensionData extension_data = it->second;
@@ -334,16 +333,11 @@ bool FlushExtensionData(const ExtensionSyncTraits& traits,
       TryUpdateClient(traits.is_valid_and_syncable,
                       extensions_service, &extension_data);
       if (extension_data.NeedsUpdate(ExtensionData::CLIENT)) {
-        should_nudge_extension_updater = true;
+        extensions_service->CheckForUpdatesSoon();
       }
     }
     DCHECK(!extension_data.NeedsUpdate(ExtensionData::SERVER));
   }
-
-  if (should_nudge_extension_updater) {
-    extensions_service->CheckForUpdates();
-  }
-
   return true;
 }
 
@@ -443,7 +437,7 @@ void UpdateClient(const ExtensionSyncTraits& traits,
     TryUpdateClient(traits.is_valid_and_syncable,
                     extensions_service, &extension_data);
     if (extension_data.NeedsUpdate(ExtensionData::CLIENT)) {
-      extensions_service->CheckForUpdates();
+      extensions_service->CheckForUpdatesSoon();
     }
   }
   DCHECK(!extension_data.NeedsUpdate(ExtensionData::SERVER));

chrome/browser/sync/glue/theme_util.cc
@@ -137,7 +137,7 @@ void SetCurrentThemeFromThemeSpecifics(
       extensions_service->pending_extension_manager()->AddFromSync(
           id, update_url, &IsTheme,
           kInstallSilently, kEnableOnInstall, kEnableIncognitoOnInstall);
-      extensions_service->CheckForUpdates();
+      extensions_service->CheckForUpdatesSoon();
     }
   } else if (theme_specifics.use_system_theme_by_default()) {
     ThemeServiceFactory::GetForProfile(profile)->SetNativeTheme();

commit 0eea06f6952b120a77273e2e5f2e961874f51d7d

[Sync] Fix bug in ExtensionUpdater introduced by r81501
Basically, r81501 broke ExtensionUpdater::CheckSoon(), which is relied
on by extension/app/theme sync.

BUG=80496
TEST=Open two browser instances with theme sync enabled.  Install a new theme on one browser.  It should show up in the other.  Repeat a few times.


Review URL: http://codereview.chromium.org/6905005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82943 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/extensions/extension_updater.cc
@@ -918,7 +918,8 @@ void ExtensionUpdater::CheckSoon() {
   }
   if (BrowserThread::PostTask(
           BrowserThread::UI, FROM_HERE,
-          method_factory_.NewRunnableMethod(&ExtensionUpdater::CheckNow))) {
+          method_factory_.NewRunnableMethod(
+              &ExtensionUpdater::DoCheckSoon))) {
     will_check_soon_ = true;
   } else {
     NOTREACHED();

chrome/browser/extensions/extension_updater_unittest.cc
@@ -270,9 +270,10 @@ class ExtensionUpdaterTest : public testing::Test {
     updater->TimerFired();
   }
 
-  static void SimulateCheckSoon(ExtensionUpdater* updater) {
-    EXPECT_TRUE(updater->will_check_soon_);
-    updater->DoCheckSoon();
+  static void SimulateCheckSoon(const ExtensionUpdater& updater,
+                                MessageLoop* message_loop) {
+    EXPECT_TRUE(updater.will_check_soon_);
+    message_loop->RunAllPending();
   }
 
   // Adds a Result with the given data to results.
@@ -1133,7 +1134,7 @@ TEST(ExtensionUpdaterTest, TestCheckSoon) {
     EXPECT_TRUE(updater.WillCheckSoon());
     updater.CheckSoon();
     EXPECT_TRUE(updater.WillCheckSoon());
-    ExtensionUpdaterTest::SimulateCheckSoon(&updater);
+    ExtensionUpdaterTest::SimulateCheckSoon(updater, &message_loop);
     EXPECT_FALSE(updater.WillCheckSoon());
     updater.CheckSoon();
     EXPECT_TRUE(updater.WillCheckSoon());