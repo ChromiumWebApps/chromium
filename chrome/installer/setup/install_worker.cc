ChromiumWebApps/chromium

commit 48ddb167eaedd08cf087fcbfc5e1cecb8112c23e

Fix in-use updates where %TMP% is on a volume other than %USERPROFILE% and/or %SystemRoot%.  We now do temp work on the same volume as the eventual install.  In the case of system-level installs, this has the nice side-effect of allowing us to move the install into place rather than copy it, which should also speed up system-level installs.
BUG=70368
TEST=Set TMP and TEMP env vars to some volume other than the one holding %USERPROFILE% (for user-level installs) or %SystemRoot% (for system-level installs), then see if in-use updates work.

Review URL: http://codereview.chromium.org/6538091

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75899 0039d316-1c4b-4281-b951-d872f2087c98

chrome/installer/setup/install_worker.cc
@@ -114,15 +114,14 @@ void AddInstallerCopyTasks(const InstallerState& installer_state,
   FilePath exe_dst(installer_dir.Append(setup_path.BaseName()));
   FilePath archive_dst(installer_dir.Append(archive_path.BaseName()));
 
-  install_list->AddCopyTreeWorkItem(setup_path.value(), exe_dst.value(),
-                                    temp_path.value(), WorkItem::ALWAYS);
-  if (installer_state.system_install()) {
-    install_list->AddCopyTreeWorkItem(archive_path.value(), archive_dst.value(),
-                                      temp_path.value(), WorkItem::ALWAYS);
-  } else {
-    install_list->AddMoveTreeWorkItem(archive_path.value(), archive_dst.value(),
-                                      temp_path.value());
-  }
+  // In the past, we copied rather than moved for system level installs so that
+  // the permissions of %ProgramFiles% would be picked up.  Now that |temp_path|
+  // is in %ProgramFiles% for system level installs (and in %LOCALAPPDATA%
+  // otherwise), there is no need to do this.
+  install_list->AddMoveTreeWorkItem(setup_path.value(), exe_dst.value(),
+                                    temp_path.value());
+  install_list->AddMoveTreeWorkItem(archive_path.value(), archive_dst.value(),
+                                    temp_path.value());
 }
 
 // This method adds work items to create (or update) Chrome uninstall entry in
@@ -558,27 +557,24 @@ void AddInstallWorkItems(const InstallationState& original_state,
 
   // Extra executable for 64 bit systems.
   if (Is64bit()) {
-    install_list->AddCopyTreeWorkItem(
+    install_list->AddMoveTreeWorkItem(
         src_path.Append(installer::kWowHelperExe).value(),
         target_path.Append(installer::kWowHelperExe).value(),
-        temp_path.value(), WorkItem::ALWAYS);
-  }
-
-  // If it is system level install copy the version folder (since we want to
-  // take the permissions of %ProgramFiles% folder) otherwise just move it.
-  if (installer_state.system_install()) {
-    install_list->AddCopyTreeWorkItem(
-        src_path.AppendASCII(new_version.GetString()).value(),
-        target_path.AppendASCII(new_version.GetString()).value(),
-        temp_path.value(), WorkItem::ALWAYS);
-  } else {
-    install_list->AddMoveTreeWorkItem(
-        src_path.AppendASCII(new_version.GetString()).value(),
-        target_path.AppendASCII(new_version.GetString()).value(),
         temp_path.value());
   }
 
+  // In the past, we copied rather than moved for system level installs so that
+  // the permissions of %ProgramFiles% would be picked up.  Now that |temp_path|
+  // is in %ProgramFiles% for system level installs (and in %LOCALAPPDATA%
+  // otherwise), there is no need to do this.
+  install_list->AddMoveTreeWorkItem(
+      src_path.AppendASCII(new_version.GetString()).value(),
+      target_path.AppendASCII(new_version.GetString()).value(),
+      temp_path.value());
+
   // Copy the default Dictionaries only if the folder doesn't exist already.
+  // TODO(grt): Use AddMoveTreeWorkItem in a conditional WorkItemList, which
+  // will be more efficient in space and time.
   install_list->AddCopyTreeWorkItem(
       src_path.Append(installer::kDictionaries).value(),
       target_path.Append(installer::kDictionaries).value(),

chrome/chrome_installer.gypi
@@ -102,6 +102,7 @@
             'installer/util/product_unittest.cc',
             'installer/util/product_state_unittest.cc',
             'installer/util/run_all_unittests.cc',
+            'installer/util/self_cleaning_temp_dir_unittest.cc',
             'installer/util/set_reg_value_work_item_unittest.cc',
             'installer/util/shell_util_unittest.cc',
             'installer/util/wmi_unittest.cc',

chrome/chrome_installer_util.gypi
@@ -124,6 +124,8 @@
             'installer/util/product.h',
             'installer/util/product.cc',
             'installer/util/product_operations.h',
+            'installer/util/self_cleaning_temp_dir.cc',
+            'installer/util/self_cleaning_temp_dir.h',
             'installer/util/shell_util.cc',
             'installer/util/shell_util.h',
           ],

chrome/installer/setup/setup_main.cc
@@ -50,6 +50,7 @@
 #include "chrome/installer/util/lzma_util.h"
 #include "chrome/installer/util/master_preferences.h"
 #include "chrome/installer/util/master_preferences_constants.h"
+#include "chrome/installer/util/self_cleaning_temp_dir.h"
 #include "chrome/installer/util/shell_util.h"
 #include "chrome/installer/util/util_constants.h"
 
@@ -145,12 +146,15 @@ installer::InstallStatus RenameChromeExecutables(
   FilePath chrome_exe(target_path.Append(installer::kChromeExe));
   FilePath chrome_new_exe(target_path.Append(installer::kChromeNewExe));
 
-  // TODO(grt): Create the temp dir in the target_path rather than %TMP% and
-  // friends since it/they may be on another volume, which prevents us from
-  // moving an in-use chrome.exe out of the way.
-  ScopedTempDir temp_path;
-  if (!temp_path.CreateUniqueTempDir()) {
-    PLOG(ERROR) << "Failed to create Temp directory";
+  // Create a temporary backup directory on the same volume as chrome.exe so
+  // that chrome.exe can be moved (rather than copied) to make room for
+  // new_chrome.exe.
+  installer::SelfCleaningTempDir temp_path;
+  if (!temp_path.Initialize(target_path.DirName(),
+                            installer::kInstallTempDir)) {
+    PLOG(ERROR) << "Failed to create Temp directory "
+                << target_path.DirName()
+                       .Append(installer::kInstallTempDir).value();
     return installer::RENAME_FAILED;
   }
   scoped_ptr<WorkItemList> install_list(WorkItem::CreateWorkItemList());
@@ -180,6 +184,10 @@ installer::InstallStatus RenameChromeExecutables(
     install_list->Rollback();
     ret = installer::RENAME_FAILED;
   }
+  // temp_path's dtor will take care of deleting or scheduling itself for
+  // deletion at reboot when this scope closes.
+  VLOG(1) << "Deleting temporary directory " << temp_path.path().value();
+
   return ret;
 }
 
@@ -479,8 +487,9 @@ installer::InstallStatus InstallProductsHelper(
 
   // Create a temp folder where we will unpack Chrome archive. If it fails,
   // then we are doomed, so return immediately and no cleanup is required.
-  ScopedTempDir temp_path;
-  if (!temp_path.CreateUniqueTempDir()) {
+  installer::SelfCleaningTempDir temp_path;
+  if (!temp_path.Initialize(installer_state.target_path().DirName(),
+                            installer::kInstallTempDir)) {
     PLOG(ERROR) << "Could not create temporary path.";
     InstallUtil::WriteInstallerResult(system_install,
         installer_state.state_key(), installer::TEMP_DIR_FAILED,
@@ -630,32 +639,22 @@ installer::InstallStatus InstallProductsHelper(
     }
   }
 
-  // Delete temporary files. These include install temporary directory
-  // and master profile file if present. Note that we do not care about rollback
-  // here and we schedule for deletion on reboot below if the deletes fail. As
-  // such, we do not use DeleteTreeWorkItem.
-  VLOG(1) << "Deleting temporary directory " << temp_path.path().value();
-  bool cleanup_success = temp_path.Delete();
+  // Delete the master profile file if present. Note that we do not care about
+  // rollback here and we schedule for deletion on reboot if the delete fails.
+  // As such, we do not use DeleteTreeWorkItem.
   if (cmd_line.HasSwitch(installer::switches::kInstallerData)) {
-    std::wstring prefs_path = cmd_line.GetSwitchValueNative(
-        installer::switches::kInstallerData);
-    cleanup_success = file_util::Delete(prefs_path, true) && cleanup_success;
-  }
-
-  // The above cleanup has been observed to fail on several users machines.
-  // Specifically, it appears that the temp folder may be locked when we try
-  // to delete it. This is Rather Bad in the case where we have failed updates
-  // as we end up filling users' disks with large-ish temp files. To mitigate
-  // this, if we fail to delete the temp folders, then schedule them for
-  // deletion at next reboot.
-  if (!cleanup_success) {
-    ScheduleDirectoryForDeletion(temp_path.path().value().c_str());
-    if (cmd_line.HasSwitch(installer::switches::kInstallerData)) {
-      std::wstring prefs_path = cmd_line.GetSwitchValueNative(
-          installer::switches::kInstallerData);
-      ScheduleDirectoryForDeletion(prefs_path.c_str());
+    std::wstring prefs_path(cmd_line.GetSwitchValueNative(
+        installer::switches::kInstallerData));
+    if (!file_util::Delete(prefs_path, true)) {
+      LOG(ERROR) << "Failed deleting master preferences file " << prefs_path
+                 << ", scheduling for deletion after reboot.";
+      ScheduleFileSystemEntityForDeletion(prefs_path.c_str());
     }
   }
+
+  // temp_path's dtor will take care of deleting or scheduling itself for
+  // deletion at reboot when this scope closes.
+  VLOG(1) << "Deleting temporary directory " << temp_path.path().value();
 
   return install_status;
 }
@@ -776,6 +775,13 @@ bool HandleNonInstallCmdLineOptions(const InstallationState& original_state,
                                        FilePath(uncompressed_patch),
                                        new_setup_exe))
           status = installer::NEW_VERSION_UPDATED;
+      }
+      if (!temp_path.Delete()) {
+        // PLOG would be nice, but Delete() doesn't leave a meaningful value in
+        // the Windows last-error code.
+        LOG(WARNING) << "Scheduling temporary path " << temp_path.path().value()
+                     << " for deletion at reboot.";
+        ScheduleDirectoryForDeletion(temp_path.path().value().c_str());
       }
     }
 

chrome/installer/util/copy_tree_work_item.h
@@ -19,6 +19,11 @@
 // to the temporary directory passed in, and then copies the source hierarchy
 // to the destination location. During rollback the original destination
 // hierarchy is moved back.
+// NOTE: It is a best practice to ensure that the temporary directory is on the
+// same volume as the destination path.  If this is not the case, the existing
+// destination path is not moved, but rather copied, to the destination path.
+// This will result in in-use files being left behind, as well as potentially
+// losing ACLs or other metadata in the case of a rollback.
 class CopyTreeWorkItem : public WorkItem {
  public:
   virtual ~CopyTreeWorkItem();

chrome/installer/util/delete_after_reboot_helper.cc
@@ -79,7 +79,7 @@ bool ScheduleFileSystemEntityForDeletion(const wchar_t* path) {
     HANDLE file = ::CreateFileW(path, GENERIC_READ | GENERIC_WRITE, 0, NULL,
         OPEN_EXISTING, 0, NULL);
     if (file != INVALID_HANDLE_VALUE) {
-      PLOG(INFO) << " file not in use: " << path;
+      LOG(INFO) << " file not in use: " << path;
       ::CloseHandle(file);
     } else {
       PLOG(INFO) << " file in use (or not found?): " << path;
@@ -103,7 +103,7 @@ bool ScheduleDirectoryForDeletion(const wchar_t* dir_name) {
     if (::GetLastError() == ERROR_FILE_NOT_FOUND) {
       return true;  // Ok if directory is missing
     } else {
-      LOG(ERROR) << "Could not GetFileAttributes for " << dir_name;
+      PLOG(ERROR) << "Could not GetFileAttributes for " << dir_name;
       return false;
     }
   }
@@ -384,7 +384,7 @@ bool RemoveFromMovesPendingReboot(const wchar_t* directory) {
   }
   std::vector<char> buffer;
   StringArrayToMultiSZBytes(strings_to_keep, &buffer);
-  DCHECK(buffer.size() > 0);
+  DCHECK_GT(buffer.size(), 0U);
   if (buffer.empty())
     return false;
   return (session_manager_key.WriteValue(kPendingFileRenameOps, &buffer[0],

chrome/installer/util/self_cleaning_temp_dir.cc
@@ -1,0 +1,117 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/installer/util/self_cleaning_temp_dir.h"
+
+#include <windows.h>
+
+#include "base/file_util.h"
+#include "base/logging.h"
+#include "chrome/installer/util/delete_after_reboot_helper.h"
+
+namespace installer {
+
+// Populates |base_dir| with the topmost directory in the hierarchy of
+// |temp_parent_dir| that does not exist.  If |temp_parent_dir| exists,
+// |base_dir| is cleared.
+// static
+void SelfCleaningTempDir::GetTopDirToCreate(const FilePath& temp_parent_dir,
+                                        FilePath* base_dir) {
+  DCHECK(base_dir);
+
+  if (file_util::PathExists(temp_parent_dir)) {
+    // Empty base_dir means that we didn't create any extra directories.
+    base_dir->clear();
+  } else {
+    FilePath parent_dir(temp_parent_dir);
+    do {
+      *base_dir = parent_dir;
+      parent_dir = parent_dir.DirName();
+    } while (parent_dir != *base_dir && !file_util::PathExists(parent_dir));
+    LOG_IF(WARNING, !file_util::DirectoryExists(parent_dir))
+        << "A non-directory is at the base of the path leading to a desired "
+           "temp directory location: " << parent_dir.value();
+  }
+}
+
+SelfCleaningTempDir::SelfCleaningTempDir() {
+}
+
+SelfCleaningTempDir::~SelfCleaningTempDir() {
+  if (!path().empty() && !Delete())
+    LOG(WARNING) << "Failed to clean temp dir in dtor " << path().value();
+}
+
+bool SelfCleaningTempDir::Initialize(const FilePath& parent_dir,
+                                     const StringType& temp_name) {
+  DCHECK(parent_dir.IsAbsolute());
+  DCHECK(!temp_name.empty());
+
+  if (!path().empty()) {
+    LOG(DFATAL) << "Attempting to re-initialize a SelfSelfCleaningTempDir.";
+    return false;
+  }
+
+  FilePath temp_dir(parent_dir.Append(temp_name));
+  FilePath base_dir;
+  GetTopDirToCreate(parent_dir, &base_dir);
+
+  if (file_util::CreateDirectory(temp_dir)) {
+    base_dir_ = base_dir;
+    temp_dir_ = temp_dir;
+    return true;
+  }
+
+  return false;
+}
+
+bool SelfCleaningTempDir::Delete() {
+  if (path().empty()) {
+    LOG(DFATAL) << "Attempting to Delete an uninitialized SelfCleaningTempDir.";
+    return false;
+  }
+
+  FilePath next_dir(path().DirName());
+  bool schedule_deletes = false;
+
+  // First try to recursively delete the leaf directory managed by our
+  // ScopedTempDir.
+  if (!file_util::Delete(path(), true)) {
+    // That failed, so schedule the temp dir and its contents for deletion after
+    // reboot.
+    LOG(WARNING) << "Failed to delete temporary directory " << path().value()
+                 << ". Scheduling for deletion at reboot.";
+    schedule_deletes = true;
+    if (!ScheduleDirectoryForDeletion(path().value().c_str()))
+      return false;  // Entirely unexpected failure (Schedule logs the reason).
+  }
+
+  // Now delete or schedule all empty directories up to and including our
+  // base_dir_.  Any that can't be deleted are scheduled for deletion at reboot.
+  // This is safe since they'll only be deleted in that case if they're empty.
+  if (!base_dir_.empty()) {
+    do {
+      if (!schedule_deletes && !RemoveDirectory(next_dir.value().c_str())) {
+        PLOG_IF(WARNING, GetLastError() != ERROR_DIR_NOT_EMPTY)
+              << "Error removing directory " << next_dir.value().c_str();
+        schedule_deletes = true;
+      }
+      if (schedule_deletes) {
+        // Ignore the return code.  If we fail to schedule, go ahead and add the
+        // other parent directories anyway.
+        ScheduleFileSystemEntityForDeletion(next_dir.value().c_str());
+      }
+      if (next_dir == base_dir_)
+        break;  // We just processed the topmost directory we created.
+      next_dir = next_dir.DirName();
+    } while (true);
+  }
+
+  base_dir_.clear();
+  temp_dir_.clear();
+
+  return true;
+}
+
+}  // namespace installer

chrome/installer/util/self_cleaning_temp_dir.h
@@ -1,0 +1,56 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_UTIL_SELF_CLEANING_TEMP_DIR_H_
+#define CHROME_INSTALLER_UTIL_SELF_CLEANING_TEMP_DIR_H_
+#pragma once
+
+#include "base/basictypes.h"
+#include "base/file_path.h"
+#include "base/gtest_prod_util.h"
+
+namespace installer {
+
+// A helper class for managing a temporary directory.  In relation to
+// ScopedTempDir, this class additionally cleans up all non-empty parent
+// directories of the temporary directory that are created by an instance.
+class SelfCleaningTempDir {
+ public:
+  typedef FilePath::StringType StringType;
+
+  SelfCleaningTempDir();
+
+  // Performs a Delete().
+  ~SelfCleaningTempDir();
+
+  // Creates a temporary directory named |temp_name| under |parent_dir|,
+  // creating intermediate directories as needed.
+  bool Initialize(const FilePath& parent_dir, const StringType& temp_name);
+
+  // Returns the temporary directory created in Initialize().
+  const FilePath& path() const { return temp_dir_; }
+
+  // Deletes the temporary directory created in Initialize() and all of its
+  // contents, as well as all empty intermediate directories.  Any of these that
+  // cannot be deleted immediately are scheduled for deletion upon reboot.
+  bool Delete();
+
+ private:
+  static void GetTopDirToCreate(const FilePath& temp_parent_dir,
+                                FilePath* base_dir);
+
+  // The topmost directory created.
+  FilePath base_dir_;
+
+  // The temporary directory.
+  FilePath temp_dir_;
+
+  FRIEND_TEST_ALL_PREFIXES(SelfCleaningTempDirTest, TopLevel);
+  FRIEND_TEST_ALL_PREFIXES(SelfCleaningTempDirTest, TopLevelPlusOne);
+  DISALLOW_COPY_AND_ASSIGN(SelfCleaningTempDir);
+};
+
+}  // namespace installer
+
+#endif  // CHROME_INSTALLER_UTIL_SELF_CLEANING_TEMP_DIR_H_

commit 45bae236b03f577ed6682ef4c7ef3ee006de5e5a

Copy setup.exe rather than moving it since it is created outside of the target directory heirarchy (regression introduced in r75899).
BUG=82424
TEST=Install system-level Chrome as some admin user X.  Switch to admin user Y and try to uninstall.
R=robertshield@chromium.org

Review URL: http://codereview.chromium.org/7011018

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85159 0039d316-1c4b-4281-b951-d872f2087c98

chrome/installer/setup/install_worker.cc
@@ -97,12 +97,14 @@ void AddInstallerCopyTasks(const InstallerState& installer_state,
   FilePath exe_dst(installer_dir.Append(setup_path.BaseName()));
   FilePath archive_dst(installer_dir.Append(archive_path.BaseName()));
 
+  install_list->AddCopyTreeWorkItem(setup_path.value(), exe_dst.value(),
+                                    temp_path.value(), WorkItem::ALWAYS);
+
   // In the past, we copied rather than moved for system level installs so that
   // the permissions of %ProgramFiles% would be picked up.  Now that |temp_path|
   // is in %ProgramFiles% for system level installs (and in %LOCALAPPDATA%
-  // otherwise), there is no need to do this.
-  install_list->AddMoveTreeWorkItem(setup_path.value(), exe_dst.value(),
-                                    temp_path.value(), WorkItem::ALWAYS_MOVE);
+  // otherwise), there is no need to do this for the archive.  Setup.exe, on
+  // the other hand, is created elsewhere so it must always be copied.
   install_list->AddMoveTreeWorkItem(archive_path.value(), archive_dst.value(),
                                     temp_path.value(), WorkItem::ALWAYS_MOVE);
 }