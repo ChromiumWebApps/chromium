ChromiumWebApps/chromium

commit adc407950219a661c21804ee68729ce0fd1e460c

chromeos: Improve brightness and volume animations.
Make the onscreen brightness and volume indicators not be
janky.  Also change views::ProgressBar to permit specifying
min and max double values, instead of just using a hardcoded
integer range between 0 and 100.

BUG=chromium-os:19141,chromium-os:8473
TEST=manual: held volume keys and checked that animation is smooth

Review URL: http://codereview.chromium.org/7646004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96853 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/chromeos/setting_level_bubble.cc
@@ -4,9 +4,10 @@
 
 #include "chrome/browser/chromeos/setting_level_bubble.h"
 
+#include <algorithm>
+
 #include <gdk/gdk.h>
 
-#include "base/timer.h"
 #include "chrome/browser/chromeos/login/background_view.h"
 #include "chrome/browser/chromeos/login/login_utils.h"
 #include "chrome/browser/chromeos/login/webui_login_display.h"
@@ -19,10 +20,19 @@
 #include "ui/gfx/screen.h"
 #include "views/widget/root_view.h"
 
+using base::TimeDelta;
+using base::TimeTicks;
+using std::max;
+using std::min;
+
 namespace {
 
-const int kBubbleShowTimeoutSec = 2;
-const int kAnimationDurationMs = 200;
+// How long should the bubble be shown onscreen whenever the setting changes?
+const int kBubbleShowTimeoutMs = 1000;
+
+// How long should the level initially take to move up or down when it changes?
+// (The rate adapts to handle keyboard autorepeat.)
+const int64 kInitialAnimationDurationMs = 200;
 
 // Horizontal position of the center of the bubble on the screen: 0 is left
 // edge, 0.5 is center, 1 is right edge.
@@ -31,12 +41,12 @@ const double kBubbleXRatio = 0.5;
 // Vertical gap from the bottom of the screen in pixels.
 const int kBubbleBottomGap = 30;
 
-int LimitPercent(int percent) {
-  if (percent < 0)
-    percent = 0;
-  else if (percent > 100)
-    percent = 100;
-  return percent;
+// Duration between animation frames.
+// Chosen to match ui::SlideAnimation's kDefaultFramerateHz.
+const int kAnimationIntervalMs = 1000 / 50;
+
+double LimitPercent(double percent) {
+  return min(max(percent, 0.0), 100.0);
 }
 
 }  // namespace
@@ -75,30 +85,26 @@ static views::Widget* GetToplevelWidget() {
 SettingLevelBubble::SettingLevelBubble(SkBitmap* increase_icon,
                                        SkBitmap* decrease_icon,
                                        SkBitmap* disabled_icon)
-    : previous_percent_(-1),
-      current_percent_(-1),
+    : current_percent_(-1.0),
+      target_percent_(-1.0),
       increase_icon_(increase_icon),
       decrease_icon_(decrease_icon),
       disabled_icon_(disabled_icon),
       bubble_(NULL),
       view_(NULL),
-      animation_(this) {
-  animation_.SetSlideDuration(kAnimationDurationMs);
-  animation_.SetTweenType(ui::Tween::LINEAR);
+      is_animating_(false) {
 }
 
 SettingLevelBubble::~SettingLevelBubble() {}
 
-void SettingLevelBubble::ShowBubble(int percent, bool enabled) {
-  percent = LimitPercent(percent);
-  if (previous_percent_ == -1)
-    previous_percent_ = percent;
-  current_percent_ = percent;
+void SettingLevelBubble::ShowBubble(double percent, bool enabled) {
+  const double old_target_percent = target_percent_;
+  UpdateTargetPercent(percent);
 
   SkBitmap* icon = increase_icon_;
-  if (!enabled || current_percent_ == 0)
+  if (!enabled || target_percent_ == 0)
     icon = disabled_icon_;
-  else if (current_percent_ < previous_percent_)
+  else if (old_target_percent >= 0 && target_percent_ < old_target_percent)
     icon = decrease_icon_;
 
   if (!bubble_) {
@@ -109,7 +115,7 @@ void SettingLevelBubble::ShowBubble(int percent, bool enabled) {
     }
     DCHECK(view_ == NULL);
     view_ = new SettingLevelBubbleView;
-    view_->Init(icon, previous_percent_, enabled);
+    view_->Init(icon, current_percent_, enabled);
 
     // Calculate the position in screen coordinates that the bubble should
     // "point" at (since we use BubbleBorder::FLOAT, this position actually
@@ -123,29 +129,27 @@ void SettingLevelBubble::ShowBubble(int percent, bool enabled) {
         monitor_area.bottom() - view_size.height() / 2 - kBubbleBottomGap,
         0, 0);
 
-    // ShowFocusless doesn't set ESC accelerator.
     bubble_ = Bubble::ShowFocusless(parent_widget,
                                     position_relative_to,
                                     BubbleBorder::FLOAT,
                                     view_,  // contents
                                     this,   // delegate
                                     true);  // show while screen is locked
-    bubble_->set_fade_away_on_close(true);
+    // TODO(derat): We probably shouldn't be using Bubble.  It'd be nice to call
+    // bubble_->set_fade_away_on_close(true) here, but then, if ShowBubble()
+    // gets called while the bubble is fading away, we end up just adjusting the
+    // value on the disappearing bubble; ideally we'd have a way to cancel the
+    // fade and show the bubble at full opacity for another
+    // kBubbleShowTimeoutMs.
   } else {
     DCHECK(view_);
-    timeout_timer_.Stop();
+    hide_timer_.Stop();
     view_->SetIcon(icon);
+    view_->SetEnabled(enabled);
   }
 
-  view_->SetEnabled(enabled);
-
-  if (animation_.is_animating())
-    animation_.End();
-  animation_.Reset();
-  animation_.Show();
-
-  timeout_timer_.Start(base::TimeDelta::FromSeconds(kBubbleShowTimeoutSec),
-                       this, &SettingLevelBubble::OnTimeout);
+  hide_timer_.Start(base::TimeDelta::FromMilliseconds(kBubbleShowTimeoutMs),
+                    this, &SettingLevelBubble::OnHideTimeout);
 }
 
 void SettingLevelBubble::HideBubble() {
@@ -153,36 +157,51 @@ void SettingLevelBubble::HideBubble() {
     bubble_->Close();
 }
 
-void SettingLevelBubble::UpdateWithoutShowingBubble(int percent, bool enabled) {
+void SettingLevelBubble::UpdateWithoutShowingBubble(double percent,
+                                                    bool enabled) {
+  UpdateTargetPercent(percent);
   if (view_)
     view_->SetEnabled(enabled);
-
-  percent = LimitPercent(percent);
-
-  previous_percent_ =
-      animation_.is_animating() ?
-      animation_.GetCurrentValue() :
-      current_percent_;
-  if (previous_percent_ < 0)
-    previous_percent_ = percent;
-  current_percent_ = percent;
-
-  if (animation_.is_animating())
-    animation_.End();
-  animation_.Reset();
-  animation_.Show();
 }
 
-void SettingLevelBubble::OnTimeout() {
+void SettingLevelBubble::OnHideTimeout() {
   HideBubble();
+}
+
+void SettingLevelBubble::OnAnimationTimeout() {
+  const TimeTicks now = TimeTicks::Now();
+  const int64 remaining_ms = (target_time_ - now).InMilliseconds();
+
+  if (remaining_ms <= 0) {
+    current_percent_ = target_percent_;
+    StopAnimation();
+  } else {
+    // Figure out what fraction of the total time until we want to reach the
+    // target has elapsed since the last update.
+    const double remaining_percent = target_percent_ - current_percent_;
+    const int64 elapsed_ms =
+        (now - last_animation_update_time_).InMilliseconds();
+    current_percent_ +=
+        remaining_percent *
+        (static_cast<double>(elapsed_ms) / (elapsed_ms + remaining_ms));
+  }
+  last_animation_update_time_ = now;
+
+  if (view_)
+    view_->SetLevel(current_percent_);
 }
 
 void SettingLevelBubble::BubbleClosing(Bubble* bubble, bool) {
   DCHECK(bubble == bubble_);
-  timeout_timer_.Stop();
-  animation_.Stop();
+  hide_timer_.Stop();
+  StopAnimation();
   bubble_ = NULL;
   view_ = NULL;
+  current_percent_ = -1.0;
+  target_percent_ = -1.0;
+  target_time_ = TimeTicks();
+  last_animation_update_time_ = TimeTicks();
+  last_target_update_time_ = TimeTicks();
 }
 
 bool SettingLevelBubble::CloseOnEscape() {
@@ -193,17 +212,41 @@ bool SettingLevelBubble::FadeInOnShow() {
   return false;
 }
 
-void SettingLevelBubble::AnimationEnded(const ui::Animation* animation) {
-  previous_percent_ = current_percent_;
+void SettingLevelBubble::UpdateTargetPercent(double percent) {
+  target_percent_ = LimitPercent(percent);
+  const TimeTicks now = TimeTicks::Now();
+
+  if (current_percent_ < 0.0) {
+    // If we're setting the level for the first time, no need to animate.
+    current_percent_ = target_percent_;
+    if (view_)
+      view_->SetLevel(current_percent_);
+  } else {
+    // Use the time since the last request as a hint for the duration of the
+    // animation.  This makes us automatically adapt to the repeat rate if a key
+    // is being held down to change a setting (which prevents us from lagging
+    // behind when the key is finally released).
+    int64 duration_ms = kInitialAnimationDurationMs;
+    if (!last_target_update_time_.is_null())
+      duration_ms = min(kInitialAnimationDurationMs,
+                        (now - last_target_update_time_).InMilliseconds());
+    target_time_ = now + TimeDelta::FromMilliseconds(duration_ms);
+
+    if (!is_animating_) {
+      animation_timer_.Start(TimeDelta::FromMilliseconds(kAnimationIntervalMs),
+                             this,
+                             &SettingLevelBubble::OnAnimationTimeout);
+      is_animating_ = true;
+      last_animation_update_time_ = now;
+    }
+  }
+
+  last_target_update_time_ = now;
 }
 
-void SettingLevelBubble::AnimationProgressed(const ui::Animation* animation) {
-  if (view_) {
-    view_->SetLevel(
-        ui::Tween::ValueBetween(animation->GetCurrentValue(),
-                                previous_percent_,
-                                current_percent_));
-  }
+void SettingLevelBubble::StopAnimation() {
+  animation_timer_.Stop();
+  is_animating_ = false;
 }
 
 }  // namespace chromeos

chrome/browser/chromeos/login/update_view.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/chromeos/login/update_view.h"
 
+#include <algorithm>
 #include <string>
 
 #include "base/logging.h"
@@ -22,6 +23,7 @@
 #include "views/focus/focus_manager.h"
 #include "views/widget/widget.h"
 
+using std::max;
 using views::Background;
 using views::Label;
 using views::View;
@@ -88,6 +90,7 @@ void UpdateView::Init() {
 
   progress_bar_ = new views::ProgressBar();
   AddChildView(progress_bar_);
+  progress_bar_->SetDisplayRange(0.0, 100.0);
 
   // Curtain view.
   InitLabel(&checking_label_);
@@ -106,7 +109,7 @@ void UpdateView::Init() {
 }
 
 void UpdateView::Reset() {
-  progress_bar_->SetProgress(0);
+  progress_bar_->SetValue(0.0);
 }
 
 void UpdateView::UpdateLocalizedStrings() {
@@ -124,11 +127,12 @@ void UpdateView::UpdateLocalizedStrings() {
 }
 
 void UpdateView::AddProgress(int ticks) {
-  progress_bar_->AddProgress(ticks);
+  progress_bar_->SetValue(
+      max(progress_bar_->current_value() + ticks, 100.0));
 }
 
 void UpdateView::SetProgress(int progress) {
-  progress_bar_->SetProgress(progress);
+  progress_bar_->SetValue(progress);
 }
 
 void UpdateView::ShowManualRebootInfo() {

chrome/browser/chromeos/setting_level_bubble.h
@@ -7,10 +7,9 @@
 #pragma once
 
 #include "base/basictypes.h"
+#include "base/time.h"
 #include "base/timer.h"
 #include "chrome/browser/ui/views/bubble/bubble.h"
-#include "ui/base/animation/animation_delegate.h"
-#include "ui/base/animation/slide_animation.h"
 
 class SkBitmap;
 
@@ -20,13 +19,13 @@ class SettingLevelBubbleView;
 
 // Singleton class controlling a bubble displaying a level-based setting like
 // volume or brightness.
-class SettingLevelBubble : public BubbleDelegate,
-                           public ui::AnimationDelegate {
+class SettingLevelBubble : public BubbleDelegate {
  public:
-  void ShowBubble(int percent, bool enabled);
+  // Shows the bubble.  |percent| should be in the range [0.0, 100.0].
+  void ShowBubble(double percent, bool enabled);
   void HideBubble();
 
-  // Update the bubble's current level without showing the bubble onscreen.
+  // Updates the bubble's current level without showing the bubble onscreen.
   // We _do_ still animate the level moving to |percent| in case the bubble is
   // still visible from a previous call to ShowBubble().
   //
@@ -41,7 +40,7 @@ class SettingLevelBubble : public BubbleDelegate,
   // If we didn't update our internal state to 25% after 2), then the animation
   // displayed in response to 3) would show the bubble animating from 50% down
   // to 30%, rather than from 25% up to 30%.
-  void UpdateWithoutShowingBubble(int percent, bool enabled);
+  void UpdateWithoutShowingBubble(double percent, bool enabled);
 
  protected:
   SettingLevelBubble(SkBitmap* increase_icon,
@@ -50,20 +49,40 @@ class SettingLevelBubble : public BubbleDelegate,
   virtual ~SettingLevelBubble();
 
  private:
-  void OnTimeout();
-
   // Overridden from BubbleDelegate.
   virtual void BubbleClosing(Bubble* bubble, bool closed_by_escape) OVERRIDE;
   virtual bool CloseOnEscape() OVERRIDE;
   virtual bool FadeInOnShow() OVERRIDE;
 
-  // Overridden from ui::AnimationDelegate.
-  virtual void AnimationEnded(const ui::Animation* animation) OVERRIDE;
-  virtual void AnimationProgressed(const ui::Animation* animation) OVERRIDE;
+  // Callback for |hide_timer_|.  Closes the bubble.
+  void OnHideTimeout();
 
-  // Previous and current percentages, or -1 if not yet shown.
-  int previous_percent_;
-  int current_percent_;
+  // Callback for |animation_timer_|.  Updates the level displayed by the view,
+  // also stopping the animation if we've reached the target.
+  void OnAnimationTimeout();
+
+  // Animates towards |percent|.  Updates |target_percent_| and starts
+  // |animation_timer_| if it's not already running.  If this is the first time
+  // that the level is being set, we just update |view_| immediately and don't
+  // animate.
+  void UpdateTargetPercent(double percent);
+
+  // Stops |animation_timer_| if it's running.
+  void StopAnimation();
+
+  // Current and target percentages for the progress bar.  In the range
+  // [0.0, 100.0], or -1.0 if not yet shown.
+  double current_percent_;
+  double target_percent_;
+
+  // Time at which we'll reach |target_percent_|.
+  base::TimeTicks target_time_;
+
+  // Time at which we last updated |current_percent_|.
+  base::TimeTicks last_animation_update_time_;
+
+  // Time at which |target_percent_| was last updated.
+  base::TimeTicks last_target_update_time_;
 
   // Icons displayed in the bubble when increasing or decreasing the level or
   // when it's disabled.  Not owned by us.
@@ -74,11 +93,20 @@ class SettingLevelBubble : public BubbleDelegate,
   // Currently shown bubble or NULL.
   Bubble* bubble_;
 
-  // Its contents view, owned by Bubble.
+  // Contents view owned by Bubble.
   SettingLevelBubbleView* view_;
 
-  ui::SlideAnimation animation_;
-  base::OneShotTimer<SettingLevelBubble> timeout_timer_;
+  // Timer to hide the bubble.
+  base::OneShotTimer<SettingLevelBubble> hide_timer_;
+
+  // Timer to animate the currently-shown percent.  We use a timer instead of
+  // ui::Animation since our animations are frequently interrupted by additional
+  // changes to the level, and ui::Animation doesn't provide much control over
+  // in-progress animations, leading to mega-jank.
+  base::RepeatingTimer<SettingLevelBubble> animation_timer_;
+
+  // Is |animation_timer_| currently running?
+  bool is_animating_;
 
   DISALLOW_COPY_AND_ASSIGN(SettingLevelBubble);
 };

chrome/browser/chromeos/setting_level_bubble_view.cc
@@ -32,17 +32,15 @@ SettingLevelBubbleView::SettingLevelBubbleView()
       icon_(NULL) {
 }
 
-void SettingLevelBubbleView::Init(SkBitmap* icon,
-                                  int level_percent,
-                                  bool enabled) {
+void SettingLevelBubbleView::Init(SkBitmap* icon, double level, bool enabled) {
   DCHECK(icon);
-  DCHECK(level_percent >= 0 && level_percent <= 100);
   icon_ = icon;
   progress_bar_ = new views::ProgressBar();
   AddChildView(progress_bar_);
+  progress_bar_->SetDisplayRange(0.0, 100.0);
   progress_bar_->EnableCanvasFlippingForRTLUI(true);
   EnableCanvasFlippingForRTLUI(true);
-  SetLevel(level_percent);
+  SetLevel(level);
   SetEnabled(enabled);
 }
 
@@ -52,9 +50,8 @@ void SettingLevelBubbleView::SetIcon(SkBitmap* icon) {
   SchedulePaint();
 }
 
-void SettingLevelBubbleView::SetLevel(int level_percent) {
-  DCHECK(level_percent >= 0 && level_percent <= 100);
-  progress_bar_->SetProgress(level_percent);
+void SettingLevelBubbleView::SetLevel(double level) {
+  progress_bar_->SetValue(level);
 }
 
 void SettingLevelBubbleView::SetEnabled(bool enabled) {

chrome/browser/chromeos/setting_level_bubble_view.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -22,16 +22,16 @@ class SettingLevelBubbleView : public views::View {
  public:
   SettingLevelBubbleView();
 
-  // Initialize the view, setting the progress bar to the specified level and
-  // state.  Ownership of |icon| remains with the caller (it's probably a shared
-  // instance from ResourceBundle).
-  void Init(SkBitmap* icon, int level_percent, bool enabled);
+  // Initialize the view, setting the progress bar to the specified level in the
+  // range [0.0, 100.0] and state.  Ownership of |icon| remains with the caller
+  // (it's probably a shared instance from ResourceBundle).
+  void Init(SkBitmap* icon, double level, bool enabled);
 
   // Change the icon that we're currently displaying.
   void SetIcon(SkBitmap* icon);
 
   // Set the progress bar to the specified level and redraw it.
-  void SetLevel(int level_percent);
+  void SetLevel(double level);
 
   // Draw the progress bar in an enabled or disabled state.
   void SetEnabled(bool enabled);

views/controls/progress_bar.cc
@@ -4,6 +4,7 @@
 
 #include "views/controls/progress_bar.h"
 
+#include <algorithm>
 #include <string>
 
 #include "base/logging.h"
@@ -20,6 +21,9 @@
 #include "views/border.h"
 #include "views/painter.h"
 
+using std::max;
+using std::min;
+
 namespace {
 
 // Corner radius for the progress bar's border.
@@ -28,11 +32,11 @@ const int kCornerRadius = 3;
 // Progress bar's border width
 const int kBorderWidth = 1;
 
-static void AddRoundRectPathWithPadding(int x, int y,
-                                        int w, int h,
-                                        int corner_radius,
-                                        SkScalar padding,
-                                        SkPath* path) {
+void AddRoundRectPathWithPadding(int x, int y,
+                                 int w, int h,
+                                 int corner_radius,
+                                 SkScalar padding,
+                                 SkPath* path) {
   DCHECK(path);
   if (path == NULL)
     return;
@@ -46,22 +50,22 @@ static void AddRoundRectPathWithPadding(int x, int y,
       SkIntToScalar(corner_radius) - padding);
 }
 
-static void AddRoundRectPath(int x, int y,
-                             int w, int h,
-                             int corner_radius,
-                             SkPath* path) {
+void AddRoundRectPath(int x, int y,
+                      int w, int h,
+                      int corner_radius,
+                      SkPath* path) {
   static const SkScalar half = SkIntToScalar(1) / 2;
   AddRoundRectPathWithPadding(x, y, w, h, corner_radius, half, path);
 }
 
-static void FillRoundRect(gfx::Canvas* canvas,
-                          int x, int y,
-                          int w, int h,
-                          int corner_radius,
-                          const SkColor colors[],
-                          const SkScalar points[],
-                          int count,
-                          bool gradient_horizontal) {
+void FillRoundRect(gfx::Canvas* canvas,
+                   int x, int y,
+                   int w, int h,
+                   int corner_radius,
+                   const SkColor colors[],
+                   const SkScalar points[],
+                   int count,
+                   bool gradient_horizontal) {
   SkPath path;
   AddRoundRectPath(x, y, w, h, corner_radius, &path);
   SkPaint paint;
@@ -84,13 +88,13 @@ static void FillRoundRect(gfx::Canvas* canvas,
   canvas->AsCanvasSkia()->drawPath(path, paint);
 }
 
-static void FillRoundRect(gfx::Canvas* canvas,
-                          int x, int y,
-                          int w, int h,
-                          int corner_radius,
-                          SkColor gradient_start_color,
-                          SkColor gradient_end_color,
-                          bool gradient_horizontal) {
+void FillRoundRect(gfx::Canvas* canvas,
+                   int x, int y,
+                   int w, int h,
+                   int corner_radius,
+                   SkColor gradient_start_color,
+                   SkColor gradient_end_color,
+                   bool gradient_horizontal) {
   if (gradient_start_color != gradient_end_color) {
     SkColor colors[2] = { gradient_start_color, gradient_end_color };
     FillRoundRect(canvas, x, y, w, h, corner_radius,
@@ -106,12 +110,12 @@ static void FillRoundRect(gfx::Canvas* canvas,
   }
 }
 
-static void StrokeRoundRect(gfx::Canvas* canvas,
-                            int x, int y,
-                            int w, int h,
-                            int corner_radius,
-                            SkColor stroke_color,
-                            int stroke_width) {
+void StrokeRoundRect(gfx::Canvas* canvas,
+                     int x, int y,
+                     int w, int h,
+                     int corner_radius,
+                     SkColor stroke_color,
+                     int stroke_width) {
   SkPath path;
   AddRoundRectPath(x, y, w, h, corner_radius, &path);
   SkPaint paint;
@@ -129,11 +133,11 @@ namespace views {
 
 // static
 const char ProgressBar::kViewClassName[] = "views/ProgressBar";
-// static: progress bar's maximum value.
-const int ProgressBar::kMaxProgress = 100;
 
-
-ProgressBar::ProgressBar(): progress_(0) {
+ProgressBar::ProgressBar()
+    : min_display_value_(0.0),
+      max_display_value_(1.0),
+      current_value_(0.0) {
 }
 
 ProgressBar::~ProgressBar() {
@@ -143,7 +147,23 @@ gfx::Size ProgressBar::GetPreferredSize() {
   return gfx::Size(100, 16);
 }
 
+std::string ProgressBar::GetClassName() const {
+  return kViewClassName;
+}
+
+void ProgressBar::GetAccessibleState(ui::AccessibleViewState* state) {
+  state->role = ui::AccessibilityTypes::ROLE_PROGRESSBAR;
+  state->state = ui::AccessibilityTypes::STATE_READONLY;
+}
+
 void ProgressBar::OnPaint(gfx::Canvas* canvas) {
+  const double capped_value =
+      min(max(current_value_, min_display_value_), max_display_value_);
+  const double capped_fraction =
+      (capped_value - min_display_value_) /
+      (max_display_value_ - min_display_value_);
+  const int progress_width = static_cast<int>(width() * capped_fraction + 0.5);
+
 #if defined(OS_CHROMEOS)
   const SkColor background_colors[] = {
     SkColorSetRGB(0xBB, 0xBB, 0xBB),
@@ -173,10 +193,8 @@ void ProgressBar::OnPaint(gfx::Canvas* canvas) {
                   background_border_color,
                   kBorderWidth);
 
-  if (progress_ * width() > 1) {
-    int progress_width = progress_ * width() / kMaxProgress;
-
-    bool enabled = IsEnabled();
+  if (progress_width > 1) {
+    const bool enabled = IsEnabled();
 
     const SkColor bar_color_start = enabled ?
         SkColorSetRGB(100, 116, 147) :
@@ -253,10 +271,10 @@ void ProgressBar::OnPaint(gfx::Canvas* canvas) {
                 background_color_start,
                 background_color_end,
                 false);
-  if (progress_ * width() > 1) {
+  if (progress_width > 1) {
     FillRoundRect(canvas,
                   0, 0,
-                  progress_ * width() / kMaxProgress, height(),
+                  progress_width, height(),
                   kCornerRadius,
                   bar_color_start,
                   bar_color_end,
@@ -271,31 +289,6 @@ void ProgressBar::OnPaint(gfx::Canvas* canvas) {
 #endif
 }
 
-std::string ProgressBar::GetClassName() const {
-  return kViewClassName;
-}
-
-void ProgressBar::SetProgress(int progress) {
-  progress_ = progress;
-  if (progress_ < 0)
-    progress_ = 0;
-  else if (progress_ > kMaxProgress)
-    progress_ = kMaxProgress;
-  SchedulePaint();
-}
-
-int ProgressBar::GetProgress() const {
-  return progress_;
-}
-
-void ProgressBar::AddProgress(int tick) {
-  SetProgress(progress_ + tick);
-}
-
-void ProgressBar::SetTooltipText(const std::wstring& tooltip_text) {
-  tooltip_text_ = WideToUTF16Hack(tooltip_text);
-}
-
 bool ProgressBar::GetTooltipText(const gfx::Point& p, std::wstring* tooltip) {
   DCHECK(tooltip);
   if (tooltip == NULL)
@@ -304,14 +297,26 @@ bool ProgressBar::GetTooltipText(const gfx::Point& p, std::wstring* tooltip) {
   return !tooltip_text_.empty();
 }
 
-void ProgressBar::OnEnabledChanged() {
-  View::OnEnabledChanged();
-  // TODO(denisromanov): Need to switch progress bar color here?
+void ProgressBar::SetDisplayRange(double min_display_value,
+                                  double max_display_value) {
+  if (min_display_value != min_display_value_ ||
+      max_display_value != max_display_value_) {
+    DCHECK(min_display_value < max_display_value);
+    min_display_value_ = min_display_value;
+    max_display_value_ = max_display_value;
+    SchedulePaint();
+  }
 }
 
-void ProgressBar::GetAccessibleState(ui::AccessibleViewState* state) {
-  state->role = ui::AccessibilityTypes::ROLE_PROGRESSBAR;
-  state->state = ui::AccessibilityTypes::STATE_READONLY;
+void ProgressBar::SetValue(double value) {
+  if (value != current_value_) {
+    current_value_ = value;
+    SchedulePaint();
+  }
+}
+
+void ProgressBar::SetTooltipText(const std::wstring& tooltip_text) {
+  tooltip_text_ = WideToUTF16Hack(tooltip_text);
 }
 
 }  // namespace views

commit 083bac6bb70ed7d18c25d40112c0ae0431575a35

chromeos: Animate volume/brightness when re-showing bubbles.
This fixes a regression that I introduced in r96853.  Since
I was resetting the variable containing the current level
when the bubble was closed, the level wouldn't animate
correctly to its new position the next time the bubble was
re-shown.

BUG=chromium-os:19141
TEST=manual: waited for volume bubble to be closed, then pressed volume keys and checked that animation was visible in new bubble and that increase/decrease icon was correct
TBR=davemoore

Review URL: http://codereview.chromium.org/7701019

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97964 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/chromeos/setting_level_bubble.cc
@@ -197,8 +197,7 @@ void SettingLevelBubble::BubbleClosing(Bubble* bubble, bool) {
   StopAnimation();
   bubble_ = NULL;
   view_ = NULL;
-  current_percent_ = -1.0;
-  target_percent_ = -1.0;
+  current_percent_ = target_percent_;
   target_time_ = TimeTicks();
   last_animation_update_time_ = TimeTicks();
   last_target_update_time_ = TimeTicks();