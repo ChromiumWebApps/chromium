ChromiumWebApps/chromium

commit 95259c6a4ab6fd2c6258b488fc08c45041a8d102

aura: Use native ProcessSingleton implementations.
This makes us use either ProcessSingletonLinux or
ProcessSingletonWin.

Tested on a Chrome OS device by starting two separate Chrome
processes with the same profile directory.  The second
process just opened another window in the original Aura
desktop instead of crashing after trying to open a locked
database as before.  sky@ was kind enough to run this for me
on Windows and confirm that it behaves similarly there.

BUG=99713
TEST=manual: see above

Review URL: http://codereview.chromium.org/8382023

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107236 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/process_singleton.h
@@ -22,9 +22,9 @@
 #include "base/file_path.h"
 #endif  // defined(OS_POSIX)
 
-#if defined(USE_X11)
+#if defined(OS_LINUX)
 #include "base/scoped_temp_dir.h"
-#endif  // defined(USE_X11)
+#endif  // defined(OS_LINUX)
 
 class CommandLine;
 class FilePath;
@@ -128,9 +128,7 @@ class ProcessSingleton : public base::NonThreadSafe {
   bool locked_;
   gfx::NativeWindow foreground_window_;
 
-#if defined(USE_AURA)
-  // TODO(saintlou): The process_singleton_aura.cc is still a stub.
-#elif defined(OS_WIN)
+#if defined(OS_WIN)
   // This ugly behemoth handles startup commands sent from another process.
   LRESULT OnCopyData(HWND hwnd, const COPYDATASTRUCT* cds);
 
@@ -153,7 +151,7 @@ class ProcessSingleton : public base::NonThreadSafe {
   HWND remote_window_;  // The HWND_MESSAGE of another browser.
   HWND window_;  // The HWND_MESSAGE window.
   bool is_virtualized_;  // Stuck inside Microsoft Softricity VM environment.
-#elif defined(USE_X11)
+#elif defined(OS_LINUX)
   // Path in file system to the socket.
   FilePath socket_path_;
 

chrome/browser/process_singleton_aura.cc
@@ -1,40 +1,0 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "chrome/browser/process_singleton.h"
-
-// Look for a Chrome instance that uses the same profile directory.
-ProcessSingleton::ProcessSingleton(const FilePath& user_data_dir)
-    :
-#if defined(OS_WIN) && !defined(USE_AURA)
-    window_(NULL),
-#endif
-    locked_(false), foreground_window_(NULL) {
-}
-
-ProcessSingleton::~ProcessSingleton() {
-}
-
-ProcessSingleton::NotifyResult ProcessSingleton::NotifyOtherProcess() {
-  // TODO(beng):
-  NOTIMPLEMENTED();
-  return PROCESS_NONE;
-}
-
-ProcessSingleton::NotifyResult ProcessSingleton::NotifyOtherProcessOrCreate() {
-  // TODO(beng):
-  NOTIMPLEMENTED();
-  return PROCESS_NONE;
-}
-
-bool ProcessSingleton::Create() {
-  // TODO(beng):
-  NOTIMPLEMENTED();
-  return true;
-}
-
-void ProcessSingleton::Cleanup() {
-  // TODO(beng):
-  NOTIMPLEMENTED();
-}

chrome/browser/process_singleton_linux.cc
@@ -43,7 +43,9 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#if defined(TOOLKIT_USES_GTK)
 #include <gdk/gdk.h>
+#endif
 #include <signal.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
@@ -311,6 +313,8 @@ void DisplayProfileInUseError(const std::string& lock_path,
   if (!CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kNoProcessSingletonDialog))
     ProcessSingletonDialog::ShowAndRun(UTF16ToUTF8(error));
+#else
+  NOTIMPLEMENTED();
 #endif
 }
 
@@ -874,9 +878,11 @@ ProcessSingleton::NotifyResult ProcessSingleton::NotifyOtherProcessWithTimeout(
     // The other process is shutting down, it's safe to start a new process.
     return PROCESS_NONE;
   } else if (strncmp(buf, kACKToken, arraysize(kACKToken) - 1) == 0) {
+#if defined(TOOLKIT_USES_GTK)
     // Notify the window manager that we've started up; if we do not open a
     // window, GTK will not automatically call this for us.
     gdk_notify_startup_complete();
+#endif
     // Assume the other process is handling the request.
     return PROCESS_NOTIFIED;
   }

chrome/browser/process_singleton_win.cc
@@ -239,9 +239,13 @@ LRESULT ProcessSingleton::OnCopyData(HWND hwnd, const COPYDATASTRUCT* cds) {
   // If locked, it means we are not ready to process this message because
   // we are probably in a first run critical phase.
   if (locked_) {
+#if defined(USE_AURA)
+    NOTIMPLEMENTED();
+#else
     // Attempt to place ourselves in the foreground / flash the task bar.
     if (IsWindow(foreground_window_))
       SetForegroundWindow(foreground_window_);
+#endif
     return TRUE;
   }
 

chrome/chrome_browser.gypi
@@ -1938,7 +1938,6 @@
         'browser/process_info_snapshot.h',
         'browser/process_info_snapshot_mac.cc',
         'browser/process_singleton.h',
-        'browser/process_singleton_aura.cc',
         'browser/process_singleton_linux.cc',
         'browser/process_singleton_mac.cc',
         'browser/process_singleton_win.cc',
@@ -4148,7 +4147,6 @@
             ['exclude', '^browser/google/google_update.h'],
             ['exclude', '^browser/hang_monitor/'],
             ['exclude', '^browser/platform_util_win.cc'],
-            ['exclude', '^browser/process_singleton_win.cc'],
             ['exclude', '^browser/tab_contents/web_drag_source_win.cc'],
             ['exclude', '^browser/tab_contents/web_drag_source_win.h'],
             ['exclude', '^browser/ui/panels/auto_hiding_desktop_bar_win.cc'],
@@ -5083,7 +5081,6 @@
             ['exclude', '^browser/google/google_update.h'],
             ['exclude', '^browser/platform_util_common_linux.cc'],
             ['exclude', '^browser/platform_util_linux.cc'],
-            ['exclude', '^browser/process_singleton_linux.cc'],
             ['exclude', '^browser/ui/input_window_dialog.h'],
             ['exclude', '^browser/ui/input_window_dialog_win.cc'],
             ['include', '^browser/ui/views/aura/'],

commit 1d380bc0dd9339dbb0f1f56545aa14736a59dc59

Use ProcessSingletonLinux on OpenBSD.
This was broken by r107236.  Patch is from Robert Nagy
<robert.nagy@gmail.com>.

BUG=99713
TEST=still builds on linux

Review URL: http://codereview.chromium.org/8341047
Patch from Robert Nagy <robert.nagy@gmail.com>.

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107361 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/process_singleton.h
@@ -22,9 +22,9 @@
 #include "base/file_path.h"
 #endif  // defined(OS_POSIX)
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_OPENBSD)
 #include "base/scoped_temp_dir.h"
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_OPENBSD)
 
 class CommandLine;
 class FilePath;
@@ -151,7 +151,7 @@ class ProcessSingleton : public base::NonThreadSafe {
   HWND remote_window_;  // The HWND_MESSAGE of another browser.
   HWND window_;  // The HWND_MESSAGE window.
   bool is_virtualized_;  // Stuck inside Microsoft Softricity VM environment.
-#elif defined(OS_LINUX)
+#elif defined(OS_LINUX) || defined(OS_OPENBSD)
   // Path in file system to the socket.
   FilePath socket_path_;
 