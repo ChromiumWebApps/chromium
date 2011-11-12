ChromiumWebApps/chromium

commit df3fab136c85d7b9d6c751a7d8f0d1329a3521bf

Remove gradient_button.css
gradient_button.css duplicates button.css.

BUG=101057
TEST=


Review URL: http://codereview.chromium.org/8351065

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109201 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/ntp4/new_tab.html
@@ -26,7 +26,7 @@ document.documentElement.setAttribute('touchui', true);
 </script>
 </if>
 
-<link rel="stylesheet" href="../shared/css/gradient_button.css">
+<link rel="stylesheet" href="../shared/css/button.css">
 <link rel="stylesheet" href="../shared/css/bubble.css">
 <link rel="stylesheet" href="../shared/css/expandable_bubble.css">
 <link rel="stylesheet" href="../shared/css/menu.css">
@@ -71,7 +71,7 @@ document.documentElement.setAttribute('touchui', true);
 </head>
 
 <body>
-  <button id="login-container" hidden>
+  <button id="login-container" class="custom-appearance" hidden>
     <div id="login-status-header"></div>
     <div id="login-status-sub-header"></div>
   </button>
@@ -80,15 +80,17 @@ document.documentElement.setAttribute('touchui', true);
     <div id="notification">
       <span></span>
       <div id="notificationLinks"></div>
-      <button class="close-button"></button>
+      <button class="close-button" class="custom-appearance"></button>
     </div>
   </div>
 
   <div id="card-slider-frame">
-    <button id="page-switcher-start" class="page-switcher" tabindex="2" hidden>‹
+    <button id="page-switcher-start" class="page-switcher custom-appearance"
+        tabindex="2" hidden>‹
     </button>
     <div id="page-list"></div>
-    <button id="page-switcher-end" class="page-switcher" tabindex="2" hidden>›
+    <button id="page-switcher-end" class="page-switcher custom-appearance"
+        tabindex="2" hidden>›
     </button>
     <div id="attribution">
       <span i18n-content="attributionintro"></span>
@@ -104,7 +106,7 @@ document.documentElement.setAttribute('touchui', true);
       <ul id="dot-list">
       </ul>
 
-      <button id="recently-closed-menu-button">
+      <button id="recently-closed-menu-button" class="custom-appearance">
         <span i18n-content="recentlyclosed"></span>
         <div class="disclosure-triangle"></div>
       </button>
@@ -134,8 +136,8 @@ document.documentElement.setAttribute('touchui', true);
       <a id="login-status-advanced"
           i18n-content="login_status_advanced" href="#"></a>
     </div>
-    <button id="login-status-dismiss" i18n-content="login_status_dismiss"
-        class="gradient-button"></button>
+    <button id="login-status-dismiss" i18n-content="login_status_dismiss">
+    </button>
   </div>
 </div>
   

chrome/browser/resources/shared/css/button.css
@@ -3,9 +3,9 @@
  * found in the LICENSE file.
  */
 
-button,
-input[type='button'],
-input[type='submit'] {
+button:not(.custom-appearance),
+input[type='button']:not(.custom-appearance),
+input[type='submit']:not(.custom-appearance) {
   -webkit-border-radius: 2px;
   -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
   -webkit-user-select: none;
@@ -18,29 +18,29 @@ input[type='submit'] {
   padding: 3px 12px;
 }
 
-button:hover,
-input[type='button']:hover,
-input[type='submit']:hover {
+button:not(.custom-appearance):hover,
+input[type='button']:not(.custom-appearance):hover,
+input[type='submit']:not(.custom-appearance):hover {
   -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
   background: #ebebeb -webkit-linear-gradient(#fefefe, #f8f8f8 40%, #e9e9e9);
   border-color: #999;
   color: #222;
 }
 
-button:active,
-input[type='button']:active,
-input[type='submit']:active {
+button:not(.custom-appearance):active,
+input[type='button']:not(.custom-appearance):active,
+input[type='submit']:not(.custom-appearance):active {
   -webkit-box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.2);
   background: #ebebeb -webkit-linear-gradient(#f4f4f4, #efefef 40%, #dcdcdc);
   color: #333;
 }
 
-button[disabled],
-input[type='button'][disabled],
-input[type='submit'][disabled],
-button[disabled]:hover,
-input[type='button'][disabled]:hover,
-input[type='submit'][disabled]:hover {
+button[disabled]:not(.custom-appearance),
+input[type='button'][disabled]:not(.custom-appearance),
+input[type='submit'][disabled]:not(.custom-appearance),
+button[disabled]:not(.custom-appearance):hover,
+input[type='button'][disabled]:not(.custom-appearance):hover,
+input[type='submit'][disabled]:not(.custom-appearance):hover {
   -webkit-box-shadow: none;
   background: -webkit-linear-gradient(#fafafa, #f4f4f4 40%, #e5e5e5);
   border-color: #aaa;
@@ -51,15 +51,15 @@ input[type='submit'][disabled]:hover {
  * we stick to native focus rings. Once outlines follow border radius, we
  * can revisit.
  */
-html:not([os='mac']) button:focus,
-html:not([os='mac']) input:focus,
-html:not([os='mac']) input[type='submit']:focus,
-html:not([os='mac']) select:focus {
+html:not([os='mac']) button:not(.custom-appearance):focus,
+html:not([os='mac']) input:not(.custom-appearance):focus,
+html:not([os='mac']) input[type='submit']:not(.custom-appearance):focus,
+html:not([os='mac']) select:not(.custom-appearance):focus {
   outline-color: rgba(0, 128, 256, 0.5);
 }
 
-html[os='mac'] button:focus,
-html[os='mac'] input[type='submit']:focus {
+html[os='mac'] button:not(.custom-appearance):focus,
+html[os='mac'] input[type='submit']:not(.custom-appearance):focus {
   outline-offset: -2px;
 }
 

chrome/browser/resources/shared/css/gradient_button.css
@@ -1,38 +1,0 @@
-/* Copyright (c) 2011 The Chromium Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
-.gradient-button {
-  -webkit-border-radius: 2px;
-  -webkit-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
-  -webkit-user-select: none;
-  background: -webkit-linear-gradient(#fafafa, #f4f4f4 40%, #e5e5e5);
-  border: 1px solid #aaa;
-  color: #444;
-  font-size: inherit;
-  margin-bottom: 0px;
-  min-width: 4em;
-  padding: 3px 12px 3px 12px;
-}
-
-.gradient-button:hover {
-  -webkit-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.2);
-  background: #ebebeb -webkit-linear-gradient(#fefefe, #f8f8f8 40%, #e9e9e9);
-  border-color: #999;
-  color: #222;
-}
-
-.gradient-button:active {
-  -webkit-box-shadow: inset 0px 1px 3px rgba(0, 0, 0, 0.2);
-  background: #ebebeb -webkit-linear-gradient(#f4f4f4, #efefef 40%, #dcdcdc);
-  color: #333;
-}
-
-.gradient-button[disabled],
-.gradient-button[disabled]:hover {
-  -webkit-box-shadow: none;
-  background: -webkit-linear-gradient(#fafafa, #f4f4f4 40%, #e5e5e5);
-  border-color: #aaa;
-  color: #888;
-}

chrome/browser/resources/shared/js/cr/ui/menu_button.js
@@ -24,6 +24,10 @@ cr.define('cr.ui', function() {
       this.addEventListener('mousedown', this);
       this.addEventListener('keydown', this);
 
+      // Adding the 'custom-appearance' class prevents button.css from changing
+      // the appearance of this element.
+      this.classList.add('custom-appearance');
+
       var menu;
       if ((menu = this.getAttribute('menu')))
         this.menu = menu;

chrome/browser/resources/shared/js/cr/ui/menu_item.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -35,6 +35,10 @@ cr.define('cr.ui', function() {
         this.command = commandId;
 
       this.addEventListener('mouseup', this.handleMouseUp_);
+
+      // Adding the 'custom-appearance' class prevents button.css from changing
+      // the appearance of this element.
+      this.classList.add('custom-appearance');
     },
 
     /**

chrome/browser/resources/shared_resources.grd
@@ -20,8 +20,6 @@ without changes to the corresponding grd file. paaaae -->
                file="shared/css/checkbox.css" type="BINDATA" />
       <include name="IDR_SHARED_CSS_DIALOGS"
                file="shared/css/dialogs.css" type="BINDATA" />
-      <include name="IDR_SHARED_CSS_GRADIENT_BUTTON"
-               file="shared/css/gradient_button.css" type="BINDATA" />
       <include name="IDR_SHARED_CSS_LIST"
                file="shared/css/list.css" type="BINDATA" />
       <include name="IDR_SHARED_CSS_MENU"

commit 49b5d4f3567bcd097ee670db04462e300375f512

fix appearance of buttons in chrome://settings
non-standard buttons in chrome://settings were broken in r109201 because the specificity of the button rules got greater when :not(.custom-appearance) was added. This is a sorry and unanticipated side effect of this approach (proposed by yours truly).

BUG=none
TEST=deletable list item close buttons, subpage close buttons go back to normal look

Review URL: http://codereview.chromium.org/8510042

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109759 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/ntp4/new_tab.html
@@ -81,7 +81,8 @@ document.documentElement.setAttribute('touchui', true);
     <div id="notification">
       <span></span>
       <div id="notificationLinks"></div>
-      <button class="close-button" class="custom-appearance"></button>
+      <button class="close-button custom-appearance" class="custom-appearance">
+      </button>
     </div>
   </div>
 

chrome/browser/resources/options/chromeos/accounts_user_list.js
@@ -191,9 +191,8 @@ cr.define('options.accounts', function() {
 
       if (!this.user.owner) {
         var removeButton = this.ownerDocument.createElement('button');
-        removeButton.classList.add('raw-button');
-        removeButton.classList.add('remove-user-button');
-        removeButton.classList.add('custom-appearance');
+        removeButton.className =
+            'raw-button remove-user-button custom-appearance';
         this.appendChild(removeButton);
       }
     },

chrome/browser/resources/options/deletable_item_list.js
@@ -46,8 +46,8 @@ cr.define('options', function() {
       this.appendChild(this.contentElement_);
 
       this.closeButtonElement_ = this.ownerDocument.createElement('button');
-      this.closeButtonElement_.classList.add('raw-button');
-      this.closeButtonElement_.classList.add('close-button');
+      this.closeButtonElement_.className =
+          'raw-button close-button custom-appearance';
       this.closeButtonElement_.addEventListener('mousedown',
                                                 this.handleMouseDownUpOnClose_);
       this.closeButtonElement_.addEventListener('mouseup',

chrome/browser/resources/options/options.html
@@ -135,7 +135,7 @@
        <div id="subpage-sheet-container-1"
            class="subpage-sheet-container transparent" hidden>
          <div id="subpage-sheet-1" class="subpage-sheet">
-           <button class="raw-button close-subpage"></button>
+           <button class="raw-button close-subpage custom-appearance"></button>
            <div class="subpage-sheet-contents">
              <if expr="pp_ifdef('chromeos')">
                <include src="about_page.html">
@@ -157,7 +157,7 @@
        <div id="subpage-sheet-container-2"
            class="subpage-sheet-container transparent" hidden>
          <div id="subpage-sheet-2" class="subpage-sheet">
-           <button class="raw-button close-subpage"></button>
+           <button class="raw-button close-subpage custom-appearance"></button>
            <div class="subpage-sheet-contents">
              <if expr="pp_ifdef('chromeos')">
                <include src="chromeos/language_chewing_options.html">

chrome/browser/resources/options/search_engine_manager_engine_list.js
@@ -122,7 +122,7 @@ cr.define('options.search_engines', function() {
       // is implemented. When this is removed, remove the extra div above.
       if (engine['canBeDefault']) {
         var makeDefaultButtonEl = this.ownerDocument.createElement('button');
-        makeDefaultButtonEl.className = "raw-button";
+        makeDefaultButtonEl.className = 'raw-button custom-appearance';
         makeDefaultButtonEl.textContent =
             templateData.makeDefaultSearchEngineButton;
         makeDefaultButtonEl.onclick = function(e) {