ChromiumWebApps/chromium

commit 61c1046892028090afc10fd0e58d992e739c3b31

Print Preview: Making margin selection sticky (part 2/2, again)
This is a reland of http://codereview.chromium.org/8351048/ with minor changes
to fix the compilation errors on the shared builders.

BUG=102446
TEST=See bug description.

Review URL: http://codereview.chromium.org/8528053

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110055 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/print_preview/margin_settings.js
@@ -40,6 +40,8 @@ cr.define('print_preview', function() {
      * @return {boolean} true if they are equal.
      */
     isEqual: function(rhs) {
+      if (!rhs)
+        return false;
       return this[MarginSettings.TOP_GROUP] === rhs[MarginSettings.TOP_GROUP] &&
           this[MarginSettings.LEFT_GROUP] === rhs[MarginSettings.LEFT_GROUP] &&
           this[MarginSettings.RIGHT_GROUP] ===
@@ -48,15 +50,11 @@ cr.define('print_preview', function() {
               rhs[MarginSettings.BOTTOM_GROUP];
     },
 
-    /**
-     * Copies the four margin values from |rhs|.
-     * @param {Margins} rhs The Margins object values to be used.
-     */
-    copy: function(rhs) {
-      this[MarginSettings.TOP_GROUP] = rhs[MarginSettings.TOP_GROUP];
-      this[MarginSettings.LEFT_GROUP] = rhs[MarginSettings.LEFT_GROUP];
-      this[MarginSettings.RIGHT_GROUP] = rhs[MarginSettings.RIGHT_GROUP];
-      this[MarginSettings.BOTTOM_GROUP] = rhs[MarginSettings.BOTTOM_GROUP];
+    clone: function() {
+      return new Margins(this[MarginSettings.TOP_GROUP],
+                         this[MarginSettings.LEFT_GROUP],
+                         this[MarginSettings.RIGHT_GROUP],
+                         this[MarginSettings.BOTTOM_GROUP]);
     },
 
     /**
@@ -93,6 +91,7 @@ cr.define('print_preview', function() {
     this.contentWidth_ = width;
     this.contentHeight_ = height;
     this.margins_ = new Margins(left, top, right, bottom);
+    this.margins_.roundToLocaleUnits();
   }
 
   PageLayout.prototype = {
@@ -122,21 +121,18 @@ cr.define('print_preview', function() {
     this.marginsUI_ = null;
 
     // Holds the custom margin values in points (if set).
-    this.customMargins_ = new Margins(-1, -1, -1, -1);
+    this.customMargins_ = null;
     // Holds the previous custom margin values in points.
-    this.previousCustomMargins_ = new Margins(-1, -1, -1, -1);
+    this.previousCustomMargins_ = null;
     // Holds the width of the page in points.
     this.pageWidth_ = -1;
     // Holds the height of the page in points.
     this.pageHeight_ = -1;
-    // The last selected margin option.
-    this.lastSelectedOption_ = MarginSettings.MARGINS_VALUE_DEFAULT;
-
+    // @type {boolean} True if the margins UI should be diplayed when the next
+    //     |customEvents.PDF_LOADED| event occurs.
+    this.forceMarginsUIOnPDFLoad_ = false;
     // Holds the currently updated default page layout values.
     this.currentDefaultPageLayout = null;
-    // Holds the default page layout values when the custom margins was last
-    // selected.
-    this.previousDefaultPageLayout_ = null;
 
     // True if the margins UI should be shown regardless of mouse position.
     this.forceDisplayingMarginLines_ = true;
@@ -203,6 +199,19 @@ cr.define('print_preview', function() {
     },
 
     /**
+     * Sets |this.customMargins_| according to |margins|.
+     * @param {{marginLeft: number, marginTop: number, marginRight: number,
+     *     marginBottom: number}} margins An object holding the four margin
+     *     values.
+     */
+    set customMargins(margins) {
+      this.customMargins_.left = margins.marginLeft;
+      this.customMargins_.top = margins.marginTop;
+      this.customMargins_.right = margins.marginRight;
+      this.customMargins_.bottom = margins.marginBottom;
+    },
+
+    /**
      * @return {number} The value of the selected margin option.
      */
     get selectedMarginsValue() {
@@ -211,13 +220,26 @@ cr.define('print_preview', function() {
     },
 
     /**
-     * Sets the current margin selection to |lastUsedMarginsType|.
-     * @param {number} lastUsedMarginsType An integer value identifying a margin
+     * Sets the current margin selection to |lastUsedMarginType|.
+     * @param {number} lastUsedMarginType An integer value identifying a margin
      *     type according to MarginType enum in printing/print_job_constants.h.
+     * @param {Object} lastUsedCustomMargins The last used custom margins. If
+     *     custom margins have not been used before
+     *     |margin{Top|Bottom|Left|Right}| attributes are missing.
      */
-    setLastUsedMarginsType: function(lastUsedMarginsType) {
+    setLastUsedMargins: function(lastUsedMarginsSettings) {
+      var lastUsedMarginsType = lastUsedMarginsSettings['marginsType']
+      this.forceMarginsUIOnPDFLoad_ =
+          lastUsedMarginsType == MarginSettings.MARGINS_VALUE_CUSTOM;
       this.marginList_.selectedIndex =
           this.getMarginOptionIndexByValue_(lastUsedMarginsType);
+      if (lastUsedMarginsSettings.hasOwnProperty('marginTop') &&
+          lastUsedMarginsSettings.hasOwnProperty('marginBottom') &&
+          lastUsedMarginsSettings.hasOwnProperty('marginRight') &&
+          lastUsedMarginsSettings.hasOwnProperty('marginLeft')) {
+        this.customMargins_ = new Margins(-1, -1, -1 , -1);
+        this.customMargins = lastUsedMarginsSettings;
+      }
     },
 
     /**
@@ -290,9 +312,11 @@ cr.define('print_preview', function() {
     requestPreviewIfNeeded_: function() {
       if (!this.areMarginSettingsValid())
         return;
+
       if (this.customMargins_.isEqual(this.previousCustomMargins_))
         return;
-      this.previousCustomMargins_.copy(this.customMargins_);
+
+      this.previousCustomMargins_ = this.customMargins_.clone();
       setDefaultValuesAndRegeneratePreview(false);
     },
 
@@ -302,6 +326,8 @@ cr.define('print_preview', function() {
      * @private
      */
     onSidebarMouseOver_: function(e) {
+      $('mainview').onmouseover = this.onMainviewMouseOver_.bind(this);
+      $('navbar-container').onmouseover = null;
       if (!this.forceDisplayingMarginLines_)
         this.marginsUI.hide(false);
     },
@@ -312,6 +338,8 @@ cr.define('print_preview', function() {
      * @private
      */
     onMainviewMouseOver_: function() {
+      $('mainview').onmouseover = null;
+      $('navbar-container').onmouseover = this.onSidebarMouseOver_.bind(this);
       this.forceDisplayingMarginLines_ = false;
       this.marginsUI.show();
     },
@@ -368,7 +396,7 @@ cr.define('print_preview', function() {
      * @return {boolean} True if the margin settings are valid.
      */
     areMarginSettingsValid: function() {
-      if (this.marginsUI_ == null)
+      if (!this.isCustomMarginsSelected() || !this.marginsUI_)
         return true;
 
       var pairs = this.marginsUI.pairsAsList;
@@ -474,6 +502,8 @@ cr.define('print_preview', function() {
      * @private
      */
     removeCustomMarginEventListeners_: function() {
+      if (!this.marginsUI_)
+        return;
       $('mainview').onmouseover = null;
       $('navbar-container').onmouseover = null;
       this.eventTracker_.remove(this.marginsUI, customEvents.MARGIN_LINE_DRAG);
@@ -532,8 +562,6 @@ cr.define('print_preview', function() {
         this.onDefaultMinimumNoMarginsSelected_();
       else if (this.isCustomMarginsSelected())
         this.onCustomMarginsSelected_();
-
-      this.lastSelectedOption_ = this.selectedMarginsValue;
     },
 
     /**
@@ -543,6 +571,7 @@ cr.define('print_preview', function() {
     onDefaultMinimumNoMarginsSelected_: function() {
       this.removeCustomMarginEventListeners_();
       this.forceDisplayingMarginLines_ = true;
+      this.previousCustomMargins_ = null;
       setDefaultValuesAndRegeneratePreview(false);
     },
 
@@ -551,20 +580,18 @@ cr.define('print_preview', function() {
      * @private
      */
     onCustomMarginsSelected_: function() {
-      this.addCustomMarginEventListeners_();
+      var customMarginsNotSpecified = !this.customMargins_;
+      this.updatePageData_();
 
-      this.customMargins_ = this.currentDefaultPageLayout.margins_;
-      this.customMargins_.roundToLocaleUnits();
-      this.previousCustomMargins_.copy(this.customMargins_);
-
-      if (this.previousDefaultPageLayout_ != this.currentDefaultPageLayout) {
-        this.pageWidth_ = this.currentDefaultPageLayout.pageWidth;
-        this.pageHeight_ = this.currentDefaultPageLayout.pageHeight;
+      if (customMarginsNotSpecified) {
+        this.previousCustomMargins_ = this.customMargins_.clone();
+        this.drawCustomMarginsUI_();
+        this.addCustomMarginEventListeners_();
+        this.marginsUI.show();
+      } else {
+        this.forceMarginsUIOnPDFLoad_ = true;
+        this.requestPreviewIfNeeded_();
       }
-
-      this.previousDefaultPageLayout_ = this.currentDefaultPageLayout;
-      this.drawCustomMarginsUI_();
-      this.marginsUI.show();
     },
 
     /**
@@ -627,7 +654,8 @@ cr.define('print_preview', function() {
             MarginSettings.OPTION_INDEX_DEFAULT].selected = true;
         this.removeCustomMarginEventListeners_();
         this.forceDisplayingMarginLines_ = true;
-        this.lastSelectedOption_ = MarginSettings.MARGINS_VALUE_DEFAULT;
+        this.customMargins_ = null;
+        this.previousCustomMargins_ = null;
       }
     },
 
@@ -636,15 +664,35 @@ cr.define('print_preview', function() {
      * @private
      */
     onPDFLoaded_: function() {
-      if (!previewModifiable)
+      if (!previewModifiable) {
         fadeOutOption(this.marginsOption_);
+        return;
+      }
+
+      if (this.forceMarginsUIOnPDFLoad_) {
+        this.updatePageData_();
+        this.drawCustomMarginsUI_();
+        this.addCustomMarginEventListeners_();
+        this.marginsUI.show();
+        this.forceMarginsUIOnPDFLoad_ = false;
+      }
+    },
+
+    /**
+     * Updates |this.customMargins_|, |this.pageWidth_|, |this.pageHeight_|.
+     * @private
+     */
+    updatePageData_: function() {
+      if (!this.customMargins_)
+        this.customMargins_ = this.currentDefaultPageLayout.margins_.clone();
+
+      this.pageWidth_ = this.currentDefaultPageLayout.pageWidth;
+      this.pageHeight_ = this.currentDefaultPageLayout.pageHeight;
     }
   };
 
   return {
     MarginSettings: MarginSettings,
     PageLayout: PageLayout,
-    setNumberFormatAndMeasurementSystem:
-        MarginSettings.setNumberFormatAndMeasurementSystem,
   };
 });

chrome/browser/printing/print_preview_message_handler.cc
@@ -99,8 +99,16 @@ PrintPreviewUI* PrintPreviewMessageHandler::OnFailure(int document_cookie) {
   return static_cast<PrintPreviewUI*>(print_preview_tab->web_ui());
 }
 
-void PrintPreviewMessageHandler::OnRequestPrintPreview() {
+void PrintPreviewMessageHandler::OnRequestPrintPreview(
+    bool source_is_modifiable) {
   PrintPreviewTabController::PrintPreview(tab_contents_wrapper());
+
+  TabContentsWrapper* print_preview_tab = GetPrintPreviewTab();
+  if (!print_preview_tab || !print_preview_tab->web_ui())
+    return;
+  PrintPreviewUI* print_preview_ui =
+      static_cast<PrintPreviewUI*>(print_preview_tab->web_ui());
+  print_preview_ui->SetSourceIsModifiable(source_is_modifiable);
 }
 
 void PrintPreviewMessageHandler::OnDidGetPreviewPageCount(

chrome/browser/printing/print_preview_message_handler.h
@@ -43,7 +43,7 @@ class PrintPreviewMessageHandler : public TabContentsObserver {
   PrintPreviewUI* OnFailure(int document_cookie);
 
   // Message handlers.
-  void OnRequestPrintPreview();
+  void OnRequestPrintPreview(bool source_is_modifiable);
   void OnDidGetDefaultPageLayout(
       const printing::PageSizeMargins& page_layout_in_points);
   void OnDidGetPreviewPageCount(

chrome/browser/printing/print_system_task_proxy.cc
@@ -316,25 +316,22 @@ PrintSystemTaskProxy::~PrintSystemTaskProxy() {
 
 void PrintSystemTaskProxy::GetDefaultPrinter() {
   VLOG(1) << "Get default printer start";
-  StringValue* default_printer = NULL;
+  std::string* default_printer = NULL;
   if (PrintPreviewHandler::last_used_printer_name_ == NULL) {
-    default_printer = new StringValue(
-        print_backend_->GetDefaultPrinterName());
+    default_printer = new std::string(print_backend_->GetDefaultPrinterName());
   } else {
-    default_printer = new StringValue(
+    default_printer = new std::string(
         *PrintPreviewHandler::last_used_printer_name_);
   }
-  std::string default_printer_string;
-  default_printer->GetAsString(&default_printer_string);
   VLOG(1) << "Get default printer finished, found: "
-          << default_printer_string;
+          << default_printer;
 
-  StringValue* cloud_print_data = NULL;
+  std::string* cloud_print_data = NULL;
   if (PrintPreviewHandler::last_used_printer_cloud_print_data_ != NULL) {
-    cloud_print_data = new StringValue(
+    cloud_print_data = new std::string(
         *PrintPreviewHandler::last_used_printer_cloud_print_data_);
   } else {
-    cloud_print_data = new StringValue("");
+    cloud_print_data = new std::string;
   }
 
   BrowserThread::PostTask(
@@ -344,9 +341,9 @@ void PrintSystemTaskProxy::GetDefaultPrinter() {
 }
 
 void PrintSystemTaskProxy::SendDefaultPrinter(
-    const StringValue* default_printer, const StringValue* cloud_print_data) {
+    const std::string* default_printer, const std::string* cloud_print_data) {
   if (handler_)
-    handler_->SendDefaultPrinter(*default_printer, *cloud_print_data);
+    handler_->SendInitialSettings(*default_printer, *cloud_print_data);
   delete default_printer;
 }
 

chrome/browser/printing/print_system_task_proxy.h
@@ -55,8 +55,8 @@ class PrintSystemTaskProxy
       content::BrowserThread::UI>;
   friend class DeleteTask<PrintSystemTaskProxy>;
 
-  void SendDefaultPrinter(const base::StringValue* default_printer,
-                          const base::StringValue* cloud_print_data);
+  void SendDefaultPrinter(const std::string* default_printer,
+                          const std::string* cloud_print_data);
   void SetupPrinterList(base::ListValue* printers);
   void SendPrinterCapabilities(base::DictionaryValue* settings_info);
 

chrome/browser/resources/print_preview/print_preview.js
@@ -164,9 +164,25 @@ function onLoad() {
   $('printer-list').onchange = updateControlsWithSelectedPrinterCapabilities;
 
   previewArea.showLoadingAnimation();
-  chrome.send('getInitiatorTabTitle');
-  chrome.send('getDefaultPrinter');
-  chrome.send('getNumberFormatAndMeasurementSystem');
+  chrome.send('getInitialSettings');
+}
+
+/**
+ * @param {string} initiatorTabTitle The title of the initiator tab.
+ * @param {object} initialSettings An object containing all the initial
+ *     settings.
+ */
+function setInitialSettings(initialSettings) {
+  setInitiatorTabTitle(initialSettings['initiatorTabTitle']);
+  previewModifiable = initialSettings['previewModifiable'];
+  if (previewModifiable) {
+    print_preview.MarginSettings.setNumberFormatAndMeasurementSystem(
+        initialSettings['numberFormat'],
+        initialSettings['measurementSystem']);
+    marginSettings.setLastUsedMargins(initialSettings);
+  }
+  setDefaultPrinter(initialSettings['printerName'],
+                    initialSettings['cloudPrintData']);
 }
 
 /**
@@ -284,7 +300,9 @@ function updateControlsWithSelectedPrinterCapabilities() {
     lastSelectedPrinterIndex = selectedIndex;
 
     // Regenerate the preview data based on selected printer settings.
-    setDefaultValuesAndRegeneratePreview(true);
+    // Do not reset the margins if no preview request has been made.
+    var resetMargins = lastPreviewRequestID != initialPreviewRequestID;
+    setDefaultValuesAndRegeneratePreview(resetMargins);
   }
 }
 
@@ -304,7 +322,9 @@ function doUpdateCloudPrinterCapabilities(printer) {
   lastSelectedPrinterIndex = selectedIndex;
 
   // Regenerate the preview data based on selected printer settings.
-  setDefaultValuesAndRegeneratePreview(true);
+  // Do not reset the margins if no preview request has been made.
+  var resetMargins = lastPreviewRequestID != initialPreviewRequestID;
+  setDefaultValuesAndRegeneratePreview(resetMargins);
 }
 
 /**
@@ -578,18 +598,14 @@ function fileSelectionCompleted() {
  * @param {string} printer Name of the default printer. Empty if none.
  * @param {string} cloudPrintData Cloud print related data to restore if
  *     the default printer is a cloud printer.
- * @param {number} lastUsedMarginsType Indicates the last used margins type
- *     (matches enum MarginType in printing/print_job_constants.h.
  */
-function setDefaultPrinter(printer_name, cloudPrintData, lastUsedMarginsType) {
-  // Setting the margin selection to the last used one.
-  marginSettings.setLastUsedMarginsType(lastUsedMarginsType);
+function setDefaultPrinter(printerName, cloudPrintData) {
   // Add a placeholder value so the printer list looks valid.
   addDestinationListOption('', '', true, true, true);
-  if (printer_name) {
-    defaultOrLastUsedPrinterName = printer_name;
+  if (printerName) {
+    defaultOrLastUsedPrinterName = printerName;
     if (cloudPrintData) {
-      cloudprint.setDefaultPrinter(printer_name,
+      cloudprint.setDefaultPrinter(printerName,
                                    cloudPrintData,
                                    addDestinationListOptionAtPosition,
                                    doUpdateCloudPrinterCapabilities);
@@ -779,16 +795,13 @@ function setPluginPreviewPageCount() {
  * Update the page count and check the page range.
  * Called from PrintPreviewUI::OnDidGetPreviewPageCount().
  * @param {number} pageCount The number of pages.
- * @param {boolean} isModifiable Indicates whether the previewed document can be
- *     modified.
  * @param {number} previewResponseId The preview request id that resulted in
  *     this response.
  */
-function onDidGetPreviewPageCount(pageCount, isModifiable, previewResponseId) {
+function onDidGetPreviewPageCount(pageCount, previewResponseId) {
   if (!isExpectedPreviewResponse(previewResponseId))
     return;
   pageSettings.updateState(pageCount);
-  previewModifiable = isModifiable;
   if (!previewModifiable && pageSettings.requestPrintPreviewIfNeeded())
     return;
 
@@ -1058,8 +1071,6 @@ PrintSettings.prototype.save = function() {
 
 /**
  * Updates the title of the print preview tab according to |initiatorTabTitle|.
- * Called from PrintPreviewUI::OnGetInitiatorTabTitle as a result of sending a
- * 'getInitiatorTabTitle' message.
  * @param {string} initiatorTabTitle The title of the initiator tab.
  */
 function setInitiatorTabTitle(initiatorTabTitle) {

chrome/browser/ui/webui/print_preview_handler.cc
@@ -50,6 +50,7 @@
 #include "printing/metafile.h"
 #include "printing/metafile_impl.h"
 #include "printing/page_range.h"
+#include "printing/page_size_margins.h"
 #include "printing/print_settings.h"
 #include "unicode/ulocdata.h"
 
@@ -95,6 +96,17 @@ void ReportPrintSettingHistogram(enum PrintSettingsBuckets setting) {
   UMA_HISTOGRAM_ENUMERATION("PrintPreview.PrintSettings", setting,
                             PRINT_SETTINGS_BUCKET_BOUNDARY);
 }
+
+// Name of a dictionary fielad holdong cloud print related data;
+const char kCloudPrintData[] = "cloudPrintData";
+// Name of a dictionary field holding the initiator tab title.
+const char kInitiatorTabTitle[] = "initiatorTabTitle";
+// Name of a dictionary field holding the measurement system according to the
+// locale.
+const char kMeasurementSystem[] = "measurementSystem";
+// Name of a dictionary field holding the number format according to the locale.
+const char kNumberFormat[] = "numberFormat";
+
 
 // Get the print job settings dictionary from |args|. The caller takes
 // ownership of the returned DictionaryValue. Returns NULL on failure.
@@ -206,6 +218,8 @@ printing::ColorModels PrintPreviewHandler::last_used_color_model_ =
     printing::UNKNOWN_COLOR_MODEL;
 printing::MarginType PrintPreviewHandler::last_used_margins_type_ =
     printing::DEFAULT_MARGINS;
+printing::PageSizeMargins*
+    PrintPreviewHandler::last_used_page_size_margins_ = NULL;
 
 PrintPreviewHandler::PrintPreviewHandler()
     : print_backend_(printing::PrintBackend::CreateInstance(NULL)),
@@ -222,9 +236,6 @@ PrintPreviewHandler::~PrintPreviewHandler() {
 }
 
 void PrintPreviewHandler::RegisterMessages() {
-  web_ui_->RegisterMessageCallback("getDefaultPrinter",
-      base::Bind(&PrintPreviewHandler::HandleGetDefaultPrinter,
-                 base::Unretained(this)));
   web_ui_->RegisterMessageCallback("getPrinters",
       base::Bind(&PrintPreviewHandler::HandleGetPrinters,
                  base::Unretained(this)));
@@ -264,13 +275,9 @@ void PrintPreviewHandler::RegisterMessages() {
   web_ui_->RegisterMessageCallback("saveLastPrinter",
       base::Bind(&PrintPreviewHandler::HandleSaveLastPrinter,
                  base::Unretained(this)));
-  web_ui_->RegisterMessageCallback("getInitiatorTabTitle",
-      base::Bind(&PrintPreviewHandler::HandleGetInitiatorTabTitle,
+  web_ui_->RegisterMessageCallback("getInitialSettings",
+      base::Bind(&PrintPreviewHandler::HandleGetInitialSettings,
                  base::Unretained(this)));
-  web_ui_->RegisterMessageCallback("getNumberFormatAndMeasurementSystem",
-      base::Bind(
-          &PrintPreviewHandler::HandleGetNumberFormatAndMeasurementSystem,
-          base::Unretained(this)));
 }
 
 TabContentsWrapper* PrintPreviewHandler::preview_tab_wrapper() const {
@@ -278,16 +285,6 @@ TabContentsWrapper* PrintPreviewHandler::preview_tab_wrapper() const {
 }
 TabContents* PrintPreviewHandler::preview_tab() const {
   return web_ui_->tab_contents();
-}
-
-void PrintPreviewHandler::HandleGetDefaultPrinter(const ListValue* /*args*/) {
-  scoped_refptr<PrintSystemTaskProxy> task =
-      new PrintSystemTaskProxy(AsWeakPtr(),
-                               print_backend_.get(),
-                               has_logged_printers_count_);
-  BrowserThread::PostTask(
-      BrowserThread::FILE, FROM_HERE,
-      base::Bind(&PrintSystemTaskProxy::GetDefaultPrinter, task.get()));
 }
 
 void PrintPreviewHandler::HandleGetPrinters(const ListValue* /*args*/) {
@@ -397,10 +394,19 @@ void PrintPreviewHandler::HandlePrint(const ListValue* args) {
   last_used_color_model_ = static_cast<printing::ColorModels>(color_model);
 
   // Storing last used margin settings.
-  int margin_type;
-  if (!settings->GetInteger(printing::kSettingMarginsType, &margin_type))
-    margin_type = printing::DEFAULT_MARGINS;
-  last_used_margins_type_ = static_cast<printing::MarginType>(margin_type);
+  bool is_modifiable;
+  settings->GetBoolean(printing::kSettingPreviewModifiable, &is_modifiable);
+  if (is_modifiable) {
+    int margin_type;
+    if (!settings->GetInteger(printing::kSettingMarginsType, &margin_type))
+      margin_type = printing::DEFAULT_MARGINS;
+    last_used_margins_type_ = static_cast<printing::MarginType>(margin_type);
+    if (last_used_margins_type_ == printing::CUSTOM_MARGINS) {
+      if (!last_used_page_size_margins_)
+        last_used_page_size_margins_ = new printing::PageSizeMargins();
+      GetCustomMarginsFromJobSettings(*settings, last_used_page_size_margins_);
+    }
+  }
 
   bool print_to_pdf = false;
   settings->GetBoolean(printing::kSettingPrintToPDF, &print_to_pdf);
@@ -613,32 +619,67 @@ void PrintPreviewHandler::ReportStats() {
                        manage_printers_dialog_request_count_);
 }
 
-void PrintPreviewHandler::HandleGetInitiatorTabTitle(
-    const ListValue* /*args*/) {
-  PrintPreviewUI* print_preview_ui = static_cast<PrintPreviewUI*>(web_ui_);
-  base::StringValue tab_title(print_preview_ui->initiator_tab_title());
-  web_ui_->CallJavascriptFunction("setInitiatorTabTitle", tab_title);
-}
-
-void PrintPreviewHandler::HandleGetNumberFormatAndMeasurementSystem(
-    const ListValue* /*args*/) {
+void PrintPreviewHandler::GetNumberFormatAndMeasurementSystem(
+    base::DictionaryValue* settings) {
 
   // Getting the measurement system based on the locale.
   UErrorCode errorCode = U_ZERO_ERROR;
   const char* locale = g_browser_process->GetApplicationLocale().c_str();
-  UMeasurementSystem measurement_system =
-      ulocdata_getMeasurementSystem(locale, &errorCode);
-  if (errorCode > U_ZERO_ERROR || measurement_system == UMS_LIMIT)
-    measurement_system = UMS_SI;
+  UMeasurementSystem system = ulocdata_getMeasurementSystem(locale, &errorCode);
+  if (errorCode > U_ZERO_ERROR || system == UMS_LIMIT)
+    system = UMS_SI;
 
-  // Getting the number formatting based on the locale.
-  StringValue number_format(base::FormatDouble(123456.78, 2));
-  base::FundamentalValue system(measurement_system);
+  // Getting the number formatting based on the locale and writing to
+  // dictionary.
+  settings->SetString(kNumberFormat, base::FormatDouble(123456.78, 2));
+  settings->SetInteger(kMeasurementSystem, system);
+}
 
-  web_ui_->CallJavascriptFunction(
-      "print_preview.setNumberFormatAndMeasurementSystem",
-      number_format,
-      system);
+void PrintPreviewHandler::GetLastUsedMarginSettings(
+    base::DictionaryValue* custom_margins) {
+  custom_margins->SetInteger(printing::kSettingMarginsType,
+                             PrintPreviewHandler::last_used_margins_type_);
+  if (last_used_page_size_margins_) {
+    custom_margins->SetDouble(printing::kSettingMarginTop,
+                              last_used_page_size_margins_->margin_top);
+    custom_margins->SetDouble(printing::kSettingMarginBottom,
+                              last_used_page_size_margins_->margin_bottom);
+    custom_margins->SetDouble(printing::kSettingMarginLeft,
+                              last_used_page_size_margins_->margin_left);
+    custom_margins->SetDouble(printing::kSettingMarginRight,
+                              last_used_page_size_margins_->margin_right);
+  }
+}
+
+void PrintPreviewHandler::HandleGetInitialSettings(const ListValue* /*args*/) {
+  scoped_refptr<PrintSystemTaskProxy> task =
+      new PrintSystemTaskProxy(AsWeakPtr(),
+                               print_backend_.get(),
+                               has_logged_printers_count_);
+  BrowserThread::PostTask(
+      BrowserThread::FILE, FROM_HERE,
+      base::Bind(&PrintSystemTaskProxy::GetDefaultPrinter, task.get()));
+}
+
+void PrintPreviewHandler::SendInitialSettings(
+    const std::string& default_printer,
+    const std::string& cloud_print_data) {
+  PrintPreviewUI* print_preview_ui = static_cast<PrintPreviewUI*>(web_ui_);
+
+  base::DictionaryValue initial_settings;
+  initial_settings.SetString(kInitiatorTabTitle,
+                             print_preview_ui->initiator_tab_title());
+  initial_settings.SetBoolean(printing::kSettingPreviewModifiable,
+                              print_preview_ui->source_is_modifiable());
+  initial_settings.SetString(printing::kSettingPrinterName,
+                             default_printer);
+  initial_settings.SetString(kCloudPrintData, cloud_print_data);
+
+  if (print_preview_ui->source_is_modifiable()) {
+    GetLastUsedMarginSettings(&initial_settings);
+    GetNumberFormatAndMeasurementSystem(&initial_settings);
+  }
+  web_ui_->CallJavascriptFunction("setInitialSettings", initial_settings);
 }
 
 void PrintPreviewHandler::ActivateInitiatorTabAndClosePreviewTab() {
@@ -655,17 +696,6 @@ void PrintPreviewHandler::SendPrinterCapabilities(
   VLOG(1) << "Get printer capabilities finished";
   web_ui_->CallJavascriptFunction("updateWithPrinterCapabilities",
                                   settings_info);
-}
-
-void PrintPreviewHandler::SendDefaultPrinter(
-    const StringValue& default_printer,
-    const StringValue& cloud_print_data) {
-  base::FundamentalValue margins_type(
-      PrintPreviewHandler::last_used_margins_type_);
-  web_ui_->CallJavascriptFunction("setDefaultPrinter",
-                                  default_printer,
-                                  cloud_print_data,
-                                  margins_type);
 }
 
 void PrintPreviewHandler::SetupPrinterList(const ListValue& printers) {

commit 030bdd4e428519cb1d39aab8ec2c3d19f52a4c7a

PrintPreview: Fix a regression caused by r110055.
BUG=none
TEST=none


Review URL: http://codereview.chromium.org/8590014

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110445 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/print_preview/margin_settings.js
@@ -51,8 +51,8 @@ cr.define('print_preview', function() {
     },
 
     clone: function() {
-      return new Margins(this[MarginSettings.TOP_GROUP],
-                         this[MarginSettings.LEFT_GROUP],
+      return new Margins(this[MarginSettings.LEFT_GROUP],
+                         this[MarginSettings.TOP_GROUP],
                          this[MarginSettings.RIGHT_GROUP],
                          this[MarginSettings.BOTTOM_GROUP]);
     },