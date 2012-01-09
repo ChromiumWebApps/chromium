ChromiumWebApps/chromium

commit 43fe9f5fe0dadb1aeff0eb237185e8c71e4b9251

base::Bind: Remove OldCompletionCallback.
BUG=none
TEST=none
R=groby,awong,csilv

Review URL: http://codereview.chromium.org/9064007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116158 0039d316-1c4b-4281-b951-d872f2087c98

webkit/appcache/appcache_disk_cache.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,6 +13,12 @@
 #include "net/base/net_errors.h"
 
 namespace appcache {
+
+// TODO(ajwong): Change disk_cache's API to return results via Callback.
+struct AppCacheDiskCache::CreateBackendDataShim {
+  CreateBackendDataShim() : backend(NULL) {}
+  disk_cache::Backend* backend;
+};
 
 // An implementation of AppCacheDiskCacheInterface::Entry that's a thin
 // wrapper around disk_cache::Entry.
@@ -123,10 +129,9 @@ AppCacheDiskCache::AppCacheDiskCache()
 }
 
 AppCacheDiskCache::~AppCacheDiskCache() {
-  if (create_backend_callback_) {
-    create_backend_callback_->Cancel();
-    create_backend_callback_.release();
-    OnCreateBackendComplete(net::ERR_ABORTED);
+  if (!create_backend_callback_.IsCancelled()) {
+    create_backend_callback_.Cancel();
+    OnCreateBackendComplete(NULL, net::ERR_ABORTED);
   }
   disk_cache_.reset();
   STLDeleteElements(&active_calls_);
@@ -152,10 +157,9 @@ void AppCacheDiskCache::Disable() {
 
   is_disabled_ = true;
 
-  if (create_backend_callback_) {
-    create_backend_callback_->Cancel();
-    create_backend_callback_.release();
-    OnCreateBackendComplete(net::ERR_ABORTED);
+  if (!create_backend_callback_.IsCancelled()) {
+    create_backend_callback_.Cancel();
+    OnCreateBackendComplete(NULL, net::ERR_ABORTED);
   }
 }
 
@@ -221,26 +225,31 @@ int AppCacheDiskCache::Init(net::CacheType cache_type,
                             const net::CompletionCallback& callback) {
   DCHECK(!is_initializing() && !disk_cache_.get());
   is_disabled_ = false;
-  create_backend_callback_ = new CreateBackendCallback(
-      this, &AppCacheDiskCache::OnCreateBackendComplete);
 
+  // TODO(ajwong): Change disk_cache's API to return results via Callback.
+  disk_cache::Backend** backend_ptr = new(disk_cache::Backend*);
+  create_backend_callback_.Reset(
+      base::Bind(&AppCacheDiskCache::OnCreateBackendComplete,
+                 base::Unretained(this), base::Owned(backend_ptr)));
   int rv = disk_cache::CreateCacheBackend(
       cache_type, cache_directory, cache_size, force, cache_thread, NULL,
-      &(create_backend_callback_->backend_ptr_),
-      base::Bind(&net::OldCompletionCallbackAdapter, create_backend_callback_));
+      backend_ptr, create_backend_callback_.callback());
   if (rv == net::ERR_IO_PENDING)
     init_callback_ = callback;
   else
-    OnCreateBackendComplete(rv);
+    OnCreateBackendComplete(backend_ptr, rv);
+
   return rv;
 }
 
-void AppCacheDiskCache::OnCreateBackendComplete(int rv) {
+void AppCacheDiskCache::OnCreateBackendComplete(
+    disk_cache::Backend** backend, int rv) {
   if (rv == net::OK) {
-    disk_cache_.reset(create_backend_callback_->backend_ptr_);
-    create_backend_callback_->backend_ptr_ = NULL;
+    DCHECK(backend);
+    disk_cache_.reset(*backend);
   }
-  create_backend_callback_ = NULL;
+
+  create_backend_callback_.Cancel();
 
   // Invoke our clients callback function.
   if (!init_callback_.is_null()) {

webkit/appcache/appcache_disk_cache.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -45,22 +45,8 @@ class APPCACHE_EXPORT AppCacheDiskCache
                         const net::CompletionCallback& callback) OVERRIDE;
 
  private:
+  struct CreateBackendDataShim;
   class EntryImpl;
-
-  class CreateBackendCallback
-      : public net::CancelableOldCompletionCallback<AppCacheDiskCache> {
-   public:
-    typedef net::CancelableOldCompletionCallback<AppCacheDiskCache> BaseClass;
-    CreateBackendCallback(AppCacheDiskCache* object,
-                          void (AppCacheDiskCache::* method)(int))
-        : BaseClass(object, method), backend_ptr_(NULL) {}
-
-    disk_cache::Backend* backend_ptr_;  // Accessed directly.
-   private:
-    virtual ~CreateBackendCallback() {
-      delete backend_ptr_;
-    }
-  };
 
   // PendingCalls allow CreateEntry, OpenEntry, and DoomEntry to be called
   // immediately after construction, without waiting for the
@@ -100,19 +86,19 @@ class APPCACHE_EXPORT AppCacheDiskCache
   typedef std::set<ActiveCall*> ActiveCalls;
 
   bool is_initializing() const {
-    return create_backend_callback_.get() != NULL;
+    return !create_backend_callback_.IsCancelled();
   }
   disk_cache::Backend* disk_cache() { return disk_cache_.get(); }
   int Init(net::CacheType cache_type, const FilePath& directory,
            int cache_size, bool force, base::MessageLoopProxy* cache_thread,
            const net::CompletionCallback& callback);
-  void OnCreateBackendComplete(int rv);
+  void OnCreateBackendComplete(disk_cache::Backend** backend, int rv);
   void AddActiveCall(ActiveCall* call) { active_calls_.insert(call); }
   void RemoveActiveCall(ActiveCall* call) { active_calls_.erase(call); }
 
   bool is_disabled_;
   net::CompletionCallback init_callback_;
-  scoped_refptr<CreateBackendCallback> create_backend_callback_;
+  net::CancelableCompletionCallback create_backend_callback_;
   PendingCalls pending_calls_;
   ActiveCalls active_calls_;
   scoped_ptr<disk_cache::Backend> disk_cache_;

net/base/completion_callback.cc
@@ -1,13 +1,0 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "net/base/completion_callback.h"
-
-namespace net {
-
-void OldCompletionCallbackAdapter(OldCompletionCallback* callback, int result) {
-  callback->Run(result);
-}
-
-}  // namespace net

net/base/completion_callback.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,67 +6,16 @@
 #define NET_BASE_COMPLETION_CALLBACK_H__
 #pragma once
 
-#include "base/callback_old.h"
 #include "base/callback.h"
 #include "base/cancelable_callback.h"
-#include "net/base/net_export.h"
 
 namespace net {
 
-// A callback specialization that takes a single int parameter.  Usually this
-// is used to report a byte count or network error code.
-typedef Callback1<int>::Type OldCompletionCallback;
+// A callback specialization that takes a single int parameter. Usually this is
+// used to report a byte count or network error code.
 typedef base::Callback<void(int)> CompletionCallback;
 
-// Temporary (in the code) adapter of OldCompletionCallback for use at call
-// sites that expect a CompletionCallback.
-//
-// TODO(jhawkins): Remove once OldCompletionCallback is removed.
-void NET_EXPORT OldCompletionCallbackAdapter(OldCompletionCallback* callback,
-                                             int result);
-
-// Used to implement a OldCompletionCallback.
-template <class T>
-class OldCompletionCallbackImpl :
-    public CallbackImpl< T, void (T::*)(int), Tuple1<int> > {
- public:
-  OldCompletionCallbackImpl(T* obj, void (T::* meth)(int))
-    : CallbackImpl< T, void (T::*)(int),
-                    Tuple1<int> >::CallbackImpl(obj, meth) {
-  }
-};
-
 typedef base::CancelableCallback<void(int)> CancelableCompletionCallback;
-
-// CancelableOldCompletionCallback is used for completion callbacks
-// which may outlive the target for the method dispatch. In such a case, the
-// provider of the callback calls Cancel() to mark the callback as
-// "canceled". When the canceled callback is eventually run it does nothing
-// other than to decrement the refcount to 0 and free the memory.
-template <class T>
-class CancelableOldCompletionCallback :
-    public OldCompletionCallbackImpl<T>,
-    public base::RefCounted<CancelableOldCompletionCallback<T> > {
- public:
-  CancelableOldCompletionCallback(T* obj, void (T::* meth)(int))
-    : OldCompletionCallbackImpl<T>(obj, meth), is_canceled_(false) {
-  }
-
-  void Cancel() {
-    is_canceled_ = true;
-  }
-
-  virtual void RunWithParams(const Tuple1<int>& params) {
-    if (is_canceled_) {
-      base::RefCounted<CancelableOldCompletionCallback<T> >::Release();
-    } else {
-      OldCompletionCallbackImpl<T>::RunWithParams(params);
-    }
-  }
-
- private:
-  bool is_canceled_;
-};
 
 }  // namespace net
 

net/net.gyp
@@ -72,7 +72,6 @@
         'base/cert_verifier.h',
         'base/cert_verify_result.cc',
         'base/cert_verify_result.h',
-        'base/completion_callback.cc',
         'base/completion_callback.h',
         'base/connection_type_histograms.cc',
         'base/connection_type_histograms.h',

webkit/appcache/appcache_response.cc
@@ -1,10 +1,12 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "webkit/appcache/appcache_response.h"
 
 #include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/pickle.h"
@@ -137,13 +139,13 @@ void AppCacheResponseIO::OnRawIOComplete(int result) {
 AppCacheResponseReader::AppCacheResponseReader(
     int64 response_id, int64 group_id, AppCacheDiskCacheInterface* disk_cache)
     : AppCacheResponseIO(response_id, group_id, disk_cache),
-      range_offset_(0), range_length_(kint32max),
-      read_position_(0) {
+      range_offset_(0),
+      range_length_(kint32max),
+      read_position_(0),
+      ALLOW_THIS_IN_INITIALIZER_LIST(weak_factory_(this)) {
 }
 
 AppCacheResponseReader::~AppCacheResponseReader() {
-  if (open_callback_)
-    open_callback_.release()->Cancel();
 }
 
 void AppCacheResponseReader::ReadInfo(HttpResponseInfoIOBuffer* info_buf,
@@ -240,31 +242,33 @@ void AppCacheResponseReader::OnIOComplete(int result) {
 
 void AppCacheResponseReader::OpenEntryIfNeededAndContinue() {
   int rv;
+  AppCacheDiskCacheInterface::Entry** entry_ptr = NULL;
   if (entry_) {
     rv = net::OK;
   } else if (!disk_cache_) {
     rv = net::ERR_FAILED;
   } else {
-    open_callback_ = new EntryCallback<AppCacheResponseReader>(
-        this, &AppCacheResponseReader::OnOpenEntryComplete);
-    rv = disk_cache_->OpenEntry(
-        response_id_, &open_callback_->entry_ptr_,
-        base::Bind(&net::OldCompletionCallbackAdapter, open_callback_));
+    entry_ptr = new(AppCacheDiskCacheInterface::Entry*);
+    open_callback_ =
+        base::Bind(&AppCacheResponseReader::OnOpenEntryComplete,
+                   weak_factory_.GetWeakPtr(), base::Owned(entry_ptr));
+    rv = disk_cache_->OpenEntry(response_id_, entry_ptr, open_callback_);
   }
 
   if (rv != net::ERR_IO_PENDING)
-    OnOpenEntryComplete(rv);
+    OnOpenEntryComplete(entry_ptr, rv);
 }
 
-void AppCacheResponseReader::OnOpenEntryComplete(int rv) {
+void AppCacheResponseReader::OnOpenEntryComplete(
+    AppCacheDiskCacheInterface::Entry** entry, int rv) {
   DCHECK(info_buffer_.get() || buffer_.get());
 
-  if (open_callback_) {
+  if (!open_callback_.is_null()) {
     if (rv == net::OK) {
-      entry_ = open_callback_->entry_ptr_;
-      open_callback_->entry_ptr_ = NULL;
+      DCHECK(entry);
+      entry_ = *entry;
     }
-    open_callback_ = NULL;
+    open_callback_.Reset();
   }
 
   if (info_buffer_)
@@ -278,13 +282,14 @@ void AppCacheResponseReader::OnOpenEntryComplete(int rv) {
 AppCacheResponseWriter::AppCacheResponseWriter(
     int64 response_id, int64 group_id, AppCacheDiskCacheInterface* disk_cache)
     : AppCacheResponseIO(response_id, group_id, disk_cache),
-      info_size_(0), write_position_(0), write_amount_(0),
-      creation_phase_(INITIAL_ATTEMPT) {
+      info_size_(0),
+      write_position_(0),
+      write_amount_(0),
+      creation_phase_(INITIAL_ATTEMPT),
+      ALLOW_THIS_IN_INITIALIZER_LIST(weak_factory_(this)) {
 }
 
 AppCacheResponseWriter::~AppCacheResponseWriter() {
-  if (create_callback_)
-    create_callback_.release()->Cancel();
 }
 
 void AppCacheResponseWriter::WriteInfo(
@@ -354,6 +359,7 @@ void AppCacheResponseWriter::OnIOComplete(int result) {
 
 void AppCacheResponseWriter::CreateEntryIfNeededAndContinue() {
   int rv;
+  AppCacheDiskCacheInterface::Entry** entry_ptr = NULL;
   if (entry_) {
     creation_phase_ = NO_ATTEMPT;
     rv = net::OK;
@@ -362,46 +368,48 @@ void AppCacheResponseWriter::CreateEntryIfNeededAndContinue() {
     rv = net::ERR_FAILED;
   } else {
     creation_phase_ = INITIAL_ATTEMPT;
-    create_callback_ = new EntryCallback<AppCacheResponseWriter>(
-        this, &AppCacheResponseWriter::OnCreateEntryComplete);
-    rv = disk_cache_->CreateEntry(
-        response_id_, &create_callback_->entry_ptr_,
-        base::Bind(&net::OldCompletionCallbackAdapter, create_callback_));
+    entry_ptr = new(AppCacheDiskCacheInterface::Entry*);
+    create_callback_ =
+        base::Bind(&AppCacheResponseWriter::OnCreateEntryComplete,
+                   base::Unretained(this), base::Owned(entry_ptr));
+    rv = disk_cache_->CreateEntry(response_id_, entry_ptr, create_callback_);
   }
   if (rv != net::ERR_IO_PENDING)
-    OnCreateEntryComplete(rv);
+    OnCreateEntryComplete(entry_ptr, rv);
 }
 
-void AppCacheResponseWriter::OnCreateEntryComplete(int rv) {
+void AppCacheResponseWriter::OnCreateEntryComplete(
+    AppCacheDiskCacheInterface::Entry** entry, int rv) {
   DCHECK(info_buffer_.get() || buffer_.get());
+
+  AppCacheDiskCacheInterface::Entry** entry_ptr = NULL;
 
   if (creation_phase_ == INITIAL_ATTEMPT) {
     if (rv != net::OK) {
       // We may try to overwrite existing entries.
       creation_phase_ = DOOM_EXISTING;
-      rv = disk_cache_->DoomEntry(
-          response_id_, base::Bind(&net::OldCompletionCallbackAdapter,
-                                   create_callback_));
+      rv = disk_cache_->DoomEntry(response_id_, create_callback_);
       if (rv != net::ERR_IO_PENDING)
-        OnCreateEntryComplete(rv);
+        OnCreateEntryComplete(NULL, rv);
       return;
     }
   } else if (creation_phase_ == DOOM_EXISTING) {
     creation_phase_ = SECOND_ATTEMPT;
-    rv = disk_cache_->CreateEntry(
-        response_id_, &create_callback_->entry_ptr_,
-        base::Bind(&net::OldCompletionCallbackAdapter, create_callback_));
+    entry_ptr = new(AppCacheDiskCacheInterface::Entry*);
+    create_callback_ =
+        base::Bind(&AppCacheResponseWriter::OnCreateEntryComplete,
+                   base::Unretained(this), base::Owned(entry_ptr));
+    rv = disk_cache_->CreateEntry(response_id_, entry_ptr, create_callback_);
     if (rv != net::ERR_IO_PENDING)
-      OnCreateEntryComplete(rv);
+      OnCreateEntryComplete(entry_ptr, rv);
     return;
   }
 
-  if (create_callback_) {
-    if (rv == net::OK) {
-      entry_ = create_callback_->entry_ptr_;
-      create_callback_->entry_ptr_ = NULL;
-    }
-    create_callback_ = NULL;
+  if (!create_callback_.is_null()) {
+    if (rv == net::OK)
+      entry_ = *entry;
+
+    create_callback_.Reset();
   }
 
   if (info_buffer_)

webkit/appcache/appcache_response.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -100,21 +100,6 @@ class APPCACHE_EXPORT AppCacheResponseIO {
   int64 response_id() const { return response_id_; }
 
  protected:
-  template <class T>
-  class EntryCallback : public net::CancelableOldCompletionCallback<T> {
-   public:
-    typedef net::CancelableOldCompletionCallback<T> BaseClass;
-    EntryCallback(T* object,  void (T::* method)(int))
-        : BaseClass(object, method), entry_ptr_(NULL) {}
-
-    AppCacheDiskCacheInterface::Entry* entry_ptr_;  // Accessed directly.
-   private:
-    ~EntryCallback() {
-      if (entry_ptr_)
-        entry_ptr_->Close();
-    }
-  };
-
   AppCacheResponseIO(int64 response_id,
                      int64 group_id,
                      AppCacheDiskCacheInterface* disk_cache);
@@ -194,12 +179,13 @@ class APPCACHE_EXPORT AppCacheResponseReader : public AppCacheResponseIO {
   void ContinueReadInfo();
   void ContinueReadData();
   void OpenEntryIfNeededAndContinue();
-  void OnOpenEntryComplete(int rv);
+  void OnOpenEntryComplete(AppCacheDiskCacheInterface::Entry** entry, int rv);
 
   int range_offset_;
   int range_length_;
   int read_position_;
-  scoped_refptr<EntryCallback<AppCacheResponseReader> > open_callback_;
+  net::CompletionCallback open_callback_;
+  base::WeakPtrFactory<AppCacheResponseReader> weak_factory_;
 };
 
 // Writes new response data to storage. If the object is deleted
@@ -257,13 +243,14 @@ class APPCACHE_EXPORT AppCacheResponseWriter : public AppCacheResponseIO {
   void ContinueWriteInfo();
   void ContinueWriteData();
   void CreateEntryIfNeededAndContinue();
-  void OnCreateEntryComplete(int rv);
+  void OnCreateEntryComplete(AppCacheDiskCacheInterface::Entry** entry, int rv);
 
   int info_size_;
   int write_position_;
   int write_amount_;
   CreationPhase creation_phase_;
-  scoped_refptr<EntryCallback<AppCacheResponseWriter> > create_callback_;
+  net::CompletionCallback create_callback_;
+  base::WeakPtrFactory<AppCacheResponseWriter> weak_factory_;
 };
 
 }  // namespace appcache

commit 20fa4455121ab6dba14cf09f8a003fbb1ba2a269

Fix a use-after-free bug introduced in r116158. See comments in http://codereview.chromium.org/9064007 for details. Review URL: http://codereview.chromium.org/9127010
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116886 0039d316-1c4b-4281-b951-d872f2087c98

webkit/appcache/appcache_disk_cache.cc
@@ -14,10 +14,35 @@
 
 namespace appcache {
 
-// TODO(ajwong): Change disk_cache's API to return results via Callback.
-struct AppCacheDiskCache::CreateBackendDataShim {
-  CreateBackendDataShim() : backend(NULL) {}
-  disk_cache::Backend* backend;
+// A callback shim that provides storage for the 'backend_ptr' value
+// and will delete a resulting ptr if completion occurs after its
+// been canceled.
+class AppCacheDiskCache::CreateBackendCallbackShim
+    : public base::RefCounted<CreateBackendCallbackShim> {
+ public:
+  explicit CreateBackendCallbackShim(AppCacheDiskCache* object)
+      : backend_ptr_(NULL), appcache_diskcache_(object) {
+  }
+
+  void Cancel() {
+    appcache_diskcache_ = NULL;
+  }
+
+  void Callback(int rv) {
+    if (appcache_diskcache_)
+      appcache_diskcache_->OnCreateBackendComplete(rv);
+  }
+
+  disk_cache::Backend* backend_ptr_;  // Accessed directly.
+
+ private:
+  friend class base::RefCounted<CreateBackendCallbackShim>;
+
+  ~CreateBackendCallbackShim() {
+    delete backend_ptr_;
+  }
+
+  AppCacheDiskCache* appcache_diskcache_;  // Unowned pointer.
 };
 
 // An implementation of AppCacheDiskCacheInterface::Entry that's a thin
@@ -129,9 +154,10 @@ AppCacheDiskCache::AppCacheDiskCache()
 }
 
 AppCacheDiskCache::~AppCacheDiskCache() {
-  if (!create_backend_callback_.IsCancelled()) {
-    create_backend_callback_.Cancel();
-    OnCreateBackendComplete(NULL, net::ERR_ABORTED);
+  if (create_backend_callback_) {
+    create_backend_callback_->Cancel();
+    create_backend_callback_ = NULL;
+    OnCreateBackendComplete(net::ERR_ABORTED);
   }
   disk_cache_.reset();
   STLDeleteElements(&active_calls_);
@@ -157,9 +183,10 @@ void AppCacheDiskCache::Disable() {
 
   is_disabled_ = true;
 
-  if (!create_backend_callback_.IsCancelled()) {
-    create_backend_callback_.Cancel();
-    OnCreateBackendComplete(NULL, net::ERR_ABORTED);
+  if (create_backend_callback_) {
+    create_backend_callback_->Cancel();
+    create_backend_callback_ = NULL;
+    OnCreateBackendComplete(net::ERR_ABORTED);
   }
 }
 
@@ -225,31 +252,26 @@ int AppCacheDiskCache::Init(net::CacheType cache_type,
                             const net::CompletionCallback& callback) {
   DCHECK(!is_initializing() && !disk_cache_.get());
   is_disabled_ = false;
+  create_backend_callback_ = new CreateBackendCallbackShim(this);
 
-  // TODO(ajwong): Change disk_cache's API to return results via Callback.
-  disk_cache::Backend** backend_ptr = new(disk_cache::Backend*);
-  create_backend_callback_.Reset(
-      base::Bind(&AppCacheDiskCache::OnCreateBackendComplete,
-                 base::Unretained(this), base::Owned(backend_ptr)));
   int rv = disk_cache::CreateCacheBackend(
       cache_type, cache_directory, cache_size, force, cache_thread, NULL,
-      backend_ptr, create_backend_callback_.callback());
+      &(create_backend_callback_->backend_ptr_),
+      base::Bind(&CreateBackendCallbackShim::Callback,
+                 create_backend_callback_));
   if (rv == net::ERR_IO_PENDING)
     init_callback_ = callback;
   else
-    OnCreateBackendComplete(backend_ptr, rv);
-
+    OnCreateBackendComplete(rv);
   return rv;
 }
 
-void AppCacheDiskCache::OnCreateBackendComplete(
-    disk_cache::Backend** backend, int rv) {
+void AppCacheDiskCache::OnCreateBackendComplete(int rv) {
   if (rv == net::OK) {
-    DCHECK(backend);
-    disk_cache_.reset(*backend);
+    disk_cache_.reset(create_backend_callback_->backend_ptr_);
+    create_backend_callback_->backend_ptr_ = NULL;
   }
-
-  create_backend_callback_.Cancel();
+  create_backend_callback_ = NULL;
 
   // Invoke our clients callback function.
   if (!init_callback_.is_null()) {

webkit/appcache/appcache_disk_cache.h
@@ -45,7 +45,7 @@ class APPCACHE_EXPORT AppCacheDiskCache
                         const net::CompletionCallback& callback) OVERRIDE;
 
  private:
-  struct CreateBackendDataShim;
+  class CreateBackendCallbackShim;
   class EntryImpl;
 
   // PendingCalls allow CreateEntry, OpenEntry, and DoomEntry to be called
@@ -86,19 +86,19 @@ class APPCACHE_EXPORT AppCacheDiskCache
   typedef std::set<ActiveCall*> ActiveCalls;
 
   bool is_initializing() const {
-    return !create_backend_callback_.IsCancelled();
+    return create_backend_callback_.get() != NULL;
   }
   disk_cache::Backend* disk_cache() { return disk_cache_.get(); }
   int Init(net::CacheType cache_type, const FilePath& directory,
            int cache_size, bool force, base::MessageLoopProxy* cache_thread,
            const net::CompletionCallback& callback);
-  void OnCreateBackendComplete(disk_cache::Backend** backend, int rv);
+  void OnCreateBackendComplete(int rv);
   void AddActiveCall(ActiveCall* call) { active_calls_.insert(call); }
   void RemoveActiveCall(ActiveCall* call) { active_calls_.erase(call); }
 
   bool is_disabled_;
   net::CompletionCallback init_callback_;
-  net::CancelableCompletionCallback create_backend_callback_;
+  scoped_refptr<CreateBackendCallbackShim> create_backend_callback_;
   PendingCalls pending_calls_;
   ActiveCalls active_calls_;
   scoped_ptr<disk_cache::Backend> disk_cache_;