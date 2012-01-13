ChromiumWebApps/chromium

commit 7a5f593e856ea4a0733a8d3665b0ca5ea3410fcb

Add more per-tab preferences.
intl.global.charset_default
webkit.webprefs.global.standard_font_family
webkit.webprefs.global.fixed_font_family
webkit.webprefs.global.serif_font_family
webkit.webprefs.global.sansserif_font_family
webkit.webprefs.global.cursive_font_family
webkit.webprefs.global.fantasy_font_family
webkit.webprefs.global.default_font_size
webkit.webprefs.global.default_fixed_font_size
webkit.webprefs.global.minimum_font_size
webkit.webprefs.global.minimum_logical_font_size
webkit.webprefs.global.javascript_can_open_windows_automatically
webkit.webprefs.global.loads_images_automatically
webkit.webprefs.global.plugins_enabled

BUG=105537
TEST=PrefsTabHelperTest*,PrefsTabHelperBrowserTest*

For files that has only prefs names changes and require OWNERS approval:

TBR=erikwright@chromium.org,gene@chromium.org,csilv@chromium.org,atwilson@chromium.org,mirandac@chromium.org

Review URL: http://codereview.chromium.org/8879016

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115955 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/options/font_settings.html
@@ -5,7 +5,7 @@
     <div class="font-input-div">
       <div>
         <select id="standard-font-family" class="font-input" data-type="string"
-            pref="webkit.webprefs.standard_font_family"
+            pref="webkit.webprefs.global.standard_font_family"
             metric="Options_ChangeStandardFont"></select>
       </div>
       <div>
@@ -25,7 +25,7 @@
     <div class="font-input-div">
       <div>
         <select id="serif-font-family" class="font-input" data-type="string"
-            pref="webkit.webprefs.serif_font_family"
+            pref="webkit.webprefs.global.serif_font_family"
             metric="Options_ChangeSerifFont"></select>
       </div>
     </div>
@@ -37,7 +37,7 @@
       <div>
         <select id="sans-serif-font-family" class="font-input"
             data-type="string"
-            pref="webkit.webprefs.sansserif_font_family"
+            pref="webkit.webprefs.global.sansserif_font_family"
             metric="Options_ChangeSansSerifFont"></select>
       </div>
     </div>
@@ -48,7 +48,7 @@
     <div class="font-input-div">
       <div>
         <select id="fixed-font-family" class="font-input" data-type="string"
-            pref="webkit.webprefs.fixed_font_family"
+            pref="webkit.webprefs.global.fixed_font_family"
             metric="Options_ChangeFixedFont"></select>
       </div>
     </div>
@@ -59,7 +59,7 @@
     <div class="font-input-div">
       <div>
         <input id="minimum-font-size" type="range" min="0" max="15"
-            pref="webkit.webprefs.minimum_font_size">
+            pref="webkit.webprefs.global.minimum_font_size">
         <div>
           <span i18n-content="fontSettingsSizeTiny"></span>
           <span i18n-content="fontSettingsSizeHuge" class="font-settings-huge">
@@ -74,7 +74,7 @@
     <div class="font-input-div">
       <div>
         <select id="font-encoding" data-type="string"
-            pref="intl.charset_default"
+            pref="intl.global.charset_default"
             metric="Options_ChangeFontEncoding"></select>
       </div>
     </div>

chrome/browser/automation/automation_provider.cc
@@ -730,7 +730,8 @@ void AutomationProvider::OnSetPageFontSize(int tab_handle,
       DCHECK(tab->tab_contents()->GetBrowserContext() != NULL);
       Profile* profile =
           Profile::FromBrowserContext(tab->tab_contents()->GetBrowserContext());
-      profile->GetPrefs()->SetInteger(prefs::kWebKitDefaultFontSize, font_size);
+      profile->GetPrefs()->SetInteger(
+          prefs::kWebKitGlobalDefaultFontSize, font_size);
     }
   }
 }

chrome/browser/browser_encoding_uitest.cc
@@ -249,7 +249,7 @@ TEST_F(BrowserEncodingTest, MAYBE_TestEncodingAutoDetect) {
   // Set the default charset to one of encodings not supported by the current
   // auto-detector (Please refer to the above comments) to make sure we
   // incorrectly decode the page. Now we use ISO-8859-4.
-  ASSERT_TRUE(browser->SetStringPreference(prefs::kDefaultCharset,
+  ASSERT_TRUE(browser->SetStringPreference(prefs::kGlobalDefaultCharset,
                                            "ISO-8859-4"));
   scoped_refptr<TabProxy> tab(GetActiveTab());
   ASSERT_TRUE(tab.get());

chrome/browser/net/chrome_url_request_context.cc
@@ -279,9 +279,9 @@ void ChromeURLRequestContextGetter::Observe(
               &ChromeURLRequestContextGetter::OnAcceptLanguageChange,
               this,
               accept_language));
-    } else if (*pref_name_in == prefs::kDefaultCharset) {
+    } else if (*pref_name_in == prefs::kGlobalDefaultCharset) {
       std::string default_charset =
-          prefs->GetString(prefs::kDefaultCharset);
+          prefs->GetString(prefs::kGlobalDefaultCharset);
       BrowserThread::PostTask(
           BrowserThread::IO, FROM_HERE,
           base::Bind(
@@ -308,7 +308,7 @@ void ChromeURLRequestContextGetter::RegisterPrefsObserver(Profile* profile) {
 
   registrar_.Init(profile->GetPrefs());
   registrar_.Add(prefs::kAcceptLanguages, this);
-  registrar_.Add(prefs::kDefaultCharset, this);
+  registrar_.Add(prefs::kGlobalDefaultCharset, this);
   registrar_.Add(prefs::kClearSiteDataOnExit, this);
 }
 

chrome/browser/prefs/default_pref_store.cc
@@ -15,6 +15,11 @@ void DefaultPrefStore::SetDefaultValue(const std::string& key, Value* value) {
   SetValue(key, value);
 }
 
+void DefaultPrefStore::RemoveDefaultValue(const std::string& key) {
+  CHECK(GetValue(key, NULL) == READ_OK);
+  RemoveValue(key);
+}
+
 base::Value::Type DefaultPrefStore::GetType(const std::string& key) const {
   const Value* value;
   return GetValue(key, &value) == READ_OK ? value->GetType() : Value::TYPE_NULL;

chrome/browser/prefs/default_pref_store.h
@@ -21,6 +21,9 @@ class DefaultPrefStore : public ValueMapPrefStore {
   // Stores a new |value| for |key|. Assumes ownership of |value|.
   void SetDefaultValue(const std::string& key, Value* value);
 
+  // Removes the value for |key|.
+  void RemoveDefaultValue(const std::string& key);
+
   // Returns the registered type for |key| or Value::TYPE_NULL if the |key|
   // has not been registered.
   base::Value::Type GetType(const std::string& key) const;

chrome/browser/prefs/per_tab_user_pref_store.cc
@@ -8,5 +8,48 @@
 PerTabUserPrefStore::PerTabUserPrefStore(PersistentPrefStore* underlay)
     : OverlayUserPrefStore(underlay) {
   RegisterOverlayProperty(
-      prefs::kWebKitJavascriptEnabled, prefs::kWebKitGlobalJavascriptEnabled);
+      prefs::kWebKitJavascriptEnabled,
+      prefs::kWebKitGlobalJavascriptEnabled);
+  RegisterOverlayProperty(
+      prefs::kWebKitJavascriptCanOpenWindowsAutomatically,
+      prefs::kWebKitGlobalJavascriptCanOpenWindowsAutomatically);
+  RegisterOverlayProperty(
+      prefs::kWebKitLoadsImagesAutomatically,
+      prefs::kWebKitGlobalLoadsImagesAutomatically);
+  RegisterOverlayProperty(
+      prefs::kWebKitPluginsEnabled,
+      prefs::kWebKitGlobalPluginsEnabled);
+  RegisterOverlayProperty(
+      prefs::kDefaultCharset,
+      prefs::kGlobalDefaultCharset);
+  RegisterOverlayProperty(
+      prefs::kWebKitStandardFontFamily,
+      prefs::kWebKitGlobalStandardFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitFixedFontFamily,
+      prefs::kWebKitGlobalFixedFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitSerifFontFamily,
+      prefs::kWebKitGlobalSerifFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitSansSerifFontFamily,
+      prefs::kWebKitGlobalSansSerifFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitCursiveFontFamily,
+      prefs::kWebKitGlobalCursiveFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitFantasyFontFamily,
+      prefs::kWebKitGlobalFantasyFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitDefaultFontSize,
+      prefs::kWebKitGlobalDefaultFontSize);
+  RegisterOverlayProperty(
+      prefs::kWebKitDefaultFixedFontSize,
+      prefs::kWebKitGlobalDefaultFixedFontSize);
+  RegisterOverlayProperty(
+      prefs::kWebKitMinimumFontSize,
+      prefs::kWebKitGlobalMinimumFontSize);
+  RegisterOverlayProperty(
+      prefs::kWebKitMinimumLogicalFontSize,
+      prefs::kWebKitGlobalMinimumLogicalFontSize);
 }

chrome/browser/prefs/pref_model_associator.cc
@@ -376,6 +376,11 @@ bool PrefModelAssociator::IsPrefRegistered(const char* name) {
   return registered_preferences_.count(name) > 0;
 }
 
+void PrefModelAssociator::UnregisterPref(const char* name) {
+  DCHECK(synced_preferences_.count(name) == 0);
+  registered_preferences_.erase(name);
+}
+
 void PrefModelAssociator::ProcessPrefChange(const std::string& name) {
   if (processing_syncer_changes_)
     return;  // These are changes originating from us, ignore.

commit 001c39e131807dc1498414b103789d59c7c728e4

Fix font size settings page.
It has been broken in r115955.

R=bauerb@chromium.org
BUG=110097
TEST=changing font settings doesn't crash in debug mode, settings persist among Chrome launches

Review URL: http://codereview.chromium.org/9211002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117647 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/options/font_settings.html
@@ -10,7 +10,7 @@
       </div>
       <div>
         <input id="standard-font-size" type="range" min="0" max="24"
-            pref="webkit.webprefs.default_font_size">
+            pref="webkit.webprefs.global.default_font_size">
         <div>
           <span i18n-content="fontSettingsSizeTiny"></span>
           <span i18n-content="fontSettingsSizeHuge" class="font-settings-huge">

chrome/browser/resources/options/font_settings.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -109,8 +109,9 @@ cr.define('options', function() {
      * @private
      */
     standardFontSizeChanged_: function(el, value) {
-      Preferences.setIntegerPref('webkit.webprefs.default_fixed_font_size',
-                                 value - SIZE_DIFFERENCE_FIXED_STANDARD, '');
+      Preferences.setIntegerPref(
+        'webkit.webprefs.global.default_fixed_font_size',
+        value - SIZE_DIFFERENCE_FIXED_STANDARD, '');
     },
 
     /**
@@ -134,8 +135,8 @@ cr.define('options', function() {
      * @private
      */
     minimumFontSizeChanged_: function(el, value) {
-      Preferences.setIntegerPref('webkit.webprefs.minimum_logical_font_size',
-          value, '');
+      Preferences.setIntegerPref(
+        'webkit.webprefs.global.minimum_logical_font_size', value, '');
     },
 
     /**

chrome/browser/resources/options2/font_settings.html
@@ -10,7 +10,7 @@
       </div>
       <div>
         <input id="standard-font-size" type="range" min="0" max="24"
-            pref="webkit.webprefs.default_font_size">
+            pref="webkit.webprefs.global.default_font_size">
         <div>
           <span i18n-content="fontSettingsSizeTiny"></span>
           <span i18n-content="fontSettingsSizeHuge" class="font-settings-huge">

chrome/browser/resources/options2/font_settings.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -109,8 +109,9 @@ cr.define('options', function() {
      * @private
      */
     standardFontSizeChanged_: function(el, value) {
-      Preferences.setIntegerPref('webkit.webprefs.default_fixed_font_size',
-                                 value - SIZE_DIFFERENCE_FIXED_STANDARD, '');
+      Preferences.setIntegerPref(
+        'webkit.webprefs.global.default_fixed_font_size',
+        value - SIZE_DIFFERENCE_FIXED_STANDARD, '');
     },
 
     /**
@@ -134,8 +135,8 @@ cr.define('options', function() {
      * @private
      */
     minimumFontSizeChanged_: function(el, value) {
-      Preferences.setIntegerPref('webkit.webprefs.minimum_logical_font_size',
-          value, '');
+      Preferences.setIntegerPref(
+        'webkit.webprefs.global.minimum_logical_font_size', value, '');
     },
 
     /**