ChromiumWebApps/chromium

commit a9415292f9aedca5c528532f1260118ad384aaed

Replace DataSourceFactory with explicitly initialized DataSources.
BUG=107324
TEST=layout tests

Review URL: https://chromiumcodereview.appspot.com/9243016

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118338 0039d316-1c4b-4281-b951-d872f2087c98

webkit/media/simple_data_source.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,6 @@
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebKitPlatformSupport.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebString.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebURLLoaderOptions.h"
-#include "webkit/media/web_data_source_factory.h"
 
 using WebKit::WebString;
 using WebKit::WebURLLoaderOptions;
@@ -24,22 +23,6 @@ using WebKit::WebURLLoaderOptions;
 namespace webkit_media {
 
 static const char kDataScheme[] = "data";
-
-static WebDataSource* NewSimpleDataSource(MessageLoop* render_loop,
-                                          WebKit::WebFrame* frame,
-                                          media::MediaLog* media_log) {
-  return new SimpleDataSource(render_loop, frame);
-}
-
-// static
-media::DataSourceFactory* SimpleDataSource::CreateFactory(
-    MessageLoop* render_loop,
-    WebKit::WebFrame* frame,
-    media::MediaLog* media_log,
-    const WebDataSourceBuildObserverHack& build_observer) {
-  return new WebDataSourceFactory(render_loop, frame, media_log,
-                                  &NewSimpleDataSource, build_observer);
-}
 
 SimpleDataSource::SimpleDataSource(
     MessageLoop* render_loop,
@@ -79,7 +62,7 @@ void SimpleDataSource::Stop(const base::Closure& callback) {
 }
 
 void SimpleDataSource::Initialize(
-    const std::string& url,
+    const GURL& url,
     const media::PipelineStatusCB& callback) {
   // Reference to prevent destruction while inside the |initialize_cb_|
   // call. This is a temporary fix to prevent crashes caused by holding the
@@ -93,7 +76,7 @@ void SimpleDataSource::Initialize(
     initialize_cb_ = callback;
 
     // Validate the URL.
-    url_ = GURL(url);
+    url_ = url;
     if (!url_.is_valid()) {
       DoneInitialization_Locked(false);
       return;
@@ -103,17 +86,6 @@ void SimpleDataSource::Initialize(
     render_loop_->PostTask(FROM_HERE,
         base::Bind(&SimpleDataSource::StartTask, this));
   }
-}
-
-void SimpleDataSource::CancelInitialize() {
-  base::AutoLock auto_lock(lock_);
-  DCHECK(!initialize_cb_.is_null());
-  state_ = STOPPED;
-  initialize_cb_.Reset();
-
-  // Post a task to the render thread to cancel loading the resource.
-  render_loop_->PostTask(FROM_HERE,
-      base::Bind(&SimpleDataSource::CancelTask, this));
 }
 
 void SimpleDataSource::Read(int64 position,
@@ -263,6 +235,10 @@ bool SimpleDataSource::HasSingleOrigin() {
 
 void SimpleDataSource::Abort() {
   DCHECK(MessageLoop::current() == render_loop_);
+  base::AutoLock auto_lock(lock_);
+  state_ = STOPPED;
+  initialize_cb_.Reset();
+  CancelTask();
   frame_ = NULL;
 }
 

webkit/media/simple_data_source.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,7 +15,7 @@
 
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
-#include "media/base/filter_factories.h"
+#include "googleurl/src/gurl.h"
 #include "media/base/data_source.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebURLLoader.h"
@@ -36,13 +36,6 @@ class SimpleDataSource
     : public WebDataSource,
       public WebKit::WebURLLoaderClient {
  public:
-  // Creates a DataSourceFactory for building SimpleDataSource objects.
-  static media::DataSourceFactory* CreateFactory(
-      MessageLoop* render_loop,
-      WebKit::WebFrame* frame,
-      media::MediaLog* media_log,
-      const WebDataSourceBuildObserverHack& build_observer);
-
   SimpleDataSource(MessageLoop* render_loop, WebKit::WebFrame* frame);
   virtual ~SimpleDataSource();
 
@@ -92,9 +85,8 @@ class SimpleDataSource
       const WebKit::WebURLError&);
 
   // webkit_glue::WebDataSource implementation.
-  virtual void Initialize(const std::string& url,
+  virtual void Initialize(const GURL& url,
                           const media::PipelineStatusCB& callback) OVERRIDE;
-  virtual void CancelInitialize() OVERRIDE;
   virtual bool HasSingleOrigin() OVERRIDE;
   virtual void Abort() OVERRIDE;
 

webkit/media/simple_data_source_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -80,7 +80,7 @@ class SimpleDataSourceTest : public testing::Test {
     data_source_->set_host(&host_);
     data_source_->SetURLLoaderForTest(url_loader_);
 
-    data_source_->Initialize(url, callback);
+    data_source_->Initialize(gurl_, callback);
     MessageLoop::current()->RunAllPending();
   }
 
@@ -197,7 +197,7 @@ TEST_F(SimpleDataSourceTest, InitializeData) {
   EXPECT_CALL(host_, SetTotalBytes(sizeof(kDataUrlDecoded)));
   EXPECT_CALL(host_, SetBufferedBytes(sizeof(kDataUrlDecoded)));
 
-  data_source_->Initialize(kDataUrl,
+  data_source_->Initialize(GURL(kDataUrl),
       media::NewExpectedStatusCB(media::PIPELINE_OK));
   MessageLoop::current()->RunAllPending();
 

media/base/async_filter_factory_base.cc
@@ -1,113 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "media/base/async_filter_factory_base.h"
-
-#include "base/bind.h"
-#include "base/logging.h"
-#include "base/stl_util.h"
-
-namespace media {
-
-AsyncDataSourceFactoryBase::AsyncDataSourceFactoryBase() {}
-
-AsyncDataSourceFactoryBase::~AsyncDataSourceFactoryBase() {
-  base::AutoLock auto_lock(lock_);
-  STLDeleteElements(&outstanding_requests_);
-}
-
-void AsyncDataSourceFactoryBase::Build(const std::string& url,
-                                       const BuildCallback& callback) {
-  DCHECK(!callback.is_null());
-  BuildRequest* request = NULL;
-  {
-    base::AutoLock auto_lock(lock_);
-
-    if (url.empty()) {
-      ReportError(PIPELINE_ERROR_URL_NOT_FOUND, callback);
-      return;
-    }
-
-    if (!AllowRequests()) {
-      ReportError(DATASOURCE_ERROR_URL_NOT_SUPPORTED, callback);
-      return;
-    }
-
-    request = CreateRequest(url, callback);
-
-    if (!request) {
-      ReportError(DATASOURCE_ERROR_URL_NOT_SUPPORTED, callback);
-      return;
-    }
-
-    outstanding_requests_.insert(request);
-  }
-
-  request->Start(base::Bind(
-      &AsyncDataSourceFactoryBase::BuildRequestDone, base::Unretained(this)));
-}
-
-void AsyncDataSourceFactoryBase::ReportError(
-    PipelineStatus error,
-    const BuildCallback& callback) const {
-  DCHECK_NE(error, PIPELINE_OK);
-  DCHECK(!callback.is_null());
-
-  callback.Run(error, NULL);
-}
-
-void AsyncDataSourceFactoryBase::BuildRequestDone(BuildRequest* request) {
-  base::AutoLock auto_lock(lock_);
-  outstanding_requests_.erase(request);
-  delete request;
-}
-
-AsyncDataSourceFactoryBase::BuildRequest::BuildRequest(
-    const std::string& url, const BuildCallback& callback)
-    : url_(url),
-      callback_(callback) {
-}
-
-AsyncDataSourceFactoryBase::BuildRequest::~BuildRequest() {}
-
-void AsyncDataSourceFactoryBase::BuildRequest::Start(
-    const RequestDoneCallback& done_callback) {
-  DCHECK(!done_callback.is_null());
-  DCHECK(done_callback_.is_null());
-
-  done_callback_ = done_callback;
-  DoStart();
-  // Don't do anything after this line since the object could
-  // have been deleted at this point if the request was completed
-  // inside the call.
-}
-
-void AsyncDataSourceFactoryBase::BuildRequest::RequestComplete(
-    PipelineStatus status, DataSource* data_source) {
-  DCHECK(!callback_.is_null());
-  DCHECK(!done_callback_.is_null());
-
-  // Transfer ownership to local variables just in case the
-  // request object gets deleted by one of the callbacks.
-  RequestDoneCallback done_callback;
-  std::swap(done_callback, done_callback_);
-  BuildCallback callback;
-  std::swap(callback, callback_);
-
-  // Notify factory that this request has completed. We do this before
-  // calling |callback| so the factory doesn't consider this request
-  // pending if |callback| happens to destroy the factory.
-  //
-  // NOTE: This BuildRequest object is destroyed inside this callback so
-  //       no modifications should be made to this object after this call.
-  done_callback.Run(this);
-
-  callback.Run(status, data_source);
-}
-
-const std::string& AsyncDataSourceFactoryBase::BuildRequest::url() const {
-  return url_;
-}
-
-}  // namespace media

media/base/async_filter_factory_base.h
@@ -1,121 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#ifndef MEDIA_BASE_ASYNC_FILTER_FACTORY_BASE_H_
-#define MEDIA_BASE_ASYNC_FILTER_FACTORY_BASE_H_
-
-#include <set>
-
-#include "base/memory/scoped_ptr.h"
-#include "base/synchronization/lock.h"
-#include "media/base/filter_factories.h"
-
-namespace media {
-
-// This is a helper base class for DataSourceFactory implementation that
-// actually require asynchronous operations to build a data source. It
-// provides a common framework for dealing with an asychronous build.
-// Factories are expected to derive from this object and implement
-// AllowRequests(), CreateRequest(), and an custom implementation that derives
-// from BuildRequest.
-//
-// AllowRequests() just checks to see if the factory is in a state where it can
-// accept Build() requests. If it returns false, this base class will signal an
-// error through the BuildCallback and not call any of the other code. If
-// AllowRequests() returns true then this class will continue with the build
-// process by calling CreateRequest().
-//
-// CreateRequest() allows the derived implementation to create an instance of
-// their custom BuildRequest implementation that is specific to its asynchronous
-// build process. The custom BuildRequest should contain all of the state that
-// needs to be maintained for a particular build request. This state could
-// consist of partially initialized objects that require asynchronous operations
-// to complete before the build completes. This implementation MUST derive from
-// AsyncDataSourceFactoryBase::BuildRequest.
-//
-// Once CreateRequest() returns a BuildRequest implementation, this class adds
-// the object to its request list and then calls Start() on the BuildRequest
-// instance. BuildRequest::Start() manages storing the |done_callback| passed to
-// it and then call DoStart() on the derived object. DoStart() is where this
-// framework expects any neccesary asynchronous operations to be initiated.
-//
-// Once all asynchronous operations are completed and a fully initialized
-// DataSource object has been created, the BuildRequest instance should call
-// the RequestComplete() method. This call signals the end of the request and
-// the BuildRequest should be in a state where it can be deleted from inside
-// this call. If an error occurs during the build process, RequestComplete()
-// can also be called to signal the error.
-class MEDIA_EXPORT AsyncDataSourceFactoryBase : public DataSourceFactory {
- public:
-  AsyncDataSourceFactoryBase();
-  virtual ~AsyncDataSourceFactoryBase();
-
-  // DataSourceFactory method.
-  // Derived classes should not overload this Build() method. AllowRequests() &
-  // CreateRequest() should be implemented instead.
-  virtual void Build(const std::string& url,
-                     const BuildCallback& callback) OVERRIDE;
-
- protected:
-  class MEDIA_EXPORT BuildRequest {
-   public:
-    BuildRequest(const std::string& url, const BuildCallback& callback);
-    virtual ~BuildRequest();
-
-    typedef base::Callback<void(BuildRequest*)> RequestDoneCallback;
-    // Starts the build request.
-    void Start(const RequestDoneCallback& done_callback);
-
-    // Derived objects call this method to indicate that the build request
-    // has completed. If the build was successful |status| should be set to
-    // PIPELINE_OK and |data_source| should contain the DataSource object
-    // that was built by this request. Ownership of |data_source| is being
-    // passed in this call. If an error occurs during the build process, this
-    // method should be called with |status| set to an appropriate status code
-    // and |data_source| set to NULL.
-    //
-    // The derived object should be in a state where it can be deleted from
-    // within this call. This class as well AsyncDataSourceFactoryBase use this
-    // method to cleanup state associated with this request.
-    void RequestComplete(media::PipelineStatus status, DataSource* data_source);
-
-   protected:
-    // Implemented by the derived object to start the build. Called by Start().
-    virtual void DoStart() = 0;
-
-    // Gets the requested URL.
-    const std::string& url() const;
-
-   private:
-    std::string url_;
-    BuildCallback callback_;
-    RequestDoneCallback done_callback_;
-
-    DISALLOW_COPY_AND_ASSIGN(BuildRequest);
-  };
-
-  // Implemented by derived class. Called by Build() to check if the
-  // factory is in a state where it can accept requests.
-  virtual bool AllowRequests() const = 0;
-
-  // Implemented by derived class. Called by Build() to allow derived objects
-  // to create their own custom BuildRequest implementations.
-  virtual BuildRequest* CreateRequest(const std::string& url,
-                                      const BuildCallback& callback) = 0;
-
- private:
-  void ReportError(PipelineStatus error, const BuildCallback& callback) const;
-
-  void BuildRequestDone(BuildRequest* request);
-
-  base::Lock lock_;
-  typedef std::set<BuildRequest*> RequestSet;
-  RequestSet outstanding_requests_;
-
-  DISALLOW_COPY_AND_ASSIGN(AsyncDataSourceFactoryBase);
-};
-
-}  // namespace media
-
-#endif  // MEDIA_BASE_ASYNC_FILTER_FACTORY_BASE_H_

media/base/composite_data_source_factory.cc
@@ -1,98 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "media/base/composite_data_source_factory.h"
-
-#include "base/bind.h"
-#include "base/logging.h"
-#include "base/memory/scoped_ptr.h"
-#include "base/stl_util.h"
-
-namespace media {
-
-class CompositeDataSourceFactory::BuildRequest
-    : public AsyncDataSourceFactoryBase::BuildRequest {
- public:
-  BuildRequest(const std::string& url, const BuildCallback& callback,
-               const FactoryList& factories);
-  ~BuildRequest();
-
- protected:
-  // AsyncDataSourceFactoryBase::BuildRequest method.
-  virtual void DoStart();
-
- private:
-  void CallNextFactory();
-  void OnBuildDone(PipelineStatus status, DataSource* data_source);
-
-  FactoryList factories_;  // Not owned by this class.
-};
-
-CompositeDataSourceFactory::CompositeDataSourceFactory() {}
-
-CompositeDataSourceFactory::~CompositeDataSourceFactory() {
-  STLDeleteElements(&factories_);
-}
-
-void CompositeDataSourceFactory::AddFactory(
-    scoped_ptr<DataSourceFactory> factory) {
-  DCHECK(factory.get());
-  factories_.push_back(factory.release());
-}
-
-bool CompositeDataSourceFactory::AllowRequests() const {
-  return !factories_.empty();
-}
-
-AsyncDataSourceFactoryBase::BuildRequest*
-CompositeDataSourceFactory::CreateRequest(const std::string& url,
-                                          const BuildCallback& callback) {
-  return new BuildRequest(url, callback, factories_);
-}
-
-CompositeDataSourceFactory::BuildRequest::BuildRequest(
-    const std::string& url,
-    const BuildCallback& callback,
-    const FactoryList& factories)
-    : AsyncDataSourceFactoryBase::BuildRequest(url, callback),
-      factories_(factories){
-  DCHECK(!factories.empty());
-}
-
-CompositeDataSourceFactory::BuildRequest::~BuildRequest() {}
-
-void CompositeDataSourceFactory::BuildRequest::DoStart() {
-  CallNextFactory();
-}
-
-void CompositeDataSourceFactory::BuildRequest::CallNextFactory() {
-  DCHECK(!factories_.empty());
-
-  DataSourceFactory* factory = factories_.front();
-  factories_.pop_front();
-
-  factory->Build(url(), base::Bind(&BuildRequest::OnBuildDone,
-                                   base::Unretained(this)));
-}
-
-void CompositeDataSourceFactory::BuildRequest::OnBuildDone(
-    PipelineStatus status,
-    DataSource* data_source) {
-
-  if (status == PIPELINE_OK) {
-    DCHECK(data_source);
-    RequestComplete(status, data_source);
-    return;
-  }
-
-  DCHECK(!data_source);
-  if ((status == DATASOURCE_ERROR_URL_NOT_SUPPORTED) && !factories_.empty()) {
-    CallNextFactory();
-    return;
-  }
-
-  RequestComplete(status, data_source);
-}
-
-}  // namespace media

media/base/composite_data_source_factory.h
@@ -1,42 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#ifndef MEDIA_BASE_COMPOSITE_DATA_SOURCE_FACTORY_H_
-#define MEDIA_BASE_COMPOSITE_DATA_SOURCE_FACTORY_H_
-
-#include <list>
-#include <set>
-
-#include "base/synchronization/lock.h"
-#include "media/base/async_filter_factory_base.h"
-
-namespace media {
-
-class MEDIA_EXPORT CompositeDataSourceFactory
-    : public AsyncDataSourceFactoryBase {
- public:
-  CompositeDataSourceFactory();
-  virtual ~CompositeDataSourceFactory();
-
-  // Add factory to this composite.
-  void AddFactory(scoped_ptr<DataSourceFactory> factory);
-
- protected:
-  // AsyncDataSourceFactoryBase methods.
-  virtual bool AllowRequests() const OVERRIDE;
-  virtual AsyncDataSourceFactoryBase::BuildRequest* CreateRequest(
-      const std::string& url, const BuildCallback& callback) OVERRIDE;
-
- private:
-  class BuildRequest;
-
-  typedef std::list<DataSourceFactory*> FactoryList;
-  FactoryList factories_;
-
-  DISALLOW_COPY_AND_ASSIGN(CompositeDataSourceFactory);
-};
-
-}  // namespace media
-
-#endif  // MEDIA_BASE_COMPOSITE_DATA_SOURCE_FACTORY_H_

media/base/demuxer_factory.cc
@@ -1,12 +1,10 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/base/filter_factories.h"
+#include "media/base/demuxer_factory.h"
 
 namespace media {
-
-DataSourceFactory::~DataSourceFactory() {}
 
 DemuxerFactory::~DemuxerFactory() {}
 

commit 14c1b582e5ff7091f6a8d95c5578bea346c21dd9

Fix re-entrancy bug in SimpleDataSource::Abort() introduced in r118338.
BUG=112180
TEST=none

Review URL: https://chromiumcodereview.appspot.com/9344003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120714 0039d316-1c4b-4281-b951-d872f2087c98

webkit/media/simple_data_source.cc
@@ -270,13 +270,19 @@ void SimpleDataSource::Abort() {
   base::AutoLock auto_lock(lock_);
   state_ = STOPPED;
   initialize_cb_.Reset();
-  CancelTask();
+  CancelTask_Locked();
   frame_ = NULL;
 }
 
 void SimpleDataSource::CancelTask() {
   DCHECK(MessageLoop::current() == render_loop_);
   base::AutoLock auto_lock(lock_);
+  CancelTask_Locked();
+}
+
+void SimpleDataSource::CancelTask_Locked() {
+  DCHECK(MessageLoop::current() == render_loop_);
+  lock_.AssertAcquired();
   DCHECK_EQ(state_, STOPPED);
 
   // Cancel any pending requests.

webkit/media/simple_data_source.h
@@ -93,6 +93,7 @@ class SimpleDataSource
  private:
   // Cancels and deletes the resource loading on the render thread.
   void CancelTask();
+  void CancelTask_Locked();
 
   // Perform initialization completion tasks under a lock.
   void DoneInitialization_Locked(bool success);

webkit/media/simple_data_source_unittest.cc
@@ -127,10 +127,15 @@ class SimpleDataSourceTest : public testing::Test {
     MessageLoop::current()->RunAllPending();
   }
 
-  void DestroyDataSource() {
+  void StopAndDestroyDataSource() {
     data_source_->Stop(media::NewExpectedClosure());
     MessageLoop::current()->RunAllPending();
+    data_source_ = NULL;
+  }
 
+  void AbortAndDestroyDataSource() {
+    data_source_->Abort();
+    MessageLoop::current()->RunAllPending();
     data_source_ = NULL;
   }
 
@@ -168,21 +173,21 @@ TEST_F(SimpleDataSourceTest, InitializeHTTP) {
   InitializeDataSource(kHttpUrl,
                        media::NewExpectedStatusCB(media::PIPELINE_OK));
   RequestSucceeded();
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 }
 
 TEST_F(SimpleDataSourceTest, InitializeHTTPS) {
   InitializeDataSource(kHttpsUrl,
                        media::NewExpectedStatusCB(media::PIPELINE_OK));
   RequestSucceeded();
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 }
 
 TEST_F(SimpleDataSourceTest, InitializeFile) {
   InitializeDataSource(kFileUrl,
                        media::NewExpectedStatusCB(media::PIPELINE_OK));
   RequestSucceeded();
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 }
 
 TEST_F(SimpleDataSourceTest, InitializeData) {
@@ -201,14 +206,14 @@ TEST_F(SimpleDataSourceTest, InitializeData) {
       media::NewExpectedStatusCB(media::PIPELINE_OK));
   MessageLoop::current()->RunAllPending();
 
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 }
 
 TEST_F(SimpleDataSourceTest, RequestFailed) {
   InitializeDataSource(kHttpUrl,
       media::NewExpectedStatusCB(media::PIPELINE_ERROR_NETWORK));
   RequestFailed();
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 }
 
 static void OnStatusCB(bool* called, media::PipelineStatus status) {
@@ -222,7 +227,18 @@ TEST_F(SimpleDataSourceTest, StopWhenDownloading) {
   InitializeDataSource(kHttpUrl, base::Bind(&OnStatusCB, &was_called));
 
   EXPECT_CALL(*url_loader_, cancel());
-  DestroyDataSource();
+  StopAndDestroyDataSource();
+  EXPECT_FALSE(was_called);
+}
+
+TEST_F(SimpleDataSourceTest, AbortWhenDownloading) {
+  // The callback should be deleted, but not executed.
+  // TODO(scherkus): should this really be the behaviour?  Seems strange...
+  bool was_called = false;
+  InitializeDataSource(kHttpUrl, base::Bind(&OnStatusCB, &was_called));
+
+  EXPECT_CALL(*url_loader_, cancel());
+  AbortAndDestroyDataSource();
   EXPECT_FALSE(was_called);
 }
 
@@ -231,7 +247,7 @@ TEST_F(SimpleDataSourceTest, AsyncRead) {
                        media::NewExpectedStatusCB(media::PIPELINE_OK));
   RequestSucceeded();
   AsyncRead();
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 }
 
 // NOTE: This test will need to be reworked a little once
@@ -243,7 +259,7 @@ TEST_F(SimpleDataSourceTest, HasSingleOrigin) {
                        media::NewExpectedStatusCB(media::PIPELINE_OK));
   RequestSucceeded();
   EXPECT_TRUE(data_source_->HasSingleOrigin());
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 
   // Test redirect to the same domain.
   InitializeDataSource(kHttpUrl,
@@ -251,7 +267,7 @@ TEST_F(SimpleDataSourceTest, HasSingleOrigin) {
   Redirect(kHttpRedirectToSameDomainUrl1);
   RequestSucceeded();
   EXPECT_TRUE(data_source_->HasSingleOrigin());
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 
   // Test redirect twice to the same domain.
   InitializeDataSource(kHttpUrl,
@@ -260,7 +276,7 @@ TEST_F(SimpleDataSourceTest, HasSingleOrigin) {
   Redirect(kHttpRedirectToSameDomainUrl2);
   RequestSucceeded();
   EXPECT_TRUE(data_source_->HasSingleOrigin());
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 
   // Test redirect to a different domain.
   InitializeDataSource(kHttpUrl,
@@ -268,7 +284,7 @@ TEST_F(SimpleDataSourceTest, HasSingleOrigin) {
   Redirect(kHttpRedirectToDifferentDomainUrl1);
   RequestSucceeded();
   EXPECT_FALSE(data_source_->HasSingleOrigin());
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 
   // Test redirect to the same domain and then to a different domain.
   InitializeDataSource(kHttpUrl,
@@ -277,7 +293,7 @@ TEST_F(SimpleDataSourceTest, HasSingleOrigin) {
   Redirect(kHttpRedirectToDifferentDomainUrl1);
   RequestSucceeded();
   EXPECT_FALSE(data_source_->HasSingleOrigin());
-  DestroyDataSource();
+  StopAndDestroyDataSource();
 }
 
 }  // namespace webkit_media