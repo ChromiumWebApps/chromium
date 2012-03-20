ChromiumWebApps/chromium

commit 79b18961011b297d417e7da6f5e0413c05c7c734

Make the Views sad tab code not be tied down to TabContentsViewViews. This allows us to have the same code work with TabContentsViewWin.
BUG=98716
Review URL: https://chromiumcodereview.appspot.com/9554017

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125126 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/sad_tab_helper.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/sad_tab_observer.h"
+#include "chrome/browser/ui/sad_tab_helper.h"
 
 #include "chrome/browser/browser_shutdown.h"
 #include "content/public/browser/notification_source.h"
@@ -14,7 +14,7 @@
 #include "chrome/browser/ui/cocoa/tab_contents/sad_tab_controller.h"
 #elif defined(TOOLKIT_VIEWS)
 #include "chrome/browser/ui/views/sad_tab_view.h"
-#include "chrome/browser/ui/views/tab_contents/tab_contents_view_views.h"
+#include "ui/views/widget/widget.h"
 #elif defined(TOOLKIT_GTK)
 
 #include <gtk/gtk.h>
@@ -25,16 +25,16 @@
 
 using content::WebContents;
 
-SadTabObserver::SadTabObserver(WebContents* web_contents)
+SadTabHelper::SadTabHelper(WebContents* web_contents)
     : content::WebContentsObserver(web_contents) {
   registrar_.Add(this, content::NOTIFICATION_WEB_CONTENTS_CONNECTED,
                  content::Source<WebContents>(web_contents));
 }
 
-SadTabObserver::~SadTabObserver() {
+SadTabHelper::~SadTabHelper() {
 }
 
-void SadTabObserver::RenderViewGone(base::TerminationStatus status) {
+void SadTabHelper::RenderViewGone(base::TerminationStatus status) {
   // Only show the sad tab if we're not in browser shutdown, so that TabContents
   // objects that are not in a browser (e.g., HTML dialogs) and thus are
   // visible do not flash a sad tab page.
@@ -47,17 +47,16 @@ void SadTabObserver::RenderViewGone(base::TerminationStatus status) {
   InstallSadTab(status);
 }
 
-void SadTabObserver::Observe(int type,
-                             const content::NotificationSource& source,
-                             const content::NotificationDetails& details) {
+void SadTabHelper::Observe(int type,
+                           const content::NotificationSource& source,
+                           const content::NotificationDetails& details) {
   switch (type) {
     case content::NOTIFICATION_WEB_CONTENTS_CONNECTED:
       if (HasSadTab()) {
 #if defined(OS_MACOSX)
         sad_tab_controller_mac::RemoveSadTab(sad_tab_.get());
 #elif defined(TOOLKIT_VIEWS)
-        static_cast<TabContentsViewViews*>(web_contents()->GetView())->
-            RemoveOverlayView();
+        sad_tab_->Close();
 #elif defined(TOOLKIT_GTK)
         content::TabContentsViewGtk* view =
             static_cast<content::TabContentsViewGtk*>(
@@ -76,7 +75,7 @@ void SadTabObserver::Observe(int type,
   }
 }
 
-void SadTabObserver::InstallSadTab(base::TerminationStatus status) {
+void SadTabHelper::InstallSadTab(base::TerminationStatus status) {
 #if defined(OS_MACOSX)
   sad_tab_.reset(
       sad_tab_controller_mac::CreateSadTabController(web_contents()));
@@ -89,16 +88,19 @@ void SadTabObserver::InstallSadTab(base::TerminationStatus status) {
   // It is not possible to create a native_widget_win that has no parent in
   // and later re-parent it.
   // TODO(avi): This is a cheat. Can this be made cleaner?
-  sad_tab_params.parent_widget =
-      static_cast<TabContentsViewViews*>(web_contents()->GetView());
+  sad_tab_params.parent_widget = views::Widget::GetWidgetForNativeView(
+      web_contents()->GetView()->GetNativeView());
   sad_tab_params.ownership =
       views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   sad_tab_.reset(new views::Widget);
   sad_tab_->Init(sad_tab_params);
   sad_tab_->SetContentsView(new SadTabView(web_contents(), kind));
-  TabContentsViewViews* view = static_cast<TabContentsViewViews*>(
-      web_contents()->GetView());
-  view->InstallOverlayView(sad_tab_->GetNativeView());
+
+  views::Widget::ReparentNativeView(
+      sad_tab_->GetNativeView(), web_contents()->GetView()->GetNativeView());
+  gfx::Rect bounds;
+  web_contents()->GetView()->GetContainerBounds(&bounds);
+  sad_tab_->SetBounds(gfx::Rect(bounds.size()));
 #elif defined(TOOLKIT_GTK)
   sad_tab_.reset(new SadTabGtk(
       web_contents(),
@@ -115,6 +117,6 @@ void SadTabObserver::InstallSadTab(base::TerminationStatus status) {
 #endif
 }
 
-bool SadTabObserver::HasSadTab() {
+bool SadTabHelper::HasSadTab() {
   return sad_tab_.get() != NULL;
 }

chrome/browser/ui/sad_tab_helper.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_SAD_TAB_OBSERVER_H_
-#define CHROME_BROWSER_UI_SAD_TAB_OBSERVER_H_
+#ifndef CHROME_BROWSER_UI_SAD_TAB_HELPER_H_
+#define CHROME_BROWSER_UI_SAD_TAB_HELPER_H_
 #pragma once
 
 #include "base/memory/scoped_ptr.h"
@@ -27,18 +27,22 @@ class SadTabGtk;
 #endif
 
 // Per-tab class to manage sad tab views.
-class SadTabObserver : public content::WebContentsObserver,
-                       public content::NotificationObserver {
+class SadTabHelper : public content::WebContentsObserver,
+                     public content::NotificationObserver {
  public:
-  explicit SadTabObserver(content::WebContents* web_contents);
-  virtual ~SadTabObserver();
+  explicit SadTabHelper(content::WebContents* web_contents);
+  virtual ~SadTabHelper();
+
+  // Platform specific function to determine if there is a current sad tab page.
+  bool HasSadTab();
+
+#if defined(TOOLKIT_VIEWS)
+  views::Widget* sad_tab() { return sad_tab_.get(); }
+#endif
 
  private:
   // Platform specific function to get an instance of the sad tab page.
   void InstallSadTab(base::TerminationStatus status);
-
-  // Platform specific function to determine if there is a current sad tab page.
-  bool HasSadTab();
 
   // Overridden from content::WebContentsObserver:
   virtual void RenderViewGone(base::TerminationStatus status) OVERRIDE;
@@ -66,7 +70,7 @@ class SadTabObserver : public content::WebContentsObserver,
   scoped_ptr<SadTabGtk> sad_tab_;
 #endif
 
-  DISALLOW_COPY_AND_ASSIGN(SadTabObserver);
+  DISALLOW_COPY_AND_ASSIGN(SadTabHelper);
 };
 
-#endif  // CHROME_BROWSER_UI_SAD_TAB_OBSERVER_H_
+#endif  // CHROME_BROWSER_UI_SAD_TAB_HELPER_H_

chrome/browser/ui/tab_contents/tab_contents_wrapper.cc
@@ -38,7 +38,7 @@
 #include "chrome/browser/ui/intents/web_intent_picker_controller.h"
 #include "chrome/browser/ui/pdf/pdf_tab_observer.h"
 #include "chrome/browser/ui/prefs/prefs_tab_helper.h"
-#include "chrome/browser/ui/sad_tab_observer.h"
+#include "chrome/browser/ui/sad_tab_helper.h"
 #include "chrome/browser/ui/search_engines/search_engine_tab_helper.h"
 #include "chrome/browser/ui/snapshot_tab_helper.h"
 #include "chrome/browser/ui/sync/one_click_signin_helper.h"
@@ -97,6 +97,7 @@ TabContentsWrapper::TabContentsWrapper(WebContents* contents)
   prefs_tab_helper_.reset(new PrefsTabHelper(contents));
   prerender_tab_helper_.reset(new prerender::PrerenderTabHelper(this));
   restore_tab_helper_.reset(new RestoreTabHelper(contents));
+  sad_tab_helper_.reset(new SadTabHelper(contents));
   search_engine_tab_helper_.reset(new SearchEngineTabHelper(contents));
   snapshot_tab_helper_.reset(new SnapshotTabHelper(contents));
   ssl_helper_.reset(new TabContentsSSLHelper(this));
@@ -121,7 +122,6 @@ TabContentsWrapper::TabContentsWrapper(WebContents* contents)
     omnibox_search_hint_.reset(new OmniboxSearchHint(this));
   pdf_tab_observer_.reset(new PDFTabObserver(this));
   plugin_observer_.reset(new PluginObserver(this));
-  sad_tab_observer_.reset(new SadTabObserver(contents));
   safe_browsing_tab_observer_.reset(
       new safe_browsing::SafeBrowsingTabObserver(this));
 

chrome/browser/ui/tab_contents/tab_contents_wrapper.h
@@ -42,7 +42,7 @@ class PrefService;
 class PrefsTabHelper;
 class Profile;
 class RestoreTabHelper;
-class SadTabObserver;
+class SadTabHelper;
 class SearchEngineTabHelper;
 class SnapshotTabHelper;
 class TabContentsSSLHelper;
@@ -172,6 +172,8 @@ class TabContentsWrapper : public content::WebContentsObserver {
     return restore_tab_helper_.get();
   }
 
+  SadTabHelper* sad_tab_helper() { return sad_tab_helper_.get(); }
+
   SearchEngineTabHelper* search_engine_tab_helper() {
     return search_engine_tab_helper_.get();
   }
@@ -240,7 +242,7 @@ class TabContentsWrapper : public content::WebContentsObserver {
   scoped_ptr<printing::PrintViewManager> print_view_manager_;
 
   scoped_ptr<RestoreTabHelper> restore_tab_helper_;
-
+  scoped_ptr<SadTabHelper> sad_tab_helper_;
   scoped_ptr<SearchEngineTabHelper> search_engine_tab_helper_;
   scoped_ptr<SnapshotTabHelper> snapshot_tab_helper_;
   scoped_ptr<TabContentsSSLHelper> ssl_helper_;
@@ -270,7 +272,6 @@ class TabContentsWrapper : public content::WebContentsObserver {
   scoped_ptr<PDFTabObserver> pdf_tab_observer_;
   scoped_ptr<PluginObserver> plugin_observer_;
   scoped_ptr<printing::PrintPreviewMessageHandler> print_preview_;
-  scoped_ptr<SadTabObserver> sad_tab_observer_;
   scoped_ptr<safe_browsing::SafeBrowsingTabObserver>
       safe_browsing_tab_observer_;
   scoped_ptr<ThumbnailGenerator> thumbnail_generation_observer_;

chrome/browser/ui/views/tab_contents/native_tab_contents_view_aura.cc
@@ -6,6 +6,8 @@
 
 #include "base/event_types.h"
 #include "base/message_loop.h"
+#include "chrome/browser/ui/sad_tab_helper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "chrome/browser/ui/views/tab_contents/native_tab_contents_view_delegate.h"
 #include "content/browser/renderer_host/render_view_host.h"
 #include "content/public/browser/render_widget_host_view.h"
@@ -270,7 +272,9 @@ void NativeTabContentsViewAura::OnBoundsChanged(const gfx::Rect& old_bounds,
 }
 
 bool NativeTabContentsViewAura::OnMouseEvent(aura::MouseEvent* event) {
-  if (!delegate_->IsShowingSadTab()) {
+  TabContentsWrapper* wrapper =
+      TabContentsWrapper::GetCurrentWrapperForContents(GetWebContents());
+  if (!wrapper || !wrapper->sad_tab_helper()->HasSadTab()) {
     switch (event->type()) {
       case ui::ET_MOUSE_EXITED:
         delegate_->OnNativeTabContentsViewMouseMove(false);

chrome/browser/ui/views/tab_contents/native_tab_contents_view_delegate.h
@@ -27,10 +27,6 @@ class NativeTabContentsViewDelegate {
   virtual content::WebContents* GetWebContents() = 0;
 
   // TODO(beng):
-  // This can die with OnNativeTabContentsViewMouseDown/Move().
-  virtual bool IsShowingSadTab() const = 0;
-
-  // TODO(beng):
   // These three can be replaced by Widget::OnSizeChanged and some new
   // notifications for show/hide.
   virtual void OnNativeTabContentsViewShown() = 0;

chrome/browser/ui/views/tab_contents/native_tab_contents_view_gtk.cc
@@ -7,6 +7,8 @@
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/tab_contents/web_drag_bookmark_handler_gtk.h"
 #include "chrome/browser/ui/gtk/constrained_window_gtk.h"
+#include "chrome/browser/ui/sad_tab_helper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "chrome/browser/ui/views/tab_contents/native_tab_contents_view_delegate.h"
 #include "content/browser/tab_contents/web_drag_dest_gtk.h"
 #include "content/browser/tab_contents/web_drag_source_gtk.h"
@@ -206,7 +208,9 @@ views::NativeWidget* NativeTabContentsViewGtk::AsNativeWidget() {
 // not NULL, else our delegate.
 gboolean NativeTabContentsViewGtk::OnMotionNotify(GtkWidget* widget,
                                                   GdkEventMotion* event) {
-  if (delegate_->IsShowingSadTab())
+  TabContentsWrapper* wrapper =
+      TabContentsWrapper::GetCurrentWrapperForContents(GetWebContents());
+  if (wrapper && wrapper->sad_tab_helper()->HasSadTab())
     return views::NativeWidgetGtk::OnMotionNotify(widget, event);
 
   delegate_->OnNativeTabContentsViewMouseMove(true);
@@ -215,7 +219,9 @@ gboolean NativeTabContentsViewGtk::OnMotionNotify(GtkWidget* widget,
 
 gboolean NativeTabContentsViewGtk::OnLeaveNotify(GtkWidget* widget,
                                                  GdkEventCrossing* event) {
-  if (delegate_->IsShowingSadTab())
+  TabContentsWrapper* wrapper =
+      TabContentsWrapper::GetCurrentWrapperForContents(GetWebContents());
+  if (wrapper && wrapper->sad_tab_helper()->HasSadTab())
     return views::NativeWidgetGtk::OnLeaveNotify(widget, event);
 
   delegate_->OnNativeTabContentsViewMouseMove(false);
@@ -224,7 +230,9 @@ gboolean NativeTabContentsViewGtk::OnLeaveNotify(GtkWidget* widget,
 
 gboolean NativeTabContentsViewGtk::OnButtonPress(GtkWidget* widget,
                                                  GdkEventButton* event) {
-  if (delegate_->IsShowingSadTab())
+  TabContentsWrapper* wrapper =
+      TabContentsWrapper::GetCurrentWrapperForContents(GetWebContents());
+  if (wrapper && wrapper->sad_tab_helper()->HasSadTab())
     return views::NativeWidgetGtk::OnButtonPress(widget, event);
   last_mouse_down_ = *event;
   return views::NativeWidgetGtk::OnButtonPress(widget, event);

chrome/browser/ui/views/tab_contents/native_tab_contents_view_win.cc
@@ -5,7 +5,9 @@
 #include "chrome/browser/ui/views/tab_contents/native_tab_contents_view_win.h"
 
 #include "base/bind.h"
+#include "chrome/browser/ui/sad_tab_helper.h"
 #include "chrome/browser/tab_contents/web_drag_bookmark_handler_win.h"
+#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "chrome/browser/ui/views/tab_contents/native_tab_contents_view_delegate.h"
 #include "content/browser/tab_contents/web_contents_drag_win.h"
 #include "content/browser/tab_contents/web_drag_dest_win.h"
@@ -183,7 +185,9 @@ void NativeTabContentsViewWin::OnHScroll(int scroll_type,
 LRESULT NativeTabContentsViewWin::OnMouseRange(UINT msg,
                                                WPARAM w_param,
                                                LPARAM l_param) {
-  if (delegate_->IsShowingSadTab())
+  TabContentsWrapper* wrapper =
+      TabContentsWrapper::GetCurrentWrapperForContents(GetWebContents());
+  if (wrapper && wrapper->sad_tab_helper()->HasSadTab())
     return NativeWidgetWin::OnMouseRange(msg, w_param, l_param);
 
   switch (msg) {

commit 020add4c982db4cdd04a5b0ea00f15bc6aea7c55

Fix crash in TabContentsViewViews::Focus. This was caused by r125126. The subtle bug is that the SadTabHelper's Observe function use to reset TabContentsViewView's Widget pointer before it deleted the sad tab Widget. With my change, TabContentsViewWin would call SadTabHelper in the middle of its sad_tab_.reset() call and would use a Widget that's being destructed.
BUG=117668
Review URL: https://chromiumcodereview.appspot.com/9733023

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127686 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/sad_tab_helper.cc
@@ -57,6 +57,10 @@ void SadTabHelper::Observe(int type,
         sad_tab_controller_mac::RemoveSadTab(sad_tab_.get());
 #elif defined(TOOLKIT_VIEWS)
         sad_tab_->Close();
+        // See http://crbug.com/117668. When the Widget is being destructed, we
+        // want calls to sad_tab() to return NULL.
+        scoped_ptr<views::Widget> local_sad_tab;
+        local_sad_tab.swap(sad_tab_);
 #elif defined(TOOLKIT_GTK)
         GtkWidget* expanded_container =
             ChromeWebContentsViewDelegateGtk::GetFor(web_contents())->