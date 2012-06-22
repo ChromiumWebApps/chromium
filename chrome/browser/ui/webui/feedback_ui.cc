ChromiumWebApps/chromium

commit 4eb4ec079921e968d446fa8086aeb862240da313

Remove BrowserList::GetLastActive usage from WebUI pages. I've plumbed the TabContents to the page bubble code so that the webui certificate viewer can get the Browser from it.
BUG=129187
Review URL: https://chromiumcodereview.appspot.com/10554010

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142963 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/webui/feedback_ui.cc
@@ -21,7 +21,7 @@
 #include "chrome/browser/feedback/feedback_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
@@ -358,9 +358,8 @@ bool FeedbackHandler::Init() {
   }
 #endif
 
-  // TODO(beng): Replace GetLastActive with a more specific method of locating
-  //             the target contents.
-  Browser* browser = BrowserList::GetLastActive();
+  Browser* browser =
+      browser::FindBrowserWithWebContents(web_ui()->GetWebContents());
   // Sanity checks.
   if (((index == 0) && (index_str != "0")) || !browser ||
       index >= browser->tab_count()) {

chrome/browser/certificate_viewer.cc
@@ -7,7 +7,9 @@
 #include "content/public/browser/cert_store.h"
 #include "net/base/x509_certificate.h"
 
-void ShowCertificateViewerByID(gfx::NativeWindow parent, int cert_id) {
+void ShowCertificateViewerByID(content::WebContents* web_contents,
+                               gfx::NativeWindow parent,
+                               int cert_id) {
   scoped_refptr<net::X509Certificate> cert;
   content::CertStore::GetInstance()->RetrieveCert(cert_id, &cert);
   if (!cert.get()) {
@@ -15,5 +17,5 @@ void ShowCertificateViewerByID(gfx::NativeWindow parent, int cert_id) {
     // we displayed the page info.
     return;
   }
-  ShowCertificateViewer(parent, cert);
+  ShowCertificateViewer(web_contents, parent, cert);
 }

chrome/browser/certificate_viewer.h
@@ -8,6 +8,10 @@
 
 #include "ui/gfx/native_widget_types.h"
 
+namespace content {
+class WebContents;
+}
+
 namespace net {
 
 class X509Certificate;
@@ -16,10 +20,13 @@ class X509Certificate;
 
 // Opens a certificate viewer under |parent| to display the certificate from
 // the |CertStore| with id |cert_id|.
-void ShowCertificateViewerByID(gfx::NativeWindow parent, int cert_id);
+void ShowCertificateViewerByID(content::WebContents* web_contents,
+                               gfx::NativeWindow parent,
+                               int cert_id);
 
 // Opens a certificate viewer under |parent| to display |cert|.
-void ShowCertificateViewer(gfx::NativeWindow parent,
+void ShowCertificateViewer(content::WebContents* web_contents,
+                           gfx::NativeWindow parent,
                            net::X509Certificate* cert);
 
 #endif  // CHROME_BROWSER_CERTIFICATE_VIEWER_H_

chrome/browser/chromeos/login/simple_web_view_dialog.cc
@@ -292,7 +292,7 @@ void SimpleWebViewDialog::ShowPageInfo(content::WebContents* web_contents,
   PageInfoBubbleView* page_info_bubble =
       new PageInfoBubbleView(
           location_bar_->location_icon_view(),
-          location_bar_->profile(),
+          web_contents,
           url, ssl,
           true,
           this);

chrome/browser/tab_contents/tab_contents_ssl_helper.cc
@@ -100,7 +100,9 @@ string16 SSLCertAddedInfoBarDelegate::GetButtonLabel(
 
 bool SSLCertAddedInfoBarDelegate::Accept() {
   ShowCertificateViewer(
-      owner()->web_contents()->GetView()->GetTopLevelNativeWindow(), cert_);
+      owner()->web_contents(),
+      owner()->web_contents()->GetView()->GetTopLevelNativeWindow(),
+      cert_);
   return false;  // Hiding the infobar just as the dialog opens looks weird.
 }
 

chrome/browser/ui/android/certificate_viewer_android.cc
@@ -7,7 +7,8 @@
 #include "base/logging.h"
 #include "net/base/x509_certificate.h"
 
-void ShowCertificateViewer(gfx::NativeWindow parent,
+void ShowCertificateViewer(content::WebContents* web_contents,
+                           gfx::NativeWindow parent,
                            net::X509Certificate* cert) {
   // TODO(yfriedman, bulach): Hook this up to Java ui code: crbug.com/114822
   NOTIMPLEMENTED();

chrome/browser/ui/browser.cc
@@ -1580,7 +1580,7 @@ void Browser::ShowPageInfo(content::WebContents* web_contents,
     window()->ShowWebsiteSettings(
         profile, tab_contents, url, ssl, show_history);
   } else {
-    window()->ShowPageInfo(profile, url, ssl, show_history);
+    window()->ShowPageInfo(web_contents, url, ssl, show_history);
   }
 }
 

chrome/browser/ui/browser_list.h
@@ -108,9 +108,7 @@ class BrowserList {
   // time by wiring context through to the relevant code rather than using
   // GetLastActive().
   friend class BrowserView;
-  friend class CertificateViewerDialog;
   friend class ChromeShellDelegate;
-  friend class FeedbackHandler;
   friend class NetworkProfileBubble;
   friend class PrintPreviewHandler;
   friend class SelectFileDialogExtension;

commit 0be84e57e5416f9e8b43289fc1dd3705cddbcb3b

Fix report feedback web ui. This regressed in r142963.
BUG=134049
Review URL: https://chromiumcodereview.appspot.com/10641014

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143602 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/webui/feedback_ui.cc
@@ -67,6 +67,8 @@ const char kCurrentScreenshotUrl[] = "chrome://screenshots/current";
 
 const char kCategoryTagParameter[] = "categoryTag=";
 const char kDescriptionParameter[] = "description=";
+const char kSessionIDParameter[] = "session_id=";
+const char kTabIndexParameter[] = "tab_index=";
 
 #if defined(OS_CHROMEOS)
 const char kSavedScreenshotsUrl[] = "chrome://screenshots/saved/";
@@ -166,9 +168,10 @@ void ShowWebFeedbackView(Browser* browser,
                                              snapshot_bounds);
   FeedbackUtil::SetScreenshotSize(success ? snapshot_bounds : gfx::Rect());
 
-  std::string feedback_url = std::string(chrome::kChromeUIFeedbackURL) +
-      "#" + base::IntToString(browser->active_index()) + "?" +
-      kDescriptionParameter +
+  std::string feedback_url = std::string(chrome::kChromeUIFeedbackURL) + "?" +
+      kSessionIDParameter + base::IntToString(browser->session_id().id()) +
+      "&" + kTabIndexParameter + base::IntToString(browser->active_index()) +
+      "&" + kDescriptionParameter +
       net::EscapeUrlEncodedData(description_template, false) + "&" +
       kCategoryTagParameter + net::EscapeUrlEncodedData(category_tag, false);
 
@@ -334,38 +337,50 @@ bool FeedbackHandler::Init() {
   ParseStandardURL(page_url.c_str(), page_url.length(), &parts);
 
   size_t params_start = page_url.find("?");
-  std::string index_str = page_url.substr(parts.ref.begin,
-                                          params_start - parts.ref.begin);
   std::string query = page_url.substr(params_start + 1);
 
-  int index = 0;
-  if (!base::StringToInt(index_str, &index))
-    return false;
+  int session_id = -1;
+  int index = -1;
 
-#if defined(OS_CHROMEOS)
   std::vector<std::string> params;
   if (Tokenize(query, std::string("&"), &params)) {
     for (std::vector<std::string>::iterator it = params.begin();
          it != params.end(); ++it) {
+      std::string query_str = *it;
+      if (StartsWithASCII(query_str, std::string(kSessionIDParameter), true)) {
+        ReplaceFirstSubstringAfterOffset(
+            &query_str, 0, kSessionIDParameter, "");
+        if (!base::StringToInt(query_str, &session_id))
+          return false;
+        continue;
+      }
+      if (StartsWithASCII(*it, std::string(kTabIndexParameter), true)) {
+        ReplaceFirstSubstringAfterOffset(
+            &query_str, 0, kTabIndexParameter, "");
+        if (!base::StringToInt(query_str, &index))
+          return false;
+        continue;
+      }
+#if defined(OS_CHROMEOS)
       if (StartsWithASCII(*it, std::string(kTimestampParameter), true)) {
         timestamp_ = *it;
         ReplaceFirstSubstringAfterOffset(&timestamp_,
                                          0,
                                          kTimestampParameter,
                                          "");
-        break;
+        continue;
       }
+#endif
     }
   }
-#endif
 
-  Browser* browser =
-      browser::FindBrowserWithWebContents(web_ui()->GetWebContents());
-  // Sanity checks.
-  if (((index == 0) && (index_str != "0")) || !browser ||
-      index >= browser->tab_count()) {
+  if (session_id == -1 || index == -1)
     return false;
-  }
+
+  Browser* browser = browser::FindBrowserWithID(session_id);
+  // Sanity checks.
+  if (!browser || index >= browser->tab_count())
+    return false;
 
   WebContents* target_tab = browser->GetWebContentsAt(index);
   if (target_tab) {

chrome/browser/ui/webui/bidi_checker_web_ui_test.cc
@@ -185,11 +185,11 @@ IN_PROC_BROWSER_TEST_F(WebUIBidiCheckerBrowserTestLTR, TestFeedbackPage) {
   // The bugreport page receives its contents as GET arguments. Here we provide
   // a custom, Hebrew typed, description message.
   RunBidiCheckerOnPage(
-      "chrome://feedback#0?description=%D7%91%D7%93%D7%99%D7%A7%D7%94");
+      "chrome://feedback?session_id=1&tab_index=0&description=%D7%91%D7%93%D7%99%D7%A7%D7%94");
 }
 
 IN_PROC_BROWSER_TEST_F(WebUIBidiCheckerBrowserTestRTL, TestFeedbackPage) {
-  RunBidiCheckerOnPage("chrome://feedback#0?description=test");
+  RunBidiCheckerOnPage("chrome://feedback?session_id=1&tab_index=0&description=test");
 }
 
 //==============================