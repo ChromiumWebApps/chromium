ChromiumWebApps/chromium

commit b170daf3f5ce4572e86d1fb716a6e68b27c1b550

Remove PPAPI tests from NaCl's SCons build that are not automated or are dead.
BUG= http://code.google.com/p/chromium/issues/detail?id=154400


Review URL: https://chromiumcodereview.appspot.com/11088017

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161425 0039d316-1c4b-4281-b951-d872f2087c98

ppapi/native_client/ppapi_scons_files.py
@@ -40,21 +40,8 @@ nonvariant_test_scons_files = [
     'tests/ppapi_browser/ppp_instance/nacl.scons',
     'tests/ppapi_browser/progress_events/nacl.scons',
     'tests/ppapi_browser/stress_many_nexes/nacl.scons',
-    'tests/ppapi_example_2d/nacl.scons',
-    'tests/ppapi_example_audio/nacl.scons',
-    'tests/ppapi_example_events/nacl.scons',
-    # TODO(dspringer): re-enable test once the 3D ABI has stabilized. See
-    # http://code.google.com/p/nativeclient/issues/detail?id=2060
-    # 'tests/ppapi_example_gles2/nacl.scons',
-    'tests/ppapi_example_post_message/nacl.scons',
     'tests/ppapi_geturl/nacl.scons',
-    'tests/ppapi_gles_book/nacl.scons',
     'tests/ppapi_messaging/nacl.scons',
-    # Broken by Chrome change
-    # http://code.google.com/p/nativeclient/issues/detail?id=2480
-    #'tests/ppapi_simple_tests/nacl.scons',
-    'tests/ppapi_test_example/nacl.scons',
     'tests/ppapi_test_lib/nacl.scons',
-    'tests/ppapi_tests/nacl.scons',
 ]
 

ppapi/native_client/tests/earth/nacl.scons
@@ -1,77 +1,0 @@
-# -*- python -*-
-# Copyright (c) 2012 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-Import('env')
-
-if (not env.Bit('bitcode')):
-  flags = ['-mfpmath=sse', '-msse2', '-O3', '-ffast-math',
-           '-fomit-frame-pointer']
-  env.Append(CCFLAGS=flags)
-  env.Append(CXXFLAGS=flags)
-
-earthlib = env.ComponentLibrary('earthlib', ['earth.cc'])
-
-# build, C then C++
-cobj=['pepper_c.c']
-c_nexe_name = env.ProgramNameForNmf('earth_c')
-cnexe = env.ComponentProgram(c_nexe_name, cobj,
-                            EXTRA_LIBS=['earthlib',
-                                        'ppapi',
-                                        'm', 'pthread'])
-env.Publish(c_nexe_name, 'run', ['earth_c.html'])
-
-node = env.DemoSelLdrNacl('demo_earth_c', cnexe, args=[])
-# Note: Make this available from top level
-Alias('demo_earth_c', node)
-
-ccobj=['pepper_cc.cc']
-cc_nexe_name = env.ProgramNameForNmf('earth_cc')
-ccnexe = env.ComponentProgram(cc_nexe_name, ccobj,
-                            EXTRA_LIBS=['earthlib',
-                                        'ppapi_cpp',
-                                        'm', 'pthread'])
-env.Publish(cc_nexe_name, 'run', ['earth_cc.html'])
-
-node = env.DemoSelLdrNacl('demo_earth_cc', ccnexe, args=[])
-# Note: Make this available from top level
-Alias('demo_earth_cc', node)
-
-
-# Validator tests, C then C++, but not for glibc
-# TODO(bradchen): enable these tests when ncval works with glibc DSOs
-if not env.Bit('nacl_glibc'):
-  node = env.CommandValidatorTestNacl(
-    'earth_test_val_c.out',
-    image=[cnexe],
-    )
-  env.AddNodeToTestSuite(node, ['validator_tests', 'small_tests'],
-                         'run_earth_c')
-  node = env.CommandValidatorTestNacl(
-    'earth_test_val_cc.out',
-    image=[ccnexe],
-    )
-  env.AddNodeToTestSuite(node, ['validator_tests', 'small_tests'],
-                         'run_earth_cc')
-
-
-# browser tests, C then C++
-
-node = env.PPAPIBrowserTester(
-    'earth_browser_test_c.out',
-    url='earth_c.html',
-    nmf_names=['earth_c'],
-    files=env.ExtractPublishedFiles(c_nexe_name),
-    is_broken=env.PPAPIBrowserTesterIsBroken())
-
-env.AddNodeToTestSuite(node, ['chrome_browser_tests'], 'earth_browser_test_c')
-
-node = env.PPAPIBrowserTester(
-    'earth_browser_test_cc.out',
-    url='earth_cc.html',
-    nmf_names=['earth_cc'],
-    files=env.ExtractPublishedFiles(cc_nexe_name),
-    is_broken=env.PPAPIBrowserTesterIsBroken())
-
-env.AddNodeToTestSuite(node, ['chrome_browser_tests'], 'earth_browser_test_cc')

ppapi/native_client/tests/ppapi_example_2d/nacl.scons
@@ -1,22 +1,0 @@
-# -*- python -*-
-# Copyright (c) 2012 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# This is a Pepper 2D example from ppapi/examples/2d.
-
-Import('env')
-
-env.Prepend(CPPDEFINES=['XP_UNIX'])
-
-nexe_name = env.ProgramNameForNmf('ppapi_example_2d')
-obj = env.ComponentObject(
-    'graphics_2d_example',
-    '$SOURCE_ROOT/ppapi/examples/2d/graphics_2d_example.c'
-    )
-ppapi_example_2d_nexe = env.ComponentProgram(nexe_name, [obj],
-                                             EXTRA_LIBS=['ppapi'])
-
-# Note that the html is required to run this program.
-env.Publish(nexe_name, 'run',
-            ['ppapi_example_2d.html', 'ppapi_example_2d.nmf' ])

ppapi/native_client/tests/ppapi_example_audio/nacl.scons
@@ -1,27 +1,0 @@
-# -*- python -*-
-# Copyright (c) 2012 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# This is a pepper audio example from examples/audio.
-
-Import('env')
-
-env.Prepend(CPPDEFINES=['XP_UNIX'])
-env.Replace(TEST_DIR='${SOURCE_ROOT}/ppapi/native_client/tests/' +
-                     'ppapi_example_audio/')
-
-nexe_name = env.ProgramNameForNmf('ppapi_example_audio')
-nexe = env.ComponentProgram(nexe_name,
-                            [ 'audio.cc' ],
-                            EXTRA_LIBS=['ppapi',
-                                        'ppapi_cpp',
-                                        'platform',
-                                        'gio',
-                                        'pthread',
-                                        'm'])
-
-# Note that the html is required to run this program.
-env.Publish(nexe_name, 'run',
-            ['ppapi_example_audio.html'])
-

ppapi/native_client/tests/ppapi_example_events/nacl.scons
@@ -1,21 +1,0 @@
-# -*- python -*-
-# Copyright (c) 2012 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-Import('env')
-
-# TODO(robertm): those should not be necessary once we go -std=c99
-env.FilterOut(CFLAGS=['-pedantic'])
-env.FilterOut(CCFLAGS=['-pedantic'])
-
-nexe = env.ProgramNameForNmf('ppapi_example_events')
-env.Alias('ppapi_example_events${PROGSUFFIX}',
-          ['$STAGING_DIR/%s${PROGSUFFIX}' % nexe])
-ppapi_example_events_nexe = env.ComponentProgram(nexe,
-                                                 ['ppapi_example_events.cc',],
-                                                 EXTRA_LIBS=['ppapi',
-                                                             'ppapi_cpp'])
-env.Publish(nexe, 'run',
-            ['ppapi_example_events.html',
-             'ppapi_example_events.nmf'])

ppapi/native_client/tests/ppapi_example_gles2/nacl.scons
@@ -1,39 +1,0 @@
-# -*- python -*-
-# Copyright (c) 2012 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# This is a Pepper 3D (OpenGLES2) example from ppapi/examples/gles2.
-
-Import('env')
-
-env.Prepend(CPPDEFINES=['XP_UNIX'])
-env.Append(CPPPATH=['$SOURCE_ROOT/ppapi/lib/gl/include'])
-env.Replace(TEST_DIR='${SOURCE_ROOT}/ppapi/native_client/tests/' +
-                     'ppapi_example_gles2/')
-
-nexe_name = env.ProgramNameForNmf('ppapi_example_gles2')
-obj = env.ComponentObject(
-    'gles2',
-    '$SOURCE_ROOT/ppapi/examples/gles2/gles2.cc'
-    )
-nexe = env.ComponentProgram(nexe_name, [obj],
-                            EXTRA_LIBS=['ppapi', 'ppapi_cpp'])
-
-# Note that the html is required to run this program.
-env.Publish(nexe_name, 'run',
-            ['ppapi_example_gles2.html'])
-
-test = env.PPAPIBrowserTester('ppapi_example_gles2_test.out',
-                              url='ppapi_example_gles2.html',
-                              nmf_names=['ppapi_example_gles2'],
-                              files=env.ExtractPublishedFiles(nexe_name),
-                              args=['--enable_experimental_js'])
-
-env.AddNodeToTestSuite(test,
-                       ['chrome_browser_tests'],
-                       'run_ppapi_example_gles2_test',
-                       # TODO(cstefansen): When fixed set to
-                       # env.PPAPIBrowserTesterIsBroken().
-                       # code.google.com/p/nativeclient/issues/detail?id=1936
-                       is_broken=env.Bit('nacl_glibc'))

ppapi/native_client/tests/ppapi_example_post_message/nacl.scons
@@ -1,36 +1,0 @@
-# -*- python -*-
-# Copyright (c) 2012 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# This is a Pepper PostMessage example from ppapi/examples/scripting.
-
-Import('env')
-
-env.Prepend(CPPDEFINES=['XP_UNIX'])
-env.Replace(TEST_DIR='${SOURCE_ROOT}/ppapi/native_client/tests/' +
-                     'ppapi_example_post_message/')
-
-nexe_name = env.ProgramNameForNmf('ppapi_example_post_message')
-obj = env.ComponentObject(
-    'post_message',
-    '$SOURCE_ROOT/ppapi/examples/scripting/post_message.cc'
-    )
-nexe = env.ComponentProgram(nexe_name, [obj],
-                            EXTRA_LIBS=['ppapi', 'ppapi_cpp'])
-
-# Note that the html is required to run this program.
-env.Publish(nexe_name, 'run',
-            ['ppapi_example_post_message.html'])
-
-
-test = env.PPAPIBrowserTester(
-    'ppapi_example_post_message_test.out',
-    url='ppapi_example_post_message.html',
-    nmf_names=['ppapi_example_post_message'],
-    files=env.ExtractPublishedFiles(nexe_name))
-
-env.AddNodeToTestSuite(test,
-                       ['chrome_browser_tests'],
-                       'run_ppapi_example_post_message_test',
-                       is_broken=env.PPAPIBrowserTesterIsBroken())

commit 36ac5fbe3d4784fbed7d1040f1334ceb54497fe4

Remove more tests from the NaCl SCons build.
 * pnacl_test_example was a smoke test that broke in revision r161425.

 * browser_startup_time was a performance test that needs to be redone to work on the Chrome bots.

 * stress_many_nexes was a manual test.

BUG= http://code.google.com/p/chromium/issues/detail?id=154400

Review URL: https://chromiumcodereview.appspot.com/11145002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162173 0039d316-1c4b-4281-b951-d872f2087c98

ppapi/native_client/chrome_main.scons
@@ -40,7 +40,6 @@ def ExtendFileList(existing, additional):
 ppapi_scons_files['nonvariant_test_scons_files'] = ExtendFileList(
     ppapi_scons_files.get('nonvariant_test_scons_files', []), [
         'tests/nacl_browser/browser_dynamic_library/nacl.scons',
-        'tests/nacl_browser/browser_startup_time/nacl.scons',
         'tests/nacl_browser/inbrowser_test_runner/nacl.scons',
         'tests/nacl_browser/manifest_file/nacl.scons',
         'tests/nacl_browser/nameservice/nacl.scons',

ppapi/native_client/ppapi_scons_files.py
@@ -36,7 +36,6 @@ nonvariant_test_scons_files = [
     'tests/ppapi_browser/ppp_input_event/nacl.scons',
     'tests/ppapi_browser/ppp_instance/nacl.scons',
     'tests/ppapi_browser/progress_events/nacl.scons',
-    'tests/ppapi_browser/stress_many_nexes/nacl.scons',
     'tests/ppapi_messaging/nacl.scons',
     'tests/ppapi_test_lib/nacl.scons',
 ]

ppapi/native_client/tests/nacl_browser/browser_startup_time/browser_startup_time.html
@@ -1,23 +1,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-  <!-- Copyright (c) 2011 The Chromium Authors. All rights reserved. -->
-  <!-- Measures startup time for ppapi_test_example (a dependency). -->
-  <head>
-    <title> Startup Timing Test </title>
-    <script type="text/javascript" src="nacltest.js"></script>
-    <script type="text/javascript" src="browser_startup_time.js"></script>
-  </head>
-  <body>
-    <h1>Startup Timing Test</h1>
-    <embed type="application/x-nacl" id="nacl_server"
-           name="nacl_module" width="0" height="0"
-           src="browser_startup_time.nmf" />
-    <script type="text/javascript">
-      //<![CDATA[
-      var loadingTester = new LoadingTester($('nacl_server'));
-      loadingTester.setupLoad();
-      //]]>
-    </script>
-  </body>
-</html>

ppapi/native_client/tests/nacl_browser/browser_startup_time/browser_startup_time.js
@@ -1,46 +1,0 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Times how long the nexe took to load, then smoke tests it.
-
-function LoadingTester(plugin) {
-  // Workaround how JS binds 'this'
-  var this_ = this;
-
-  this.rpc_ = new RPCWrapper();
-
-  this.setupLoad = function() {
-    var loadBegin = (new Date).getTime();
-    this_.rpc_.startup();
-    var waiter = new NaClWaiter();
-
-    waiter.waitFor(plugin);
-    waiter.run(
-        function(loaded, waiting) {
-          var loadEnd = (new Date).getTime();
-          logLoadStatus(this_.rpc_, true, loaded, waiting);
-          // Log the total in-browser load time (for first load).
-          this_.rpc_.logTimeData('Total',
-                                (loadEnd - loadBegin));
-          this_.doSmokeTests_(plugin);
-        },
-        function() {
-          this_.rpc_.ping();
-        }
-      );
-  }
-
-  this.doSmokeTests_ = function(plugin) {
-    var tester = new Tester();
-    tester.addAsyncTest('TestHello', function(status) {
-        plugin.addEventListener('message', function(message_event) {
-            this.removeEventListener('message', arguments.callee, false);
-            status.assertEqual(message_event.data, 'hello from NaCl');
-            status.pass();
-          }, false);
-        plugin.postMessage('hello');
-      });
-    tester.run();
-  }
-}

ppapi/native_client/tests/nacl_browser/browser_startup_time/hello_world.cc
@@ -1,62 +1,0 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// A basic PPAPI-based hello world. Used to measure the size and startup
-// time of a really small program.
-
-#include "ppapi/cpp/instance.h"
-#include "ppapi/cpp/module.h"
-#include "ppapi/cpp/var.h"
-
-namespace {
-// The expected string sent by the browser.
-const char* const kHelloString = "hello";
-
-// The string sent back to the browser upon receipt of a message
-// containing "hello".
-const char* const kReplyString = "hello from NaCl";
-}  // namespace
-
-namespace hello_world {
-class HelloWorldInstance : public pp::Instance {
- public:
-  explicit HelloWorldInstance(PP_Instance instance) : pp::Instance(instance) {}
-  virtual ~HelloWorldInstance() {}
-  virtual void HandleMessage(const pp::Var& var_message);
-};
-
-void HelloWorldInstance::HandleMessage(const pp::Var& var_message) {
-  if (!var_message.is_string()) {
-    return;
-  }
-  std::string message = var_message.AsString();
-  pp::Var var_reply;
-  if (message == kHelloString)
-    var_reply = pp::Var(kReplyString);
-  PostMessage(var_reply);
-}
-
-class HelloWorldModule : public pp::Module {
- public:
-  HelloWorldModule() : pp::Module() {}
-  virtual ~HelloWorldModule() {}
-
-  virtual pp::Instance* CreateInstance(PP_Instance instance) {
-    return new HelloWorldInstance(instance);
-  }
-};
-}  // namespace hello_world
-
-namespace pp {
-/// Factory function called by the browser when the module is first loaded.
-/// The browser keeps a singleton of this module.  It calls the
-/// CreateInstance() method on the object you return to make instances.  There
-/// is one instance per <embed> tag on the page.  This is the main binding
-/// point for your NaCl module with the browser.
-/// @return new HelloWorldModule.
-/// @note The browser is responsible for deleting returned @a Module.
-Module* CreateModule() {
-  return new hello_world::HelloWorldModule();
-}
-}  // namespace pp

ppapi/native_client/tests/nacl_browser/browser_startup_time/nacl.scons
@@ -1,73 +1,0 @@
-# -*- python -*-
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# Test startup time for loading a basic PPAPI nexe in the browser.
-
-Import('env')
-
-nexe_name = env.ProgramNameForNmf('browser_startup_time')
-stripped_nexe = env.ProgramNameForNmf('browser_startup_time_stripped')
-
-browser_startup_nexe = env.ComponentProgram(
-    nexe_name,
-    ['hello_world.cc'],
-    EXTRA_LIBS=['ppapi_cpp'])
-
-env.Publish(nexe_name, 'run',
-            ['browser_startup_time.html',
-             'browser_startup_time.js'])
-
-test_name = 'browser_startup_time.out'
-output_processor_1 = ['${PYTHON}',
-                      str(env.File(
-                          '${SCONSTRUCT_DIR}/tools/process_perf_output.py')),
-                      test_name,
-                      env.GetPerfEnvDescription()]
-output_processor_c = ['${PYTHON}',
-                      str(env.File(
-                          '${SCONSTRUCT_DIR}/tools/process_perf_combined.py'))]
-
-
-node = env.PPAPIBrowserTester(
-    test_name,
-    url='browser_startup_time.html',
-    nmf_names=['browser_startup_time'],
-    files=env.ExtractPublishedFiles(nexe_name),
-    # Smooth out the timing data.
-    num_runs=5,
-    process_output_single=output_processor_1,
-    process_output_combined=output_processor_c,
-    # Assume verbosity level of 1 will give us NaClLogs for sel_ldr times.
-    log_verbosity=1)
-
-# TODO(jvoung): use a single test suite when either
-# (a) performance_tests are always run, or
-# (b) we have decided to just fold this test into chrome_browser_tests.
-test_suites = ['chrome_browser_tests', 'performance_tests']
-env.AddNodeToTestSuite(node,
-                       test_suites,
-                       'run_browser_startup_time_test',
-                       # This test relies on NACLLOG, which must be able to
-                       # open a file. This won't work with the outer sandbox
-                       # which is enabled in some cases.
-                       is_broken=(env.PPAPIBrowserTesterIsBroken() or
-                                  True))
-
-# Also check the size of this nexe.
-node = env.FileSizeTest('browser_startup_size.out',
-                        browser_startup_nexe)
-env.AddNodeToTestSuite(node,
-                       test_suites,
-                       'run_browser_startup_size_test')
-
-# Check size of stripped nexe.
-browser_startup_stripped_nexe = env.StripExecutable(
-    stripped_nexe,
-    browser_startup_nexe)
-node = env.FileSizeTest('browser_startup_stripped_size.out',
-                        browser_startup_stripped_nexe)
-env.AddNodeToTestSuite(node,
-                       test_suites,
-                       'run_browser_startup_stripped_size_test')

ppapi/native_client/tests/nacl_browser/pnacl_client_translator/nacl.scons
@@ -19,52 +19,14 @@ if not (env.Bit('bitcode') and env.Bit('pnacl_generate_pexe')):
 if env.Bit('nacl_glibc') and env.Bit('target_x86_64'):
   Return()
 
-#----------------------------------------------------------------------
-# Test pexe translation.
-
-# Specify the object file to prevent junk from getting left over
-# in source directory. SCons will default to point "-o" to the source
-# directory if it is an absolute directory.
-test_example_obj = env.ComponentObject(
-    'pnacl_test_example_obj',
-    '${SCONSTRUCT_DIR}/tests/ppapi_test_example/ppapi_test_example.cc')
-
-pexe_name = env.ProgramNameForNmf('pnacl_test_example')
-
-test_example_pexe = env.ComponentProgram(
-    pexe_name,
-    [test_example_obj],
-    EXTRA_LIBS=['ppapi',
-                'ppapi_test_lib',
-                'platform', # for CHECK
-                'pthread',
-                'gio',
-                ])
-
-# Grab test routines.
-borrowed_file = '${STAGING_DIR}/ppapi_test_example.js',
-
-env.Publish(pexe_name,
-            'run',
-            ['pnacl_test_example.html'])
-
-node = env.PPAPIBrowserTester(
-    'pnacl_test_example.out',
-    url='pnacl_test_example.html',
-    nmf_names=['pnacl_test_example'],
-    files=env.ExtractPublishedFiles(pexe_name) + [borrowed_file])
-
-env.AddNodeToTestSuite(node,
-                       ['chrome_browser_tests'],
-                       'run_pnacl_example_browser_test',
-                       is_broken=env.PPAPIBrowserTesterIsBroken())
-
 #### Test error handling.
 
 # We should add more cases, e.g., try a bad library to crash ld,
 # try a more malicious bitcode, try to exhaust resources, etc.
 # The current llc tests, don't actually crash LLC, they only
 # make llc exit w/ an abnormal status.
+
+program_fragment_obj = env.ComponentObject('program_fragment.cc')
 
 pnacl_bad_files = env.Replicate(
     '${STAGING_DIR}',
@@ -76,14 +38,10 @@ pnacl_bad_files = env.Replicate(
      # This one is the hello world bitcode .ll w/ a missing instruction.
      'bad2.pexe',
      'pnacl_bad2_pexe.nmf',
-     # Borrow the bitcode file from the test above and treat it as a
-     # pexe so that the compile succeeds but linking fails.
-     'pnacl_test_example_obj.bc',
+     program_fragment_obj,
      'pnacl_bad_pexe_undefined_syms.nmf',
      'pnacl_bad_test.html',
      ])
-
-env.Depends(pnacl_bad_files, test_example_obj)
 
 # Borrow bad load test harness.
 borrowed_file = '${STAGING_DIR}/ppapi_bad.js',

ppapi/native_client/tests/nacl_browser/pnacl_client_translator/pnacl_bad_pexe_undefined_syms.nmf
@@ -3,6 +3,6 @@
     "x86-32": {"url": "non_existent.nexe"},
     "x86-64": {"url": "non_existent.nexe"},
     "arm":    {"url": "non_existent.nexe"},
-    "portable": {"pnacl-translate": {"url": "pnacl_test_example_obj.bc"} }
+    "portable": {"pnacl-translate": {"url": "program_fragment.bc"} }
   }
 }

ppapi/native_client/tests/nacl_browser/pnacl_client_translator/pnacl_test_example.html
@@ -1,108 +1,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!--
-  Copyright (c) 2012 The Chromium Authors. All rights reserved.
-  Use of this source code is governed by a BSD-style license that can be
-  found in the LICENSE file.
--->
-<html>
-<head>
-  <META HTTP-EQUIV="Pragma" CONTENT="no-cache" />
-  <META HTTP-EQUIV="Expires" CONTENT="-1" />
-  <title>PNACL Test Example (PPAPI Test Example Clone)</title>
-  <script type="text/javascript" src="nacltest.js"></script>
-  <script type="text/javascript" src="ppapi_test_example.js"></script>
-  <script type="text/javascript">
-    //<![CDATA[
-    function pageDidLoad() {
-      // Set up a ticker so that we know that the UI thread is not blocked.
-      var quantum = 50;
-      var startTime = (new Date).getTime();
-      var done = false;
-      var updateUI = function() {
-         var start_end_div = $('start_end');
-         var now = (new Date).getTime();
-         start_end_div.innerText = String(now - startTime);
-         updateCanvas();
-         if (!done) {
-           setTimeout(updateUI, quantum);
-         }
-      }
-      setTimeout(updateUI, quantum);
-
-      var updateProgress = function() {
-         var progress_div = $('progress_area');
-         progress_div.innerText += '.';
-      }
-
-      var handleLoadEnd = function() {
-         done = true;
-      }
-
-      // For canvas drawing.
-      var hpos = 0;
-      var color = 0;
-      // The colors of the rainbow.
-      var palette =
-        new Array("#ff0000", "#ff7f00", "#ffff00", "#00ff00", "#0000ff",
-                  "#6600ff", "#8b00ff");
-      var updateCanvas = function() {
-        var canvas = document.getElementById('canvas');
-        var hsize = canvas.width;
-        var boxHeight = canvas.height;
-        var ctx = canvas.getContext('2d');
-        ctx.fillStyle = palette[color];
-        if (!ctx) {
-          return;
-        }
-        boxWidth = Math.min(boxHeight, hsize - hpos);
-        ctx.fillRect(hpos, 0, boxWidth, boxHeight);
-        hpos += boxWidth;
-        if (hpos >= hsize) {
-          hpos = 0;
-          color++;
-          if (color >= palette.length) {
-            color = 0;
-          }
-        }
-      }
-
-      var nexe_div = $('test_nexe');
-      nexe_div.addEventListener('loadstart', updateProgress, true);
-      nexe_div.addEventListener('progress', updateProgress, true);
-      nexe_div.addEventListener('load', updateProgress, true);
-      nexe_div.addEventListener('loadend', handleLoadEnd, true);
-    }
-//]]>
-  </script>
-</head>
-<body onload="pageDidLoad()">
-
-  <h1>PNACL Test Example</h1>
-
-  <embed type="application/x-nacl" id="test_nexe"
-          name="nacl_module"
-          src="pnacl_test_example.nmf"
-          width="20" height="20"
-         style="background-color:gray" />
-
-  <script type="text/javascript">
-    //<![CDATA[
-    var tester = new Tester();
-    setupTests(tester, $('test_nexe'));
-    tester.waitFor($('test_nexe'));
-    tester.run();
-    //]]>
-  </script>
-
-  <p>Page start to loadend (millisecs):</p>
-  <div id="start_end"> 0.0 </div>
-  <p> This will animate when the UI thread is not blocked (until loadend):</p>
-  <canvas id="canvas" width="200" height="10">
-  </canvas>
-
-  <p> Download progress:</p>
-  <div id="progress_area" style="font-size:28pt; color:#FF9900;"> </div>
-
-</body>
-</html>

ppapi/native_client/tests/nacl_browser/pnacl_client_translator/program_fragment.cc
@@ -1,0 +1,10 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Never defined.  This will end badly when we try to link.
+extern void bar();
+
+void foo() {
+  bar();
+}

ppapi/native_client/tests/ppapi_browser/stress_many_nexes/nacl.scons
@@ -1,36 +1,0 @@
-# -*- python -*-
-# Copyright (c) 2012 The Native Client Authors.  All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-
-Import('env')
-
-stress_files = [
-    env.File('stress_many_nexes.html'),
-    env.File('${SCONSTRUCT_DIR}/tools/browser_tester/browserdata/nacltest.js'),
-    ]
-replicated_files = env.Replicate('${STAGING_DIR}', stress_files)
-env.Alias('all_programs', replicated_files)
-
-# NOTE: Since NMFs are now autogenerated there is no ppapi_ppb_core.nmf
-# file that is checked-in and staged anymore.  This test will need to
-# be converted to have its own nexe and its own nmf if we ever choose
-# to enable it (is_broken=True below).
-core = env.File('${STAGING_DIR}/ppapi_ppb_core_${TARGET_FULLARCH}${PROGSUFFIX}')
-nmf = env.File('${STAGING_DIR}/ppapi_ppb_core.nmf')
-stress_files.extend([core, nmf])
-
-node = env.PPAPIBrowserTester(
-    'stress_many_nexes_test.out',
-    url='stress_many_nexes.html',
-    test_args=(('count', 100), ('parallel', 0)),
-    timeout=200,
-    files=stress_files)
-
-env.AddNodeToTestSuite(node,
-                       ['chrome_browser_tests'],
-                       'run_stress_many_nexes_test',
-                       # Don't run this test automatically. It can cause random
-                       # processes to die on a machine, etc.
-                       is_broken=True)

ppapi/native_client/tests/ppapi_browser/stress_many_nexes/stress_many_nexes.html
@@ -1,87 +1,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-  <!--
-  Copyright (c) 2011 The Chromium Authors. All rights reserved.
-  Use of this source code is governed by a BSD-style license that can be
-  found in the LICENSE file.
-  -->
-  <head>
-    <META HTTP-EQUIV="Pragma" CONTENT="no-cache" />
-    <META HTTP-EQUIV="Expires" CONTENT="-1" />
-    <script type="text/javascript" src="nacltest.js"> </script>
-    <title>Browser Stress Test</title>
-    <style type="text/css">
-      .naclModule { background-color: gray; margin: 2px 2px; }
-      .naclPassed { background-color: green; margin: 2px 2px; }
-      .naclFailed { background-color: red; margin: 2px 2px; }
-    </style>
-  </head>
-  <body id="body">
-<script type="text/javascript">
-//<![CDATA[
-
-function createEmbed(id) {
-  return createNaClEmbed({
-    id: id,
-    src: 'ppapi_ppb_core.nmf',
-    width: 100,
-    height: 20
-  });
-}
-
-
-function checkEmbed(test, embed) {
-  embed.className = 'naclFailed';
-  test.assertEqual(embed.readyState, 4);
-  test.assertEqual(embed.lastError, '');
-  embed.className = 'naclPassed';
-  test.pass();
-}
-
-
-function addParallelTest(tester, i) {
-  var name = 'nacl_' + i;
-  var embed = createEmbed(name);
-  tester.waitFor(embed);
-  tester.addAsyncTest(name, function(test) {
-    checkEmbed(test, embed);
-  });
-  document.body.appendChild(embed);
-}
-
-
-function addSerialTest(tester, i) {
-  var name = 'nacl_' + i;
-  tester.addAsyncTest(name, function(test){
-    var embed = createEmbed(name);
-    test.expectEvent(embed, 'loadend', function() {
-      checkEmbed(test, embed);
-    });
-    document.body.appendChild(embed);
-  });
-}
-
-
-function initTests(tester) {
-  var args = getTestArguments({'count': '100', 'parallel': '0'});
-  for (var i = 0; i < args.count; i++) {
-    if (parseInt(args.parallel)) {
-      // Faster, lets the browser determine load order and interleaving.
-      addParallelTest(tester, i);
-    } else {
-      // Slower, forces sequential load order.
-      addSerialTest(tester, i);
-    }
-  }
-}
-
-
-var tester = new Tester($('body'));
-initTests(tester);
-tester.run();
-
-  //]]>
-  </script>
-  </body>
-</html>