ChromiumWebApps/chromium

commit 80ad8627f8ba99044defe46259b410ee676bd230

Push the screen rect of the page and browser to the renderer to avoid sync IPCs to get this. The old approach doesn't work in Win Aura since the data is only available on the UI thread and we can't make a sync IPC call to it.
This change also removes the last remaining usage of the view HWND. I'll clean that up in a future change to not clutter this one.
Review URL: https://codereview.chromium.org/11364061

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166446 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/renderer_host/render_widget_host_impl.cc
@@ -133,6 +133,7 @@ RenderWidgetHostImpl::RenderWidgetHostImpl(RenderWidgetHostDelegate* delegate,
       repaint_ack_pending_(false),
       resize_ack_pending_(false),
       should_auto_resize_(false),
+      waiting_for_screen_rects_ack_(false),
       mouse_move_pending_(false),
       mouse_wheel_pending_(false),
       select_range_pending_(false),
@@ -251,6 +252,26 @@ void RenderWidgetHostImpl::ResetSizeAndRepaintPendingFlags() {
   in_flight_size_.SetSize(0, 0);
 }
 
+void RenderWidgetHostImpl::SendScreenRects() {
+  if (waiting_for_screen_rects_ack_)
+    return;
+
+  if (is_hidden_) {
+    // On GTK, this comes in for backgrounded tabs. Ignore, to match what
+    // happens on Win & Mac, and when the view is shown it'll call this again.
+    return;
+  }
+
+  if (!view_)
+    return;
+
+  last_view_screen_rect_ = view_->GetViewBounds();
+  last_window_screen_rect_ = view_->GetBoundsInRootWindow();
+  Send(new ViewMsg_UpdateScreenRects(
+      GetRoutingID(), last_view_screen_rect_, last_window_screen_rect_));
+  waiting_for_screen_rects_ack_ = true;
+}
+
 void RenderWidgetHostImpl::Init() {
   DCHECK(process_->HasConnection());
 
@@ -293,6 +314,8 @@ bool RenderWidgetHostImpl::OnMessageReceived(const IPC::Message &msg) {
     IPC_MESSAGE_HANDLER(ViewHostMsg_RenderViewReady, OnMsgRenderViewReady)
     IPC_MESSAGE_HANDLER(ViewHostMsg_RenderViewGone, OnMsgRenderViewGone)
     IPC_MESSAGE_HANDLER(ViewHostMsg_Close, OnMsgClose)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_UpdateScreenRects_ACK,
+                        OnMsgUpdateScreenRectsAck)
     IPC_MESSAGE_HANDLER(ViewHostMsg_RequestMove, OnMsgRequestMove)
     IPC_MESSAGE_HANDLER(ViewHostMsg_SetTooltipText, OnMsgSetTooltipText)
     IPC_MESSAGE_HANDLER(ViewHostMsg_PaintAtSize_ACK, OnMsgPaintAtSizeAck)
@@ -320,10 +343,6 @@ bool RenderWidgetHostImpl::OnMessageReceived(const IPC::Message &msg) {
     IPC_MESSAGE_HANDLER(ViewHostMsg_UnlockMouse, OnMsgUnlockMouse)
     IPC_MESSAGE_HANDLER(ViewHostMsg_ShowDisambiguationPopup,
                         OnMsgShowDisambiguationPopup)
-#if defined(OS_POSIX) || defined(USE_AURA)
-    IPC_MESSAGE_HANDLER(ViewHostMsg_GetWindowRect, OnMsgGetWindowRect)
-    IPC_MESSAGE_HANDLER(ViewHostMsg_GetRootWindowRect, OnMsgGetRootWindowRect)
-#endif
 #if defined(OS_MACOSX)
     IPC_MESSAGE_HANDLER(ViewHostMsg_PluginFocusChanged,
                         OnMsgPluginFocusChanged)
@@ -396,6 +415,8 @@ void RenderWidgetHostImpl::WasShown() {
   if (!is_hidden_)
     return;
   is_hidden_ = false;
+
+  SendScreenRects();
 
   BackingStore* backing_store = BackingStoreManager::Lookup(this);
   // If we already have a backing store for this widget, then we don't need to
@@ -1142,6 +1163,8 @@ void RenderWidgetHostImpl::RendererExited(base::TerminationStatus status,
   // from a crashed renderer.
   renderer_initialized_ = false;
 
+  waiting_for_screen_rects_ack_ = false;
+
   // Must reset these to ensure that mouse move/wheel events work with a new
   // renderer.
   mouse_move_pending_ = false;
@@ -1329,6 +1352,7 @@ void RenderWidgetHostImpl::RendererIsResponsive() {
 }
 
 void RenderWidgetHostImpl::OnMsgRenderViewReady() {
+  SendScreenRects();
   WasResized();
 }
 
@@ -1373,6 +1397,19 @@ void RenderWidgetHostImpl::OnMsgSetTooltipText(
   }
   if (GetView())
     view_->SetTooltipText(wrapped_tooltip_text);
+}
+
+void RenderWidgetHostImpl::OnMsgUpdateScreenRectsAck() {
+  waiting_for_screen_rects_ack_ = false;
+  if (!view_)
+    return;
+
+  if (view_->GetViewBounds() == last_view_screen_rect_ &&
+      view_->GetBoundsInRootWindow() == last_window_screen_rect_) {
+    return;
+  }
+
+  SendScreenRects();
 }
 
 void RenderWidgetHostImpl::OnMsgRequestMove(const gfx::Rect& pos) {
@@ -1875,20 +1912,6 @@ void RenderWidgetHostImpl::OnMsgShowDisambiguationPopup(
   Send(new ViewMsg_ReleaseDisambiguationPopupDIB(GetRoutingID(),
                                                  dib->handle()));
 }
-
-#if defined(OS_POSIX) || defined(USE_AURA)
-void RenderWidgetHostImpl::OnMsgGetWindowRect(gfx::NativeViewId window_id,
-                                              gfx::Rect* results) {
-  if (view_)
-    *results = view_->GetViewBounds();
-}
-
-void RenderWidgetHostImpl::OnMsgGetRootWindowRect(gfx::NativeViewId window_id,
-                                                  gfx::Rect* results) {
-  if (view_)
-    *results = view_->GetBoundsInRootWindow();
-}
-#endif
 
 #if defined(OS_WIN)
 void RenderWidgetHostImpl::OnWindowlessPluginDummyWindowCreated(

content/browser/renderer_host/render_message_filter.cc
@@ -341,13 +341,6 @@ bool RenderMessageFilter::OnMessageReceived(const IPC::Message& message,
                                             bool* message_was_ok) {
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP_EX(RenderMessageFilter, message, *message_was_ok)
-#if defined(OS_WIN) && !defined(USE_AURA)
-    // On Windows, we handle these on the IO thread to avoid a deadlock with
-    // plugins.  On non-Windows systems, we need to handle them on the UI
-    // thread.
-    IPC_MESSAGE_HANDLER(ViewHostMsg_GetWindowRect, OnGetWindowRect)
-    IPC_MESSAGE_HANDLER(ViewHostMsg_GetRootWindowRect, OnGetRootWindowRect)
-#endif
     IPC_MESSAGE_HANDLER(ViewHostMsg_GenerateRoutingID, OnGenerateRoutingID)
     IPC_MESSAGE_HANDLER(ViewHostMsg_CreateWindow, OnMsgCreateWindow)
     IPC_MESSAGE_HANDLER(ViewHostMsg_CreateWidget, OnMsgCreateWidget)

content/browser/renderer_host/render_message_filter.h
@@ -143,13 +143,6 @@ class RenderMessageFilter : public BrowserMessageFilter {
   void SendLoadFontReply(IPC::Message* reply, FontLoader::Result* result);
 #endif
 
-#if defined(OS_WIN) && !defined(USE_AURA)
-  // On Windows, we handle these on the IO thread to avoid a deadlock with
-  // plugins.  On non-Windows systems, we need to handle them on the UI thread.
-  void OnGetWindowRect(gfx::NativeViewId window, gfx::Rect* rect);
-  void OnGetRootWindowRect(gfx::NativeViewId window, gfx::Rect* rect);
-#endif
-
   void OnGetPlugins(bool refresh, IPC::Message* reply_msg);
   void GetPluginsCallback(IPC::Message* reply_msg,
                           const std::vector<webkit::WebPluginInfo>& plugins);

content/browser/renderer_host/render_message_filter_win.cc
@@ -1,54 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "content/browser/renderer_host/render_message_filter.h"
-
-#include "content/common/view_messages.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebScreenInfo.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/win/WebScreenInfoFactory.h"
-
-namespace content {
-namespace {
-
-// Returns the "visible window rect" for |window|, defined roughly as "what the
-// user thinks of as the window".  See comments below regarding the difference
-// between this and the true window rect when the window is maximized.
-gfx::Rect GetVisibleWindowRect(HWND window) {
-  // We get null |window_id|s passed into the two functions below; see bug 9060.
-  // Probably this should be removed and we should prevent this case.
-  if (!IsWindow(window))
-    return gfx::Rect();
-
-  RECT window_rect = {0};
-  GetWindowRect(window, &window_rect);
-  gfx::Rect rect(window_rect);
-
-  // Maximized windows are outdented from the work area by the frame thickness
-  // even though this "frame" is not painted.  This confuses code (and people)
-  // that think of a maximized window as corresponding exactly to the work area.
-  // Correct for this by subtracting the frame thickness back off.
-  if (IsZoomed(window)) {
-    rect.Inset(GetSystemMetrics(SM_CXSIZEFRAME),
-               GetSystemMetrics(SM_CYSIZEFRAME));
-  }
-  return rect;
-}
-
-}  // namespace
-
-// TODO(shess): Provide a mapping from reply_msg->routing_id() to HWND
-// so that we can eliminate the NativeViewId parameter.
-
-void RenderMessageFilter::OnGetWindowRect(gfx::NativeViewId window_id,
-                                          gfx::Rect* rect) {
-  *rect = GetVisibleWindowRect(gfx::NativeViewFromId(window_id));
-}
-
-void RenderMessageFilter::OnGetRootWindowRect(gfx::NativeViewId window_id,
-                                              gfx::Rect* rect) {
-  *rect = GetVisibleWindowRect(GetAncestor(gfx::NativeViewFromId(window_id),
-                               GA_ROOT));
-}
-
-}  // namespace content

content/browser/renderer_host/render_view_host_impl.cc
@@ -1149,6 +1149,7 @@ void RenderViewHostImpl::OnMsgRunModal(int opener_id, IPC::Message* reply_msg) {
 
 void RenderViewHostImpl::OnMsgRenderViewReady() {
   render_view_termination_status_ = base::TERMINATION_STATUS_STILL_RUNNING;
+  SendScreenRects();
   WasResized();
   delegate_->RenderViewReady(this);
 }

content/browser/renderer_host/render_widget_host_impl.h
@@ -440,6 +440,9 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
 
   void DetachDelegate();
 
+  // Update the renderer's cache of the screen rect of the view and window.
+  void SendScreenRects();
+
  protected:
   virtual RenderWidgetHostImpl* AsRenderWidgetHostImpl() OVERRIDE;
 
@@ -499,7 +502,6 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
   // as opposed to visa versa.
   void SetShouldAutoResize(bool enable);
 
- protected:
   // Expose increment/decrement of the in-flight event count, so
   // RenderViewHostImpl can account for in-flight beforeunload/unload events.
   int increment_in_flight_event_count() { return ++in_flight_event_count_; }
@@ -543,6 +545,7 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
   void OnMsgRenderViewReady();
   void OnMsgRenderViewGone(int status, int error_code);
   void OnMsgClose();
+  void OnMsgUpdateScreenRectsAck();
   void OnMsgRequestMove(const gfx::Rect& pos);
   void OnMsgSetTooltipText(const string16& tooltip_text,
                            WebKit::WebTextDirection text_direction_hint);
@@ -583,10 +586,6 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
                                     const gfx::Size& size,
                                     const TransportDIB::Id& id);
 
-#if defined(OS_POSIX) || defined(USE_AURA)
-  void OnMsgGetWindowRect(gfx::NativeViewId window_id, gfx::Rect* results);
-  void OnMsgGetRootWindowRect(gfx::NativeViewId window_id, gfx::Rect* results);
-#endif
 #if defined(OS_MACOSX)
   void OnMsgPluginFocusChanged(bool focused, int plugin_id);
   void OnMsgStartPluginIme();
@@ -724,6 +723,10 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
   // True if the render widget host should track the render widget's size as
   // opposed to visa versa.
   bool should_auto_resize_;
+
+  bool waiting_for_screen_rects_ack_;
+  gfx::Rect last_view_screen_rect_;
+  gfx::Rect last_window_screen_rect_;
 
   // True if a mouse move event was sent to the render view and we are waiting
   // for a corresponding ViewHostMsg_HandleInputEvent_ACK message.

content/browser/renderer_host/render_widget_host_view_base.cc
@@ -406,6 +406,13 @@ void RenderWidgetHostViewBase::SetBrowserAccessibilityManager(
 }
 
 void RenderWidgetHostViewBase::UpdateScreenInfo(gfx::NativeView view) {
+  RenderWidgetHostImpl* impl = NULL;
+  if (GetRenderWidgetHost())
+    impl = RenderWidgetHostImpl::From(GetRenderWidgetHost());
+
+  if (impl)
+    impl->SendScreenRects();
+
   gfx::Display display =
       gfx::Screen::GetScreenFor(view)->GetDisplayNearestPoint(
           GetViewBounds().origin());
@@ -414,12 +421,9 @@ void RenderWidgetHostViewBase::UpdateScreenInfo(gfx::NativeView view) {
     return;
   current_display_area_ = display.bounds();
   current_device_scale_factor_ = display.device_scale_factor();
-  if (GetRenderWidgetHost()) {
-    RenderWidgetHostImpl* impl =
-        RenderWidgetHostImpl::From(GetRenderWidgetHost());
+  if (impl)
     impl->NotifyScreenInfoChanged();
   }
-}
 
 class BasicMouseWheelSmoothScrollGesture
     : public SmoothScrollGesture {

content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -139,7 +139,7 @@ static float ScaleFactor(NSView* view) {
 - (void)keyEvent:(NSEvent*)theEvent wasKeyEquivalent:(BOOL)equiv;
 - (void)cancelChildPopups;
 - (void)windowDidChangeBackingProperties:(NSNotification*)notification;
-- (void)windowChangedScreen:(NSNotification*)notification;
+- (void)windowChangedGlobalFrame:(NSNotification*)notification;
 - (void)checkForPluginImeCancellation;
 - (void)updateTabBackingStoreScaleFactor;
 - (NSRect)firstViewRectForCharacterRange:(NSRange)theRange
@@ -1569,13 +1569,6 @@ void RenderWidgetHostViewMac::SetTextInputActive(bool active) {
            selector:@selector(globalFrameDidChange:)
                name:NSViewGlobalFrameDidChangeNotification
              object:self];
-    if ([self window]) {
-      [[NSNotificationCenter defaultCenter]
-          addObserver:self
-             selector:@selector(windowChangedScreen:)
-                 name:NSWindowDidChangeScreenNotification
-               object:[self window]];
-    }
   }
   return self;
 }
@@ -2130,28 +2123,40 @@ void RenderWidgetHostViewMac::SetTextInputActive(bool active) {
 - (void)viewWillMoveToWindow:(NSWindow*)newWindow {
   // We're messing with the window, so do this to ensure no flashes. This one
   // prevents a flash when the current tab is closed.
-  [[self window] disableScreenUpdatesUntilFlush];
+  NSWindow* oldWindow = [self window];
+  [oldWindow disableScreenUpdatesUntilFlush];
 
-  if ([self window]) {
-    [[NSNotificationCenter defaultCenter]
+  NSNotificationCenter* notificationCenter =
+      [NSNotificationCenter defaultCenter];
+  if (oldWindow) {
+    [notificationCenter
         removeObserver:self
                   name:NSWindowDidChangeBackingPropertiesNotification
-                object:[self window]];
-    [[NSNotificationCenter defaultCenter]
+                object:oldWindow];
+    [notificationCenter
         removeObserver:self
-                  name:NSWindowDidChangeScreenNotification
-                object:[self window]];
+                  name:NSWindowDidMoveNotification
+                object:oldWindow];
+    [notificationCenter
+        removeObserver:self
+                  name:NSWindowDidEndLiveResizeNotification
+                object:oldWindow];
   }
   if (newWindow) {
-    [[NSNotificationCenter defaultCenter]
+    [notificationCenter
         addObserver:self
            selector:@selector(windowDidChangeBackingProperties:)
                name:NSWindowDidChangeBackingPropertiesNotification
              object:newWindow];
-    [[NSNotificationCenter defaultCenter]
+    [notificationCenter
         addObserver:self
-           selector:@selector(windowChangedScreen:)
-               name:NSWindowDidChangeScreenNotification
+           selector:@selector(windowChangedGlobalFrame:)
+               name:NSWindowDidMoveNotification
+             object:newWindow];
+    [notificationCenter
+        addObserver:self
+           selector:@selector(windowChangedGlobalFrame:)
+               name:NSWindowDidEndLiveResizeNotification
              object:newWindow];
   }
 }
@@ -2206,7 +2211,7 @@ void RenderWidgetHostViewMac::SetTextInputActive(bool active) {
   handlingGlobalFrameDidChange_ = NO;
 }
 
-- (void)windowChangedScreen:(NSNotification*)notification {
+- (void)windowChangedGlobalFrame:(NSNotification*)notification {
   renderWidgetHostView_->UpdateScreenInfo(
       renderWidgetHostView_->GetNativeView());
 }

commit 168dac6293496992304a0912ae206334e6eb938e

Fix the html select tag showing up at the wrong position. This was a regression from r166446.
The fix is to watch for the top level aura::Window's movement. In Ash, this would be an aura::Window that is the parent of the WebContents. In desktop Aura, this would be the aura::RootWindow. The change watches for this in WebContentsViewAura, similar to how WebContentsViewWin does this with PositionChangedMessageFilter. The reason is that the parent being watched needs to be updated with tab drags, so it's more convenient to do this at WebContentsViewAura's parent, instead of doing it at the RenderWidgetHostViewAura level (since that would need a list of parents).

This change also fixes OnHostMoved not being called on Windows.

BUG=161174
Review URL: https://codereview.chromium.org/11308083

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168558 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/renderer_host/render_widget_host_impl.cc
@@ -261,7 +261,7 @@ void RenderWidgetHostImpl::ResetSizeAndRepaintPendingFlags() {
 }
 
 void RenderWidgetHostImpl::SendScreenRects() {
-  if (waiting_for_screen_rects_ack_)
+  if (!renderer_initialized_ || waiting_for_screen_rects_ack_)
     return;
 
   if (is_hidden_) {

content/browser/web_contents/web_contents_view_aura.cc
@@ -26,7 +26,9 @@
 #include "ui/aura/client/drag_drop_client.h"
 #include "ui/aura/client/drag_drop_delegate.h"
 #include "ui/aura/root_window.h"
+#include "ui/aura/root_window_observer.h"
 #include "ui/aura/window.h"
+#include "ui/aura/window_observer.h"
 #include "ui/base/clipboard/custom_data_helper.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/dragdrop/os_exchange_data.h"
@@ -234,6 +236,65 @@ int ConvertAuraEventFlagsToWebInputEventModifiers(int aura_event_flags) {
 
 }  // namespace
 
+class WebContentsViewAura::WindowObserver
+    : public aura::WindowObserver, public aura::RootWindowObserver {
+ public:
+  explicit WindowObserver(WebContentsViewAura* view)
+      : view_(view), parent_(NULL) {}
+  virtual ~WindowObserver() {
+    if (parent_)
+      parent_->RemoveObserver(this);
+  }
+
+  // Overridden from aura::WindowObserver:
+  virtual void OnWindowParentChanged(aura::Window* window,
+                                     aura::Window* parent) OVERRIDE {
+    if (parent_)
+      parent_->RemoveObserver(this);
+    parent_ = parent;
+    if (parent)
+      parent->AddObserver(this);
+  }
+
+  virtual void OnWindowBoundsChanged(aura::Window* window,
+                                     const gfx::Rect& old_bounds,
+                                     const gfx::Rect& new_bounds) {
+    // This is for the Ash case.
+    SendScreenRects();
+  }
+
+  virtual void OnWindowAddedToRootWindow(aura::Window* window) OVERRIDE {
+    window->GetRootWindow()->AddRootWindowObserver(this);
+  }
+
+  virtual void OnWindowRemovingFromRootWindow(aura::Window* window) OVERRIDE {
+    window->GetRootWindow()->RemoveRootWindowObserver(this);
+  }
+
+  // Overridden RootWindowObserver:
+  virtual void OnRootWindowMoved(const aura::RootWindow* root,
+                                 const gfx::Point& new_origin) OVERRIDE {
+    // This is for the desktop case (i.e. Aura desktop).
+    SendScreenRects();
+  }
+
+ private:
+  void SendScreenRects() {
+    if (!view_->view_)
+      return;
+    RenderWidgetHostImpl::From(view_->view_->GetRenderWidgetHost())->
+        SendScreenRects();
+  }
+
+  WebContentsViewAura* view_;
+
+  // We cache the old parent so that we can unregister when it's not the parent
+  // anymore.
+  aura::Window* parent_;
+
+  DISALLOW_COPY_AND_ASSIGN(WindowObserver);
+};
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // WebContentsViewAura, public:
@@ -255,6 +316,10 @@ WebContentsViewAura::WebContentsViewAura(
 // WebContentsViewAura, private:
 
 WebContentsViewAura::~WebContentsViewAura() {
+  if (!window_)
+    return;
+
+  window_->RemoveObserver(window_observer_.get());
   // Window needs a valid delegate during its destructor, so we explicitly
   // delete it here.
   window_.reset();
@@ -420,6 +485,9 @@ void WebContentsViewAura::CreateView(const gfx::Size& initial_size) {
   window_->layer()->SetMasksToBounds(true);
   window_->SetName("WebContentsViewAura");
 
+  window_observer_.reset(new WindowObserver(this));
+  window_->AddObserver(window_observer_.get());
+
   // delegate_->GetDragDestDelegate() creates a new delegate on every call.
   // Hence, we save a reference to it locally. Similar model is used on other
   // platforms as well.
@@ -439,8 +507,7 @@ RenderWidgetHostView* WebContentsViewAura::CreateViewForWidget(
     return render_widget_host->GetView();
   }
 
-  view_ = RenderWidgetHostView::CreateViewForWidget(
-      render_widget_host);
+  view_ = RenderWidgetHostView::CreateViewForWidget(render_widget_host);
   view_->InitAsChild(NULL);
   GetNativeView()->AddChild(view_->GetNativeView());
   view_->Show();

content/browser/web_contents/web_contents_view_aura.h
@@ -42,6 +42,9 @@ class CONTENT_EXPORT WebContentsViewAura
                       WebContentsViewDelegate* delegate);
 
  private:
+  class WindowObserver;
+  friend class WindowObserver;
+
   virtual ~WebContentsViewAura();
 
   void SizeChangedCommon(const gfx::Size& size);
@@ -162,6 +165,8 @@ class CONTENT_EXPORT WebContentsViewAura
 
   scoped_ptr<aura::Window> window_;
   scoped_ptr<aura::Window> overscroll_window_;
+
+  scoped_ptr<WindowObserver> window_observer_;
 
   // The WebContentsImpl whose contents we display.
   WebContentsImpl* web_contents_;

ui/aura/root_window_host_win.cc
@@ -264,6 +264,11 @@ LRESULT RootWindowHostWin::OnNCActivate(UINT message,
   return DefWindowProc(hwnd(), message, w_param, l_param);
 }
 
+void RootWindowHostWin::OnMove(const CPoint& point) {
+  if (delegate_)
+    delegate_->OnHostMoved(gfx::Point(point.x, point.y));
+}
+
 void RootWindowHostWin::OnPaint(HDC dc) {
   delegate_->OnHostPaint();
   ValidateRect(hwnd(), NULL);

ui/aura/root_window_host_win.h
@@ -63,6 +63,7 @@ class RootWindowHostWin : public RootWindowHost, public ui::WindowImpl {
     MESSAGE_HANDLER_EX(WM_NCACTIVATE, OnNCActivate)
 
     MSG_WM_CLOSE(OnClose)
+    MSG_WM_MOVE(OnMove)
     MSG_WM_PAINT(OnPaint)
     MSG_WM_SIZE(OnSize)
   END_MSG_MAP()
@@ -72,6 +73,7 @@ class RootWindowHostWin : public RootWindowHost, public ui::WindowImpl {
   LRESULT OnMouseRange(UINT message, WPARAM w_param, LPARAM l_param);
   LRESULT OnCaptureChanged(UINT message, WPARAM w_param, LPARAM l_param);
   LRESULT OnNCActivate(UINT message, WPARAM w_param, LPARAM l_param);
+  void OnMove(const CPoint& point);
   void OnPaint(HDC dc);
   void OnSize(UINT param, const CSize& size);
 

ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
@@ -644,6 +644,8 @@ void DesktopRootWindowHostWin::HandleEndWMSizeMove() {
 
 void DesktopRootWindowHostWin::HandleMove() {
   native_widget_delegate_->OnNativeWidgetMove();
+  if (root_window_host_delegate_)
+    root_window_host_delegate_->OnHostMoved(GetBounds().origin());
 }
 
 void DesktopRootWindowHostWin::HandleWorkAreaChanged() {