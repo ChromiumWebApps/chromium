ChromiumWebApps/chromium

commit db9d45a5f52c126f7f414d7531ccd40cd6e2d313

Revert 169654 - FindLastActiveWithProfile now requires a HostDesktopType.
BUG=129187
TEST=none

Review URL: https://chromiumcodereview.appspot.com/11365128

Speculative revert to fix Win Aura crash in unit test
ExtensionServiceTest.LoadExtensionsWithPlugins.

TBR=grt@chromium.org
Review URL: https://codereview.chromium.org/11413189

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169664 0039d316-1c4b-4281-b951-d872f2087c98

PRESUBMIT.py
@@ -152,6 +152,15 @@ _BANNED_CPP_FUNCTIONS = (
       (),
     ),
     (
+      'chrome::FindLastActiveWithProfile',
+      (
+       'This function is deprecated and we\'re working on removing it. Pass',
+       'more context to get a Browser*, like a WebContents, window, or session',
+       'id. Talk to robertshield@ for more information.',
+      ),
+      True,
+    ),
+    (
       'browser::FindAnyBrowser',
       (
        'This function is deprecated and we\'re working on removing it. Pass',

chrome/browser/app_controller_mac.mm
@@ -102,8 +102,7 @@ bool g_is_opening_new_window = false;
 // not possible. If the last active browser is minimized (in particular, if
 // there are only minimized windows), it will unminimize it.
 Browser* ActivateBrowser(Profile* profile) {
-  Browser* browser = chrome::FindLastActiveWithProfile(profile,
-      chrome::HOST_DESKTOP_TYPE_NATIVE);
+  Browser* browser = chrome::FindLastActiveWithProfile(profile);
   if (browser)
     browser->window()->Activate();
   return browser;

chrome/browser/background/background_contents_service.cc
@@ -24,7 +24,6 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
-#include "chrome/browser/ui/host_desktop.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
@@ -616,8 +615,7 @@ void BackgroundContentsService::AddWebContents(
     bool user_gesture,
     bool* was_blocked) {
   Browser* browser = chrome::FindLastActiveWithProfile(
-      Profile::FromBrowserContext(new_contents->GetBrowserContext()),
-      chrome::GetActiveDesktop());
+      Profile::FromBrowserContext(new_contents->GetBrowserContext()));
   if (browser) {
     chrome::AddWebContents(browser, NULL, new_contents, disposition,
                            initial_pos, user_gesture, was_blocked);

chrome/browser/background/background_mode_manager.cc
@@ -29,7 +29,6 @@
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
-#include "chrome/browser/ui/host_desktop.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
@@ -94,8 +93,7 @@ void BackgroundModeManager::BackgroundModeData::ExecuteCommand(int item) {
 }
 
 Browser* BackgroundModeManager::BackgroundModeData::GetBrowserWindow() {
-  Browser* browser = chrome::FindLastActiveWithProfile(profile_,
-      chrome::GetActiveDesktop());
+  Browser* browser = chrome::FindLastActiveWithProfile(profile_);
   return browser ? browser : chrome::OpenEmptyWindow(profile_);
 }
 

chrome/browser/chromeos/drive/drive_file_system_util.cc
@@ -98,8 +98,7 @@ void GetHostedDocumentURLBlockingThread(const FilePath& drive_cache_path,
 }
 
 void OpenEditURLUIThread(Profile* profile, const GURL* edit_url) {
-  Browser* browser = chrome::FindLastActiveWithProfile(profile,
-      chrome::HOST_DESKTOP_TYPE_ASH);
+  Browser* browser = chrome::FindLastActiveWithProfile(profile);
   if (browser) {
     browser->OpenURL(content::OpenURLParams(*edit_url, content::Referrer(),
         CURRENT_TAB, content::PAGE_TRANSITION_TYPED, false));

chrome/browser/chromeos/extensions/file_manager_util.cc
@@ -612,8 +612,7 @@ bool ExecuteDefaultHandler(Profile* profile, const FilePath& path) {
 
   std::string extension_id = handler->extension_id();
   std::string action_id = handler->id();
-  Browser* browser = chrome::FindLastActiveWithProfile(profile,
-      chrome::HOST_DESKTOP_TYPE_ASH);
+  Browser* browser = chrome::FindLastActiveWithProfile(profile);
 
   // If there is no browsers for the profile, bail out. Return true so warning
   // about file type not being supported is not displayed.

chrome/browser/download/chrome_download_manager_delegate.cc
@@ -36,7 +36,6 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
-#include "chrome/browser/ui/host_desktop.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/extensions/feature_switch.h"
 #include "chrome/common/extensions/user_script.h"
@@ -266,8 +265,7 @@ WebContents* ChromeDownloadManagerDelegate::
 #else
   // Start the download in the last active browser. This is not ideal but better
   // than fully hiding the download from the user.
-  Browser* last_active = chrome::FindLastActiveWithProfile(profile_,
-      chrome::GetActiveDesktop());
+  Browser* last_active = chrome::FindLastActiveWithProfile(profile_);
   return last_active ? chrome::GetActiveWebContents(last_active) : NULL;
 #endif
 }

chrome/browser/download/download_crx_util.cc
@@ -12,7 +12,6 @@
 #include "chrome/browser/extensions/webstore_installer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/host_desktop.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "content/public/browser/download_item.h"
@@ -45,12 +44,9 @@ ExtensionInstallPrompt* CreateExtensionInstallPrompt(
   } else {
     content::WebContents* web_contents = download_item.GetWebContents();
     if (!web_contents) {
-      chrome::HostDesktopType active_desktop = chrome::GetActiveDesktop();
-      Browser* browser = chrome::FindLastActiveWithProfile(profile,
-          active_desktop);
+      Browser* browser = chrome::FindLastActiveWithProfile(profile);
       if (!browser)
-        browser = new Browser(Browser::CreateParams(Browser::TYPE_TABBED,
-                                                    profile, active_desktop));
+        browser = new Browser(Browser::CreateParams(profile));
       web_contents = browser->tab_strip_model()->GetActiveWebContents();
     }
     return new ExtensionInstallPrompt(web_contents);

commit 282c30dab31bf524b95fcb5f4451654bee5a8c39

Fixing PRESUBMIT.py broken by r169664.
TBR=joi@chromium.org

Review URL: https://codereview.chromium.org/11308228

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169666 0039d316-1c4b-4281-b951-d872f2087c98

PRESUBMIT.py
@@ -159,6 +159,7 @@ _BANNED_CPP_FUNCTIONS = (
        'id. Talk to robertshield@ for more information.',
       ),
       True,
+      (),
     ),
     (
       'browser::FindAnyBrowser',