ChromiumWebApps/chromium

commit e14bf0f0055f91aaf63c53f9c7ae01d14dd6f244

Alternate NTP: Move bookmark bar to bottom
This CL moves the detached bookmark bar to the bottom of the new tab page.

Some differences between this and the old detached bookmark bar:
  - view is horizontally centered
  - view has a maximum width of 720
  - view is hidden if the height of the new tab page goes below 558
  - view doesn't animate when attached or detached from the toolbar

Some things that this CL doesn't do yet:
  - update the look of the detached bookmark bar
  - hide the attached bookmark bar when showing instant search results

Screenshot: http://i.imgur.com/ihkO0.png

BUG=160843
TBR=ben@chromium.org

Review URL: https://chromiumcodereview.appspot.com/11428149

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171075 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h
@@ -286,6 +286,13 @@ willAnimateFromState:(bookmarks::VisualState)oldState
 // Update the visible state of the bookmark bar.
 - (void)updateVisibility;
 
+// Hides or shows the bookmark bar depending on the current state.
+- (void)updateHiddenState;
+
+// Returns YES if the bookmark bar should be shown at the bottom of the content
+// view when detached.
+- (BOOL)shouldShowAtBottomWhenDetached;
+
 // Turn on or off the bookmark bar and prevent or reallow its appearance. On
 // disable, toggle off if shown. On enable, show only if needed. App and popup
 // windows do not show a bookmark bar.

chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.mm
@@ -44,6 +44,8 @@
 #import "chrome/browser/ui/cocoa/toolbar/toolbar_controller.h"
 #import "chrome/browser/ui/cocoa/view_id_util.h"
 #import "chrome/browser/ui/cocoa/view_resizer.h"
+#include "chrome/browser/ui/search/search.h"
+#include "chrome/browser/ui/search/search_ui.h"
 #include "chrome/browser/ui/webui/ntp/app_launcher_handler.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/pref_names.h"
@@ -255,6 +257,11 @@ void RecordAppLaunch(Profile* profile, GURL url) {
         rb.GetNativeImageNamed(IDR_BOOKMARK_BAR_FOLDER).CopyNSImage());
     defaultImage_.reset(
         rb.GetNativeImageNamed(IDR_DEFAULT_FAVICON).CopyNSImage());
+
+    // Disable animations if the detached bookmark bar will be shown at the
+    // bottom of the new tab page.
+    if ([self shouldShowAtBottomWhenDetached])
+      ignoreAnimations_ = YES;
 
     // Register for theme changes, bookmark button pulsing, ...
     NSNotificationCenter* defaultCenter = [NSNotificationCenter defaultCenter];
@@ -473,6 +480,17 @@ void RecordAppLaunch(Profile* profile, GURL url) {
 // simply update based on what we're told.
 - (void)updateVisibility {
   [self showBookmarkBarWithAnimation:NO];
+}
+
+- (void)updateHiddenState {
+  BOOL oldHidden = [[self view] isHidden];
+  BOOL newHidden = ![self isVisible];
+  if (oldHidden != newHidden)
+    [[self view] setHidden:newHidden];
+}
+
+- (BOOL)shouldShowAtBottomWhenDetached {
+  return chrome::search::IsInstantExtendedAPIEnabled(browser_->profile());
 }
 
 - (void)setBookmarkBarEnabled:(BOOL)enabled {
@@ -2278,6 +2296,13 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
 
 // (BookmarkBarState protocol)
 - (BOOL)isVisible {
+  if ([self shouldShowAtBottomWhenDetached] &&
+      visualState_ == bookmarks::kDetachedState &&
+      [self currentTabContentsHeight] <
+          chrome::search::kMinContentHeightForBottomBookmarkBar) {
+    return NO;
+  }
+
   return barIsEnabled_ && (visualState_ == bookmarks::kShowingState ||
                            visualState_ == bookmarks::kDetachedState ||
                            lastVisualState_ == bookmarks::kShowingState ||
@@ -2338,8 +2363,9 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
 #pragma mark BookmarkBarToolbarViewController Protocol
 
 - (int)currentTabContentsHeight {
-  WebContents* wc = chrome::GetActiveWebContents(browser_);
-  return wc ? wc->GetView()->GetContainerSize().height() : 0;
+  BrowserWindowController* browserController =
+      [BrowserWindowController browserWindowControllerForView:[self view]];
+  return NSHeight([[browserController tabContentArea] frame]);
 }
 
 - (ui::ThemeProvider*)themeProvider {

chrome/browser/policy/policy_browsertest.cc
@@ -738,13 +738,7 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, ForceSafeSearch) {
 IN_PROC_BROWSER_TEST_F(PolicyTest, ReplaceSearchTerms) {
   MakeRequestFail("search.example");
 
-  CommandLine::ForCurrentProcess()->AppendSwitch(
-      switches::kEnableInstantExtendedAPI);
-
-  // Adding the kEnableInstantExtendedAPI is not enough since
-  // IsInstantExtendedAPIEnabled does not return true on CHANNEL_DEV.
-  if (!chrome::search::IsInstantExtendedAPIEnabled(browser()->profile()))
-    return;
+  chrome::search::EnableInstantExtendedAPIForTesting();
 
   // Verifies that a default search is made using the provider configured via
   // policy. Also checks that default search can be completely disabled.

chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller_browsertest.mm
@@ -1,0 +1,120 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h"
+
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window.h"
+#import "chrome/browser/ui/cocoa/browser_window_controller.h"
+#import "chrome/browser/ui/cocoa/download/download_shelf_controller.h"
+#include "chrome/browser/ui/search/search.h"
+#include "chrome/browser/ui/search/search_ui.h"
+#include "chrome/common/url_constants.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "content/public/test/test_utils.h"
+#include "googleurl/src/gurl.h"
+
+class BottomBookmarkBarControllerTest : public InProcessBrowserTest {
+ public:
+  BottomBookmarkBarControllerTest() {
+    chrome::search::EnableInstantExtendedAPIForTesting();
+  }
+
+  virtual void SetUpOnMainThread() OVERRIDE {
+    AddTabAtIndex(1,
+                  GURL(chrome::kChromeUINewTabURL),
+                  content::PAGE_TRANSITION_LINK);
+    EXPECT_TRUE([GetBookmarkBarController() shouldShowAtBottomWhenDetached]);
+    EXPECT_FALSE([[GetBookmarkBarController() view] isHidden]);
+  }
+
+  BrowserWindowController* GetBrowserWindowController() const {
+    NSWindow* window = browser()->window()->GetNativeWindow();
+    return [BrowserWindowController browserWindowControllerForWindow:window];
+  }
+
+  BookmarkBarController* GetBookmarkBarController() const {
+    return [GetBrowserWindowController() bookmarkBarController];
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(BottomBookmarkBarControllerTest);
+};
+
+// Verify that the bookmark bar is positioned correctly when detached and
+// attached.
+IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, Position) {
+  // Verify detached bookmark bar is at the bottom of content area.
+  NSRect bookmark_bar_frame = [[GetBookmarkBarController() view] frame];
+  EXPECT_EQ(0, NSMinY(bookmark_bar_frame));
+
+  // Attach the bookmark bar and verify it's above the content area.
+  browser()->window()->ToggleBookmarkBar();
+  content::RunAllPendingInMessageLoop();
+  NSRect content_frame = [[GetBrowserWindowController() tabContentArea] frame];
+  bookmark_bar_frame = [[GetBookmarkBarController() view] frame];
+  EXPECT_EQ(NSMinX(content_frame), NSMinX(bookmark_bar_frame));
+  EXPECT_EQ(NSMaxY(content_frame), NSMinY(bookmark_bar_frame));
+}
+
+// Test that detached bookmark bar is positioned correctly when the download
+// shelf is visible.
+IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, PositionWithDownloads) {
+  // Show the download shelf.
+  DownloadShelfController* download_shelf_controller =
+      [GetBrowserWindowController() downloadShelf];
+  [download_shelf_controller show:nil];
+  EXPECT_FALSE([[download_shelf_controller view] isHidden]);
+
+  // Verify that the bookmark bar is at the bottom of the content area but above
+  // the download shelf.
+  NSRect content_frame = [[GetBrowserWindowController() tabContentArea] frame];
+  NSRect bookmark_bar_frame = [[GetBookmarkBarController() view] frame];
+  EXPECT_EQ(NSMinY(content_frame), NSMinY(bookmark_bar_frame));
+  EXPECT_NE(0, NSMinY(bookmark_bar_frame));
+  EXPECT_EQ(NSMinY(content_frame), NSMinY(bookmark_bar_frame));
+}
+
+// Verify that the bookmark bar shrinks when the window is narrow.
+IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, NarrowWindow) {
+  NSWindow* window = browser()->window()->GetNativeWindow();
+  NSRect window_frame = [window frame];
+  CGFloat expected_width =
+      roundf(chrome::search::kMaxWidthForBottomBookmarkBar / 2.0);
+  window_frame.size.width = expected_width +
+      chrome::search::kHorizontalPaddingForBottomBookmarkBar * 2;
+  [window setFrame:window_frame display:YES];
+  EXPECT_FALSE([[GetBookmarkBarController() view] isHidden]);
+
+  NSRect frame = [[GetBookmarkBarController() view] frame];
+  EXPECT_EQ(chrome::search::kHorizontalPaddingForBottomBookmarkBar,
+            NSMinX(frame));
+  EXPECT_EQ(expected_width, NSWidth(frame));
+}
+
+// Verify that the bookmark bar's width doesn't go above its maximum value.
+IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, WideWindow) {
+  NSWindow* window = browser()->window()->GetNativeWindow();
+  NSRect window_frame = [window frame];
+  window_frame.size.width = chrome::search::kMaxWidthForBottomBookmarkBar +
+      chrome::search::kHorizontalPaddingForBottomBookmarkBar * 2 + 100;
+  [window setFrame:window_frame display:YES];
+  EXPECT_FALSE([[GetBookmarkBarController() view] isHidden]);
+
+  NSRect frame = [[GetBookmarkBarController() view] frame];
+  EXPECT_EQ(roundf((NSWidth(window_frame) - NSWidth(frame)) / 2.0),
+            NSMinX(frame));
+  EXPECT_EQ(chrome::search::kMaxWidthForBottomBookmarkBar, NSWidth(frame));
+}
+
+// Verify that the bookmark bar is hidden if the content area height falls
+// below a minimum value.
+IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, MinContentHeight) {
+  NSWindow* window = browser()->window()->GetNativeWindow();
+  NSRect window_frame = [window frame];
+  window_frame.size.height =
+      chrome::search::kMinContentHeightForBottomBookmarkBar;
+  [window setFrame:window_frame display:YES];
+  EXPECT_TRUE([[GetBookmarkBarController() view] isHidden]);
+}

chrome/browser/ui/cocoa/browser_window_controller.h
@@ -264,6 +264,8 @@ class WebContents;
 // tab/tab contents).
 - (void)updateBookmarkBarVisibilityWithAnimation:(BOOL)animate;
 
+- (BookmarkBarController*)bookmarkBarController;
+
 - (BOOL)isDownloadShelfVisible;
 
 // Lazily creates the download shelf in visible state if it doesn't exist yet.

chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -1442,6 +1442,10 @@ enum {
                withAnimation:animate];
 }
 
+- (BookmarkBarController*)bookmarkBarController {
+  return bookmarkBarController_;
+}
+
 - (BOOL)isDownloadShelfVisible {
   return downloadShelfController_ != nil &&
       [downloadShelfController_ isVisible];
@@ -1769,6 +1773,11 @@ enum {
   // do this.
   if ([findBarCocoaController_ isFindBarVisible])
     [self layoutSubviews];
+
+  if ([self placeBookmarkBarBelowInfoBar] &&
+      [bookmarkBarController_ shouldShowAtBottomWhenDetached]) {
+    [self layoutBottomBookmarkBarInContentFrame:[[self tabContentArea] frame]];
+  }
 }
 
 // Handle the openLearnMoreAboutCrashLink: action from SadTabController when

chrome/browser/ui/cocoa/browser_window_controller_private.h
@@ -65,9 +65,12 @@
 // call it with the appropriate |maxY| which depends on whether or not the
 // bookmark bar is shown as the NTP bubble or not (use
 // |-placeBookmarkBarBelowInfoBar|).
-- (CGFloat)layoutBookmarkBarAtMinX:(CGFloat)minX
-                              maxY:(CGFloat)maxY
-                             width:(CGFloat)width;
+- (CGFloat)layoutTopBookmarkBarAtMinX:(CGFloat)minX
+                                 maxY:(CGFloat)maxY
+                                width:(CGFloat)width;
+
+// Lays out the bookmark at the bottom of the content area.
+- (void)layoutBottomBookmarkBarInContentFrame:(NSRect)contentFrame;
 
 // Lay out the view which draws the background for the floating bar when in
 // presentation mode, with the given frame and presentation-mode-status. Should

chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -32,6 +32,7 @@
 #import "chrome/browser/ui/cocoa/tabs/tab_strip_view.h"
 #import "chrome/browser/ui/cocoa/toolbar/toolbar_controller.h"
 #include "chrome/browser/ui/fullscreen/fullscreen_controller.h"
+#include "chrome/browser/ui/search/search_ui.h"
 #include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -212,7 +213,7 @@ willPositionSheet:(NSWindow*)sheet
   // immediately below the toolbar.
   BOOL placeBookmarkBarBelowInfoBar = [self placeBookmarkBarBelowInfoBar];
   if (!placeBookmarkBarBelowInfoBar)
-    maxY = [self layoutBookmarkBarAtMinX:minX maxY:maxY width:width];
+    maxY = [self layoutTopBookmarkBarAtMinX:minX maxY:maxY width:width];
 
   // The floating bar backing view doesn't actually add any height.
   NSRect floatingBarBackingRect =
@@ -235,12 +236,18 @@ willPositionSheet:(NSWindow*)sheet
   // presentation mode in which case it's at the top of the visual content area.
   maxY = [self layoutInfoBarAtMinX:minX maxY:maxY width:width];
 
-  // If the bookmark bar is detached, place it next in the visual content area.
-  if (placeBookmarkBarBelowInfoBar)
-    maxY = [self layoutBookmarkBarAtMinX:minX maxY:maxY width:width];
-
   // Place the download shelf, if any, at the bottom of the view.
   minY = [self layoutDownloadShelfAtMinX:minX minY:minY width:width];
+
+  // Place the bookmark bar.
+  if (placeBookmarkBarBelowInfoBar) {
+    if ([bookmarkBarController_ shouldShowAtBottomWhenDetached]) {
+      [self layoutBottomBookmarkBarInContentFrame:
+          NSMakeRect(minX, minY, width, maxY - minY)];
+    } else {
+      maxY = [self layoutTopBookmarkBarAtMinX:minX maxY:maxY width:width];
+    }
+  }
 
   // Finally, the content area takes up all of the remaining space.
   NSRect contentAreaRect = NSMakeRect(minX, minY, width, maxY - minY);
@@ -370,26 +377,50 @@ willPositionSheet:(NSWindow*)sheet
       [bookmarkBarController_ isAnimatingFromState:bookmarks::kDetachedState];
 }
 
-- (CGFloat)layoutBookmarkBarAtMinX:(CGFloat)minX
-                              maxY:(CGFloat)maxY
-                             width:(CGFloat)width {
+- (CGFloat)layoutTopBookmarkBarAtMinX:(CGFloat)minX
+                                 maxY:(CGFloat)maxY
+                               width:(CGFloat)width {
+  [bookmarkBarController_ updateHiddenState];
+
   NSView* bookmarkBarView = [bookmarkBarController_ view];
-  NSRect bookmarkBarFrame = [bookmarkBarView frame];
-  BOOL oldHidden = [bookmarkBarView isHidden];
-  BOOL newHidden = ![self isBookmarkBarVisible];
-  if (oldHidden != newHidden)
-    [bookmarkBarView setHidden:newHidden];
-  bookmarkBarFrame.origin.x = minX;
-  bookmarkBarFrame.origin.y = maxY - NSHeight(bookmarkBarFrame);
-  bookmarkBarFrame.size.width = width;
-  [bookmarkBarView setFrame:bookmarkBarFrame];
-  maxY -= NSHeight(bookmarkBarFrame);
+  NSRect frame = [bookmarkBarView frame];
+  frame.origin.x = minX;
+  frame.origin.y = maxY - NSHeight(frame);
+  frame.size.width = width;
+  [bookmarkBarView setFrame:frame];
+  maxY -= NSHeight(frame);
+
+  // Pin the bookmark bar to the top of the window and make the width flexible.
+  [bookmarkBarView setAutoresizingMask:NSViewWidthSizable | NSViewMinYMargin];
 
   // TODO(viettrungluu): Does this really belong here? Calling it shouldn't be
   // necessary in the non-NTP case.
   [bookmarkBarController_ layoutSubviews];
 
   return maxY;
+}
+
+- (void)layoutBottomBookmarkBarInContentFrame:(NSRect)contentFrame {
+  [bookmarkBarController_ updateHiddenState];
+
+  NSView* bookmarkBarView = [bookmarkBarController_ view];
+  NSRect frame;
+  frame.size.width = NSWidth(contentFrame) -
+      chrome::search::kHorizontalPaddingForBottomBookmarkBar * 2;
+  frame.size.width = std::min(frame.size.width,
+      static_cast<CGFloat>(chrome::search::kMaxWidthForBottomBookmarkBar));
+  frame.size.height = NSHeight([bookmarkBarView frame]);
+  frame.origin.x = NSMinX(contentFrame) +
+      roundf((NSWidth(contentFrame)- frame.size.width) / 2.0);
+  frame.origin.y = NSMinY(contentFrame);
+  [bookmarkBarView setFrame:frame];
+
+  // Disable auto-resizing.
+  [bookmarkBarView setAutoresizingMask:0];
+
+  // TODO(viettrungluu): Does this really belong here? Calling it shouldn't be
+  // necessary in the non-NTP case.
+  [bookmarkBarController_ layoutSubviews];
 }
 
 - (void)layoutFloatingBarBackingView:(NSRect)frame

commit a70fcc778be6675b740f6a921a9d69bdda7f9a1c

Alternate NTP: Enable bookmark delete animation
With instant extended enabled, deleting a bookmark wouldn't show the "poof" animation.

This bug was caused by r171075 where I disabled state transition animations when instant extended was enabled.

The flag I used to disable state transition animations also controlled the delete animation.

To fix this I'm cleaning up the various animation flags. Currently animations are controlled in two ways:
  (1) setting ignoreAnimations_ to NO
  (2) overriding -[BookmarkBarController animationEnabled] and returning NO
I'm renaming ignoreAnimations_ to stateAnimationsEnabled_. I'm replacing -[BookmarkBarController animationEnabled] with innerContentAnimationsEnabled_. This should make it easier to understand what the different flags do.

BUG=167713

Review URL: https://codereview.chromium.org/11783100

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176263 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h
@@ -252,9 +252,13 @@ willAnimateFromState:(BookmarkBar::State)oldState
   // or a folder menu.
   BOOL showFolderMenus_;
 
-  // Set to YES to prevent any node animations. Useful for unit testing so that
-  // incomplete animations do not cause valgrind complaints.
-  BOOL ignoreAnimations_;
+  // If YES then state changes (for example, from hidden to shown) are animated.
+  // This is turned off for instant extended and for unit tests.
+  BOOL stateAnimationsEnabled_;
+
+  // If YES then changes inside the bookmark bar (for example, removing a
+  // bookmark) are animated. This is turned off for unit tests.
+  BOOL innerContentAnimationsEnabled_;
 
   // YES if there is a possible drop about to happen in the bar.
   BOOL hasInsertionPos_;
@@ -272,6 +276,8 @@ willAnimateFromState:(BookmarkBar::State)oldState
 @property(readonly, nonatomic) BOOL isAnimationRunning;
 @property(assign, nonatomic) id<BookmarkBarControllerDelegate> delegate;
 @property(readonly, nonatomic) BOOL isEmpty;
+@property(assign, nonatomic) BOOL stateAnimationsEnabled;
+@property(assign, nonatomic) BOOL innerContentAnimationsEnabled;
 
 // Initializes the bookmark bar controller with the given browser
 // profile and delegates.
@@ -407,8 +413,6 @@ willAnimateFromState:(BookmarkBar::State)oldState
 - (NSMenu *)menuForFolderNode:(const BookmarkNode*)node;
 - (NSMenu*)buttonContextMenu;
 - (void)setButtonContextMenu:(id)menu;
-// Set to YES in order to prevent animations.
-- (void)setIgnoreAnimations:(BOOL)ignore;
 @end
 
 #endif  // CHROME_BROWSER_UI_COCOA_BOOKMARKS_BOOKMARK_BAR_CONTROLLER_H_

chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.mm
@@ -220,7 +220,6 @@ void RecordAppLaunch(Profile* profile, GURL url) {
 - (void)setNodeForBarMenu;
 - (void)watchForExitEvent:(BOOL)watch;
 - (void)resetAllButtonPositionsWithAnimation:(BOOL)animate;
-- (BOOL)animationEnabled;
 
 @end
 
@@ -231,6 +230,8 @@ void RecordAppLaunch(Profile* profile, GURL url) {
 @synthesize isAnimationRunning = isAnimationRunning_;
 @synthesize delegate = delegate_;
 @synthesize isEmpty = isEmpty_;
+@synthesize stateAnimationsEnabled = stateAnimationsEnabled_;
+@synthesize innerContentAnimationsEnabled = innerContentAnimationsEnabled_;
 
 - (id)initWithBrowser:(Browser*)browser
          initialWidth:(CGFloat)initialWidth
@@ -255,10 +256,11 @@ void RecordAppLaunch(Profile* profile, GURL url) {
     defaultImage_.reset(
         rb.GetNativeImageNamed(IDR_DEFAULT_FAVICON).CopyNSImage());
 
-    // Disable animations if the detached bookmark bar will be shown at the
-    // bottom of the new tab page.
-    if ([self shouldShowAtBottomWhenDetached])
-      ignoreAnimations_ = YES;
+    innerContentAnimationsEnabled_ = YES;
+    // Disable state animations (for example, showing or hiding the bookmark
+    // bar) if the detached bookmark bar will be shown at the bottom of the new
+    // tab page.
+    stateAnimationsEnabled_ = ![self shouldShowAtBottomWhenDetached];
 
     // Register for theme changes, bookmark button pulsing, ...
     NSNotificationCenter* defaultCenter = [NSNotificationCenter defaultCenter];
@@ -277,12 +279,6 @@ void RecordAppLaunch(Profile* profile, GURL url) {
     [[self animatableView] setResizeDelegate:resizeDelegate];
   }
   return self;
-}
-
-// Can be overridden in a test subclass if a simplistic test is being confused
-// by asynchronous animation or is running needlessly slow.
-- (BOOL)animationEnabled {
-  return YES;
 }
 
 - (void)pulseBookmarkNotification:(NSNotification*)notification {
@@ -642,7 +638,7 @@ void RecordAppLaunch(Profile* profile, GURL url) {
 
 - (IBAction)openBookmark:(id)sender {
   BOOL isMenuItem = [[sender cell] isFolderButtonCell];
-  BOOL animate = isMenuItem && [self animationEnabled];
+  BOOL animate = isMenuItem && innerContentAnimationsEnabled_;
   if (animate)
     [self doMenuFlashOnSeparateThread:sender];
   DCHECK([sender respondsToSelector:@selector(bookmarkNode)]);
@@ -993,7 +989,7 @@ void RecordAppLaunch(Profile* profile, GURL url) {
 
 // (Private)
 - (void)showBookmarkBarWithAnimation:(BOOL)animate {
-  if (animate && !ignoreAnimations_) {
+  if (animate && stateAnimationsEnabled_) {
     // If |-doBookmarkBarAnimation| does the animation, we're done.
     if ([self doBookmarkBarAnimation])
       return;
@@ -1481,7 +1477,7 @@ void RecordAppLaunch(Profile* profile, GURL url) {
   isAnimationRunning_ = YES;
 
   // Animate only if told to and if bar is enabled.
-  if (animate && !ignoreAnimations_ && barIsEnabled_) {
+  if (animate && stateAnimationsEnabled_ && barIsEnabled_) {
     [self closeAllBookmarkFolders];
     // Take care of any animation cases we know how to handle.
 
@@ -1510,7 +1506,7 @@ void RecordAppLaunch(Profile* profile, GURL url) {
 - (void)updateState:(BookmarkBar::State)newState
          changeType:(BookmarkBar::AnimateChangeType)changeType {
   BOOL animate = changeType == BookmarkBar::ANIMATE_STATE_CHANGE &&
-                 !ignoreAnimations_;
+                 stateAnimationsEnabled_;
   [self moveToState:newState withAnimation:animate];
 }
 
@@ -2095,7 +2091,6 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
 // hypothetical drop with the new button having a left edge of |where|.
 // Gets called only by our view.
 - (void)setDropInsertionPos:(CGFloat)where {
-  BOOL animate = [self animationEnabled];
   if (!hasInsertionPos_ || where != insertionPos_) {
     insertionPos_ = where;
     hasInsertionPos_ = YES;
@@ -2119,7 +2114,7 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
       if (left > insertionPos_)
         buttonFrame.origin.x += paddingWidth;
       left += bookmarks::kBookmarkHorizontalPadding;
-      if (animate)
+      if (innerContentAnimationsEnabled_)
         [[button animator] setFrame:buttonFrame];
       else
         [button setFrame:buttonFrame];
@@ -2132,7 +2127,7 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
 // This is generally useful, so is called from various places internally.
 - (void)resetAllButtonPositionsWithAnimation:(BOOL)animate {
   CGFloat left = bookmarks::kBookmarkHorizontalPadding;
-  animate &= [self animationEnabled];
+  animate &= innerContentAnimationsEnabled_;
 
   for (NSButton* button in buttons_.get()) {
     // Hidden buttons get no space.
@@ -2807,7 +2802,7 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
       // If we are deleting a button whose folder is currently open, close it!
       [self closeAllBookmarkFolders];
     }
-    if (animate && !ignoreAnimations_ && [self isVisible] &&
+    if (animate && innerContentAnimationsEnabled_ && [self isVisible] &&
         [[self browserWindow] isMainWindow]) {
       NSPoint poofPoint = [oldButton screenLocationForRemoveAnimation];
       NSShowAnimationEffect(NSAnimationEffectDisappearingItemDefault, poofPoint,
@@ -2847,10 +2842,6 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
 // to minimize touching the object passed in (likely a mock).
 - (void)setButtonContextMenu:(id)menu {
   buttonContextMenu_ = menu;
-}
-
-- (void)setIgnoreAnimations:(BOOL)ignore {
-  ignoreAnimations_ = ignore;
 }
 
 @end

chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller_unittest.mm
@@ -39,11 +39,25 @@
 // Unit tests don't need time-consuming asynchronous animations.
 @interface BookmarkBarControllerTestable : BookmarkBarController {
 }
+
 @end
+
 @implementation BookmarkBarControllerTestable
-- (BOOL)animationEnabled {
-  return NO;
+
+- (id)initWithBrowser:(Browser*)browser
+         initialWidth:(CGFloat)initialWidth
+             delegate:(id<BookmarkBarControllerDelegate>)delegate
+       resizeDelegate:(id<ViewResizer>)resizeDelegate {
+  if ((self = [super initWithBrowser:browser
+                        initialWidth:initialWidth
+                            delegate:delegate
+                      resizeDelegate:resizeDelegate])) {
+    [self setStateAnimationsEnabled:NO];
+    [self setInnerContentAnimationsEnabled:NO];
+  }
+  return self;
 }
+
 @end
 
 // Just like a BookmarkBarController but openURL: is stubbed out.
@@ -553,7 +567,6 @@ TEST_F(BookmarkBarControllerTest, NoItemContainerGoesAway) {
 // Confirm off the side button only enabled when reasonable.
 TEST_F(BookmarkBarControllerTest, OffTheSideButtonHidden) {
   BookmarkModel* model = BookmarkModelFactory::GetForProfile(profile());
-  [bar_ setIgnoreAnimations:YES];
 
   [bar_ loaded:model];
   EXPECT_TRUE([bar_ offTheSideButtonIsHidden]);
@@ -603,7 +616,6 @@ TEST_F(BookmarkBarControllerTest, OffTheSideButtonHidden) {
 // in this area to reproduce the crash.
 TEST_F(BookmarkBarControllerTest, DeleteFromOffTheSideWhileItIsOpen) {
   BookmarkModel* model = BookmarkModelFactory::GetForProfile(profile());
-  [bar_ setIgnoreAnimations:YES];
   [bar_ loaded:model];
 
   // Add a lot of bookmarks (per the bug).
@@ -1566,6 +1578,7 @@ TEST_F(BookmarkBarControllerTest, NodeDeletedWhileContextMenuIsOpen) {
 
 TEST_F(BookmarkBarControllerTest, CloseFolderOnAnimate) {
   BookmarkModel* model = BookmarkModelFactory::GetForProfile(profile());
+  [bar_ setStateAnimationsEnabled:YES];
   const BookmarkNode* parent = model->bookmark_bar_node();
   const BookmarkNode* folder = model->AddFolder(parent,
                                                 parent->child_count(),