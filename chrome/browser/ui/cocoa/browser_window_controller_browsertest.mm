ChromiumWebApps/chromium

commit fcd2416310ffaa25d1e143cc5ac6a9b32dd3a225

Fix bookmark bar hit testing
With r178392 bookmark bar hit testing stopped working.

The problem was that the bookmark bar now overlapped the tab contents area.

Fix was to take into account the overlap between the bookmark bar and the content area when doing hit testing.

BUG=172211
UNITTEST=BrowserWindowControllerTest.BookmarkBarHitTest. Verified that the test failed before my patch and passed after it.

Review URL: https://chromiumcodereview.appspot.com/12091008

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179650 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -18,6 +18,7 @@
 #include "chrome/browser/ui/browser_window_state.h"
 #include "chrome/browser/ui/cocoa/last_active_browser_cocoa.h"
 #import "chrome/browser/ui/cocoa/browser/avatar_button_controller.h"
+#import "chrome/browser/ui/cocoa/dev_tools_controller.h"
 #import "chrome/browser/ui/cocoa/fast_resize_view.h"
 #import "chrome/browser/ui/cocoa/find_bar/find_bar_cocoa_controller.h"
 #import "chrome/browser/ui/cocoa/floating_bar_backing_view.h"
@@ -914,6 +915,9 @@ willPositionSheet:(NSWindow*)sheet
 
   // Prevent the fast resize view from drawing white over the bookmark bar.
   [[self tabContentArea] setContentOffset:toolbarToWebContentsOffset_];
+
+  // Prevent the dev tools splitter from overlapping the bookmark bar.
+  [devToolsController_ setTopContentOffset:toolbarToWebContentsOffset_];
 }
 
 - (void)updateSubviewZOrder:(BOOL)inPresentationMode {

chrome/browser/ui/cocoa/browser_window_controller_unittest.mm
@@ -141,8 +141,7 @@ TEST_F(BrowserWindowControllerTest, TestFullScreenWindow) {
 
 TEST_F(BrowserWindowControllerTest, TestNormal) {
   // Force the bookmark bar to be shown.
-  profile()->GetPrefs()->
-      SetBoolean(prefs::kShowBookmarkBar, true);
+  profile()->GetPrefs()->SetBoolean(prefs::kShowBookmarkBar, true);
   [controller_ browserWindow]->BookmarkBarStateChanged(
       BookmarkBar::DONT_ANIMATE_STATE_CHANGE);
 
@@ -458,8 +457,7 @@ TEST_F(BrowserWindowControllerTest, TestResizeViews) {
 
 TEST_F(BrowserWindowControllerTest, TestResizeViewsWithBookmarkBar) {
   // Force a display of the bookmark bar.
-  profile()->GetPrefs()->
-      SetBoolean(prefs::kShowBookmarkBar, true);
+  profile()->GetPrefs()->SetBoolean(prefs::kShowBookmarkBar, true);
   [controller_ browserWindow]->BookmarkBarStateChanged(
       BookmarkBar::DONT_ANIMATE_STATE_CHANGE);
 
@@ -505,8 +503,7 @@ TEST_F(BrowserWindowControllerTest, TestResizeViewsWithBookmarkBar) {
   CheckViewPositions(controller_);
 
   // Remove the bookmark bar and recheck
-  profile()->GetPrefs()->
-      SetBoolean(prefs::kShowBookmarkBar, false);
+  profile()->GetPrefs()->SetBoolean(prefs::kShowBookmarkBar, false);
   [controller_ resizeView:bookmark newHeight:0];
   CheckViewPositions(controller_);
 
@@ -801,6 +798,21 @@ TEST_F(BrowserWindowControllerTest, TestSigninMenuItemWithNonSeparator) {
   EXPECT_TRUE([followingNonSeparator isEnabled]);
   EXPECT_FALSE([signinMenuItem isHidden]);
   EXPECT_TRUE([followingNonSeparator isHidden]);
+}
+
+// Verify that hit testing works correctly when the bookmark bar overlaps
+// web contents.
+TEST_F(BrowserWindowControllerTest, BookmarkBarHitTest) {
+  profile()->GetPrefs()->SetBoolean(prefs::kShowBookmarkBar, true);
+  [controller_ browserWindow]->BookmarkBarStateChanged(
+      BookmarkBar::DONT_ANIMATE_STATE_CHANGE);
+
+  NSView* bookmarkView = [controller_ bookmarkView];
+  NSView* contentView = [[controller_ window] contentView];
+  NSPoint point = [bookmarkView convertPoint:NSMakePoint(1, 1)
+                                      toView:[contentView superview]];
+
+  EXPECT_TRUE([[contentView hitTest:point] isDescendantOf:bookmarkView]);
 }
 
 @interface BrowserWindowControllerFakeFullscreen : BrowserWindowController {

chrome/browser/ui/cocoa/dev_tools_controller.h
@@ -10,9 +10,7 @@
 #include "base/memory/scoped_nsobject.h"
 #include "chrome/browser/devtools/devtools_window.h"
 
-@class NSSplitView;
-@class NSView;
-
+@class GraySplitView;
 class Profile;
 
 namespace content {
@@ -25,7 +23,7 @@ class WebContents;
 @interface DevToolsController : NSObject<NSSplitViewDelegate> {
  @private
   // A view hosting docked devTools contents.
-  scoped_nsobject<NSSplitView> splitView_;
+  scoped_nsobject<GraySplitView> splitView_;
 
   DevToolsDockSide dockSide_;
 
@@ -48,6 +46,9 @@ class WebContents;
 // the actual resize).
 - (void)updateDevToolsForWebContents:(content::WebContents*)contents
                          withProfile:(Profile*)profile;
+
+- (void)setTopContentOffset:(CGFloat)offset;
+
 @end
 
 #endif  // CHROME_BROWSER_UI_COCOA_DEV_TOOLS_CONTROLLER_H_

chrome/browser/ui/cocoa/dev_tools_controller.mm
@@ -17,15 +17,32 @@
 
 using content::WebContents;
 
-@interface GraySplitView : NSSplitView
+@interface GraySplitView : NSSplitView {
+  CGFloat topContentOffset_;
+}
+
+@property(assign, nonatomic) CGFloat topContentOffset;
+
 - (NSColor*)dividerColor;
+
 @end
 
 
 @implementation GraySplitView
+
+@synthesize topContentOffset = topContentOffset_;
+
 - (NSColor*)dividerColor {
   return [NSColor darkGrayColor];
 }
+
+- (NSView*)hitTest:(NSPoint)point {
+  NSPoint viewPoint = [self convertPoint:point fromView:[self superview]];
+  if (viewPoint.y < topContentOffset_)
+    return nil;
+  return [super hitTest:point];
+}
+
 @end
 
 
@@ -97,6 +114,10 @@ using content::WebContents;
     dockSide_ = devToolsWindow_->dock_side();
     [self showDevToolsContainer];
   }
+}
+
+- (void)setTopContentOffset:(CGFloat)offset {
+  [splitView_ setTopContentOffset:offset];
 }
 
 - (void)showDevToolsContainer {

chrome/browser/ui/cocoa/fast_resize_view.mm
@@ -46,6 +46,15 @@
   NSRectFill(dirtyRect);
 }
 
+- (NSView*)hitTest:(NSPoint)point {
+  NSView* result = [super hitTest:point];
+  // Never return this view during hit testing. This allows overlapping views to
+  // get events even when they are not topmost.
+  if ([result isEqual:self])
+    return nil;
+  return result;
+}
+
 @end
 
 @implementation FastResizeView (PrivateMethods)

commit 19f242670641c3c1efb5c2039754b45ebde471a6

Alternate NTP: Fix hit testing on instant search results
Clicking on the first instant search result would incorrectly cause a
click on the bookmark bar.

This was a regression caused by r179650. In that revision I changed
the dev tools view to ignore clicks when it overlapped the bookmark
bar.

My original code didn't handle the case where instant search results
were showing. This CL correctly handles all cases.

BUG=174016
TEST=Navigated to cnn.com and typed a query in the omnibox. Verified
that clicking on the first search result worked.
Also added a regression test
BrowserWindowControllerTest.InstantSearchResultsHitTest. Verified that
the test failed without my patch and passes with it.


Review URL: https://chromiumcodereview.appspot.com/12217050

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181224 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/browser_window_controller_browsertest.mm
@@ -437,3 +437,20 @@ IN_PROC_BROWSER_TEST_F(BrowserWindowControllerTest, ContentOffsetInstantNPT) {
   EXPECT_EQ(GetViewHeight(VIEW_ID_INFO_BAR), [preview previewOffset]);
   EXPECT_EQ(GetViewHeight(VIEW_ID_INFO_BAR), [preview activeContainerOffset]);
 }
+
+// Verify that if bookmark bar is underneath instant search results then
+// clicking on instant search results still works.
+IN_PROC_BROWSER_TEST_F(BrowserWindowControllerTest,
+                       InstantSearchResultsHitTest) {
+  browser()->window()->ToggleBookmarkBar();
+  ShowInstantResults();
+
+  NSView* bookmarkView = [[controller() bookmarkBarController] view];
+  NSView* contentView = [[controller() window] contentView];
+  NSPoint point = [bookmarkView convertPoint:NSMakePoint(1, 1)
+                                      toView:[contentView superview]];
+
+  EXPECT_FALSE([[contentView hitTest:point] isDescendantOf:bookmarkView]);
+  EXPECT_TRUE([[contentView hitTest:point]
+      isDescendantOf:[controller() tabContentArea]]);
+}

chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -917,7 +917,10 @@ willPositionSheet:(NSWindow*)sheet
   [[self tabContentArea] setContentOffset:toolbarToWebContentsOffset_];
 
   // Prevent the dev tools splitter from overlapping the bookmark bar.
-  [devToolsController_ setTopContentOffset:toolbarToWebContentsOffset_];
+  if ([self isShowingInstantResults])
+    [devToolsController_ setTopContentOffset:previewOffset];
+  else
+    [devToolsController_ setTopContentOffset:toolbarToWebContentsOffset_];
 }
 
 - (void)updateSubviewZOrder:(BOOL)inPresentationMode {