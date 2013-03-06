ChromiumWebApps/chromium

commit ccf456dc41e2977ba48d4fc12ddfb25e867e0d8d

Separate invalidator and sync client ID (part 1/2)
This change implements support for setting the sync client ID and
invalidator client ID separately.  The two IDs are managed separately
and both of them are sent up to the server.  This change includes some
additional changes to support the transition, such as a new field in
the about:sync UI.

At this point, the IDs are not different.  Both are initialized from the
same source: the sync database.  The work to store and manage the
invalidator's ID separate from sync was begun in r180907 and will be
finished in a separate commit.  We will not be able to complete that
work until the server supports separate IDs.

BUG=124142


Review URL: https://chromiumcodereview.appspot.com/12256033

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185721 0039d316-1c4b-4281-b951-d872f2087c98

sync/internal_api/sync_manager_impl.cc
@@ -415,11 +415,17 @@ void SyncManagerImpl::Init(
   connection_manager_->set_client_id(directory()->cache_guid());
   connection_manager_->AddListener(this);
 
-  // Retrieve and set the sync notifier id.
-  std::string unique_id = directory()->cache_guid();
-  DVLOG(1) << "Read notification unique ID: " << unique_id;
-  allstatus_.SetUniqueId(unique_id);
-  invalidator_->SetUniqueId(unique_id);
+  std::string sync_id = directory()->cache_guid();
+
+  // TODO(rlarocque): The invalidator client ID should be independent from the
+  // sync client ID.  See crbug.com/124142.
+  const std::string invalidator_client_id = sync_id;
+
+  allstatus_.SetSyncId(sync_id);
+  allstatus_.SetInvalidatorClientId(invalidator_client_id);
+
+  DVLOG(1) << "Setting sync client ID: " << sync_id;
+  DVLOG(1) << "Setting invalidator client ID: " << invalidator_client_id;
 
   // Build a SyncSessionContext and store the worker in it.
   DVLOG(1) << "Sync is bringing up SyncSessionContext.";
@@ -434,7 +440,8 @@ void SyncManagerImpl::Init(
       &throttled_data_type_tracker_,
       listeners,
       &debug_info_event_listener_,
-      &traffic_recorder_).Pass();
+      &traffic_recorder_,
+      invalidator_client_id).Pass();
   session_context_->set_account_name(credentials.email);
   scheduler_ = internal_components_factory->BuildScheduler(
       name_, session_context_.get()).Pass();

chrome/browser/sync/about_sync_util.cc
@@ -179,7 +179,8 @@ scoped_ptr<DictionaryValue> ConstructAboutInformation(
   StringSyncStat server_url(section_version, "Server URL");
 
   ListValue* section_credentials = AddSection(stats_list, kCredentialsTitle);
-  StringSyncStat client_id(section_credentials, "Client ID");
+  StringSyncStat sync_id(section_credentials, "Sync Client ID");
+  StringSyncStat invalidator_id(section_credentials, "Invalidator Client ID");
   StringSyncStat username(section_credentials, "Username");
   BoolSyncStat is_token_available(section_credentials, "Sync Token Available");
 
@@ -289,8 +290,10 @@ scoped_ptr<DictionaryValue> ConstructAboutInformation(
 
   server_url.SetValue(service->sync_service_url().spec());
 
-  if (is_status_valid && !full_status.unique_id.empty())
-    client_id.SetValue(full_status.unique_id);
+  if (is_status_valid && !full_status.sync_id.empty())
+    sync_id.SetValue(full_status.sync_id);
+  if (is_status_valid && !full_status.invalidator_client_id.empty())
+    invalidator_id.SetValue(full_status.invalidator_client_id);
   if (service->signin())
     username.SetValue(service->signin()->GetAuthenticatedUsername());
   is_token_available.SetValue(service->IsSyncTokenAvailable());

sync/engine/all_status.cc
@@ -167,9 +167,15 @@ void AllStatus::SetKeystoreMigrationTime(const base::Time& migration_time) {
   status_.keystore_migration_time = migration_time;
 }
 
-void AllStatus::SetUniqueId(const std::string& guid) {
+void AllStatus::SetSyncId(const std::string& sync_id) {
   ScopedStatusLock lock(this);
-  status_.unique_id = guid;
+  status_.sync_id = sync_id;
+}
+
+void AllStatus::SetInvalidatorClientId(
+    const std::string& invalidator_client_id) {
+  ScopedStatusLock lock(this);
+  status_.invalidator_client_id = invalidator_client_id;
 }
 
 void AllStatus::IncrementNudgeCounter(NudgeSource source) {

sync/engine/all_status.h
@@ -58,7 +58,8 @@ class AllStatus : public SyncEngineEventListener {
   void SetHasKeystoreKey(bool has_keystore_key);
   void SetKeystoreMigrationTime(const base::Time& migration_time);
 
-  void SetUniqueId(const std::string& guid);
+  void SetSyncId(const std::string& sync_id);
+  void SetInvalidatorClientId(const std::string& invalidator_client_id);
 
   void IncrementNudgeCounter(NudgeSource source);
 

sync/engine/sync_scheduler_unittest.cc
@@ -128,7 +128,8 @@ class SyncSchedulerTest : public testing::Test {
             connection_.get(), directory(), workers,
             &extensions_activity_monitor_, throttled_data_type_tracker_.get(),
             std::vector<SyncEngineEventListener*>(), NULL, NULL,
-            true  /* enable keystore encryption */));
+            true,  // enable keystore encryption
+            "fake_invalidator_client_id"));
     context_->set_routing_info(routing_info_);
     context_->set_notifications_enabled(true);
     context_->set_account_name("Test");

sync/engine/sync_scheduler_whitebox_unittest.cc
@@ -53,7 +53,8 @@ class SyncSchedulerWhiteboxTest : public testing::Test {
             workers, &extensions_activity_monitor_,
             throttled_data_type_tracker_.get(),
             std::vector<SyncEngineEventListener*>(), NULL, NULL,
-            true  /* enable keystore encryption */));
+            true,  // enable keystore encryption
+            "fake_invalidator_client_id"));
     context_->set_notifications_enabled(true);
     context_->set_account_name("Test");
     scheduler_.reset(

sync/engine/sync_session_job_unittest.cc
@@ -57,7 +57,8 @@ class SyncSessionJobTest : public testing::Test {
         std::vector<SyncEngineEventListener*>(),
         NULL,  // |debug_info_getter|
         NULL,  // |traffic_recorder|
-        true   /* |enable keystore encryption| */));
+        true,  // |enable keystore encryption|
+        "fake_invalidator_client_id"));
     context_->set_routing_info(routes_);
   }
 

sync/engine/syncer_proto_util.cc
@@ -363,6 +363,7 @@ SyncerError SyncerProtoUtil::PostClientToServerMessage(
   AddBagOfChips(session->context()->directory(), msg);
   msg->set_api_key(google_apis::GetAPIKey());
   msg->mutable_client_status()->CopyFrom(session->context()->client_status());
+  msg->set_invalidator_client_id(session->context()->invalidator_client_id());
 
   syncable::Directory* dir = session->context()->directory();
 

commit 19672dde4eb2d4a6dcaf5888def22a297df24abf

Set invalidation client ID during startup
This fixes a bug introduced in r185721.

BUG=180410


Review URL: https://chromiumcodereview.appspot.com/12497006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186413 0039d316-1c4b-4281-b951-d872f2087c98

sync/internal_api/sync_manager_impl.cc
@@ -426,6 +426,7 @@ void SyncManagerImpl::Init(
 
   DVLOG(1) << "Setting sync client ID: " << sync_id;
   DVLOG(1) << "Setting invalidator client ID: " << invalidator_client_id;
+  invalidator_->SetUniqueId(invalidator_client_id);
 
   // Build a SyncSessionContext and store the worker in it.
   DVLOG(1) << "Sync is bringing up SyncSessionContext.";