ChromiumWebApps/chromium

commit 21dc4c2c1747a71efebb36067577df3af367d01f

Clean up SocketStream send operation by utilizing DrainableIOBuffer.
- Remove unnecessary variables, write_buf_, write_buf_offset_ and write_buf_size_, and utilize the DrainableIOBuffer |current_write_buf_| to do what they were doing.
- Use waiting_for_write_completion_ instead of current_write_buf_ to remember
  whether Write operation is ongoing or not.
- Make DidSendData void and return OK outside it.
- In SendData(), now max_pending_send_allowed_ check is done even if current_write_buf_ (formerly write_buf_) is NULL
- Rewrite comments.

TEST=net_unittests --gtest_filter='SocketStreamTest.*'

Review URL: https://chromiumcodereview.appspot.com/12742018

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192080 0039d316-1c4b-4281-b951-d872f2087c98

net/socket_stream/socket_stream.cc
@@ -105,10 +105,8 @@ SocketStream::SocketStream(const GURL& url, Delegate* delegate)
           io_callback_(base::Bind(&SocketStream::OnIOCompleted,
                                   base::Unretained(this)))),
       read_buf_(NULL),
-      write_buf_(NULL),
       current_write_buf_(NULL),
-      write_buf_offset_(0),
-      write_buf_size_(0),
+      waiting_for_write_completion_(false),
       closing_(false),
       server_closed_(false),
       metrics_(new SocketStreamMetrics(url)) {
@@ -188,39 +186,58 @@ void SocketStream::Connect() {
       base::Bind(&SocketStream::DoLoop, this, OK));
 }
 
+size_t SocketStream::GetTotalSizeOfPendingWriteBufs() const {
+  size_t total_size = 0;
+  for (PendingDataQueue::const_iterator iter = pending_write_bufs_.begin();
+       iter != pending_write_bufs_.end();
+       ++iter)
+    total_size += (*iter)->size();
+  return total_size;
+}
+
 bool SocketStream::SendData(const char* data, int len) {
   DCHECK(MessageLoop::current()) <<
       "The current MessageLoop must exist";
   DCHECK_EQ(MessageLoop::TYPE_IO, MessageLoop::current()->type()) <<
       "The current MessageLoop must be TYPE_IO";
+  DCHECK_GT(len, 0);
+
   if (!socket_.get() || !socket_->IsConnected() || next_state_ == STATE_NONE)
     return false;
-  if (write_buf_) {
-    int current_amount_send = write_buf_size_ - write_buf_offset_;
-    for (PendingDataQueue::const_iterator iter = pending_write_bufs_.begin();
-         iter != pending_write_bufs_.end();
-         ++iter)
-      current_amount_send += (*iter)->size();
 
-    current_amount_send += len;
-    if (current_amount_send > max_pending_send_allowed_)
-      return false;
-
-    pending_write_bufs_.push_back(make_scoped_refptr(
-        new IOBufferWithSize(len)));
-    memcpy(pending_write_bufs_.back()->data(), data, len);
-    return true;
+  int total_buffered_bytes = len;
+  if (current_write_buf_) {
+    // Since
+    // - the purpose of this check is to limit the amount of buffer used by
+    //   this instance.
+    // - the DrainableIOBuffer doesn't release consumed memory.
+    // we need to use not BytesRemaining() but size() here.
+    total_buffered_bytes += current_write_buf_->size();
   }
-  DCHECK(!current_write_buf_);
-  write_buf_ = new IOBuffer(len);
-  memcpy(write_buf_->data(), data, len);
-  write_buf_size_ = len;
-  write_buf_offset_ = 0;
-  // Send pending data asynchronously, so that delegate won't be called
-  // back before returning SendData().
-  MessageLoop::current()->PostTask(
-      FROM_HERE,
-      base::Bind(&SocketStream::DoLoop, this, OK));
+  total_buffered_bytes += GetTotalSizeOfPendingWriteBufs();
+  if (total_buffered_bytes > max_pending_send_allowed_)
+    return false;
+
+  // TODO(tyoshino): Split data into smaller chunks e.g. 8KiB to free consumed
+  // buffer progressively
+  pending_write_bufs_.push_back(make_scoped_refptr(
+      new IOBufferWithSize(len)));
+  memcpy(pending_write_bufs_.back()->data(), data, len);
+
+  // If current_write_buf_ is not NULL, it means that a) there's ongoing write
+  // operation or b) the connection is being closed. If a), the buffer we just
+  // pushed will be automatically handled when the completion callback runs
+  // the loop, and therefore we don't need to enqueue DoLoop(). If b), it's ok
+  // to do nothing. If current_write_buf_ is NULL, to make sure DoLoop() is
+  // ran soon, enequeue it.
+  if (!current_write_buf_) {
+    // Send pending data asynchronously, so that delegate won't be called
+    // back before returning from SendData().
+    MessageLoop::current()->PostTask(
+        FROM_HERE,
+        base::Bind(&SocketStream::DoLoop, this, OK));
+  }
+
   return true;
 }
 
@@ -378,30 +395,29 @@ int SocketStream::DidReceiveData(int result) {
   return OK;
 }
 
-int SocketStream::DidSendData(int result) {
+void SocketStream::DidSendData(int result) {
   DCHECK_GT(result, 0);
+  DCHECK(current_write_buf_);
   net_log_.AddEvent(NetLog::TYPE_SOCKET_STREAM_SENT);
-  int len = result;
-  metrics_->OnWrite(len);
-  current_write_buf_ = NULL;
-  if (delegate_)
-    delegate_->OnSentData(this, len);
 
-  int remaining_size = write_buf_size_ - write_buf_offset_ - len;
-  if (remaining_size == 0) {
-    if (!pending_write_bufs_.empty()) {
-      write_buf_size_ = pending_write_bufs_.front()->size();
-      write_buf_ = pending_write_bufs_.front();
-      pending_write_bufs_.pop_front();
-    } else {
-      write_buf_size_ = 0;
-      write_buf_ = NULL;
-    }
-    write_buf_offset_ = 0;
-  } else {
-    write_buf_offset_ += len;
-  }
-  return OK;
+  int bytes_sent = result;
+
+  metrics_->OnWrite(bytes_sent);
+
+  current_write_buf_->DidConsume(result);
+
+  if (current_write_buf_->BytesRemaining())
+    return;
+
+  size_t bytes_freed = current_write_buf_->size();
+
+  current_write_buf_ = NULL;
+
+  // We freed current_write_buf_ and this instance is now able to accept more
+  // data via SendData() (note that DidConsume() doesn't free consumed memory).
+  // We can tell that to delegate_ by calling OnSentData().
+  if (delegate_)
+    delegate_->OnSentData(this, bytes_freed);
 }
 
 void SocketStream::OnIOCompleted(int result) {
@@ -420,8 +436,10 @@ void SocketStream::OnReadCompleted(int result) {
 }
 
 void SocketStream::OnWriteCompleted(int result) {
-  if (result > 0 && write_buf_) {
-    result = DidSendData(result);
+  waiting_for_write_completion_ = false;
+  if (result > 0) {
+    DidSendData(result);
+    result = OK;
   }
   DoLoop(result);
 }
@@ -1077,7 +1095,7 @@ int SocketStream::DoReadWrite(int result) {
   // If client has requested close(), and there's nothing to write, then
   // let's close the socket.
   // We don't care about receiving data after the socket is closed.
-  if (closing_ && !write_buf_ && pending_write_bufs_.empty()) {
+  if (closing_ && !current_write_buf_ && pending_write_bufs_.empty()) {
     socket_->Disconnect();
     next_state_ = STATE_CLOSE;
     return OK;
@@ -1114,28 +1132,39 @@ int SocketStream::DoReadWrite(int result) {
     DCHECK(read_buf_);
   }
 
-  if (write_buf_ && !current_write_buf_) {
-    // No write pending.
-    current_write_buf_ = new DrainableIOBuffer(write_buf_, write_buf_size_);
-    current_write_buf_->SetOffset(write_buf_offset_);
-    result = socket_->Write(current_write_buf_,
-                            current_write_buf_->BytesRemaining(),
-                            base::Bind(&SocketStream::OnWriteCompleted,
-                                       base::Unretained(this)));
-    if (result > 0) {
-      return DidSendData(result);
+  if (waiting_for_write_completion_)
+    return ERR_IO_PENDING;
+
+  if (!current_write_buf_) {
+    if (pending_write_bufs_.empty()) {
+      // Nothing buffered for send.
+      return ERR_IO_PENDING;
     }
-    // If write is not pending, return the result and do next loop (to close
-    // the connection).
-    if (result != 0 && result != ERR_IO_PENDING) {
-      next_state_ = STATE_CLOSE;
-      return result;
-    }
-    return result;
+
+    current_write_buf_ =
+        new DrainableIOBuffer(pending_write_bufs_.front(),
+                              pending_write_bufs_.front()->size());
+    pending_write_bufs_.pop_front();
   }
 
-  // We arrived here when both operation is pending.
-  return ERR_IO_PENDING;
+  result = socket_->Write(current_write_buf_,
+                          current_write_buf_->BytesRemaining(),
+                          base::Bind(&SocketStream::OnWriteCompleted,
+                                     base::Unretained(this)));
+
+  if (result == ERR_IO_PENDING) {
+    waiting_for_write_completion_ = true;
+  } else if (result < 0) {
+    // Shortcut. Enter STATE_CLOSE now by changing next_state_ here than by
+    // calling DoReadWrite() again with the error code.
+    next_state_ = STATE_CLOSE;
+  } else if (result > 0) {
+    // Write is not pending. Return OK and do next loop.
+    DidSendData(result);
+    result = OK;
+  }
+
+  return result;
 }
 
 GURL SocketStream::ProxyAuthOrigin() const {

net/socket_stream/socket_stream.h
@@ -60,10 +60,11 @@ class NET_EXPORT SocketStream
     virtual int OnStartOpenConnection(SocketStream* socket,
                                       const CompletionCallback& callback);
 
-    // Called when socket stream has been connected.  The socket stream accepts
-    // at most |max_pending_send_allowed| so that a client of the socket stream
-    // should keep track of how much it has pending and shouldn't go over
-    // |max_pending_send_allowed| bytes.
+    // Called when a socket stream has been connected.  The socket stream is
+    // allowed to buffer pending send data at most |max_pending_send_allowed|
+    // bytes.  A client of the socket stream should keep track of how much
+    // pending send data it has and must not call SendData() if the pending
+    // data goes over |max_pending_send_allowed| bytes.
     virtual void OnConnected(SocketStream* socket,
                              int max_pending_send_allowed) = 0;
 
@@ -135,10 +136,9 @@ class NET_EXPORT SocketStream
   // Once the connection is established, calls delegate's OnConnected.
   virtual void Connect();
 
-  // Requests to send |len| bytes of |data| on the connection.
-  // Returns true if |data| is buffered in the job.
-  // Returns false if size of buffered data would exceeds
-  // |max_pending_send_allowed_| and |data| is not sent at all.
+  // Buffers |data| of |len| bytes for send and returns true if successful.
+  // If size of buffered data exceeds |max_pending_send_allowed_|, sends no
+  // data and returns false. |len| must be positive.
   virtual bool SendData(const char* data, int len);
 
   // Requests to close the connection.
@@ -270,7 +270,13 @@ class NET_EXPORT SocketStream
 
   int DidEstablishConnection();
   int DidReceiveData(int result);
-  int DidSendData(int result);
+  // Given the number of bytes sent,
+  // - notifies the |delegate_| and |metrics_| of this event.
+  // - drains sent data from |current_write_buf_|.
+  // - if |current_write_buf_| has been fully sent, sets NULL to
+  //   |current_write_buf_| to get ready for next write.
+  // and then, returns OK.
+  void DidSendData(int result);
 
   void OnIOCompleted(int result);
   void OnReadCompleted(int result);
@@ -318,9 +324,17 @@ class NET_EXPORT SocketStream
   SSLConfigService* ssl_config_service() const;
   ProxyService* proxy_service() const;
 
+  // Returns the sum of the size of buffers in |pending_write_bufs_|.
+  size_t GetTotalSizeOfPendingWriteBufs() const;
+
   BoundNetLog net_log_;
 
   GURL url_;
+  // The number of bytes allowed to be buffered in this object. If the size of
+  // buffered data which is
+  //   current_write_buf_.BytesRemaining() +
+  //   sum of the size of buffers in |pending_write_bufs_|
+  // exceeds this limit, SendData() fails.
   int max_pending_send_allowed_;
   const URLRequestContext* context_;
 
@@ -358,16 +372,11 @@ class NET_EXPORT SocketStream
   scoped_refptr<IOBuffer> read_buf_;
   int read_buf_size_;
 
-  // Total amount of buffer (|write_buf_size_| - |write_buf_offset_| +
-  // sum of size of |pending_write_bufs_|) should not exceed
-  // |max_pending_send_allowed_|.
-  // |write_buf_| holds requested data and |current_write_buf_| is used
-  // for Write operation, that is, |current_write_buf_| is
-  // |write_buf_| + |write_buf_offset_|.
-  scoped_refptr<IOBuffer> write_buf_;
+  // Buffer to hold data to pass to socket_.
   scoped_refptr<DrainableIOBuffer> current_write_buf_;
-  int write_buf_offset_;
-  int write_buf_size_;
+  // True iff there's no error and this instance is waiting for completion of
+  // Write operation by socket_.
+  bool waiting_for_write_completion_;
   PendingDataQueue pending_write_bufs_;
 
   bool closing_;

net/socket_stream/socket_stream_unittest.cc
@@ -258,6 +258,13 @@ class SocketStreamTest : public PlatformTest {
     event->socket->Close();
   }
 
+  virtual void DoFailByTooBigDataAndClose(SocketStreamEvent* event) {
+    std::string frame(event->number + 1, 0x00);
+    VLOG(1) << event->number;
+    EXPECT_FALSE(event->socket->SendData(&frame[0], frame.size()));
+    event->socket->Close();
+  }
+
   virtual int DoSwitchToSpdyTest(SocketStreamEvent* event) {
     return ERR_PROTOCOL_SWITCHED;
   }
@@ -361,6 +368,43 @@ TEST_F(SocketStreamTest, CloseFlushPendingWrite) {
   EXPECT_EQ(SocketStreamEvent::EVENT_ERROR, events[6].event_type);
   EXPECT_EQ(ERR_CONNECTION_CLOSED, events[6].error_code);
   EXPECT_EQ(SocketStreamEvent::EVENT_CLOSE, events[7].event_type);
+}
+
+TEST_F(SocketStreamTest, ExceedMaxPendingSendAllowed) {
+  TestCompletionCallback test_callback;
+
+  scoped_ptr<SocketStreamEventRecorder> delegate(
+      new SocketStreamEventRecorder(test_callback.callback()));
+  delegate->SetOnConnected(base::Bind(
+      &SocketStreamTest::DoFailByTooBigDataAndClose, base::Unretained(this)));
+
+  TestURLRequestContext context;
+
+  scoped_refptr<SocketStream> socket_stream(
+      new SocketStream(GURL("ws://example.com/demo"), delegate.get()));
+
+  socket_stream->set_context(&context);
+
+  DelayedSocketData data_provider(1, NULL, 0, NULL, 0);
+
+  MockClientSocketFactory* mock_socket_factory =
+      GetMockClientSocketFactory();
+  mock_socket_factory->AddSocketDataProvider(&data_provider);
+
+  socket_stream->SetClientSocketFactory(mock_socket_factory);
+
+  socket_stream->Connect();
+
+  test_callback.WaitForResult();
+
+  const std::vector<SocketStreamEvent>& events = delegate->GetSeenEvents();
+  ASSERT_EQ(4U, events.size());
+
+  EXPECT_EQ(SocketStreamEvent::EVENT_START_OPEN_CONNECTION,
+            events[0].event_type);
+  EXPECT_EQ(SocketStreamEvent::EVENT_CONNECTED, events[1].event_type);
+  EXPECT_EQ(SocketStreamEvent::EVENT_ERROR, events[2].event_type);
+  EXPECT_EQ(SocketStreamEvent::EVENT_CLOSE, events[3].event_type);
 }
 
 TEST_F(SocketStreamTest, BasicAuthProxy) {

commit 09b0a1b94a3fff60fe9c4f9498a40ede14839d9a

[chromedriver] Use net::StreamSocket directly instead of net::WebSocketJob.
This lessens our dependence on net but also allows us to send large messages,
which was broken from r192080.
BUG=none

Review URL: https://codereview.chromium.org/13646018

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192760 0039d316-1c4b-4281-b951-d872f2087c98

chrome/test/chromedriver/net/sync_websocket_impl.cc
@@ -108,7 +108,7 @@ void SyncWebSocketImpl::Core::ConnectOnIO(
     base::AutoLock lock(lock_);
     received_queue_.clear();
   }
-  socket_.reset(new WebSocket(context_getter_, url, this));
+  socket_.reset(new WebSocket(url, this));
   socket_->Connect(base::Bind(
       &SyncWebSocketImpl::Core::OnConnectCompletedOnIO,
       this, success, event));

chrome/test/chromedriver/net/websocket.cc
@@ -5,52 +5,56 @@
 #include "chrome/test/chromedriver/net/websocket.h"
 
 #include "base/base64.h"
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/memory/scoped_vector.h"
 #include "base/rand_util.h"
 #include "base/sha1.h"
 #include "base/stringprintf.h"
-#include "base/strings/string_split.h"
+#include "base/strings/string_number_conversions.h"
+#include "net/base/address_list.h"
 #include "net/base/io_buffer.h"
+#include "net/base/ip_endpoint.h"
+#include "net/base/net_errors.h"
+#include "net/base/net_util.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/http_util.h"
-#include "net/url_request/url_request_context_getter.h"
 #include "net/websockets/websocket_frame.h"
-#include "net/websockets/websocket_job.h"
 
-WebSocket::WebSocket(
-    net::URLRequestContextGetter* context_getter,
-    const GURL& url,
-    WebSocketListener* listener)
-    : context_getter_(context_getter),
-      url_(url),
+WebSocket::WebSocket(const GURL& url, WebSocketListener* listener)
+    : url_(url),
       listener_(listener),
-      connected_(false) {
-  net::WebSocketJob::EnsureInit();
-  web_socket_ = new net::WebSocketJob(this);
+      state_(INITIALIZED),
+      write_buffer_(new net::DrainableIOBuffer(new net::IOBuffer(0), 0)),
+      read_buffer_(new net::IOBufferWithSize(4096)) {
+  net::IPAddressNumber address;
+  CHECK(net::ParseIPLiteralToNumber(url_.HostNoBrackets(), &address));
+  int port = 80;
+  base::StringToInt(url_.port(), &port);
+  net::AddressList addresses(net::IPEndPoint(address, port));
+  net::NetLog::Source source;
+  socket_.reset(new net::TCPClientSocket(addresses, NULL, source));
 }
 
 WebSocket::~WebSocket() {
   CHECK(thread_checker_.CalledOnValidThread());
-  web_socket_->Close();
-  web_socket_->DetachDelegate();
 }
 
 void WebSocket::Connect(const net::CompletionCallback& callback) {
   CHECK(thread_checker_.CalledOnValidThread());
-  CHECK_EQ(net::WebSocketJob::INITIALIZED, web_socket_->state());
-
+  CHECK_EQ(INITIALIZED, state_);
+  state_ = CONNECTING;
   connect_callback_ = callback;
-
-  scoped_refptr<net::SocketStream> socket = new net::SocketStream(
-      url_, web_socket_);
-  socket->set_context(context_getter_->GetURLRequestContext());
-
-  web_socket_->InitSocketStream(socket);
-  web_socket_->Connect();
+  int code = socket_->Connect(base::Bind(
+      &WebSocket::OnSocketConnect, base::Unretained(this)));
+  if (code != net::ERR_IO_PENDING)
+    OnSocketConnect(code);
 }
 
 bool WebSocket::Send(const std::string& message) {
   CHECK(thread_checker_.CalledOnValidThread());
+  if (state_ != OPEN)
+    return false;
 
   net::WebSocketFrameHeader header;
   header.final = true;
@@ -70,12 +74,16 @@ bool WebSocket::Send(const std::string& message) {
   std::string masked_message = message;
   net::MaskWebSocketFramePayload(
       masking_key, 0, &masked_message[0], masked_message.length());
-  std::string data = header_str + masked_message;
-  return web_socket_->SendData(data.c_str(), data.length());
+  Write(header_str + masked_message);
+  return true;
 }
 
-void WebSocket::OnConnected(net::SocketStream* socket,
-                            int max_pending_send_allowed) {
+void WebSocket::OnSocketConnect(int code) {
+  if (code != net::OK) {
+    Close(code);
+    return;
+  }
+
   CHECK(base::Base64Encode(base::RandBytesAsString(16), &sec_key_));
   std::string handshake = base::StringPrintf(
       "GET %s HTTP/1.1\r\n"
@@ -90,61 +98,132 @@ void WebSocket::OnConnected(net::SocketStream* socket,
       url_.path().c_str(),
       url_.host().c_str(),
       sec_key_.c_str());
-  if (!web_socket_->SendData(handshake.c_str(), handshake.length()))
-    OnConnectFinished(net::ERR_FAILED);
+  Write(handshake);
+  Read();
 }
 
-void WebSocket::OnSentData(net::SocketStream* socket,
-                           int amount_sent) {}
+void WebSocket::Write(const std::string& data) {
+  pending_write_ += data;
+  if (!write_buffer_->BytesRemaining())
+    ContinueWritingIfNecessary();
+}
 
-void WebSocket::OnReceivedData(net::SocketStream* socket,
-                               const char* data, int len) {
-  net::WebSocketJob::State state = web_socket_->state();
-  if (!connect_callback_.is_null()) {
-    // WebSocketJob guarantees the first OnReceivedData call contains all
-    // the response headers.
-    const char kMagicKey[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-    std::string websocket_accept;
-    CHECK(base::Base64Encode(base::SHA1HashString(sec_key_ + kMagicKey),
-                             &websocket_accept));
-    scoped_refptr<net::HttpResponseHeaders> headers(
-        new net::HttpResponseHeaders(
-            net::HttpUtil::AssembleRawHeaders(data, len)));
-    if (headers->response_code() != 101 ||
-        !headers->HasHeaderValue("Upgrade", "WebSocket") ||
-        !headers->HasHeaderValue("Connection", "Upgrade") ||
-        !headers->HasHeaderValue("Sec-WebSocket-Accept", websocket_accept)) {
-      OnConnectFinished(net::ERR_FAILED);
+void WebSocket::OnWrite(int code) {
+  if (!socket_->IsConnected()) {
+    // Supposedly if |StreamSocket| is closed, the error code may be undefined.
+    Close(net::ERR_FAILED);
+    return;
+  }
+  if (code < 0) {
+    Close(code);
+    return;
+  }
+
+  write_buffer_->DidConsume(code);
+  ContinueWritingIfNecessary();
+}
+
+void WebSocket::ContinueWritingIfNecessary() {
+  if (!write_buffer_->BytesRemaining()) {
+    if (pending_write_.empty())
       return;
-    }
-    OnConnectFinished(
-        state == net::WebSocketJob::OPEN ? net::OK : net::ERR_FAILED);
-  } else if (connected_) {
-    ScopedVector<net::WebSocketFrameChunk> frame_chunks;
-    CHECK(parser_.Decode(data, len, &frame_chunks));
-    for (size_t i = 0; i < frame_chunks.size(); ++i) {
-      scoped_refptr<net::IOBufferWithSize> buffer = frame_chunks[i]->data;
-      if (buffer)
-        next_message_ += std::string(buffer->data(), buffer->size());
-      if (frame_chunks[i]->final_chunk) {
-        listener_->OnMessageReceived(next_message_);
-        next_message_.clear();
-      }
+    write_buffer_ = new net::DrainableIOBuffer(
+        new net::StringIOBuffer(pending_write_),
+        pending_write_.length());
+    pending_write_.clear();
+  }
+  int code = socket_->Write(
+      write_buffer_,
+      write_buffer_->BytesRemaining(),
+      base::Bind(&WebSocket::OnWrite, base::Unretained(this)));
+  if (code != net::ERR_IO_PENDING)
+    OnWrite(code);
+}
+
+void WebSocket::Read() {
+  int code = socket_->Read(
+      read_buffer_,
+      read_buffer_->size(),
+      base::Bind(&WebSocket::OnRead, base::Unretained(this)));
+  if (code != net::ERR_IO_PENDING)
+    OnRead(code);
+}
+
+void WebSocket::OnRead(int code) {
+  if (code <= 0) {
+    Close(code ? code : net::ERR_FAILED);
+    return;
+  }
+
+  if (state_ == CONNECTING)
+    OnReadDuringHandshake(read_buffer_->data(), code);
+  else if (state_ == OPEN)
+    OnReadDuringOpen(read_buffer_->data(), code);
+
+  if (state_ != CLOSED)
+    Read();
+}
+
+void WebSocket::OnReadDuringHandshake(const char* data, int len) {
+  handshake_response_ += std::string(data, len);
+  int headers_end = net::HttpUtil::LocateEndOfHeaders(
+      handshake_response_.data(), handshake_response_.size(), 0);
+  if (headers_end == -1)
+    return;
+
+  const char kMagicKey[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+  std::string websocket_accept;
+  CHECK(base::Base64Encode(base::SHA1HashString(sec_key_ + kMagicKey),
+                           &websocket_accept));
+  scoped_refptr<net::HttpResponseHeaders> headers(
+      new net::HttpResponseHeaders(
+          net::HttpUtil::AssembleRawHeaders(
+              handshake_response_.data(), headers_end)));
+  if (headers->response_code() != 101 ||
+      !headers->HasHeaderValue("Upgrade", "WebSocket") ||
+      !headers->HasHeaderValue("Connection", "Upgrade") ||
+      !headers->HasHeaderValue("Sec-WebSocket-Accept", websocket_accept)) {
+    Close(net::ERR_FAILED);
+    return;
+  }
+  std::string leftover_message = handshake_response_.substr(headers_end);
+  handshake_response_.clear();
+  sec_key_.clear();
+  state_ = OPEN;
+  InvokeConnectCallback(net::OK);
+  if (!leftover_message.empty())
+    OnReadDuringOpen(leftover_message.c_str(), leftover_message.length());
+}
+
+void WebSocket::OnReadDuringOpen(const char* data, int len) {
+  ScopedVector<net::WebSocketFrameChunk> frame_chunks;
+  CHECK(parser_.Decode(data, len, &frame_chunks));
+  for (size_t i = 0; i < frame_chunks.size(); ++i) {
+    scoped_refptr<net::IOBufferWithSize> buffer = frame_chunks[i]->data;
+    if (buffer)
+      next_message_ += std::string(buffer->data(), buffer->size());
+    if (frame_chunks[i]->final_chunk) {
+      listener_->OnMessageReceived(next_message_);
+      next_message_.clear();
     }
   }
 }
 
-void WebSocket::OnClose(net::SocketStream* socket) {
-  if (!connect_callback_.is_null())
-    OnConnectFinished(net::ERR_CONNECTION_CLOSED);
-  else
-    listener_->OnClose();
-}
-
-void WebSocket::OnConnectFinished(net::Error error) {
-  if (error == net::OK)
-    connected_ = true;
+void WebSocket::InvokeConnectCallback(int code) {
   net::CompletionCallback temp = connect_callback_;
   connect_callback_.Reset();
-  temp.Run(error);
+  CHECK(!temp.is_null());
+  temp.Run(code);
 }
+
+void WebSocket::Close(int code) {
+  socket_->Disconnect();
+  if (!connect_callback_.is_null())
+    InvokeConnectCallback(code);
+  if (state_ == OPEN)
+    listener_->OnClose();
+
+  state_ = CLOSED;
+}
+
+

chrome/test/chromedriver/net/websocket.h
@@ -8,30 +8,27 @@
 #include <string>
 
 #include "base/basictypes.h"
-#include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
 #include "base/threading/thread_checker.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/completion_callback.h"
-#include "net/base/net_errors.h"
-#include "net/socket_stream/socket_stream.h"
+#include "net/socket/tcp_client_socket.h"
 #include "net/websockets/websocket_frame_parser.h"
 
 namespace net {
-class URLRequestContextGetter;
-class WebSocketJob;
-}  // namespace net
+class DrainableIOBuffer;
+class IOBufferWithSize;
+}
 
 class WebSocketListener;
 
 // A text-only, non-thread safe WebSocket. Must be created and used on a single
 // thread. Intended particularly for use with net::HttpServer.
-class WebSocket : public net::SocketStream::Delegate {
+class WebSocket {
  public:
-  WebSocket(net::URLRequestContextGetter* context_getter,
-            const GURL& url,
-            WebSocketListener* listener);
+  // |url| must be an IP v4/v6 literal, not a host name.
+  WebSocket(const GURL& url, WebSocketListener* listener);
   virtual ~WebSocket();
 
   // Initializes the WebSocket connection. Invokes the given callback with
@@ -41,29 +38,45 @@ class WebSocket : public net::SocketStream::Delegate {
   // Sends the given message and returns true on success.
   bool Send(const std::string& message);
 
-  // Overridden from net::SocketStream::Delegate:
-  virtual void OnConnected(net::SocketStream* socket,
-                           int max_pending_send_allowed) OVERRIDE;
-  virtual void OnSentData(net::SocketStream* socket,
-                          int amount_sent) OVERRIDE;
-  virtual void OnReceivedData(net::SocketStream* socket,
-                              const char* data,
-                              int len) OVERRIDE;
-  virtual void OnClose(net::SocketStream* socket) OVERRIDE;
-
  private:
-  void OnConnectFinished(net::Error error);
+  enum State {
+    INITIALIZED,
+    CONNECTING,
+    OPEN,
+    CLOSED
+  };
+
+  void OnSocketConnect(int code);
+
+  void Write(const std::string& data);
+  void OnWrite(int code);
+  void ContinueWritingIfNecessary();
+
+  void Read();
+  void OnRead(int code);
+  void OnReadDuringHandshake(const char* data, int len);
+  void OnReadDuringOpen(const char* data, int len);
+
+  void InvokeConnectCallback(int code);
+  void Close(int code);
 
   base::ThreadChecker thread_checker_;
-  scoped_refptr<net::URLRequestContextGetter> context_getter_;
+
   GURL url_;
-  scoped_refptr<net::WebSocketJob> web_socket_;
   WebSocketListener* listener_;
+  State state_;
+  scoped_ptr<net::TCPClientSocket> socket_;
+
   net::CompletionCallback connect_callback_;
   std::string sec_key_;
+  std::string handshake_response_;
+
+  scoped_refptr<net::DrainableIOBuffer> write_buffer_;
+  std::string pending_write_;
+
+  scoped_refptr<net::IOBufferWithSize> read_buffer_;
   net::WebSocketFrameParser parser_;
   std::string next_message_;
-  bool connected_;
 
   DISALLOW_COPY_AND_ASSIGN(WebSocket);
 };

chrome/test/chromedriver/net/websocket_unittest.cc
@@ -84,9 +84,7 @@ class CloseListener : public WebSocketListener {
 
 class WebSocketTest : public testing::Test {
  public:
-  WebSocketTest()
-      : context_getter_(
-            new net::TestURLRequestContextGetter(loop_.message_loop_proxy())) {}
+  WebSocketTest() {}
   virtual ~WebSocketTest() {}
 
   virtual void SetUp() OVERRIDE {
@@ -101,8 +99,7 @@ class WebSocketTest : public testing::Test {
   scoped_ptr<WebSocket> CreateWebSocket(const GURL& url,
                                         WebSocketListener* listener) {
     int error;
-    scoped_ptr<WebSocket> sock(new WebSocket(
-        context_getter_, url, listener));
+    scoped_ptr<WebSocket> sock(new WebSocket(url, listener));
     base::RunLoop run_loop;
     sock->Connect(base::Bind(&OnConnectFinished, &run_loop, &error));
     loop_.PostDelayedTask(
@@ -134,14 +131,13 @@ class WebSocketTest : public testing::Test {
 
   MessageLoopForIO loop_;
   TestHttpServer server_;
-  scoped_refptr<net::URLRequestContextGetter> context_getter_;
 };
 
 }  // namespace
 
 TEST_F(WebSocketTest, CreateDestroy) {
   CloseListener listener(NULL);
-  WebSocket sock(context_getter_, GURL("http://ok"), &listener);
+  WebSocket sock(GURL("ws://127.0.0.1:2222"), &listener);
 }
 
 TEST_F(WebSocketTest, Connect) {