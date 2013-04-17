ChromiumWebApps/chromium

commit 305b8e8c9152dc31b1e580f8442e70dd07d2137f

Make URLDataSource::GetSource() const.
The method is logically const, all callers are already const, and I
wanted to call this from a const method.

BUG=

Review URL: https://codereview.chromium.org/13945023

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194605 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/webui/devtools_ui.cc
@@ -97,7 +97,7 @@ class BundledDataSource : public content::URLDataSource {
   }
 
   // content::URLDataSource implementation.
-  virtual std::string GetSource() OVERRIDE {
+  virtual std::string GetSource() const OVERRIDE {
     return chrome::kChromeUIDevToolsBundledHost;
   }
 
@@ -139,7 +139,7 @@ class RemoteDataSource : public content::URLDataSource {
   }
 
   // content::URLDataSource implementation.
-  virtual std::string GetSource() OVERRIDE {
+  virtual std::string GetSource() const OVERRIDE {
     return chrome::kChromeUIDevToolsRemoteHost;
   }
 

chrome/browser/printing/cloud_print/cloud_print_setup_source.cc
@@ -43,7 +43,7 @@ void AddString(DictionaryValue* dictionary,
 CloudPrintSetupSource::CloudPrintSetupSource() {
 }
 
-std::string CloudPrintSetupSource::GetSource() {
+std::string CloudPrintSetupSource::GetSource() const {
   return chrome::kChromeUICloudPrintSetupHost;
 }
 

chrome/browser/printing/cloud_print/cloud_print_setup_source.h
@@ -14,7 +14,7 @@ class CloudPrintSetupSource : public content::URLDataSource {
   CloudPrintSetupSource();
 
   // content::URLDataSource implementation.
-  virtual std::string GetSource() OVERRIDE;
+  virtual std::string GetSource() const OVERRIDE;
   virtual void StartDataRequest(
       const std::string& path,
       bool is_incognito,

chrome/browser/search/local_ntp_source.cc
@@ -31,7 +31,7 @@ LocalNtpSource::LocalNtpSource() {
 LocalNtpSource::~LocalNtpSource() {
 }
 
-std::string LocalNtpSource::GetSource() {
+std::string LocalNtpSource::GetSource() const {
   return chrome::kChromeSearchLocalNtpHost;
 }
 

chrome/browser/search/local_ntp_source.h
@@ -19,7 +19,7 @@ class LocalNtpSource : public content::URLDataSource {
   virtual ~LocalNtpSource();
 
   // Overridden from content::URLDataSource:
-  virtual std::string GetSource() OVERRIDE;
+  virtual std::string GetSource() const OVERRIDE;
   virtual void StartDataRequest(
       const std::string& path,
       bool is_incognito,

chrome/browser/search/local_omnibox_popup_source.cc
@@ -32,7 +32,7 @@ LocalOmniboxPopupSource::LocalOmniboxPopupSource() {
 LocalOmniboxPopupSource::~LocalOmniboxPopupSource() {
 }
 
-std::string LocalOmniboxPopupSource::GetSource() {
+std::string LocalOmniboxPopupSource::GetSource() const {
   return chrome::kChromeSearchLocalOmniboxPopupHost;
 }
 

chrome/browser/search/local_omnibox_popup_source.h
@@ -19,7 +19,7 @@ class LocalOmniboxPopupSource : public content::URLDataSource {
   virtual ~LocalOmniboxPopupSource();
 
   // Overridden from content::URLDataSource:
-  virtual std::string GetSource() OVERRIDE;
+  virtual std::string GetSource() const OVERRIDE;
   virtual void StartDataRequest(
       const std::string& path,
       bool is_incognito,

chrome/browser/ui/webui/about_ui.cc
@@ -966,7 +966,7 @@ AboutUIHTMLSource::AboutUIHTMLSource(const std::string& source_name,
 
 AboutUIHTMLSource::~AboutUIHTMLSource() {}
 
-std::string AboutUIHTMLSource::GetSource() {
+std::string AboutUIHTMLSource::GetSource() const {
   return source_name_;
 }
 

commit 28e0a0e1548565a7acaa7de7f246f476ec66917f

Compilation broken with debug_devtools after r194605
Local environment:
$ cat ~/.gyp/include.gypi
{
  'variables': {
    'debug_devtools': 1,
  },
}

The build fails with the following error:

../../chrome/browser/ui/webui/devtools_ui.cc: In constructor 'DevToolsUI::DevToolsUI(content::WebUI*)':
../../chrome/browser/ui/webui/devtools_ui.cc:239:59: error: cannot allocate an object of abstract type '{anonymous}::LocalhostDataSource'
../../chrome/browser/ui/webui/devtools_ui.cc:172:7: note:   because the following virtual functions are pure within '{anonymous}::LocalhostDataSource':
../../content/public/browser/url_data_source.h:40:23: note: 	virtual std::string content::URLDataSource::GetSource() const

Review URL: https://codereview.chromium.org/14328007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194641 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/webui/devtools_ui.cc
@@ -176,7 +176,7 @@ class LocalhostDataSource : public content::URLDataSource {
   }
 
   // content::URLDataSource implementation.
-  virtual std::string GetSource() OVERRIDE {
+  virtual std::string GetSource() const OVERRIDE {
     return "localhost";
   }
 