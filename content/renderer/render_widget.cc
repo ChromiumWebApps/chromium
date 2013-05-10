ChromiumWebApps/chromium

commit 20fbfc2ddfa29ca384c805fa7028a9a8b5040f0a

[content shell] forcibly resize webview before each test
Since we short-circuit resize requests during layout tests, the browser does
not necessarily know the current size of the WebView, and so requesting a
resize from the browser process might be ignored, because the RWHV thinks that
it already has the correct size.

BUG=111316
TEST=mac bot magically becomes non-flaky
R=marja@chromium.org, piman@chromium.org

Review URL: https://codereview.chromium.org/15027006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198991 0039d316-1c4b-4281-b951-d872f2087c98

content/renderer/render_widget.cc
@@ -406,14 +406,14 @@ void RenderWidget::Resize(const gfx::Size& new_size,
         // Resize should have caused an invalidation of the entire view.
         DCHECK(paint_aggregator_.HasPendingUpdate());
       }
-
-      // Send the Resize_ACK flag once we paint again if requested.
-      if (resize_ack == SEND_RESIZE_ACK)
-        set_next_paint_is_resize_ack();
     }
-  } else {
+  } else if (!RenderThreadImpl::current()->short_circuit_size_updates()) {
     resize_ack = NO_RESIZE_ACK;
   }
+
+  // Send the Resize_ACK flag once we paint again if requested.
+  if (resize_ack == SEND_RESIZE_ACK)
+    set_next_paint_is_resize_ack();
 
   if (fullscreen_change)
     DidToggleFullscreen();
@@ -1390,24 +1390,26 @@ void RenderWidget::didScrollRect(int dx, int dy,
 
 void RenderWidget::didAutoResize(const WebSize& new_size) {
   if (size_.width() != new_size.width || size_.height() != new_size.height) {
-    size_ = new_size;
-    // If we don't clear PaintAggregator after changing autoResize state, then
-    // we might end up in a situation where bitmap_rect is larger than the
-    // view_size. By clearing PaintAggregator, we ensure that we don't end up
-    // with invalid damage rects.
-    paint_aggregator_.ClearPendingUpdate();
-
-    if (auto_resize_mode_)
-      AutoResizeCompositor();
-
     if (RenderThreadImpl::current()->short_circuit_size_updates()) {
       setWindowRect(WebRect(rootWindowRect().x,
                             rootWindowRect().y,
                             new_size.width,
                             new_size.height));
     } else {
-      need_update_rect_for_auto_resize_ = true;
+      size_ = new_size;
+
+      // If we don't clear PaintAggregator after changing autoResize state, then
+      // we might end up in a situation where bitmap_rect is larger than the
+      // view_size. By clearing PaintAggregator, we ensure that we don't end up
+      // with invalid damage rects.
+      paint_aggregator_.ClearPendingUpdate();
     }
+
+    if (auto_resize_mode_)
+      AutoResizeCompositor();
+
+    if (!RenderThreadImpl::current()->short_circuit_size_updates())
+      need_update_rect_for_auto_resize_ = true;
   }
 }
 

content/shell/common/shell_messages.h
@@ -22,6 +22,7 @@ IPC_STRUCT_TRAITS_MEMBER(enable_pixel_dumping)
 IPC_STRUCT_TRAITS_MEMBER(layout_test_timeout)
 IPC_STRUCT_TRAITS_MEMBER(allow_external_pages)
 IPC_STRUCT_TRAITS_MEMBER(expected_pixel_hash)
+IPC_STRUCT_TRAITS_MEMBER(initial_size)
 IPC_STRUCT_TRAITS_END()
 
 // Tells the renderer to reset all test runners.

content/shell/common/shell_test_configuration.h
@@ -9,6 +9,7 @@
 
 #include "base/files/file_path.h"
 #include "googleurl/src/gurl.h"
+#include "ui/gfx/size.h"
 
 namespace content {
 
@@ -36,6 +37,9 @@ struct ShellTestConfiguration {
 
   // The expected MD5 hash of the pixel results.
   std::string expected_pixel_hash;
+
+  // The initial size of the test window.
+  gfx::Size initial_size;
 };
 
 }  // namespace content

content/shell/renderer/webkit_test_runner.cc
@@ -665,6 +665,9 @@ void WebKitTestRunner::OnSetTestConfiguration(
   test_config_ = params;
   is_main_window_ = true;
 
+  ForceResizeRenderView(
+      render_view(),
+      WebSize(params.initial_size.width(), params.initial_size.height()));
   setFocus(proxy_, true);
 
   WebTestInterfaces* interfaces =

content/shell/webkit_test_controller.cc
@@ -210,17 +210,17 @@ bool WebKitTestController::PrepareForLayoutTest(
       ShellContentBrowserClient::Get()->browser_context();
   if (test_url.spec().find("compositing/") != std::string::npos)
     is_compositing_test_ = true;
-  gfx::Size initial_size(kTestWindowWidthDip, kTestWindowHeightDip);
+  initial_size_ = gfx::Size(kTestWindowWidthDip, kTestWindowHeightDip);
   // The W3C SVG layout tests use a different size than the other layout tests.
   if (test_url.spec().find("W3C-SVG-1.1") != std::string::npos)
-    initial_size = gfx::Size(kTestSVGWindowWidthDip, kTestSVGWindowHeightDip);
+    initial_size_ = gfx::Size(kTestSVGWindowWidthDip, kTestSVGWindowHeightDip);
   if (!main_window_) {
     main_window_ = content::Shell::CreateNewWindow(
         browser_context,
         GURL(),
         NULL,
         MSG_ROUTING_NONE,
-        initial_size);
+        initial_size_);
     WebContentsObserver::Observe(main_window_->web_contents());
     send_configuration_to_next_host_ = true;
     current_pid_ = base::kNullProcessId;
@@ -228,10 +228,10 @@ bool WebKitTestController::PrepareForLayoutTest(
   } else {
 #if (defined(OS_WIN) && !defined(USE_AURA)) || defined(TOOLKIT_GTK)
     // Shell::SizeTo is not implemented on all platforms.
-    main_window_->SizeTo(initial_size.width(), initial_size.height());
+    main_window_->SizeTo(initial_size_.width(), initial_size_.height());
 #endif
     main_window_->web_contents()->GetRenderViewHost()->GetView()
-        ->SetSize(initial_size);
+        ->SetSize(initial_size_);
     main_window_->web_contents()->GetRenderViewHost()->WasResized();
     RenderViewHost* render_view_host =
         main_window_->web_contents()->GetRenderViewHost();
@@ -448,6 +448,7 @@ void WebKitTestController::SendTestConfiguration() {
   params.allow_external_pages = CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kAllowExternalPages);
   params.expected_pixel_hash = expected_pixel_hash_;
+  params.initial_size = initial_size_;
   render_view_host->Send(new ShellViewMsg_SetTestConfiguration(
       render_view_host->GetRoutingID(), params));
 }

content/shell/webkit_test_controller.h
@@ -17,6 +17,7 @@
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/render_view_host_observer.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "ui/gfx/size.h"
 #include "webkit/glue/webpreferences.h"
 
 class SkBitmap;
@@ -164,6 +165,7 @@ class WebKitTestController : public base::NonThreadSafe,
   // Per test config.
   bool enable_pixel_dumping_;
   std::string expected_pixel_hash_;
+  gfx::Size initial_size_;
   GURL test_url_;
 
   // True if the WebPreferences of newly created RenderViewHost should be

commit 0b70dbec6b58a41414033bfc8cfc16c920af2375

Don't send a resize ack when resizing to 0x0
I accidentially broke this in r198991.

I wonder whether it was better to just have the browser tell the
renderer whether it's waiting for an ACK or not. Currently, the browser
always requests an ACK but only expects one if the size changed to a
non-empty rectangle.

BUG=239439
R=piman@chromium.org

Review URL: https://codereview.chromium.org/14620015

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199515 0039d316-1c4b-4281-b951-d872f2087c98

content/renderer/render_widget.cc
@@ -402,18 +402,16 @@ void RenderWidget::Resize(const gfx::Size& new_size,
     // ensures that we only resize as fast as we can paint.  We only need to
     // send an ACK if we are resized to a non-empty rect.
     webwidget_->resize(new_size);
-    if (!new_size.IsEmpty()) {
-      if (!is_accelerated_compositing_active_) {
-        // Resize should have caused an invalidation of the entire view.
-        DCHECK(paint_aggregator_.HasPendingUpdate());
-      }
-    }
+
+    // Resize should have caused an invalidation of the entire view.
+    DCHECK(new_size.IsEmpty() || is_accelerated_compositing_active_ ||
+           paint_aggregator_.HasPendingUpdate());
   } else if (!RenderThreadImpl::current()->short_circuit_size_updates()) {
     resize_ack = NO_RESIZE_ACK;
   }
 
   // Send the Resize_ACK flag once we paint again if requested.
-  if (resize_ack == SEND_RESIZE_ACK)
+  if (resize_ack == SEND_RESIZE_ACK && !new_size.IsEmpty())
     set_next_paint_is_resize_ack();
 
   if (fullscreen_change)