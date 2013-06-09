ChromiumWebApps/chromium

commit fcf46787a2401ba4b624df05bef80fbc7824f2f1

[Mac][MC] Open the tray to the left if there is not enough room on screen to the right.
BUG=247428
TEST=Have few status items in the menu bar, open the tray. It opens to the left.

Review URL: https://chromiumcodereview.appspot.com/15792027

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204993 0039d316-1c4b-4281-b951-d872f2087c98

ui/message_center/cocoa/tray_controller.mm
@@ -48,14 +48,20 @@
   return self;
 }
 
-- (void)dealloc {
-  [super dealloc];
-}
+- (void)showTrayAtRightOf:(NSPoint)rightPoint atLeftOf:(NSPoint)leftPoint {
+  NSScreen* screen = [[NSScreen screens] objectAtIndex:0];
+  NSRect screenFrame = [screen visibleFrame];
 
-- (void)showTrayAt:(NSPoint)point {
   NSRect frame = [[viewController_ view] frame];
-  frame.origin.x = point.x;
-  frame.origin.y = point.y - NSHeight(frame);
+
+  if (rightPoint.x + NSWidth(frame) < NSWidth(screenFrame)) {
+    frame.origin.x = rightPoint.x;
+    frame.origin.y = rightPoint.y - NSHeight(frame);
+  } else {
+    frame.origin.x = leftPoint.x - NSWidth(frame);
+    frame.origin.y = leftPoint.y - NSHeight(frame);
+  }
+
   [[self window] setFrame:frame display:YES];
   [viewController_ scrollToTop];
   [self showWindow:nil];

chrome/browser/ui/cocoa/notifications/message_center_tray_bridge.mm
@@ -84,7 +84,10 @@ bool MessageCenterTrayBridge::ShowMessageCenter() {
 
   [status_item_view_ setHighlight:YES];
   NSRect frame = [[status_item_view_ window] frame];
-  [tray_controller_ showTrayAt:NSMakePoint(NSMinX(frame), NSMinY(frame))];
+  [tray_controller_ showTrayAtRightOf:NSMakePoint(NSMinX(frame),
+                                                  NSMinY(frame))
+                             atLeftOf:NSMakePoint(NSMaxX(frame),
+                                                  NSMinY(frame))];
   return true;
 }
 

ui/message_center/cocoa/tray_controller.h
@@ -31,8 +31,11 @@ MESSAGE_CENTER_EXPORT
 // Designated initializer.
 - (id)initWithMessageCenterTray:(message_center::MessageCenterTray*)tray;
 
-// Opens the message center tray with the window's upper-left corner at |point|.
-- (void)showTrayAt:(NSPoint)point;
+// Opens the message center tray to either the right or left of the given
+// points, depending on if it will fit on screen. It will try opening to the
+// right first, but if the window does not fit on screen, it will open to the
+// left.
+- (void)showTrayAtRightOf:(NSPoint)rightPoint atLeftOf:(NSPoint)leftPoint;
 
 // Callback from MessageCenterTrayDelegate, used to update the tray content.
 - (void)onMessageCenterTrayChanged;

ui/message_center/cocoa/tray_controller_unittest.mm
@@ -1,0 +1,59 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ui/message_center/cocoa/tray_controller.h"
+
+#include "base/memory/scoped_nsobject.h"
+#include "base/memory/scoped_ptr.h"
+#import "ui/base/test/ui_cocoa_test_helper.h"
+#include "ui/message_center/message_center.h"
+#include "ui/message_center/message_center_tray.h"
+
+namespace {
+
+class TrayControllerTest : public ui::CocoaTest {
+ public:
+  virtual void SetUp() OVERRIDE {
+    ui::CocoaTest::SetUp();
+    message_center::MessageCenter::Initialize();
+    tray_.reset(new message_center::MessageCenterTray(
+        NULL, message_center::MessageCenter::Get()));
+    controller_.reset(
+        [[MCTrayController alloc] initWithMessageCenterTray:tray_.get()]);
+  }
+
+  virtual void TearDown() OVERRIDE {
+    controller_.reset();
+    tray_.reset();
+    message_center::MessageCenter::Shutdown();
+    ui::CocoaTest::TearDown();
+  }
+
+ protected:
+  scoped_ptr<message_center::MessageCenterTray> tray_;
+  scoped_nsobject<MCTrayController> controller_;
+};
+
+TEST_F(TrayControllerTest, OpenLeftRight) {
+  NSScreen* screen = [[NSScreen screens] objectAtIndex:0];
+  NSRect screen_frame = [screen visibleFrame];
+
+  const CGFloat y = NSMaxY(screen_frame);
+
+  // With ample room to the right, it should open to the right.
+  NSPoint right_point = NSMakePoint(0, y);
+  NSPoint left_point = NSMakePoint(NSMaxX(screen_frame), y);
+
+  [controller_ showTrayAtRightOf:right_point atLeftOf:left_point];
+  NSRect window_frame = [[controller_ window] frame];
+  EXPECT_EQ(right_point.x, NSMinX(window_frame));
+
+  // With little room on the right, it should open to the left.
+  right_point = NSMakePoint(NSMaxX(screen_frame) - 10, y);
+  [controller_ showTrayAtRightOf:right_point atLeftOf:left_point];
+  window_frame = [[controller_ window] frame];
+  EXPECT_EQ(left_point.x - NSWidth(window_frame), NSMinX(window_frame));
+}
+
+}  // namespace

ui/message_center/message_center.gyp
@@ -142,6 +142,7 @@
         'cocoa/popup_controller_unittest.mm',
         'cocoa/settings_controller_unittest.mm',
         'cocoa/status_item_view_unittest.mm',
+        'cocoa/tray_controller_unittest.mm',
         'cocoa/tray_view_controller_unittest.mm',
         'message_center_tray_unittest.cc',
         'fake_message_center.h',

commit 18976e1a78dfee9032b1cafcfa5584c3fd4e5eb7

mac notifications: Open tray to the right if dock is pinned left and there is room.
Fixes a regression from r204993.

BUG=247428
TEST=Have enough menu extras that tray opens to right normally. Pin dock on
left side of the screen, make it large-ish. Tray still opens to the right.

R=rsesek@chromium.org

Review URL: https://codereview.chromium.org/16443004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205128 0039d316-1c4b-4281-b951-d872f2087c98

ui/message_center/cocoa/tray_controller.mm
@@ -54,7 +54,7 @@
 
   NSRect frame = [[viewController_ view] frame];
 
-  if (rightPoint.x + NSWidth(frame) < NSWidth(screenFrame)) {
+  if (rightPoint.x + NSWidth(frame) < NSMaxX(screenFrame)) {
     frame.origin.x = rightPoint.x;
     frame.origin.y = rightPoint.y - NSHeight(frame);
   } else {