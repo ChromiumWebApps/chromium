ChromiumWebApps/chromium

commit 6fb4e231e146a2646f398ec48fd59584e3d1c58b

Remove -Wno-deprecated-register from YCM.
It is causing libclang (3.3) to crash.

R=thakis@chromium.org

Review URL: https://codereview.chromium.org/18292009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210438 0039d316-1c4b-4281-b951-d872f2087c98

tools/vim/chromium.ycm_extra_conf.py
@@ -193,6 +193,9 @@ def FlagsForFile(filename):
                                                      filename)
   final_flags = flags + chrome_flags
 
+  # This causes libclang (3.3) to crash. Remove it until things are fixed.
+  final_flags.remove('-Wno-deprecated-register')
+
   return {
     'flags': final_flags,
     'do_cache': True

commit 06a4f3ee2228d4a1533bb857949ca38ccf4cddee

Fix YCM file.
I broke this in r210438.
list.remove(X) throws if X is not in the list.

R=thakis@chromium.org

Review URL: https://codereview.chromium.org/18209012

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210467 0039d316-1c4b-4281-b951-d872f2087c98

tools/vim/chromium.ycm_extra_conf.py
@@ -172,6 +172,10 @@ def GetClangCommandFromNinjaForFilename(chrome_root, filename):
         abs_path = os.path.normpath(os.path.join(out_dir, flag[2:]))
         chrome_flags.append('-I' + abs_path)
     elif flag.startswith('-') and flag[1] in 'DWFfmO':
+      if flag == '-Wno-deprecated-register':
+        # This flag causes libclang (3.3) to crash. Remove it until things
+        # are fixed.
+        continue
       chrome_flags.append(flag)
 
   return chrome_flags
@@ -192,9 +196,6 @@ def FlagsForFile(filename):
   chrome_flags = GetClangCommandFromNinjaForFilename(chrome_root,
                                                      filename)
   final_flags = flags + chrome_flags
-
-  # This causes libclang (3.3) to crash. Remove it until things are fixed.
-  final_flags.remove('-Wno-deprecated-register')
 
   return {
     'flags': final_flags,