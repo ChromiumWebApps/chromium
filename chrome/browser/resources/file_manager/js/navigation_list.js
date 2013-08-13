ChromiumWebApps/chromium

commit 01dca7c24a1b798ae75829c999eeec307c8eb089

Load VolumeManager asynchronously.
VolumeManager is moving to background page, and this is its preparation.
Removed some VolumeManager.getInstance() invocation, instead starts to pass
VolumeManager instance from FileManager.

BUG=268817
TEST=Ran browser_tests --gtest_filter="*FileSystemExtensionApiTest*:*FileManagerBrowserTest*" and tested manually.

Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=216155

Review URL: https://chromiumcodereview.appspot.com/22329004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217204 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/file_manager/js/navigation_list.js
@@ -147,8 +147,10 @@ NavigationListItem.prototype.decorate = function() {
 /**
  * Associate a path with this item.
  * @param {string} path Path of this item.
+ * @param {string=} opt_deviceType The type of the device. Available iff the
+ *     path represents removable storage.
  */
-NavigationListItem.prototype.setPath = function(path) {
+NavigationListItem.prototype.setPath = function(path, opt_deviceType) {
   if (this.path_)
     console.warn('NavigationListItem.setPath should be called only once.');
 
@@ -157,9 +159,8 @@ NavigationListItem.prototype.setPath = function(path) {
   var rootType = PathUtil.getRootType(path);
 
   this.iconDiv_.setAttribute('volume-type-icon', rootType);
-  if (rootType === RootType.REMOVABLE) {
-    this.iconDiv_.setAttribute('volume-subtype',
-        VolumeManager.getInstance().getDeviceType(path));
+  if (opt_deviceType) {
+    this.iconDiv_.setAttribute('volume-subtype', opt_deviceType);
   }
 
   this.label_.textContent = PathUtil.getFolderLabel(path);
@@ -242,24 +243,26 @@ NavigationList.prototype = {
 
 /**
  * @param {HTMLElement} el Element to be DirectoryItem.
+ * @param {VolumeManager} volumeManager The VolumeManager of the system.
  * @param {DirectoryModel} directoryModel Current DirectoryModel.
  *     folders.
  */
-NavigationList.decorate = function(el, directoryModel) {
+NavigationList.decorate = function(el, volumeManager, directoryModel) {
   el.__proto__ = NavigationList.prototype;
-  el.decorate(directoryModel);
+  el.decorate(volumeManager, directoryModel);
 };
 
 /**
+ * @param {VolumeManager} volumeManager The VolumeManager of the system.
  * @param {DirectoryModel} directoryModel Current DirectoryModel.
  */
-NavigationList.prototype.decorate = function(directoryModel) {
+NavigationList.prototype.decorate = function(volumeManager, directoryModel) {
   cr.ui.List.decorate(this);
   this.__proto__ = NavigationList.prototype;
 
   this.fileManager_ = null;
   this.directoryModel_ = directoryModel;
-  this.volumeManager_ = VolumeManager.getInstance();
+  this.volumeManager_ = volumeManager;
   this.selectionModel = new cr.ui.ListSingleSelectionModel();
 
   this.directoryModel_.addEventListener('directory-changed',
@@ -313,7 +316,7 @@ NavigationList.prototype.removeChild = function(item) {
  */
 NavigationList.prototype.renderRoot_ = function(path) {
   var item = new NavigationListItem();
-  item.setPath(path);
+  item.setPath(path, this.volumeManager_.getDeviceType(path));
 
   var handleClick = function() {
     if (item.selected && path !== this.directoryModel_.getCurrentDirPath()) {

chrome/browser/resources/file_manager/action_choice.html
@@ -38,6 +38,7 @@
     <script src="chrome://resources/js/cr/ui/list_single_selection_model.js"></script>
     <script src="chrome://resources/js/cr/ui/list_selection_controller.js"></script>
 
+    <script src="js/async_util.js"></script>
     <script src="js/util.js"></script>
     <script src="js/file_type.js"></script>
     <script src="js/path_util.js"></script>

chrome/browser/resources/file_manager/gallery.html
@@ -42,6 +42,7 @@
     <script src="chrome://resources/js/cr/ui/grid.js"></script>
 
     <script src="js/file_type.js"></script>
+    <script src="js/async_util.js"></script>
     <script src="js/util.js"></script>
     <script src="js/volume_manager.js"></script>
 

chrome/browser/resources/file_manager/js/action_choice.js
@@ -13,16 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
  *
  * @param {HTMLElement} dom Container.
  * @param {FileSystem} filesystem Local file system.
+ * @param {VolumeManager} volumeManager VolueManager of the app.
  * @param {Object} params Parameters.
  * @constructor
  */
-function ActionChoice(dom, filesystem, params) {
+function ActionChoice(dom, filesystem, volumeManager, params) {
   this.dom_ = dom;
   this.filesystem_ = filesystem;
   this.params_ = params;
   this.document_ = this.dom_.ownerDocument;
   this.metadataCache_ = this.params_.metadataCache;
-  this.volumeManager_ = VolumeManager.getInstance();
+  this.volumeManager_ = volumeManager;
   this.volumeManager_.addEventListener('externally-unmounted',
      this.onDeviceUnmounted_.bind(this));
   this.initDom_();
@@ -102,7 +103,10 @@ ActionChoice.load = function(opt_filesystem, opt_params) {
 
   var onFilesystem = function(filesystem) {
     var dom = document.querySelector('.action-choice');
-    ActionChoice.instance = new ActionChoice(dom, filesystem, params);
+    VolumeManager.getInstance(function(volumeManager) {
+      ActionChoice.instance = new ActionChoice(
+          dom, filesystem, volumeManager, params);
+    });
   };
 
   chrome.fileBrowserPrivate.getStrings(function(strings) {
@@ -328,6 +332,7 @@ ActionChoice.prototype.loadSource_ = function(source, callback) {
   }.bind(this);
 
   var onReady = function() {
+    // TODO(hidehiko): Replace filesystem by volumeManager.
     util.resolvePath(this.filesystem_.root, source, onEntry, function() {
       this.recordAction_('error');
       this.close_();

chrome/browser/resources/file_manager/js/action_choice_scripts.js
@@ -29,6 +29,7 @@
 (function() {
 // 'strict mode' is invoked for this scope.
 
+//<include src="async_util.js"/>
 //<include src="util.js"/>
 //<include src="file_type.js"/>
 //<include src="path_util.js"/>

chrome/browser/resources/file_manager/js/directory_tree.js
@@ -292,7 +292,6 @@ DirectoryItem.prototype.decorate = function(
   this.hasChildren = false;
 
   this.addEventListener('expand', this.onExpand_.bind(this), false);
-  var volumeManager = VolumeManager.getInstance();
   var icon = this.querySelector('.icon');
   icon.classList.add('volume-icon');
   var iconType = PathUtil.getRootType(path);
@@ -309,7 +308,7 @@ DirectoryItem.prototype.decorate = function(
         if (!PathUtil.isUnmountableByUser(path))
           return;
 
-        volumeManager.unmount(path, function() {}, function() {});
+        tree.volumeManager.unmount(path, function() {}, function() {});
       }.bind(this));
 
   if (this.parentTree_.contextMenuForSubitems)
@@ -455,10 +454,11 @@ function DirectoryTree() {}
  * Decorates an element.
  * @param {HTMLElement} el Element to be DirectoryTree.
  * @param {DirectoryModel} directoryModel Current DirectoryModel.
+ * @param {VolumeManager} volumeManager VolumeManager of the system.
  */
-DirectoryTree.decorate = function(el, directoryModel) {
+DirectoryTree.decorate = function(el, directoryModel, volumeManager) {
   el.__proto__ = DirectoryTree.prototype;
-  (/** @type {DirectoryTree} */ el).decorate(directoryModel);
+  (/** @type {DirectoryTree} */ el).decorate(directoryModel, volumeManager);
 };
 
 DirectoryTree.prototype = {
@@ -471,9 +471,31 @@ DirectoryTree.prototype = {
    */
   set expanded(value) {},
 
+  /**
+   * The DirectoryEntry corresponding to this DirectoryItem. This may be
+   * a dummy DirectoryEntry.
+   * @type {DirectoryEntry|Object}
+   * @override
+   **/
+  get entry() {
+    return this.dirEntry_;
+  },
+
+  /**
+   * The DirectoryModel this tree corresponds to.
+   * @type {DirectoryModel}
+   */
   get directoryModel() {
     return this.directoryModel_;
-  }
+  },
+
+  /**
+   * The VolumeManager instance of the system.
+   * @type {VolumeManager}
+   */
+  get volumeManager() {
+    return this.volumeManager_;
+  },
 };
 
 cr.defineProperty(DirectoryTree, 'contextMenuForSubitems', cr.PropertyKind.JS);
@@ -481,11 +503,13 @@ cr.defineProperty(DirectoryTree, 'contextMenuForSubitems', cr.PropertyKind.JS);
 /**
  * Decorates an element.
  * @param {DirectoryModel} directoryModel Current DirectoryModel.
+ * @param {VolumeManager} volumeManager VolumeManager of the system.
  */
-DirectoryTree.prototype.decorate = function(directoryModel) {
+DirectoryTree.prototype.decorate = function(directoryModel, volumeManager) {
   cr.ui.Tree.prototype.decorate.call(this);
 
   this.directoryModel_ = directoryModel;
+  this.volumeManager_ = volumeManager;
   this.entries_ = DirectoryTreeUtil.generateTopLevelEntries();
 
   this.fileFilter_ = this.directoryModel_.getFileFilter();

chrome/browser/resources/file_manager/js/file_manager.js
@@ -273,6 +273,8 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
     }.bind(this));
 
     // Mount Drive if enabled.
+    // TODO(hidehiko): Mounting state of Drive file system should be handled
+    // by C++ backend side.
     if (this.isDriveEnabled())
       this.volumeManager_.mountDrive(function() {}, function() {});
   };
@@ -681,10 +683,13 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
     this.initializeQueue_.add(this.initGeneral_.bind(this), [], 'initGeneral');
     this.initializeQueue_.add(this.initStrings_.bind(this), [], 'initStrings');
     this.initializeQueue_.add(
+        this.initVolumeManager_.bind(this), [], 'initVolumeManager');
+    this.initializeQueue_.add(
         this.initPreferences_.bind(this), [], 'initPreferences');
     this.initializeQueue_.add(
         this.initFileSystem_.bind(this),
-        ['initGeneral', 'initPreferences'], 'initFileSystem');
+        ['initGeneral', 'initPreferences', 'initVolumeManager'],
+        'initFileSystem');
 
     this.initializeQueue_.run();
   };
@@ -716,7 +721,6 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
    * @private
    */
   FileManager.prototype.initGeneral_ = function(callback) {
-    this.volumeManager_ = VolumeManager.getInstance();
     if (window.appState) {
       this.params_ = window.appState.params || {};
       this.defaultPath = window.appState.defaultPath;
@@ -749,6 +753,18 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
         callback();
       });
     }
+  };
+
+  /**
+   * Initializes the VolumeManager instance.
+   * @param {function()} callback Completion callback.
+   * @private
+   */
+  FileManager.prototype.initVolumeManager_ = function(callback) {
+    VolumeManager.getInstance(function(volumeManager) {
+      this.volumeManager_ = volumeManager;
+      callback();
+    }.bind(this));
   };
 
   /**
@@ -1183,7 +1199,9 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
     DirectoryTree.decorate(this.directoryTree_, this.directoryModel_);
 
     this.navigationList_ = this.dialogDom_.querySelector('#volume-list');
-    NavigationList.decorate(this.navigationList_, this.directoryModel_);
+    NavigationList.decorate(this.navigationList_,
+                            this.volumeManager_,
+                            this.directoryModel_);
     this.navigationList_.fileManager = this;
     this.navigationList_.dataModel =
         new NavigationListModel(this.directoryModel_.getRootsList(),

chrome/browser/resources/file_manager/js/file_tasks.js
@@ -580,7 +580,8 @@ FileTasks.prototype.openGalleryInternal_ = function(urls) {
       onMaximize: onMaximize,
       displayStringFunction: strf
     };
-    galleryFrame.contentWindow.Gallery.open(context, allUrls, urls);
+    galleryFrame.contentWindow.Gallery.open(
+        context, fm.volumeManager_, allUrls, urls);
   }.bind(this);
 
   galleryFrame.src = 'gallery.html';

commit 1f93c2ca5fd5211c73dd0044dc1307ccb7a74dce

Fix folder-shortcut regression by fixing the exception in Files.app.
Folder shortcuts had gone due to the regression by r217204. Exception is happened when a nav-bar item has non-root path. This patch fixes it and makes folder shortcut back.

BUG=none
TEST=Folder shortcuts are shown. Browser_test passes.
TBR=hidehiko@chromium.org

Review URL: https://codereview.chromium.org/23093002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217292 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/file_manager/js/navigation_list.js
@@ -316,7 +316,9 @@ NavigationList.prototype.removeChild = function(item) {
  */
 NavigationList.prototype.renderRoot_ = function(path) {
   var item = new NavigationListItem();
-  item.setPath(path, this.volumeManager_.getDeviceType(path));
+  var deviceType =
+      PathUtil.isRootPath(path) && this.volumeManager_.getDeviceType(path);
+  item.setPath(path, deviceType);
 
   var handleClick = function() {
     if (item.selected && path !== this.directoryModel_.getCurrentDirPath()) {