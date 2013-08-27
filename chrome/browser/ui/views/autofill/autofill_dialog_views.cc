ChromiumWebApps/chromium

commit 93773d08322990032231aa26c4eba4f02014de99

Stop menus from being double shown.
When the account chooser or any detail section is clicked while a menu is
already showing from a previous click, the showing menu should be hidden rather
than quickly closed and reshown. This is accomplished by changing the controls
showing the menus to MenuButtons, which Just Work™. 

This patch also modifies views::MenuController to better handle showing a menu
under the mouse cursor and wrangles some constants. Releasing the mouse on top 
of a menu quickly after showing should now be ignored (based on some simple
heuristics).

R=estade@chromium.org,sadrul@chromium.org,sky@chromium.org
BUG=259553
TEST=menu hides when clicking on opening button

Review URL: https://chromiumcodereview.appspot.com/19272008

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219567 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/autofill/autofill_dialog_views.cc
@@ -39,9 +39,9 @@
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/blue_button.h"
 #include "ui/views/controls/button/checkbox.h"
-#include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/button/label_button_border.h"
+#include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/combobox/combobox.h"
 #include "ui/views/controls/focusable_border.h"
 #include "ui/views/controls/image_view.h"
@@ -115,6 +115,7 @@ const SkColor kGreyTextColor = SkColorSetRGB(102, 102, 102);
 
 const char kNotificationAreaClassName[] = "autofill/NotificationArea";
 const char kOverlayViewClassName[] = "autofill/OverlayView";
+const char kSuggestedButtonClassName[] = "autofill/SuggestedButton";
 
 typedef ui::MultiAnimation::Part Part;
 typedef ui::MultiAnimation::Parts Parts;
@@ -507,19 +508,17 @@ gfx::Rect AutofillDialogViews::ErrorBubble::GetBoundsForWidget() {
 AutofillDialogViews::AccountChooser::AccountChooser(
     AutofillDialogViewDelegate* delegate)
     : image_(new views::ImageView()),
-      label_(new views::Label()),
-      arrow_(new views::ImageView()),
+      menu_button_(new views::MenuButton(NULL, base::string16(), this, true)),
       link_(new views::Link()),
       delegate_(delegate) {
   SetLayoutManager(
       new views::BoxLayout(views::BoxLayout::kHorizontal, 0, 0,
                            kAroundTextPadding));
   AddChildView(image_);
-  AddChildView(label_);
 
-  arrow_->SetImage(ui::ResourceBundle::GetSharedInstance().GetImageNamed(
-      IDR_MENU_DROPARROW).ToImageSkia());
-  AddChildView(arrow_);
+  menu_button_->set_background(NULL);
+  menu_button_->set_border(NULL);
+  AddChildView(menu_button_);
 
   link_->set_listener(this);
   AddChildView(link_);
@@ -532,11 +531,10 @@ void AutofillDialogViews::AccountChooser::Update() {
 
   gfx::Image icon = delegate_->AccountChooserImage();
   image_->SetImage(icon.AsImageSkia());
-  label_->SetText(delegate_->AccountChooserText());
+  menu_button_->SetText(delegate_->AccountChooserText());
 
   bool show_link = !delegate_->MenuModelForAccountChooser();
-  label_->SetVisible(!show_link);
-  arrow_->SetVisible(!show_link);
+  menu_button_->SetVisible(!show_link);
   link_->SetText(delegate_->SignInLinkText());
   link_->SetVisible(show_link);
 
@@ -545,32 +543,24 @@ void AutofillDialogViews::AccountChooser::Update() {
   PreferredSizeChanged();
 }
 
-bool AutofillDialogViews::AccountChooser::OnMousePressed(
-    const ui::MouseEvent& event) {
-  // Return true so we get the release event.
-  if (delegate_->MenuModelForAccountChooser())
-    return event.IsOnlyLeftMouseButton();
-
-  return false;
-}
-
-void AutofillDialogViews::AccountChooser::OnMouseReleased(
-    const ui::MouseEvent& event) {
-  if (!HitTestPoint(event.location()))
-    return;
+void AutofillDialogViews::AccountChooser::OnMenuButtonClicked(
+    views::View* source,
+    const gfx::Point& point) {
+  DCHECK_EQ(menu_button_, source);
 
   ui::MenuModel* model = delegate_->MenuModelForAccountChooser();
   if (!model)
     return;
 
   menu_runner_.reset(new views::MenuRunner(model));
-  ignore_result(
-      menu_runner_->RunMenuAt(GetWidget(),
+  if (menu_runner_->RunMenuAt(source->GetWidget(),
                               NULL,
-                              GetBoundsInScreen(),
+                              source->GetBoundsInScreen(),
                               views::MenuItemView::TOPRIGHT,
-                              ui::MENU_SOURCE_MOUSE,
-                              0));
+                              ui::MENU_SOURCE_NONE,
+                              0) == views::MenuRunner::MENU_DELETED) {
+    return;
+  }
 }
 
 void AutofillDialogViews::AccountChooser::LinkClicked(views::Link* source,
@@ -1003,7 +993,48 @@ ui::MouseEvent AutofillDialogViews::SectionContainer::ProxyEvent(
   return event_copy;
 }
 
-// AutofilDialogViews::SuggestionView ------------------------------------------
+// AutofillDialogViews::SuggestedButton ----------------------------------------
+
+AutofillDialogViews::SuggestedButton::SuggestedButton(
+    views::MenuButtonListener* listener)
+    : views::MenuButton(NULL, base::string16(), listener, false) {
+  set_border(views::Border::CreateEmptyBorder(kMenuButtonTopInset,
+                                              kDialogEdgePadding,
+                                              kMenuButtonBottomInset,
+                                              0));
+}
+
+AutofillDialogViews::SuggestedButton::~SuggestedButton() {}
+
+gfx::Size AutofillDialogViews::SuggestedButton::GetPreferredSize() {
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
+  return rb.GetImageNamed(ResourceIDForState()).Size();
+}
+
+const char* AutofillDialogViews::SuggestedButton::GetClassName() const {
+  return kSuggestedButtonClassName;
+}
+
+void AutofillDialogViews::SuggestedButton::PaintChildren(gfx::Canvas* canvas) {}
+
+void AutofillDialogViews::SuggestedButton::OnPaint(gfx::Canvas* canvas) {
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
+  canvas->DrawImageInt(*rb.GetImageSkiaNamed(ResourceIDForState()), 0, 0);
+}
+
+int AutofillDialogViews::SuggestedButton::ResourceIDForState() const {
+  views::Button::ButtonState button_state = state();
+  if (button_state == views::Button::STATE_PRESSED)
+    return IDR_AUTOFILL_DIALOG_MENU_BUTTON_P;
+  else if (button_state == views::Button::STATE_HOVERED)
+    return IDR_AUTOFILL_DIALOG_MENU_BUTTON_H;
+  else if (button_state == views::Button::STATE_DISABLED)
+    return IDR_AUTOFILL_DIALOG_MENU_BUTTON_D;
+  DCHECK_EQ(views::Button::STATE_NORMAL, button_state);
+  return IDR_AUTOFILL_DIALOG_MENU_BUTTON;
+}
+
+// AutofillDialogViews::SuggestionView -----------------------------------------
 
 AutofillDialogViews::SuggestionView::SuggestionView(
     AutofillDialogViews* autofill_dialog)
@@ -1646,43 +1677,8 @@ views::NonClientFrameView* AutofillDialogViews::CreateNonClientFrameView(
 
 void AutofillDialogViews::ButtonPressed(views::Button* sender,
                                         const ui::Event& event) {
-  if (sender->GetAncestorWithClassName(kOverlayViewClassName)) {
-    delegate_->OverlayButtonPressed();
-    return;
-  }
-
-  // TODO(estade): Should the menu be shown on mouse down?
-  DetailsGroup* group = NULL;
-  for (DetailGroupMap::iterator iter = detail_groups_.begin();
-       iter != detail_groups_.end(); ++iter) {
-    if (sender == iter->second.suggested_button) {
-      group = &iter->second;
-      break;
-    }
-  }
-  DCHECK(group);
-
-  if (!group->suggested_button->visible())
-    return;
-
-  menu_runner_.reset(new views::MenuRunner(
-                         delegate_->MenuModelForSection(group->section)));
-
-  group->container->SetActive(true);
-  views::Button::ButtonState state = group->suggested_button->state();
-  group->suggested_button->SetState(views::Button::STATE_PRESSED);
-  // Ignore the result since we don't need to handle a deleted menu specially.
-  gfx::Rect bounds = group->suggested_button->GetBoundsInScreen();
-  bounds.Inset(group->suggested_button->GetInsets());
-  ignore_result(
-      menu_runner_->RunMenuAt(sender->GetWidget(),
-                              NULL,
-                              bounds,
-                              views::MenuItemView::TOPRIGHT,
-                              ui::GetMenuSourceTypeForEvent(event),
-                              0));
-  group->container->SetActive(false);
-  group->suggested_button->SetState(state);
+  DCHECK(sender->GetAncestorWithClassName(kOverlayViewClassName));
+  delegate_->OverlayButtonPressed();
 }
 
 void AutofillDialogViews::ContentsChanged(views::Textfield* sender,
@@ -1745,6 +1741,41 @@ void AutofillDialogViews::OnSelectedIndexChanged(views::Combobox* combobox) {
 void AutofillDialogViews::StyledLabelLinkClicked(const ui::Range& range,
                                                  int event_flags) {
   delegate_->LegalDocumentLinkClicked(range);
+}
+
+void AutofillDialogViews::OnMenuButtonClicked(views::View* source,
+                                              const gfx::Point& point) {
+  DCHECK_EQ(kSuggestedButtonClassName, source->GetClassName());
+
+  DetailsGroup* group = NULL;
+  for (DetailGroupMap::iterator iter = detail_groups_.begin();
+       iter != detail_groups_.end(); ++iter) {
+    if (source == iter->second.suggested_button) {
+      group = &iter->second;
+      break;
+    }
+  }
+  DCHECK(group);
+
+  if (!group->suggested_button->visible())
+    return;
+
+  menu_runner_.reset(new views::MenuRunner(
+                         delegate_->MenuModelForSection(group->section)));
+
+  group->container->SetActive(true);
+  views::Button::ButtonState state = group->suggested_button->state();
+  group->suggested_button->SetState(views::Button::STATE_PRESSED);
+  if (menu_runner_->RunMenuAt(source->GetWidget(),
+                              NULL,
+                              group->suggested_button->GetBoundsInScreen(),
+                              views::MenuItemView::TOPRIGHT,
+                              ui::MENU_SOURCE_NONE,
+                              0) == views::MenuRunner::MENU_DELETED) {
+    return;
+  }
+  group->container->SetActive(false);
+  group->suggested_button->SetState(state);
 }
 
 gfx::Size AutofillDialogViews::CalculatePreferredSize() {
@@ -1909,28 +1940,13 @@ views::View* AutofillDialogViews::CreateInputsContainer(DialogSection section) {
   SuggestionView* suggested_info = new SuggestionView(this);
   info_view->AddChildView(suggested_info);
 
+  DetailsGroup* group = GroupForSection(section);
   // TODO(estade): It might be slightly more OO if this button were created
   // and listened to by the section container.
-  views::ImageButton* menu_button = new views::ImageButton(this);
-  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  menu_button->SetImage(views::Button::STATE_NORMAL,
-      rb.GetImageSkiaNamed(IDR_AUTOFILL_DIALOG_MENU_BUTTON));
-  menu_button->SetImage(views::Button::STATE_PRESSED,
-      rb.GetImageSkiaNamed(IDR_AUTOFILL_DIALOG_MENU_BUTTON_P));
-  menu_button->SetImage(views::Button::STATE_HOVERED,
-      rb.GetImageSkiaNamed(IDR_AUTOFILL_DIALOG_MENU_BUTTON_H));
-  menu_button->SetImage(views::Button::STATE_DISABLED,
-      rb.GetImageSkiaNamed(IDR_AUTOFILL_DIALOG_MENU_BUTTON_D));
-  menu_button->set_border(views::Border::CreateEmptyBorder(
-      kMenuButtonTopInset,
-      kDialogEdgePadding,
-      kMenuButtonBottomInset,
-      0));
-
-  DetailsGroup* group = GroupForSection(section);
-  group->suggested_button = menu_button;
+  group->suggested_button = new SuggestedButton(this);
   group->manual_input = manual_inputs;
   group->suggested_info = suggested_info;
+
   return info_view;
 }
 

chrome/browser/ui/views/autofill/autofill_dialog_views.h
@@ -17,6 +17,7 @@
 #include "chrome/browser/ui/autofill/testable_autofill_dialog_view.h"
 #include "ui/base/animation/animation_delegate.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/button/menu_button_listener.h"
 #include "ui/views/controls/combobox/combobox_listener.h"
 #include "ui/views/controls/link_listener.h"
@@ -41,7 +42,6 @@ class BubbleBorder;
 class Checkbox;
 class Combobox;
 class FocusManager;
-class ImageButton;
 class ImageView;
 class Label;
 class LabelButton;
@@ -73,7 +73,8 @@ class AutofillDialogViews : public AutofillDialogView,
                             public views::TextfieldController,
                             public views::FocusChangeListener,
                             public views::ComboboxListener,
-                            public views::StyledLabelListener {
+                            public views::StyledLabelListener,
+                            public views::MenuButtonListener {
  public:
   explicit AutofillDialogViews(AutofillDialogViewDelegate* delegate);
   virtual ~AutofillDialogViews();
@@ -169,6 +170,10 @@ class AutofillDialogViews : public AutofillDialogView,
   virtual void StyledLabelLinkClicked(const ui::Range& range, int event_flags)
       OVERRIDE;
 
+  // views::MenuButtonListener implementation.
+  virtual void OnMenuButtonClicked(views::View* source,
+                                   const gfx::Point& point) OVERRIDE;
+
  private:
   // A class that creates and manages a widget for error messages.
   class ErrorBubble : public views::WidgetObserver {
@@ -204,6 +209,7 @@ class AutofillDialogViews : public AutofillDialogView,
   // switch accounts.
   class AccountChooser : public views::View,
                          public views::LinkListener,
+                         public views::MenuButtonListener,
                          public base::SupportsWeakPtr<AccountChooser> {
    public:
     explicit AccountChooser(AutofillDialogViewDelegate* delegate);
@@ -212,22 +218,19 @@ class AutofillDialogViews : public AutofillDialogView,
     // Updates the view based on the state that |delegate_| reports.
     void Update();
 
-    // views::View implementation.
-    virtual bool OnMousePressed(const ui::MouseEvent& event) OVERRIDE;
-    virtual void OnMouseReleased(const ui::MouseEvent& event) OVERRIDE;
-
     // views::LinkListener implementation.
     virtual void LinkClicked(views::Link* source, int event_flags) OVERRIDE;
+
+    // views::MenuButtonListener implementation.
+    virtual void OnMenuButtonClicked(views::View* source,
+                                     const gfx::Point& point) OVERRIDE;
 
    private:
     // The icon for the currently in-use account.
     views::ImageView* image_;
 
-    // The label for the currently in-use account.
-    views::Label* label_;
-
-    // The drop arrow.
-    views::ImageView* arrow_;
+    // The button for showing a menu to change the currently in-use account.
+    views::MenuButton* menu_button_;
 
     // The sign in link.
     views::Link* link_;
@@ -379,6 +382,24 @@ class AutofillDialogViews : public AutofillDialogView,
     DISALLOW_COPY_AND_ASSIGN(SectionContainer);
   };
 
+  class SuggestedButton : public views::MenuButton {
+   public:
+    explicit SuggestedButton(views::MenuButtonListener* listener);
+    virtual ~SuggestedButton();
+
+    // views::MenuButton implementation.
+    virtual gfx::Size GetPreferredSize() OVERRIDE;
+    virtual const char* GetClassName() const OVERRIDE;
+    virtual void PaintChildren(gfx::Canvas* canvas) OVERRIDE;
+    virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
+
+   private:
+    // Returns the corred resource ID (i.e. IDR_*) for the current |state()|.
+    int ResourceIDForState() const;
+
+    DISALLOW_COPY_AND_ASSIGN(SuggestedButton);
+  };
+
   // A view that contains a suggestion (such as a known address) and a link to
   // edit the suggestion.
   class SuggestionView : public views::View {
@@ -456,7 +477,7 @@ class AutofillDialogViews : public AutofillDialogView,
     // visible IFF |manual_input| is not visible.
     SuggestionView* suggested_info;
     // The view that allows selecting other data suggestions.
-    views::ImageButton* suggested_button;
+    SuggestedButton* suggested_button;
   };
 
   // Area for displaying that status of various steps in an Autocheckout flow.

chrome/browser/ui/views/bookmarks/bookmark_bar_view_test.cc
@@ -116,7 +116,7 @@ class BookmarkBarViewEventTestBase : public ViewEventTestBase {
         model_(NULL) {}
 
   virtual void SetUp() OVERRIDE {
-    views::MenuController::TurnOffContextMenuSelectionHoldForTest();
+    views::MenuController::TurnOffMenuSelectionHoldForTest();
     BookmarkBarView::DisableAnimationsForTesting(true);
 
     profile_.reset(new TestingProfile());

chrome/browser/ui/window_sizer/window_sizer_ash_uitest.cc
@@ -167,7 +167,7 @@ IN_PROC_BROWSER_TEST_F(WindowSizerContextMenuTest,
   // Don't shutdown when closing the last browser window.
   chrome::StartKeepAlive();
 
-  views::MenuController::TurnOffContextMenuSelectionHoldForTest();
+  views::MenuController::TurnOffMenuSelectionHoldForTest();
 
   ash::Shell::RootWindowList root_windows = ash::Shell::GetAllRootWindows();
 

ui/views/controls/button/menu_button.cc
@@ -18,6 +18,7 @@
 #include "ui/gfx/screen.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/menu_button_listener.h"
+#include "ui/views/mouse_constants.h"
 #include "ui/views/widget/root_view.h"
 #include "ui/views/widget/widget.h"
 
@@ -32,7 +33,6 @@ static const int kDefaultMenuOffsetY = -4;
 
 // static
 const char MenuButton::kViewClassName[] = "MenuButton";
-const int64 MenuButton::kMinimumTimeBetweenButtonClicks = 100;
 const int MenuButton::kMenuMarkerPaddingLeft = 3;
 const int MenuButton::kMenuMarkerPaddingRight = -1;
 
@@ -182,10 +182,8 @@ bool MenuButton::OnMousePressed(const ui::MouseEvent& event) {
         HitTestPoint(event.location()) &&
         GetDragOperations(event.location()) == ui::DragDropTypes::DRAG_NONE) {
       TimeDelta delta = Time::Now() - menu_closed_time_;
-      int64 delta_in_milliseconds = delta.InMilliseconds();
-      if (delta_in_milliseconds > kMinimumTimeBetweenButtonClicks) {
+      if (delta.InMilliseconds() > kMinimumMsBetweenButtonClicks)
         return Activate();
-      }
     }
   }
   return true;

ui/views/controls/button/menu_button.h
@@ -28,10 +28,6 @@ class VIEWS_EXPORT MenuButton : public TextButton {
  public:
   static const char kViewClassName[];
 
-  // The amount of time, in milliseconds, we wait before allowing another mouse
-  // pressed event to show the menu.
-  static const int64 kMinimumTimeBetweenButtonClicks;
-
   // How much padding to put on the left and right of the menu marker.
   static const int kMenuMarkerPaddingLeft;
   static const int kMenuMarkerPaddingRight;

ui/views/controls/combobox/native_combobox_views.cc
@@ -19,11 +19,11 @@
 #include "ui/views/background.h"
 #include "ui/views/border.h"
 #include "ui/views/color_constants.h"
-#include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/combobox/combobox.h"
 #include "ui/views/controls/focusable_border.h"
 #include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/controls/menu/submenu_view.h"
+#include "ui/views/mouse_constants.h"
 #include "ui/views/widget/root_view.h"
 #include "ui/views/widget/widget.h"
 
@@ -100,7 +100,7 @@ bool NativeComboboxViews::OnMousePressed(const ui::MouseEvent& mouse_event) {
   combobox_->RequestFocus();
   const base::TimeDelta delta = base::Time::Now() - closed_time_;
   if (mouse_event.IsLeftMouseButton() &&
-      (delta.InMilliseconds() > MenuButton::kMinimumTimeBetweenButtonClicks)) {
+      delta.InMilliseconds() > kMinimumMsBetweenButtonClicks) {
     UpdateFromModel();
     ShowDropDownMenu(ui::MENU_SOURCE_MOUSE);
   }

ui/views/controls/menu/menu_controller.cc
@@ -21,13 +21,16 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/screen.h"
+#include "ui/gfx/vector2d.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/menu/menu_config.h"
 #include "ui/views/controls/menu/menu_controller_delegate.h"
+#include "ui/views/controls/menu/menu_host_root_view.h"
 #include "ui/views/controls/menu/menu_scroll_view_container.h"
 #include "ui/views/controls/menu/submenu_view.h"
 #include "ui/views/drag_utils.h"
+#include "ui/views/mouse_constants.h"
 #include "ui/views/view_constants.h"
 #include "ui/views/views_delegate.h"
 #include "ui/views/widget/root_view.h"
@@ -68,11 +71,16 @@ namespace {
 // When showing context menu on mouse down, the user might accidentally select
 // the menu item on the subsequent mouse up. To prevent this, we add the
 // following delay before the user is able to select an item.
-static int context_menu_selection_hold_time_ms = 200;
+static int menu_selection_hold_time_ms = kMinimumMsPressedToActivate;
 
 // The spacing offset for the bubble tip.
 const int kBubbleTipSizeLeftRight = 12;
 const int kBubbleTipSizeTopBottom = 11;
+
+// The maximum distance (in DIPS) that the mouse can be moved before it should
+// trigger a mouse menu item activation (regardless of how long the menu has
+// been showing).
+const float kMaximumLengthMovedToActivate = 4.0f;
 
 // Returns true if the mnemonic of |menu| matches key.
 bool MatchesMnemonic(MenuItemView* menu, char16 key) {
@@ -268,7 +276,7 @@ struct MenuController::SelectByCharDetails {
 MenuController::State::State()
     : item(NULL),
       submenu_open(false),
-      anchor(views::MenuItemView::TOPLEFT),
+      anchor(MenuItemView::TOPLEFT),
       context_menu(false) {}
 
 MenuController::State::~State() {}
@@ -295,12 +303,26 @@ MenuItemView* MenuController::Run(Widget* parent,
   drag_in_progress_ = false;
   closing_event_time_ = base::TimeDelta();
   menu_start_time_ = base::TimeTicks::Now();
+  menu_start_mouse_press_loc_ = gfx::Point();
 
   // If we are shown on mouse press, we will eat the subsequent mouse down and
   // the parent widget will not be able to reset its state (it might have mouse
   // capture from the mouse down). So we clear its state here.
-  if (parent && parent->GetRootView())
-    parent->GetRootView()->SetMouseHandler(NULL);
+  if (parent) {
+    View* root_view = parent->GetRootView();
+    if (root_view) {
+      root_view->SetMouseHandler(NULL);
+      const ui::Event* event =
+          static_cast<internal::RootView*>(root_view)->current_event();
+      if (event && event->type() == ui::ET_MOUSE_PRESSED) {
+        gfx::Point screen_loc(
+            static_cast<const ui::MouseEvent*>(event)->location());
+        View::ConvertPointToScreen(
+            static_cast<View*>(event->target()), &screen_loc);
+        menu_start_mouse_press_loc_ = screen_loc;
+      }
+    }
+  }
 
   bool nested_menu = showing_;
   if (showing_) {
@@ -485,9 +507,9 @@ void MenuController::OnMouseReleased(SubmenuView* source,
     // If it is from an empty menu, use parent context menu instead of that.
     if (menu == NULL &&
         part.submenu->child_count() == 1 &&
-        part.submenu->child_at(0)->id()
-           == views::MenuItemView::kEmptyMenuItemViewID)
+        part.submenu->child_at(0)->id() == MenuItemView::kEmptyMenuItemViewID) {
       menu = part.parent;
+    }
 
     if (menu != NULL && ShowContextMenu(menu, source, event,
                                         ui::MENU_SOURCE_MOUSE))
@@ -504,6 +526,19 @@ void MenuController::OnMouseReleased(SubmenuView* source,
       SendMouseReleaseToActiveView(source, event);
       return;
     }
+    // If a mouse release was received quickly after showing.
+    base::TimeDelta time_shown = base::TimeTicks::Now() - menu_start_time_;
+    if (time_shown.InMilliseconds() < menu_selection_hold_time_ms) {
+      // And it wasn't far from the mouse press location.
+      gfx::Point screen_loc(event.location());
+      View::ConvertPointToScreen(source->GetScrollViewContainer(), &screen_loc);
+      gfx::Vector2d moved = screen_loc - menu_start_mouse_press_loc_;
+      if (moved.Length() < kMaximumLengthMovedToActivate) {
+        // Ignore the mouse release as it was likely this menu was shown under
+        // the mouse and the action was just a normal click.
+        return;
+      }
+    }
     if (part.menu->GetDelegate()->ShouldExecuteCommandWithoutClosingMenu(
             part.menu->GetCommand(), event)) {
       part.menu->GetDelegate()->ExecuteCommand(part.menu->GetCommand(),
@@ -512,11 +547,11 @@ void MenuController::OnMouseReleased(SubmenuView* source,
     }
     if (!part.menu->NonIconChildViewsCount() &&
         part.menu->GetDelegate()->IsTriggerableEvent(part.menu, event)) {
-      int64 time_since_menu_start =
-          (base::TimeTicks::Now() - menu_start_time_).InMilliseconds();
+      base::TimeDelta shown_time = base::TimeTicks::Now() - menu_start_time_;
       if (!state_.context_menu || !View::ShouldShowContextMenuOnMousePress() ||
-          time_since_menu_start > context_menu_selection_hold_time_ms)
+          shown_time.InMilliseconds() > menu_selection_hold_time_ms) {
         Accept(part.menu, event.flags());
+      }
       return;
     }
   } else if (part.type == MenuPart::MENU_ITEM) {
@@ -740,7 +775,7 @@ void MenuController::UpdateSubmenuSelection(SubmenuView* submenu) {
     gfx::Point point = GetScreen()->GetCursorScreenPoint();
     const SubmenuView* root_submenu =
         submenu->GetMenuItem()->GetRootMenuItem()->GetSubmenu();
-    views::View::ConvertPointFromScreen(
+    View::ConvertPointFromScreen(
         root_submenu->GetWidget()->GetRootView(), &point);
     HandleMouseLocation(submenu, point);
   }
@@ -753,8 +788,8 @@ void MenuController::OnWidgetDestroying(Widget* widget) {
 }
 
 // static
-void MenuController::TurnOffContextMenuSelectionHoldForTest() {
-  context_menu_selection_hold_time_ms = -1;
+void MenuController::TurnOffMenuSelectionHoldForTest() {
+  menu_selection_hold_time_ms = -1;
 }
 
 void MenuController::SetSelection(MenuItemView* menu_item,
@@ -914,7 +949,7 @@ void MenuController::StartDrag(SubmenuView* source,
   View::ConvertPointToTarget(NULL, item, &press_loc);
   gfx::Point widget_loc(press_loc);
   View::ConvertPointToWidget(item, &widget_loc);
-  scoped_ptr<gfx::Canvas> canvas(views::GetCanvasForDragImage(
+  scoped_ptr<gfx::Canvas> canvas(GetCanvasForDragImage(
       source->GetWidget(), gfx::Size(item->width(), item->height())));
   item->PaintButton(canvas.get(), MenuItemView::PB_FOR_DRAG);
 
@@ -2079,8 +2114,8 @@ void MenuController::RepostEvent(SubmenuView* source,
 
     if (submenu->GetWidget()->GetNativeView() &&
         GetWindowThreadProcessId(
-            views::HWNDForNativeView(submenu->GetWidget()->GetNativeView()),
-            NULL) != GetWindowThreadProcessId(window, NULL)) {
+            HWNDForNativeView(submenu->GetWidget()->GetNativeView()), NULL) !=
+                GetWindowThreadProcessId(window, NULL)) {
       // Even though we have mouse capture, windows generates a mouse event
       // if the other window is in a separate thread. Don't generate an event in
       // this case else the target window can get double events leading to bad
@@ -2232,9 +2267,8 @@ void MenuController::SendMouseReleaseToActiveView(SubmenuView* event_source,
   View::ConvertPointToScreen(event_source->GetScrollViewContainer(),
                              &target_loc);
   View::ConvertPointToTarget(NULL, active_mouse_view_, &target_loc);
-  ui::MouseEvent release_event(ui::ET_MOUSE_RELEASED,
-                           target_loc, target_loc,
-                           event.flags());
+  ui::MouseEvent release_event(ui::ET_MOUSE_RELEASED, target_loc, target_loc,
+                               event.flags());
   // Reset the active_mouse_view_ before sending mouse released. That way if it
   // calls back to us, we aren't in a weird state.
   View* active_view = active_mouse_view_;

commit 07fad1b7dc5ea1b971db5ad507e2ae484f61f8c7

Fix suggested button margins broken in r219567.
R=estade@chromium.org
BUG=none (yet)
TEST=visual

Review URL: https://chromiumcodereview.appspot.com/23468002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219683 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/autofill/autofill_dialog_views.cc
@@ -1008,7 +1008,10 @@ AutofillDialogViews::SuggestedButton::~SuggestedButton() {}
 
 gfx::Size AutofillDialogViews::SuggestedButton::GetPreferredSize() {
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  return rb.GetImageNamed(ResourceIDForState()).Size();
+  gfx::Size size = rb.GetImageNamed(ResourceIDForState()).Size();
+  const gfx::Insets insets = GetInsets();
+  size.Enlarge(insets.width(), insets.height());
+  return size;
 }
 
 const char* AutofillDialogViews::SuggestedButton::GetClassName() const {
@@ -1019,7 +1022,9 @@ void AutofillDialogViews::SuggestedButton::PaintChildren(gfx::Canvas* canvas) {}
 
 void AutofillDialogViews::SuggestedButton::OnPaint(gfx::Canvas* canvas) {
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  canvas->DrawImageInt(*rb.GetImageSkiaNamed(ResourceIDForState()), 0, 0);
+  const gfx::Insets insets = GetInsets();
+  canvas->DrawImageInt(*rb.GetImageSkiaNamed(ResourceIDForState()),
+                       insets.left(), insets.top());
 }
 
 int AutofillDialogViews::SuggestedButton::ResourceIDForState() const {

chrome/browser/ui/views/autofill/autofill_dialog_views.h
@@ -382,6 +382,7 @@ class AutofillDialogViews : public AutofillDialogView,
     DISALLOW_COPY_AND_ASSIGN(SectionContainer);
   };
 
+  // A button to show address or billing suggestions.
   class SuggestedButton : public views::MenuButton {
    public:
     explicit SuggestedButton(views::MenuButtonListener* listener);