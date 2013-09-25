ChromiumWebApps/chromium

commit d99d3452c47534e1b48a0d2322d0a6d28df91a29

Don't PostProcessActivateMessage when it's not for our HWND
WM_ACTIVATE is sent to both the window being activated and the window
that was last activated. The one being deactivated (i.e. ours) is also
sent the message with a WA_ACTIVE wparam, and an HWND of the other
window (that's actually being activated). This was getting passed
through to views as if it was an activate of our window, so things were
getting saved/restored at the wrong times.

So, only process the message when it's our window which is what views
is actually expecting.

R=sky@chromium.org
BUG=283770

Review URL: https://chromiumcodereview.appspot.com/23440056

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224304 0039d316-1c4b-4281-b951-d872f2087c98

ui/views/win/hwnd_message_handler.cc
@@ -899,8 +899,10 @@ LRESULT HWNDMessageHandler::OnWndProc(UINT message,
   }
 
   // Only top level widget should store/restore focus.
-  if (message == WM_ACTIVATE && delegate_->CanSaveFocus())
+  if (message == WM_ACTIVATE && delegate_->CanSaveFocus() &&
+      reinterpret_cast<HWND>(l_param) == window) {
     PostProcessActivateMessage(LOWORD(w_param));
+  }
   if (message == WM_ENABLE && restore_focus_when_enabled_) {
     // This path should be executed only for top level as
     // restore_focus_when_enabled_ is set in PostProcessActivateMessage.

commit 055f9bf9a6fad09b58700eaeb7148e8547e2cf45

Fix activation problems seen in non AURA chrome Windows which regressed in revision r224304
We introduced a check for whether the lParam is our HWND. That is not correct in the deactivation
case where WM_ACTIVATE is sent to the window being deactivated with lParam being the handle to the
window being activated and vice versa. In the minimize case lParam is 0 with wParam containing the
value 0x200001 which indicates that the window is minimized but is active for tab preview purposes.
The window check basically meant that we never saved the view which had focus when we lost activation
and thus were unable to restore it when we got activated.

Reverted the check for the window introduced in r224304 and added a check for whether the window is
being minimized in the HWNDMessageHandler::PostProcessActivateMessage function. We treat it as inactive
and save away the last focused view and restore it when the window is restored./

BUG=297525
R=sky@chromium.org, sky

Review URL: https://codereview.chromium.org/24468003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225142 0039d316-1c4b-4281-b951-d872f2087c98

ui/views/win/hwnd_message_handler.cc
@@ -905,10 +905,9 @@ LRESULT HWNDMessageHandler::OnWndProc(UINT message,
   }
 
   // Only top level widget should store/restore focus.
-  if (message == WM_ACTIVATE && delegate_->CanSaveFocus() &&
-      reinterpret_cast<HWND>(l_param) == window) {
-    PostProcessActivateMessage(LOWORD(w_param));
-  }
+  if (message == WM_ACTIVATE && delegate_->CanSaveFocus())
+    PostProcessActivateMessage(LOWORD(w_param), !!HIWORD(w_param));
+
   if (message == WM_ENABLE && restore_focus_when_enabled_) {
     // This path should be executed only for top level as
     // restore_focus_when_enabled_ is set in PostProcessActivateMessage.
@@ -959,9 +958,10 @@ void HWNDMessageHandler::SetInitialFocus() {
   }
 }
 
-void HWNDMessageHandler::PostProcessActivateMessage(int activation_state) {
+void HWNDMessageHandler::PostProcessActivateMessage(int activation_state,
+                                                    bool minimized) {
   DCHECK(delegate_->CanSaveFocus());
-  if (WA_INACTIVE == activation_state) {
+  if (WA_INACTIVE == activation_state || minimized) {
     // We might get activated/inactivated without being enabled, so we need to
     // clear restore_focus_when_enabled_.
     restore_focus_when_enabled_ = false;

ui/views/win/hwnd_message_handler.h
@@ -180,7 +180,7 @@ class VIEWS_EXPORT HWNDMessageHandler :
 
   // Called after the WM_ACTIVATE message has been processed by the default
   // windows procedure.
-  void PostProcessActivateMessage(int activation_state);
+  void PostProcessActivateMessage(int activation_state, bool minimized);
 
   // Enables disabled owner windows that may have been disabled due to this
   // window's modality.