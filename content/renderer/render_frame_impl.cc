ChromiumWebApps/chromium

commit 93cabae2a3c1453a1736b3a0f5f76bdc18ac1e41

Cancel resources in the plugin process when a user presses the stop button or window.stop() is called.
BUG=286074
R=ananta@chromium.org

Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=225099

Review URL: https://codereview.chromium.org/24302012

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226243 0039d316-1c4b-4281-b951-d872f2087c98

content/renderer/render_frame_impl.cc
@@ -8,6 +8,7 @@
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "content/child/appcache/appcache_dispatcher.h"
+#include "content/child/plugin_messages.h"
 #include "content/child/quota_dispatcher.h"
 #include "content/child/request_extra_data.h"
 #include "content/common/socket_stream_handle_data.h"
@@ -22,6 +23,7 @@
 #include "content/renderer/browser_plugin/browser_plugin.h"
 #include "content/renderer/browser_plugin/browser_plugin_manager.h"
 #include "content/renderer/internal_document_state_data.h"
+#include "content/renderer/npapi/plugin_channel_host.h"
 #include "content/renderer/render_thread_impl.h"
 #include "content/renderer/render_view_impl.h"
 #include "content/renderer/renderer_webapplicationcachehost_impl.h"
@@ -703,6 +705,13 @@ void RenderFrameImpl::didRunInsecureContent(
       render_view_->GetRoutingID(),
       origin.toString().utf8(),
       target));
+}
+
+void RenderFrameImpl::didAbortLoading(WebKit::WebFrame* frame) {
+#if defined(ENABLE_PLUGINS)
+  PluginChannelHost::Broadcast(
+      new PluginHostMsg_DidAbortLoading(render_view_->GetRoutingID()));
+#endif
 }
 
 void RenderFrameImpl::didExhaustMemoryAvailableForScript(

content/child/npapi/webplugin_delegate_impl.h
@@ -42,7 +42,6 @@ class CARenderer;
 
 namespace content {
 class PluginInstance;
-class PluginURLFetcher;
 class WebPlugin;
 
 #if defined(OS_MACOSX)

content/child/plugin_messages.h
@@ -192,6 +192,9 @@ IPC_MESSAGE_CONTROL1(PluginMsg_ResetModalDialogEvent,
 IPC_MESSAGE_ROUTED1(PluginMsg_FetchURL,
                     PluginMsg_FetchURL_Params)
 
+IPC_MESSAGE_CONTROL1(PluginHostMsg_DidAbortLoading,
+                     int /* render_view_id */)
+
 #if defined(OS_WIN)
 IPC_MESSAGE_ROUTED4(PluginMsg_ImeCompositionUpdated,
                     string16 /* text */,

content/plugin/plugin_channel.cc
@@ -13,6 +13,7 @@
 #include "build/build_config.h"
 #include "content/child/child_process.h"
 #include "content/child/npapi/plugin_instance.h"
+#include "content/child/npapi/webplugin_delegate_impl.h"
 #include "content/child/plugin_messages.h"
 #include "content/common/plugin_process_messages.h"
 #include "content/plugin/plugin_thread.h"
@@ -263,6 +264,7 @@ bool PluginChannel::OnControlMessageReceived(const IPC::Message& msg) {
                                     OnDestroyInstance)
     IPC_MESSAGE_HANDLER(PluginMsg_GenerateRouteID, OnGenerateRouteID)
     IPC_MESSAGE_HANDLER(PluginProcessMsg_ClearSiteData, OnClearSiteData)
+    IPC_MESSAGE_HANDLER(PluginHostMsg_DidAbortLoading, OnDidAbortLoading)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
   DCHECK(handled);
@@ -329,6 +331,15 @@ void PluginChannel::OnClearSiteData(const std::string& site,
     }
   }
   Send(new PluginProcessHostMsg_ClearSiteDataResult(success));
+}
+
+void PluginChannel::OnDidAbortLoading(int render_view_id) {
+  for (size_t i = 0; i < plugin_stubs_.size(); ++i) {
+    if (plugin_stubs_[i]->webplugin()->host_render_view_routing_id() ==
+            render_view_id) {
+      plugin_stubs_[i]->delegate()->instance()->CloseStreams();
+    }
+  }
 }
 
 }  // namespace content

content/plugin/plugin_channel.h
@@ -81,6 +81,7 @@ class PluginChannel : public NPChannelBase {
   void OnClearSiteData(const std::string& site,
                        uint64 flags,
                        uint64 max_age);
+  void OnDidAbortLoading(int render_view_id);
 
   std::vector<scoped_refptr<WebPluginDelegateStub> > plugin_stubs_;
 

content/plugin/webplugin_delegate_stub.h
@@ -48,6 +48,7 @@ class WebPluginDelegateStub : public IPC::Listener,
   virtual bool Send(IPC::Message* msg) OVERRIDE;
 
   int instance_id() { return instance_id_; }
+  WebPluginDelegateImpl* delegate() { return delegate_; }
   WebPluginProxy* webplugin() { return webplugin_; }
 
  private:

content/renderer/render_frame_impl.h
@@ -133,6 +133,7 @@ class CONTENT_EXPORT RenderFrameImpl
   virtual void didRunInsecureContent(WebKit::WebFrame* frame,
                                      const WebKit::WebSecurityOrigin& origin,
                                      const WebKit::WebURL& target);
+  virtual void didAbortLoading(WebKit::WebFrame* frame);
   virtual void didExhaustMemoryAvailableForScript(
       WebKit::WebFrame* frame);
   virtual void didCreateScriptContext(WebKit::WebFrame* frame,

commit 268d4089eb9ebc9cdd997a3998228fd848da8fc5

When stopping plugin requests due to window.stop() or a user pressing the stop button, only do this when the frame getting the notification is the main frame. Blink calls us back for all frames of a page when navigating, so if the main frame is playing a video and a subframe navigates, we were incorrectly stopping all plugin requests.
This is a regression from r226243 and fixes Netflix playback.

BUG=304845,286074
R=tburkard@chromium.org

Review URL: https://codereview.chromium.org/27351004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228809 0039d316-1c4b-4281-b951-d872f2087c98

content/renderer/render_frame_impl.cc
@@ -752,6 +752,8 @@ void RenderFrameImpl::didRunInsecureContent(
 
 void RenderFrameImpl::didAbortLoading(WebKit::WebFrame* frame) {
 #if defined(ENABLE_PLUGINS)
+  if (frame != render_view_->webview()->mainFrame())
+    return;
   PluginChannelHost::Broadcast(
       new PluginHostMsg_DidAbortLoading(render_view_->GetRoutingID()));
 #endif