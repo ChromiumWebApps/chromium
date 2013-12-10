ChromiumWebApps/chromium

commit 60586c42d848a087e938e3c561541804f1b9eac5

Settings: Do not call preventDefault() for a mousedown event targetting an edit-on-click control.
This CL fixes the following five bugs:

crbug.com/281239: 'Behavior' SELECT doesn't reset the value on canceling editing
crbug.com/306355: Text fields in lists don't getting dragged on the first click.
crbug.com/316527: 'Select all' context menu item on an editable text selects whole the page.
crbug.com/318688: Text drag&drop in a list removes the content of the source textfield.
crbug.com/324772: Touching a 'Behavior' column in content settings shows software keyboard.

There were two problems:

* If a mousedown event is kicked on an element representing static text version
  of an edit-on-click control, we called preventDefault() because there are no
  focusable elements between the element and the ListItem root.
  InlineEditableItem gives focus on the editable control in this case
  (editClickTarget_), but preventDefault() affected text drag and context menu
  unexpectedly.
  We need to avoid preventDefault() in this case. But this case was not
  detectable in handleMouseDown() in list.js. This CL makes the static text
  element focusable during mousedown processing, and move the focus to the
  editable control later.

* As for content settings, clicking on a staic text element in Behavior column
  gave focus on the first INPUT element in the ListItem, not the corresponding
  SELECT element.
  We should follow a way implemented in InlineEditableItem for this control
  too. We register pair of elements to InlineEditableItem::editFields_, and
  remove isExtraFocusableControl.

BUG=281239, 306355, 316527, 318688, 324772

Review URL: https://codereview.chromium.org/105733006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239172 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/options/inline_editable_list.js
@@ -95,10 +95,8 @@ cr.define('options', function() {
      * Updates the edit state based on the current selected and lead states.
      */
     updateEditState: function() {
-      if (this.editable) {
-        this.editing = this.selected && this.lead &&
-          !this.isExtraFocusableControl(document.activeElement);
-      }
+      if (this.editable)
+        this.editing = this.selected && this.lead;
     },
 
     /**
@@ -126,12 +124,21 @@ cr.define('options', function() {
         this.editClickTarget_ = null;
 
         if (focusElement) {
-          focusElement.focus();
-          // select() doesn't work well in mousedown event handler.
-          setTimeout(function() {
-              if (focusElement.ownerDocument.activeElement == focusElement)
-                focusElement.select();
-          }, 0);
+          var self = this;
+          // We should delay to give focus on |focusElement| if this is called
+          // in mousedown event handler. If we did give focus immediately, Blink
+          // would try to focus on an ancestor of the mousedown target element,
+          // and remove focus from |focusElement|.
+          if (focusElement.staticVersion &&
+              focusElement.staticVersion.hasAttribute('tabindex')) {
+            setTimeout(function() {
+              if (self.editing)
+                self.focusAndMaybeSelect_(focusElement);
+              focusElement.staticVersion.removeAttribute('tabindex');
+            }, 0);
+          } else {
+            this.focusAndMaybeSelect_(focusElement);
+          }
         }
       } else {
         if (!this.editCancelled_ && this.hasBeenEdited &&
@@ -146,6 +153,18 @@ cr.define('options', function() {
           cr.dispatchSimpleEvent(this, 'canceledit', true);
         }
       }
+    },
+
+    /**
+     * Focus on the specified element, and select the editable text in it
+     * if possible.
+     * @param {!Element} control An element to be focused.
+     * @private
+     */
+    focusAndMaybeSelect_: function(control) {
+      control.focus();
+      if (control.tagName == 'INPUT')
+        control.select();
     },
 
     /**
@@ -215,9 +234,9 @@ cr.define('options', function() {
      */
     createEditableTextCell: function(text) {
       var container = this.ownerDocument.createElement('div');
-
+      var textEl;
       if (!this.isPlaceholder) {
-        var textEl = this.ownerDocument.createElement('div');
+        textEl = this.ownerDocument.createElement('div');
         textEl.className = 'static-text';
         textEl.textContent = text;
         textEl.setAttribute('displaymode', 'static');
@@ -229,7 +248,6 @@ cr.define('options', function() {
       inputEl.value = text;
       if (!this.isPlaceholder) {
         inputEl.setAttribute('displaymode', 'edit');
-        inputEl.staticVersion = textEl;
       } else {
         // At this point |this| is not attached to the parent list yet, so give
         // a short timeout in order for the attachment to occur.
@@ -251,9 +269,21 @@ cr.define('options', function() {
          window.setTimeout(handler, 0);
       });
       container.appendChild(inputEl);
-      this.editFields_.push(inputEl);
+      this.addEditField(inputEl, textEl);
 
       return container;
+    },
+
+    /**
+     * Register an edit field.
+     * @param {!Element} control An editable element. It's a form control
+     *     element typically.
+     * @param {Element} staticElement An element representing non-editable
+     *     state.
+     */
+    addEditField: function(control, staticElement) {
+      control.staticVersion = staticElement;
+      this.editFields_.push(control);
     },
 
     /**
@@ -342,30 +372,16 @@ cr.define('options', function() {
         return;
 
       var clickTarget = e.target;
-      if (this.isExtraFocusableControl(clickTarget)) {
-        clickTarget.focus();
-        return;
-      }
-
       var editFields = this.editFields_;
       for (var i = 0; i < editFields.length; i++) {
+        if (editFields[i].staticVersion == clickTarget)
+          clickTarget.tabIndex = 0;
         if (editFields[i] == clickTarget ||
             editFields[i].staticVersion == clickTarget) {
           this.editClickTarget_ = editFields[i];
           return;
         }
       }
-    },
-
-    /**
-     * Check if the specified element is a focusable form control which is in
-     * the list item and not in |editFields_|.
-     * @param {!Element} element An element.
-     * @return {boolean} Returns true if the element is one of focusable
-     *     controls in this list item.
-     */
-    isExtraFocusableControl: function(element) {
-      return false;
     },
   };
 

ui/webui/resources/js/cr/ui/list.js
@@ -1298,24 +1298,23 @@ cr.define('cr.ui', function() {
     //
     // [1] For example, clicking non-focusable area gives focus on the first
     // form control in the item.
-    if (!tryFocusOnAncestor(e.target, listItem) &&
+    if (!containsFocusableElement(e.target, listItem) &&
         listItem.contains(listItem.ownerDocument.activeElement)) {
       e.preventDefault();
     }
   }
 
   /**
-   * Try focusing on |eventTarget| or its ancestor under |root|.
+   * Check if |start| or its ancestor under |root| is focusable.
    * This is a helper for handleMouseDown.
-   * @param {!Element} start An element which we start to try.
-   * @param {!Element} root An element which we finish to try.
-   * @return {boolean} True if we focused on an element successfully.
+   * @param {!Element} start An element which we start to check.
+   * @param {!Element} root An element which we finish to check.
+   * @return {boolean} True if we found a focusable element.
    */
-  function tryFocusOnAncestor(start, root) {
+  function containsFocusableElement(start, root) {
     for (var element = start; element && element != root;
         element = element.parentElement) {
-      element.focus();
-      if (root.ownerDocument.activeElement == element)
+      if (element.tabIndex >= 0)
         return true;
     }
     return false;

chrome/browser/resources/options/content_settings_exceptions_area.js
@@ -104,6 +104,7 @@ cr.define('options.contentSettings', function() {
         this.editable = false;
       }
 
+      this.addEditField(select, this.settingLabel);
       this.contentElement.appendChild(select);
       select.className = 'exception-setting';
       select.setAttribute('aria-labelledby', 'exception-behavior-column');
@@ -371,11 +372,6 @@ cr.define('options.contentSettings', function() {
 
       chrome.send('setException',
                   [this.contentType, this.mode, newPattern, newSetting]);
-    },
-
-    /** @override */
-    isExtraFocusableControl: function(element) {
-      return element === this.select;
     },
   };
 

chrome/browser/resources/options/options_page.css
@@ -332,7 +332,12 @@ list > :not([editing]) [displaymode='edit'] {
 }
 
 list > [editing] [displaymode='static'] {
-  display: none;
+  /* Don't use display:none because we need to keep an element focusable. */
+  left: 0;
+  opacity: 0;
+  pointer-events: none;
+  position: absolute;
+  top: -10em;
 }
 
 list > [editing] input:invalid {

commit 098595e749e752273d3a9f7644be8318b560bd55

Settings: Can't activate URL fields in 'Default search settings' section.
This is a regression by r239172.

Bug detail:
These URL fields are input[disabled], which are not focusable. After r239172, a
static version of a URL field gets focus by mousedown, and we try to move focus
to the URL field later. But the URL field is not focusable and we loses
focus. This behavior worked before r239172 because the owner <list> got focus by
mousedown.

Fix:
We focus on the owner <list> explicity in such case. It's same as r239171 or
earlier.
Also, this CL fixes a disabled element issue in containsFocusableElement().
This doesn't affect any behavior, and it just improves correctness.

BUG=326863

Review URL: https://codereview.chromium.org/109943002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239726 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/options/inline_editable_list.js
@@ -132,8 +132,11 @@ cr.define('options', function() {
           if (focusElement.staticVersion &&
               focusElement.staticVersion.hasAttribute('tabindex')) {
             setTimeout(function() {
-              if (self.editing)
+              if (self.editing) {
+                if (focusElement.disabled)
+                  self.parentNode.focus();
                 self.focusAndMaybeSelect_(focusElement);
+              }
               focusElement.staticVersion.removeAttribute('tabindex');
             }, 0);
           } else {

ui/webui/resources/js/cr/ui/list.js
@@ -1314,7 +1314,7 @@ cr.define('cr.ui', function() {
   function containsFocusableElement(start, root) {
     for (var element = start; element && element != root;
         element = element.parentElement) {
-      if (element.tabIndex >= 0)
+      if (element.tabIndex >= 0 && !element.disabled)
         return true;
     }
     return false;