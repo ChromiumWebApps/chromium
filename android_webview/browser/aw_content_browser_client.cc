ChromiumWebApps/chromium

commit f3add92d73df86fa62bf727b0016e0c62149b326

A bunch of work to transition from render_view_id to render_frame_id.
-remove ResourceRequestInfo::GetRenderViewForRequest
-reduce usage of GetAssociatedRenderView and add GetAssociatedRenderFrame which will replace it
-move the cookie jar from RenderView to RenderFrame
-switch most of the usages of RenderView to RenderFrame in WebPluginImpl

BUG=304341
R=boliu@chromium.org, nasko@chromium.org

Review URL: https://codereview.chromium.org/114573003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242200 0039d316-1c4b-4281-b951-d872f2087c98

android_webview/browser/aw_content_browser_client.cc
@@ -258,13 +258,13 @@ bool AwContentBrowserClient::AllowGetCookie(const GURL& url,
                                             const net::CookieList& cookie_list,
                                             content::ResourceContext* context,
                                             int render_process_id,
-                                            int render_view_id) {
+                                            int render_frame_id) {
   return AwCookieAccessPolicy::GetInstance()->AllowGetCookie(url,
                                                              first_party,
                                                              cookie_list,
                                                              context,
                                                              render_process_id,
-                                                             render_view_id);
+                                                             render_frame_id);
 }
 
 bool AwContentBrowserClient::AllowSetCookie(const GURL& url,
@@ -272,14 +272,14 @@ bool AwContentBrowserClient::AllowSetCookie(const GURL& url,
                                             const std::string& cookie_line,
                                             content::ResourceContext* context,
                                             int render_process_id,
-                                            int render_view_id,
+                                            int render_frame_id,
                                             net::CookieOptions* options) {
   return AwCookieAccessPolicy::GetInstance()->AllowSetCookie(url,
                                                              first_party,
                                                              cookie_line,
                                                              context,
                                                              render_process_id,
-                                                             render_view_id,
+                                                             render_frame_id,
                                                              options);
 }
 

android_webview/browser/aw_contents_io_thread_client.h
@@ -56,11 +56,11 @@ class AwContentsIoThreadClient {
   virtual CacheMode GetCacheMode() const = 0;
 
   // This will attempt to fetch the AwContentsIoThreadClient for the given
-  // |render_process_id|, |render_view_id| pair.
+  // |render_process_id|, |render_frame_id| pair.
   // This method can be called from any thread.
   // An empty scoped_ptr is a valid return value.
   static scoped_ptr<AwContentsIoThreadClient> FromID(int render_process_id,
-                                                     int render_view_id);
+                                                     int render_frame_id);
 
   // This method is called on the IO thread only.
   virtual scoped_ptr<InterceptedRequestData> ShouldInterceptRequest(

android_webview/native/aw_contents_io_thread_client_impl.cc
@@ -15,6 +15,7 @@
 #include "base/memory/scoped_ptr.h"
 #include "base/synchronization/lock.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/resource_request_info.h"
@@ -30,7 +31,7 @@ using base::android::JavaRef;
 using base::android::ScopedJavaLocalRef;
 using base::LazyInstance;
 using content::BrowserThread;
-using content::RenderViewHost;
+using content::RenderFrameHost;
 using content::WebContents;
 using std::map;
 using std::pair;
@@ -49,56 +50,56 @@ struct IoThreadClientData {
 IoThreadClientData::IoThreadClientData() : pending_association(false) {}
 
 typedef map<pair<int, int>, IoThreadClientData>
-    RenderViewHostToIoThreadClientType;
+    RenderFrameHostToIoThreadClientType;
 
-static pair<int, int> GetRenderViewHostIdPair(RenderViewHost* rvh) {
-  return pair<int, int>(rvh->GetProcess()->GetID(), rvh->GetRoutingID());
+static pair<int, int> GetRenderFrameHostIdPair(RenderFrameHost* rfh) {
+  return pair<int, int>(rfh->GetProcess()->GetID(), rfh->GetRoutingID());
 }
 
-// RvhToIoThreadClientMap -----------------------------------------------------
-class RvhToIoThreadClientMap {
+// RfhToIoThreadClientMap -----------------------------------------------------
+class RfhToIoThreadClientMap {
  public:
-  static RvhToIoThreadClientMap* GetInstance();
-  void Set(pair<int, int> rvh_id, const IoThreadClientData& client);
-  bool Get(pair<int, int> rvh_id, IoThreadClientData* client);
-  void Erase(pair<int, int> rvh_id);
+  static RfhToIoThreadClientMap* GetInstance();
+  void Set(pair<int, int> rfh_id, const IoThreadClientData& client);
+  bool Get(pair<int, int> rfh_id, IoThreadClientData* client);
+  void Erase(pair<int, int> rfh_id);
 
  private:
-  static LazyInstance<RvhToIoThreadClientMap> g_instance_;
+  static LazyInstance<RfhToIoThreadClientMap> g_instance_;
   base::Lock map_lock_;
-  RenderViewHostToIoThreadClientType rvh_to_io_thread_client_;
+  RenderFrameHostToIoThreadClientType rfh_to_io_thread_client_;
 };
 
 // static
-LazyInstance<RvhToIoThreadClientMap> RvhToIoThreadClientMap::g_instance_ =
+LazyInstance<RfhToIoThreadClientMap> RfhToIoThreadClientMap::g_instance_ =
     LAZY_INSTANCE_INITIALIZER;
 
 // static
-RvhToIoThreadClientMap* RvhToIoThreadClientMap::GetInstance() {
+RfhToIoThreadClientMap* RfhToIoThreadClientMap::GetInstance() {
   return g_instance_.Pointer();
 }
 
-void RvhToIoThreadClientMap::Set(pair<int, int> rvh_id,
+void RfhToIoThreadClientMap::Set(pair<int, int> rfh_id,
                                  const IoThreadClientData& client) {
   base::AutoLock lock(map_lock_);
-  rvh_to_io_thread_client_[rvh_id] = client;
+  rfh_to_io_thread_client_[rfh_id] = client;
 }
 
-bool RvhToIoThreadClientMap::Get(
-    pair<int, int> rvh_id, IoThreadClientData* client) {
+bool RfhToIoThreadClientMap::Get(
+    pair<int, int> rfh_id, IoThreadClientData* client) {
   base::AutoLock lock(map_lock_);
-  RenderViewHostToIoThreadClientType::iterator iterator =
-      rvh_to_io_thread_client_.find(rvh_id);
-  if (iterator == rvh_to_io_thread_client_.end())
+  RenderFrameHostToIoThreadClientType::iterator iterator =
+      rfh_to_io_thread_client_.find(rfh_id);
+  if (iterator == rfh_to_io_thread_client_.end())
     return false;
 
   *client = iterator->second;
   return true;
 }
 
-void RvhToIoThreadClientMap::Erase(pair<int, int> rvh_id) {
+void RfhToIoThreadClientMap::Erase(pair<int, int> rfh_id) {
   base::AutoLock lock(map_lock_);
-  rvh_to_io_thread_client_.erase(rvh_id);
+  rfh_to_io_thread_client_.erase(rfh_id);
 }
 
 // ClientMapEntryUpdater ------------------------------------------------------
@@ -108,8 +109,8 @@ class ClientMapEntryUpdater : public content::WebContentsObserver {
   ClientMapEntryUpdater(JNIEnv* env, WebContents* web_contents,
                         jobject jdelegate);
 
-  virtual void RenderViewCreated(RenderViewHost* render_view_host) OVERRIDE;
-  virtual void RenderViewDeleted(RenderViewHost* render_view_host) OVERRIDE;
+  virtual void RenderFrameCreated(RenderFrameHost* render_frame_host) OVERRIDE;
+  virtual void RenderFrameDeleted(RenderFrameHost* render_frame_host) OVERRIDE;
   virtual void WebContentsDestroyed(WebContents* web_contents) OVERRIDE;
 
  private:
@@ -124,20 +125,20 @@ ClientMapEntryUpdater::ClientMapEntryUpdater(JNIEnv* env,
   DCHECK(web_contents);
   DCHECK(jdelegate);
 
-  if (web_contents->GetRenderViewHost())
-    RenderViewCreated(web_contents->GetRenderViewHost());
+  if (web_contents->GetMainFrame())
+    RenderFrameCreated(web_contents->GetMainFrame());
 }
 
-void ClientMapEntryUpdater::RenderViewCreated(RenderViewHost* rvh) {
+void ClientMapEntryUpdater::RenderFrameCreated(RenderFrameHost* rfh) {
   IoThreadClientData client_data;
   client_data.io_thread_client = jdelegate_;
   client_data.pending_association = false;
-  RvhToIoThreadClientMap::GetInstance()->Set(
-      GetRenderViewHostIdPair(rvh), client_data);
+  RfhToIoThreadClientMap::GetInstance()->Set(
+      GetRenderFrameHostIdPair(rfh), client_data);
 }
 
-void ClientMapEntryUpdater::RenderViewDeleted(RenderViewHost* rvh) {
-  RvhToIoThreadClientMap::GetInstance()->Erase(GetRenderViewHostIdPair(rvh));
+void ClientMapEntryUpdater::RenderFrameDeleted(RenderFrameHost* rfh) {
+  RfhToIoThreadClientMap::GetInstance()->Erase(GetRenderFrameHostIdPair(rfh));
 }
 
 void ClientMapEntryUpdater::WebContentsDestroyed(WebContents* web_contents) {
@@ -150,10 +151,10 @@ void ClientMapEntryUpdater::WebContentsDestroyed(WebContents* web_contents) {
 
 // static
 scoped_ptr<AwContentsIoThreadClient>
-AwContentsIoThreadClient::FromID(int render_process_id, int render_view_id) {
-  pair<int, int> rvh_id(render_process_id, render_view_id);
+AwContentsIoThreadClient::FromID(int render_process_id, int render_frame_id) {
+  pair<int, int> rfh_id(render_process_id, render_frame_id);
   IoThreadClientData client_data;
-  if (!RvhToIoThreadClientMap::GetInstance()->Get(rvh_id, &client_data))
+  if (!RfhToIoThreadClientMap::GetInstance()->Get(rfh_id, &client_data))
     return scoped_ptr<AwContentsIoThreadClient>();
 
   JNIEnv* env = AttachCurrentThread();
@@ -169,8 +170,8 @@ void AwContentsIoThreadClientImpl::RegisterPendingContents(
     WebContents* web_contents) {
   IoThreadClientData client_data;
   client_data.pending_association = true;
-  RvhToIoThreadClientMap::GetInstance()->Set(
-      GetRenderViewHostIdPair(web_contents->GetRenderViewHost()), client_data);
+  RfhToIoThreadClientMap::GetInstance()->Set(
+      GetRenderFrameHostIdPair(web_contents->GetMainFrame()), client_data);
 }
 
 // static

commit 04741728117ac420a3b4fd483a96a3a087e79b17

Fix race condition introduced in r242200 where AwContentsIoThreadClient is queried before a subframe's entry is added.
This happened because the entry was updated on the UI thread through WebContentsObserver::RenderFrameCreated. The race condition was that the IO thread could dispatch a resource request for that subframe before the map is updated.

The fix is to send a message from the renderer to the IO thread when a subframe is created so that the map is updated before any resouce requests arrive.

BUG=304341
R=mkosiba@chromium.org

Review URL: https://codereview.chromium.org/135443002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244521 0039d316-1c4b-4281-b951-d872f2087c98

android_webview/browser/aw_content_browser_client.cc
@@ -7,6 +7,7 @@
 #include "android_webview/browser/aw_browser_context.h"
 #include "android_webview/browser/aw_browser_main_parts.h"
 #include "android_webview/browser/aw_contents_client_bridge_base.h"
+#include "android_webview/browser/aw_contents_io_thread_client.h"
 #include "android_webview/browser/aw_cookie_access_policy.h"
 #include "android_webview/browser/aw_quota_permission_context.h"
 #include "android_webview/browser/aw_web_preferences_populater.h"
@@ -56,6 +57,7 @@ public:
   void OnShouldOverrideUrlLoading(int routing_id,
                                   const base::string16& url,
                                   bool* ignore_navigation);
+  void OnSubFrameCreated(int parent_render_frame_id, int child_render_frame_id);
 
 private:
   virtual ~AwContentsMessageFilter();
@@ -85,6 +87,7 @@ bool AwContentsMessageFilter::OnMessageReceived(const IPC::Message& message,
   IPC_BEGIN_MESSAGE_MAP_EX(AwContentsMessageFilter, message, *message_was_ok)
       IPC_MESSAGE_HANDLER(AwViewHostMsg_ShouldOverrideUrlLoading,
                           OnShouldOverrideUrlLoading)
+      IPC_MESSAGE_HANDLER(AwViewHostMsg_SubFrameCreated, OnSubFrameCreated)
       IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
   return handled;
@@ -104,6 +107,12 @@ void AwContentsMessageFilter::OnShouldOverrideUrlLoading(
     LOG(WARNING) << "Failed to find the associated render view host for url: "
                  << url;
   }
+}
+
+void AwContentsMessageFilter::OnSubFrameCreated(int parent_render_frame_id,
+                                                int child_render_frame_id) {
+  AwContentsIoThreadClient::SubFrameCreated(
+      process_id_, parent_render_frame_id, child_render_frame_id);
 }
 
 class AwAccessTokenStore : public content::AccessTokenStore {

android_webview/browser/aw_contents_io_thread_client.h
@@ -62,6 +62,11 @@ class AwContentsIoThreadClient {
   static scoped_ptr<AwContentsIoThreadClient> FromID(int render_process_id,
                                                      int render_frame_id);
 
+  // Called on the IO thread when a subframe is created.
+  static void SubFrameCreated(int render_process_id,
+                              int parent_render_frame_id,
+                              int child_render_frame_id);
+
   // This method is called on the IO thread only.
   virtual scoped_ptr<InterceptedRequestData> ShouldInterceptRequest(
       const GURL& location,

android_webview/common/render_view_messages.h
@@ -110,3 +110,8 @@ IPC_SYNC_MESSAGE_CONTROL2_1(AwViewHostMsg_ShouldOverrideUrlLoading,
                             int /* render_frame_id id */,
                             base::string16 /* in - url */,
                             bool /* out - result */)
+
+// Sent when a subframe is created.
+IPC_MESSAGE_CONTROL2(AwViewHostMsg_SubFrameCreated,
+                     int /* parent_render_frame_id */,
+                     int /* child_render_frame_id */)

android_webview/native/aw_contents_io_thread_client_impl.cc
@@ -166,6 +166,22 @@ AwContentsIoThreadClient::FromID(int render_process_id, int render_frame_id) {
 }
 
 // static
+void AwContentsIoThreadClient::SubFrameCreated(int render_process_id,
+                                               int parent_render_frame_id,
+                                               int child_render_frame_id) {
+  pair<int, int> parent_rfh_id(render_process_id, parent_render_frame_id);
+  pair<int, int> child_rfh_id(render_process_id, child_render_frame_id);
+  IoThreadClientData client_data;
+  if (!RfhToIoThreadClientMap::GetInstance()->Get(parent_rfh_id,
+                                                  &client_data)) {
+    NOTREACHED();
+    return;
+  }
+
+  RfhToIoThreadClientMap::GetInstance()->Set(child_rfh_id, client_data);
+}
+
+// static
 void AwContentsIoThreadClientImpl::RegisterPendingContents(
     WebContents* web_contents) {
   IoThreadClientData client_data;

android_webview/renderer/aw_content_renderer_client.cc
@@ -21,6 +21,7 @@
 #include "content/public/renderer/navigation_state.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
+#include "content/public/renderer/render_view.h"
 #include "net/base/net_errors.h"
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/WebKit/public/platform/WebURL.h"
@@ -116,6 +117,17 @@ bool AwContentRendererClient::HandleNavigation(
 void AwContentRendererClient::RenderFrameCreated(
     content::RenderFrame* render_frame) {
   new AwPermissionClient(render_frame);
+
+  // TODO(jam): when a RenderFrame is per WebFrame, this can be simplified by
+  // getting a RenderFrame's WebFrame and calling its parent() method.
+  content::RenderFrame* parent_frame =
+      render_frame->GetRenderView()->GetMainRenderFrame();
+  if (parent_frame && parent_frame != render_frame) {
+    // Avoid any race conditions from having the browser's UI thread tell the IO
+    // thread that a subframe was created.
+    RenderThread::Get()->Send(new AwViewHostMsg_SubFrameCreated(
+        parent_frame->GetRoutingID(), render_frame->GetRoutingID()));
+  }
 }
 
 void AwContentRendererClient::RenderViewCreated(