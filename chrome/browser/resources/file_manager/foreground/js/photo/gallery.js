ChromiumWebApps/chromium

commit 409b46cad7f4fd721e410d2aaeb0c3bc3efe84aa

Files.app: Fix exceptions in the gallery.
The CL fixes the following two errors.

* The toURL method of an entry variable is called though the variable can be null when we select multiple items.
* Passes entries to the executeTask private API, but the API takes URLs.

BUG=340552
TEST=execute the code path.

Review URL: https://codereview.chromium.org/155593003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249027 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/file_manager/foreground/js/photo/gallery.js
@@ -637,42 +637,42 @@ Gallery.prototype.onKeyDown_ = function(event) {
  * @private
  */
 Gallery.prototype.updateSelectionAndState_ = function() {
+  var numSelectedItems = this.selectionModel_.length;
   var displayName = '';
+  var selectedEntryURL = null;
 
-  var selectedItems = this.getSelectedItems();
-  if (selectedItems.length === 1) {
-    var item = selectedItems[0];
-    var entry = item.getEntry();
-    window.top.document.title = entry.name;
-    displayName = ImageUtil.getDisplayNameFromName(entry.name);
-  } else if (selectedItems.length > 1 && this.context_.curDirEntry) {
-    // If the Gallery was opened on search results the search query will not be
-    // recorded in the app state and the relaunch will just open the gallery
-    // in the curDirEntry directory.
-    window.top.document.title = this.context_.curDirEntry.name;
-    displayName =
-        this.displayStringFunction_('GALLERY_ITEMS_SELECTED',
-                                    selectedItems.length);
-  }
-
-  window.top.util.updateAppState(
-      null,  // Keep the current directory.
-      entry.toURL(),  // Update the selection.
-      {gallery: (this.currentMode_ === this.mosaicMode_ ? 'mosaic' : 'slide')});
-
-  // We can't rename files in readonly directory.
-  // We can only rename a single file.
-  this.filenameEdit_.disabled = selectedItems.length !== 1 ||
-                                this.context_.readonlyDirName;
-
-  this.filenameEdit_.value = displayName;
-
-  if (this.selectionModel_.selectedIndexes.length) {
+  // If it's selecting something, update the variable values.
+  if (numSelectedItems) {
     var selectedIndex = this.selectionModel_.selectedIndex;
     var selectedItem =
         this.dataModel_.item(this.selectionModel_.selectedIndex);
     this.selectedEntry_ = selectedItem.getEntry();
+    selectedEntryURL = this.selectedEntry_.toURL();
+
+    if (numSelectedItems === 1) {
+      window.top.document.title = this.selectedEntry_.name;
+      displayName = ImageUtil.getDisplayNameFromName(this.selectedEntry_.name);
+    } else if (this.context_.curDirEntry) {
+      // If the Gallery was opened on search results the search query will not
+      // be recorded in the app state and the relaunch will just open the
+      // gallery in the curDirEntry directory.
+      window.top.document.title = this.context_.curDirEntry.name;
+      displayName =
+          this.displayStringFunction_('GALLERY_ITEMS_SELECTED',
+                                      numSelectedItems);
+    }
   }
+
+  window.top.util.updateAppState(
+      null,  // Keep the current directory.
+      selectedEntryURL,  // Update the selection.
+      {gallery: (this.currentMode_ === this.mosaicMode_ ? 'mosaic' : 'slide')});
+
+  // We can't rename files in readonly directory.
+  // We can only rename a single file.
+  this.filenameEdit_.disabled = numSelectedItems !== 1 ||
+                                this.context_.readonlyDirName;
+  this.filenameEdit_.value = displayName;
 };
 
 /**
@@ -824,7 +824,9 @@ Gallery.prototype.updateShareMenu_ = function() {
       item.style.backgroundImage = 'url(' + task.iconUrl + ')';
       item.addEventListener('click', function(taskId) {
         this.toggleShare_();  // Hide the menu.
-        this.executeWhenReady(api.executeTask.bind(api, taskId, entries));
+        // TODO(hirono): Use entries instead of URLs.
+        this.executeWhenReady(
+            api.executeTask.bind(api, taskId, util.entriesToURLs(entries)));
       }.bind(this, task.taskId));
     }
 

commit 4204077d751a6919848e4abeb79d137fc0af8392

[Galley] Fix an exception in clicking a thumbnail
This patch fixes the exception in clicking a thumbnail. It is a regression caused by r249027 and a possible cause of crbug.com/340624.

The 'length' property of the selection model is NOT the number of the selected item. To get it, we need to get the length of 'selectedIndex' property.

BUG=340624
TEST=manually

Review URL: https://codereview.chromium.org/135973012

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251290 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/file_manager/foreground/js/photo/gallery.js
@@ -637,13 +637,12 @@ Gallery.prototype.onKeyDown_ = function(event) {
  * @private
  */
 Gallery.prototype.updateSelectionAndState_ = function() {
-  var numSelectedItems = this.selectionModel_.length;
+  var numSelectedItems = this.selectionModel_.selectedIndexes.length;
   var displayName = '';
   var selectedEntryURL = null;
 
   // If it's selecting something, update the variable values.
   if (numSelectedItems) {
-    var selectedIndex = this.selectionModel_.selectedIndex;
     var selectedItem =
         this.dataModel_.item(this.selectionModel_.selectedIndex);
     this.selectedEntry_ = selectedItem.getEntry();